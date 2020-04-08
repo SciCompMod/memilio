ea5925cd76dc3d0d381ee9693deb5be8ccb47ec8
Kühn <Martin.Kuehn@dlr.de>
2020-04-08 17:52:08 +0200

    euler and euler test translated to C++ with template and vectors

---
+++ cpp/euler.h
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <vector>
+
+/**
+ * Simple explicit euler integration y(t+1) = y(t) + h*f(t,y)
+ * for ODE y'(t) = f(t,y)
+ * @param[in] yt value of y at t, y(t)
+ * @param[in] dt current time step h=dt
+ * @param[in] f right hand side of ODE f(t,y)
+ * @param[out] ytp1 approximated value y(t+1)
+ */
+template <typename T>
+void explicit_euler(std::vector<T> const &yt, const T dt, std::vector<T> const &f, std::vector<T> &ytp1) {
+
+  for (size_t i=0;i<yt.size();i++) 
+  {
+    ytp1[i] = yt[i] + dt * f[i];
+  }
+
+}
