f128a60b256038a21f410719d7683751d6cab01a
Kathrin Rack <kathrin.rack@dlr.de>
2020-04-14 10:29:07 +0000

    Working script to get and process data from RKI.

---
+++ rki_data/README.md
@@ -0,0 +1,17 @@
+This script (without plotting instead with outputting) could be used as a cronjob to get data from rki and get every statistic we need
+ 
+Call python3 getGeoJsonIntoPandasDataFrame_for_RKI.py
+
+than data is downloaded and improved.
+The full data set is stored in a json.
+Different statistic is plotted.
+One statistic is as an example also outputted as json (This could be easily done for every statistic we need and than plotted with javascript)
+
+
+Call python3 getGeoJsonIntoPandasDataFrame_for_RKI.py PLOT_DATA=True
+
+To use stored data to not download it all the time.
+
+Needed python packages:
+- pandas
+- matplotlib
---
+++ rki_data/getGeoJsonIntoPandasDataFrame_for_RKI.py
@@ -0,0 +1,154 @@
+import sys
+from urllib.request import urlopen
+import json
+import pandas
+import matplotlib.pyplot as plt
+
+GET_DATA = True
+PLOT_DATA = False
+
+def loadGeojson( itemId, apiUrl = 'https://opendata.arcgis.com/datasets/', 
+                 extension = 'geojson' ):
+    """ Loads ArcGIS data sets in GeoJSON format. (pandas DataFrame)
+
+    This routine loads ArcGIS data sets in GeoJSON format of the given public 
+    data item ID into a pandas DataFrame and returns the DataFrame. Trivial 
+    information gets removed by JSON normalization and dropping of always 
+    constant data fields.
+
+    Keyword arguments:
+    itemId -- ArcGIS public data item ID (string)
+    apiUrl -- ArcGIS data sets API URL (string, default
+              'https://opendata.arcgis.com/datasets/')
+    extension -- Data format extension (string, default 'geojson')
+
+    """
+    url = apiUrl + itemId + '_0.' + extension
+
+    with urlopen( url ) as res:
+        data = json.loads( res.read().decode() )
+
+    # Shape data:
+    df = pandas.json_normalize( data, 'features' )
+
+    # Make-up (removing trivial information):
+    df.drop( columns = ['type', 'geometry'], inplace = True )
+    df.rename( columns = lambda s: s[11:], inplace = True )
+
+    return df
+
+def loadCsv( itemId, apiUrl = 'https://opendata.arcgis.com/datasets/', 
+                 extension = 'csv' ):
+    """ Loads ArcGIS data sets in CSV format. (pandas DataFrame)
+
+    This routine loads ArcGIS data sets in CSV format of the given public data 
+    item ID into a pandas DataFrame and returns the DataFrame. 
+
+    Keyword arguments:
+    itemId -- ArcGIS public data item ID (string)
+    apiUrl -- ArcGIS data sets API URL (string, default
+              'https://opendata.arcgis.com/datasets/')
+    extension -- Data format extension (string, default 'csv')
+
+    """
+    url = apiUrl + itemId + '_0.' + extension
+
+    df = pandas.read_csv( url )
+
+    return df
+
+def main(get_data, plot_data):
+
+   if(get_data):
+
+      # Supported data formats:
+      load = { 
+         'csv': loadCsv,
+         'geojson': loadGeojson
+       }
+
+      # ArcGIS public data item ID:
+      itemId = 'dd4580c810204019a7b8eb3e0b329dd6'
+
+      # Get data:
+      df = load['csv'](itemId)
+
+      # Correct Timestampes:
+      # for col in [ 'Meldedatum', 'Refdatum' ]:
+      #   df[col] = df[col].astype( 'datetime64' ).dt.tz_localize('Europe/Berlin')
+
+      # convert "Datenstand" to real date:
+      df.Datenstand = pandas.to_datetime( df.Datenstand, format='%d.%m.%Y, %H:%M Uhr').dt.tz_localize('Europe/Berlin')  
+
+      # output data to not always download it
+      df.to_json(r"FullData.json")
+
+   elif(plot_data):
+      df = pandas.read_json("FullData.json")
+      # read json file
+
+   # Preperation for plotting/output:
+
+   # Correct Timestampes:
+   for col in [ 'Meldedatum', 'Refdatum' ]:
+      df[col] = df[col].astype( 'datetime64[ns]' ).dt.tz_localize('Europe/Berlin')
+
+   # Use "Meldedatum" as time
+   # "Refdatum" may act different to official describtion
+   # on https://npgeo-corona-npgeo-de.hub.arcgis.com/datasets/dd4580c810204019a7b8eb3e0b329dd6_0,
+   # be careful (big difference identified between "Refdatum" and "Meldedatum").
+   dateToUse = 'Meldedatum'
+   df.sort_values( dateToUse, inplace = True )
+
+   # NeuerFall: Infected (incl. recovered) over "Meldedatum":
+   gbNF = df[df.NeuerFall >= 0].groupby( dateToUse ).sum()
+   gbNFcumsum = gbNF.AnzahlFall.cumsum()
+   gbNFcumsum.to_json("gbNF.json")
+   gbNFcumsum.plot( title = 'COVID-19 infections', grid = True, 
+                               style = '-o' )
+   plt.tight_layout()
+   plt.show()
+
+   # Dead over "Meldedatum":
+   gbNT = df[df.NeuerTodesfall >= 0].groupby( dateToUse ).sum()
+   gbNT.AnzahlTodesfall.cumsum().plot( title = 'COVID-19 deads', grid = True, 
+                                    style = '-o' )
+   plt.tight_layout()
+   plt.show()
+
+   # Dead by "Altersgruppe":
+   gbNTAG = df[df.NeuerTodesfall >= 0].groupby( 'Altersgruppe' ).sum()
+   gbNTAG.AnzahlTodesfall.plot( title = 'COVID-19 deads', grid = True, 
+                             kind = 'bar' )
+   plt.tight_layout()
+   plt.show()
+
+
+if __name__ == "__main__":
+
+
+   largv = len(sys.argv)
+
+   if largv > 1:
+      for i in range(1,largv):
+
+          arg = sys.argv[i]
+
+          if "GET_DATA" in arg:
+
+             arg_split = arg.split("=")
+             if len(arg_split) == 2:
+                 GET_DATA = arg_split[1]
+             else:
+                 pass
+
+          elif "PLOT_DATA" in arg:
+
+             arg_split = arg.split("=")
+             if len(arg_split) == 2:
+                 PLOT_DATA = arg_split[1]
+                 GET_DATA=False
+             else:
+                 pass
+
+   main(GET_DATA, PLOT_DATA)
