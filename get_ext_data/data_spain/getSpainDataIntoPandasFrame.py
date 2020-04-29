a49f54a3ae522504b6e859070c70afde1bebbe29
Martin Joachim Kühn <Martin.Kuehn@DLR.de>
2020-04-29 18:57:09 +0200

    Get Data for Spain for all ages or all states

--- get_ext_data/data_spain/getSpainDataIntoPandasFrame.py
+++ get_ext_data/data_spain/getSpainDataIntoPandasFrame.py
@@ -3,6 +3,7 @@ from urllib.request import urlopen
 import json
 import pandas
 import matplotlib.pyplot as plt
+import numpy as np
 
 import outputDict as od
 
@@ -44,16 +45,18 @@ def main(get_data, read_data, make_plot, out_form):
       df_age = loadCsv(CSVfile  = 'nacional_covid19_rango_edad')
       
       if df_age.empty != True:
-         # standardization of column titles from Spanish to English
-         df_age.rename({'fecha': 'date', 'rango_edad': 'age', 'sexo': 'gender', 'casos_confirmados': 'confirmed', 
-               'hospitalizados': 'hospitalized', 'ingresos_uci': 'icu', 'fallecidos' : 'deaths'}, axis=1, inplace=True)
+         # standardization of column titles from Spanish to English # the stupid character in front of 'fecha' is correct here.. There is a bug in the original file..
+         df_age.rename({'﻿fecha': 'date', 'rango_edad': 'age', 'sexo': 'gender', 'casos_confirmados': 'confirmed', 
+               'hospitalizados': 'hospitalized', 'ingresos_uci': 'icu', 'fallecidos' : 'deaths'}, axis=1, inplace=True) 
 
          print("Read Spanish age data from online. Available columns:", df_age.columns)
 
-         # translate column gender from Spanish to English
+         # translate column gender from Spanish to English and standardize
          df_age.loc[df_age.gender=='ambos', ['gender']] = 'both'
          df_age.loc[df_age.gender=='mujeres', ['gender']] = 'female'
          df_age.loc[df_age.gender=='hombres', ['gender']] = 'male'
+         df_age.loc[df_age.age=='80 y +', ['age']] = '80+'
+         df_age.loc[df_age.age=='Total', ['age']] = 'all'
 
          # Correct Timestamps:
          df_age['date'] = df_age['date'].astype( 'datetime64[ns]' ).dt.tz_localize('Europe/Berlin')
@@ -71,6 +74,18 @@ def main(get_data, read_data, make_plot, out_form):
                'TestAc+': 'confirmed_anti', 'Hospitalizados': 'hospitalized', 'UCI': 'icu', 'Fallecidos' : 'deaths', 'Recuperados' : 'recovered'}, axis=1, inplace=True)
 
          print("Read Spanish states data from online. Available columns:", df_state.columns)
+
+         
+         # fill empty cells (nan values) with zero
+         df_state.replace(np.nan, 0, inplace=True)
+
+
+         # remove special characters
+         df_state.loc[df_state.state=="Andalucía", ['state']] = "Andalucia"
+         df_state.loc[df_state.state=="Castilla y León", ['state']] = "Castilla y Leon"
+         df_state.loc[df_state.state=="Cataluña", ['state']] = "Cataluna"
+         df_state.loc[df_state.state=="País Vasco", ['state']] = "Pais Vasco"
+         df_state.loc[df_state.state=="Aragón", ['state']] = "Aragon"
 
          # Correct Timestamps:
          df_state['date'] = df_state['date'].astype( 'datetime64[ns]' ).dt.tz_localize('Europe/Berlin')
@@ -94,17 +109,10 @@ def main(get_data, read_data, make_plot, out_form):
 
 
 
-   # Preparation for plotting/output:
-
-   # translate from Spanish to English
-   df_age.loc[df_age.gender=='ambos', ['gender']] = 'both'
-   df_age.loc[df_age.gender=='mujeres', ['gender']] = 'female'
-   df_age.loc[df_age.gender=='hombres', ['gender']] = 'male'
-   df_age.loc[df_age.age=='80 y +', ['age']] = '80+'
-   df_age.loc[df_age.age=='Total', ['age']] = 'all'
+   # Preparation for plotting/output age specific data:
 
    # only consider men AND women (through information on gender away)
-   df_age = df_age.loc[df.gender=='both']
+   df_age = df_age.loc[df_age.gender=='both']
 
    # write file for all age groups summed together
    df_agesum = df_age.loc[df_age.age=='all']
@@ -120,8 +128,24 @@ def main(get_data, read_data, make_plot, out_form):
 
 
 
-      # Preparation for plotting/output:
+   # Preparation for plotting/output:
 
+   PCR_ONLY = True # if pcr only is used 
+   # dff = df_state['state'].unique()
+
+
+   # if PCR only is used, the number of confirmed cases is the number of people being tested positive with PCR test
+   # otherwise, the number of positive antibody tests is also taken into account
+   if PCR_ONLY:
+      df_state.loc[df_state.confirmed_total==0, ['confirmed_total']] = df_state.confirmed_pcr
+   else:
+      df_state.loc[df_state.confirmed_total==0, ['confirmed_total']] = df_state.confirmed_pcr + df_state.confirmed_anti
+
+   states_array = df_state.state.unique()
+
+   # output json
+   # call df_state.to_json("spain_all_state.json", orient='records'), or to hdf5 alternatively
+   getattr(df_state, od.outForm[out_form][0])("spain_all_state" + od.outForm[out_form][1], **od.outForm[out_form][2])
 
 
 
