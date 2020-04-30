9b76220512e6bfeef4eaad42a0df153d95b8b52a
Johannes Holke <johannes.holke@dlr.de>
2020-04-30 16:05:14 +0200

    Simplify parameter studies. Remove template, add namespace

--- cpp/epidemiology/parameter_studies/parameter_studies.h
+++ cpp/epidemiology/parameter_studies/parameter_studies.h
@@ -2,16 +2,17 @@
 #define PARAMETER_STUDIES_H
 
 #include <iostream>
+#include <epidemiology/parameter_studies/parameter_space.h>
+
+namespace epi
+{
 
 // The function type for the kind of simulation that we want to run
-template<typename T>
-using seir_simulation_function_t = void (*)(const T t0, const T tmax, const T dt, struct seirParam<T> const& params, std::vector<std::vector<T> >& seir)
+using seir_simulation_function_t = void (*)(const double t0, const double tmax, const double dt, struct seirParam<double> const& params, std::vector<std::vector<double> >& seir);
 
 // TODO: document class
 // TODO: document input file convention
 
-// template parameter is the datatype of the associated seirParams
-template <typename T>
 class parameter_study_t
 {
 public:
@@ -22,6 +23,7 @@ public:
 
     // Carry out all simulations in the parameter study.
     void run();
+
 private:
     // The path of the file storing the parameter ranges
     std::string parameter_file;
@@ -33,26 +35,35 @@ private:
     seir_simulation_function_t simulation_function;
 
     // Start time (should be the same for all simulations)
-    T t0;
+    double t0;
     // End time (should be the same for all simulations)
-    T tmax;
+    double tmax;
     // time step (should be the same for all simulations)
-    T dt;
+    double dt;
+};
+
+parameter_study_t::parameter_study_t (std::string &parameter_filename) 
+: parameter_space(parameter_filename)
+{
+    // TODO: Read these from file as well
+    t0 = 0;
+    tmax = 1;
+    dt = 0.1;
 }
 
-template <typename T>
 void parameter_study_t::run ()
 {
     // Iterate over all parameters in the parameter space
+    #if 0
     for(parameter_space_t::ConstIterator param_it = parameter_space.BeginConst(iCell); param_it.IsValid(); ++param_it){
         // Get the current parameters
-        const struct seirParam<T> &params = *param_it;
+        const struct seirParam<double> &params = *param_it;
 
         // Print the parameters if we are in debug mode
         // TODO: Should we get an own debugging mode use it instead of NDEBUG
         // TODO: Replace cout with logging function once we have one.
 #ifndef NDEBUG
-        std::cout << "Starting simulation with params:\n" << std::end;
+        std::cout << "Starting simulation with params:\n" << std::endl;
         printSeirParams (params);
 #endif
         // The vector in which we store the result.
@@ -64,6 +75,9 @@ void parameter_study_t::run ()
         // Call the simulation function
         simulation_function (paramter_space.t0, paramter_space.tmax, paramter_space.dt, params, result_vector);
     }
+    #endif
 }
+
+} // namespace epi
 
 #endif // PARAMETER_STUDIES_H
