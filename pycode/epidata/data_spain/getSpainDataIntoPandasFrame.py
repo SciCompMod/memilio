06220afb1e1ef91e2d3142473e3f5558bfac1a03
Martin Joachim Kühn <Martin.Kuehn@DLR.de>
2020-05-13 15:14:05 +0200

    correction of yesterday's bug

--- pycode/epidata/data_spain/getSpainDataIntoPandasFrame.py
+++ pycode/epidata/data_spain/getSpainDataIntoPandasFrame.py
@@ -52,12 +52,12 @@ def loadCsv( githubUrl = 'https://raw.githubusercontent.com/datadista/datasets/m
     return df
 
 
-def main(get_data, read_data, make_plot, out_form):
+def main(read_data, make_plot, out_form):
 
    AgesJSONData = 'raw_spain_all_age.json'
    StatJSONData = 'raw_spain_all_state.json'
 
-   if(get_data):
+   if(READ_DATA):
   
       # Get data:
       # https://raw.githubusercontent.com/datadista/datasets/master/COVID%2019/nacional_covid19_rango_edad.csv
@@ -65,21 +65,21 @@ def main(get_data, read_data, make_plot, out_form):
       
       if df_age.empty != True:
          # standardization of column titles from Spanish to English # the stupid character in front of 'fecha' is correct here.. There is a bug in the original file..
-         df_age.rename({'fecha': 'date', 'rango_edad': 'age', 'sexo': 'gender', 'casos_confirmados': 'confirmed', 
-               'hospitalizados': 'hospitalized', 'ingresos_uci': 'icu', 'fallecidos' : 'deaths'}, axis=1, inplace=True) 
+         df_age.rename({'fecha': 'Date', 'rango_edad': 'Age', 'sexo': 'Gender', 'casos_confirmados': 'Confirmed', 
+               'hospitalizados': 'Hospitalized', 'ingresos_uci': 'ICU', 'fallecidos' : 'Deaths'}, axis=1, inplace=True) 
 
          print("Read Spanish age data from online. Available columns:", df_age.columns)
 
          # translate column gender from Spanish to English and standardize
-         df_age.loc[df_age.gender=='ambos', ['gender']] = 'both'
-         df_age.loc[df_age.gender=='mujeres', ['gender']] = 'female'
-         df_age.loc[df_age.gender=='hombres', ['gender']] = 'male'
-         df_age.loc[df_age.age=='80 y +', ['age']] = '80+'
-         df_age.loc[df_age.age=='90 y +', ['age']] = '90+'
-         df_age.loc[df_age.age=='Total', ['age']] = 'all'
+         df_age.loc[df_age.Gender=='ambos', ['Gender']] = 'both'
+         df_age.loc[df_age.Gender=='mujeres', ['Gender']] = 'female'
+         df_age.loc[df_age.Gender=='hombres', ['Gender']] = 'male'
+         df_age.loc[df_age.Age=='80 y +', ['Age']] = '80+'
+         df_age.loc[df_age.Age=='90 y +', ['Age']] = '90+'
+         df_age.loc[df_age.Age=='Total', ['Age']] = 'all'
 
          # Correct Timestamps:
-         df_age['date'] = df_age['date'].astype( 'datetime64[ns]' ).dt.tz_localize('Europe/Berlin')
+         df_age['Date'] = df_age['Date'].astype( 'datetime64[ns]' ).dt.tz_localize('Europe/Berlin')
 
          # output data to not always download it
          df_age.to_json(AgesJSONData)
@@ -90,8 +90,8 @@ def main(get_data, read_data, make_plot, out_form):
       
       if df_state.empty != True:
          # standardization of column titles from Spanish to English
-         df_state.rename({'Fecha': 'date', 'cod_ine': 'id_state', 'CCAA': 'state', 'Casos': 'confirmed_total', 'PCR+': 'confirmed_pcr', 
-               'TestAc+': 'confirmed_ab', 'Hospitalizados': 'hospitalized', 'UCI': 'icu', 'Fallecidos' : 'deaths', 'Recuperados' : 'recovered'}, axis=1, inplace=True)
+         df_state.rename({'Fecha': 'Date', 'cod_ine': 'ID_State', 'CCAA': 'State', 'Casos': 'Confirmed_total', 'PCR+': 'Confirmed_PCR', 
+               'TestAc+': 'Confirmed_AB', 'Hospitalizados': 'Hospitalized', 'UCI': 'ICU', 'Fallecidos' : 'Deaths', 'Recuperados' : 'Recovered'}, axis=1, inplace=True)
 
          print("Read Spanish states data from online. Available columns:", df_state.columns)
 
@@ -101,20 +101,20 @@ def main(get_data, read_data, make_plot, out_form):
 
 
          # remove special characters
-         df_state.loc[df_state.state=="Andalucía", ['state']] = "Andalucia"
-         df_state.loc[df_state.state=="Castilla y León", ['state']] = "Castilla y Leon"
-         df_state.loc[df_state.state=="Cataluña", ['state']] = "Cataluna"
-         df_state.loc[df_state.state=="País Vasco", ['state']] = "Pais Vasco"
-         df_state.loc[df_state.state=="Aragón", ['state']] = "Aragon"
+         df_state.loc[df_state.State=="Andalucía", ['State']] = "Andalucia"
+         df_state.loc[df_state.State=="Castilla y León", ['State']] = "Castilla y Leon"
+         df_state.loc[df_state.State=="Cataluña", ['State']] = "Cataluna"
+         df_state.loc[df_state.State=="País Vasco", ['State']] = "Pais Vasco"
+         df_state.loc[df_state.State=="Aragón", ['State']] = "Aragon"
 
          # Correct Timestamps:
-         df_state['date'] = df_state['date'].astype( 'datetime64[ns]' ).dt.tz_localize('Europe/Berlin')
+         df_state['Date'] = df_state['Date'].astype( 'datetime64[ns]' ).dt.tz_localize('Europe/Berlin')
 
          # output data to not always download it
          df_state.to_json(StatJSONData)
          
 
-   elif(read_data):
+   else:
       # if once dowloaded just read json file
 
       #### ages' data
@@ -132,17 +132,17 @@ def main(get_data, read_data, make_plot, out_form):
    # Preparation for plotting/output age specific data:
 
    # only consider men AND women (through information on gender away)
-   df_age = df_age.loc[df_age.gender=='both']
+   df_age = df_age.loc[df_age.Gender=='both']
 
    # write file for all age groups summed together
-   df_agesum = df_age.loc[df_age.age=='all']
+   df_agesum = df_age.loc[df_age.Age=='all']
       # call to df_ageall.to_json("all_age.json", orient='records')
    getattr(df_agesum, od.outForm[out_form][0])("spain" + od.outForm[out_form][1], **od.outForm[out_form][2])
 
 
    # write file with information on all age groups separately
    # age_groups = ['0-9','10-19','20-29','30-39','40-49','50-59','60-69','70-79','80+']
-   df_agesep = df_age.loc[df_age.age!='all']
+   df_agesep = df_age.loc[df_age.Age!='all']
       # call to df_ageall.to_json("all_age.json", orient='records')
    getattr(df_agesep, od.outForm[out_form][0])("spain_all_age" + od.outForm[out_form][1], **od.outForm[out_form][2])
 
@@ -157,11 +157,11 @@ def main(get_data, read_data, make_plot, out_form):
    # if PCR only is used, the number of confirmed cases is the number of people being tested positive with PCR test
    # otherwise, the number of positive antibody tests is also taken into account
    if PCR_ONLY:
-      df_state.loc[df_state.confirmed_total==0, ['confirmed_total']] = df_state.confirmed_pcr
+      df_state.loc[df_state.Confirmed_total==0, ['Confirmed_total']] = df_state.Confirmed_PCR
    else:
-      df_state.loc[df_state.confirmed_total==0, ['confirmed_total']] = df_state.confirmed_pcr + df_state.confirmed_ab
+      df_state.loc[df_state.Confirmed_total==0, ['Confirmed_total']] = df_state.Confirmed_PCR + df_state.Confirmed_AB
 
-   states_array = df_state.state.unique()
+   states_array = df_state.State.unique()
 
    # output json
    # call df_state.to_json("spain_all_state.json", orient='records'), or to hdf5 alternatively
@@ -172,7 +172,6 @@ def main(get_data, read_data, make_plot, out_form):
 
 if __name__ == "__main__":
 
-   GET_DATA = True
    READ_DATA = True
    MAKE_PLOT = True
    OUT_FORM = "json"
@@ -216,5 +215,5 @@ if __name__ == "__main__":
           else:
              print("Warning: your argument:", arg, "is ignored.")
 
-   main(GET_DATA, READ_DATA, MAKE_PLOT, OUT_FORM)  
+   main(READ_DATA, MAKE_PLOT, OUT_FORM)  
     
