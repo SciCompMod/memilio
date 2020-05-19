276d153ae6eacc117b26ecf85e2c65d4bc986bfb
Abele, Daniel <daniel.abele@dlr.de>
2020-05-19 11:28:47 +0200

    add utils for slicing and reshaping eigen matrices

---
+++ cpp/epidemiology/eigen_util.h
@@ -0,0 +1,155 @@
+#include <Eigen/Core>
+#include <utility>
+#include <iostream>
+
+namespace epi
+{
+
+/**
+ * @brief sequence of indices
+ */
+template <typename T>
+struct Seq {
+    Seq(T start, T n, T stride = 1)
+        : start(start)
+        , n(n)
+        , stride(stride)
+    {
+        assert(start >= 0);
+        assert(n >= 0);
+        assert(stride >= 1);
+    }
+    T start, n, stride = 1;
+};
+
+/**
+ * @brief removes ref and cv qualifiers from a type T (part of std from c++17)
+ */
+template <class T>
+using remove_cvref = std::remove_reference_t<std::remove_cv_t<T>>;
+
+/**
+ * @brief check if Eigen::Matrix type M is a dynamic vector type.
+ */
+template <class M>
+struct is_dynamic_vector {
+    static constexpr bool value =
+        (remove_cvref<M>::RowsAtCompileTime == Eigen::Dynamic && remove_cvref<M>::ColsAtCompileTime == 1) ||
+        (remove_cvref<M>::RowsAtCompileTime == 1 && remove_cvref<M>::ColsAtCompileTime == Eigen::Dynamic);
+};
+
+/**
+ * @brief check if Eigen::Matrix type M is a dynamic matrix type.
+ */
+template <class M>
+struct is_dynamic_matrix {
+    static constexpr bool value =
+        remove_cvref<M>::RowsAtCompileTime == Eigen::Dynamic && remove_cvref<M>::ColsAtCompileTime == Eigen::Dynamic;
+};
+
+/**
+ * @brief number of rows (columns) of a row (column) major matrix.
+ */
+template <class M>
+Eigen::Index major_size(M&& m)
+{
+    return remove_cvref<M>::IsRowMajor ? m.rows() : m.cols();
+}
+
+/**
+ * @brief number of columns (rows) of a row (column) major matrix.
+ */
+template <class M>
+Eigen::Index minor_size(M&& m)
+{
+    return remove_cvref<M>::IsRowMajor ? m.cols() : m.rows();
+}
+
+/**
+ * @brief take a regular slice of a row or column vector.
+ * The slices shares the same memory as the original vector, no copying is performed,
+ * changes to the slice are also made to the original vector.
+ * Assign to a different vector of compatible size if you need a copy.
+ * @param v Row or column vector to take a slice of
+ * @param elems sequence of row or column indices
+ * @returns vector expression with selected entries from the input vector
+ */
+template <class V, std::enable_if_t<is_dynamic_vector<V>::value, int> = 0>
+auto slice(V&& v, Seq<Eigen::Index> elems)
+{
+    return Eigen::Map<remove_cvref<V>, 0, Eigen::Stride<Eigen::Dynamic, Eigen::Dynamic>>(
+        v.data() + elems.start, elems.n, Eigen::Stride<Eigen::Dynamic, Eigen::Dynamic>{1, elems.stride});
+}
+
+/**
+ * @brief take a regular slice of a matrix.
+ * The slices shares the same memory as the original matrix, no copying is performed,
+ * changes to the slice are also made to the original matrix.
+ * Assign to a different matrix of compatible size if you need a copy.
+ * @param m Matrix to take a slice of
+ * @param rows sequence of row indices
+ * @param cols sequence of column indices
+ * @returns matrix expression with selected entries from the input matrix
+ */
+template <class M, std::enable_if_t<is_dynamic_matrix<M>::value, int> = 0>
+auto slice(M&& m, Seq<Eigen::Index> rows, Seq<Eigen::Index> cols)
+{
+    using PlainM = remove_cvref<M>;
+    using MapT   = Eigen::Map<PlainM, Eigen::Unaligned, Eigen::Stride<Eigen::Dynamic, Eigen::Dynamic>>;
+
+    assert(rows.start + rows.stride * rows.n <= m.rows());
+    assert(cols.start + cols.stride * cols.n <= m.rows());
+
+    auto majSpec   = PlainM::IsRowMajor ? rows : cols;
+    auto minSpec   = PlainM::IsRowMajor ? cols : rows;
+    auto majStride = majSpec.stride * minor_size(m);
+    auto minStride = minSpec.stride;
+    auto data      = m.data() + majSpec.start * minor_size(m) + minSpec.start;
+
+    return MapT(data, rows.n, cols.n, {majStride, minStride});
+}
+
+/**
+ * @brief reshape the matrix.
+ * Total number of entries before and after the reshape must be the same.
+ * The new matrix shares memory with the input matrix, no copying is performed, changes to the new matrix
+ * will be made to the input matrix as well.
+ * Assign to another matrix of compatible size if you need a copy.
+ * @param m matrix to reshape
+ * @param rows number of rows of the new matrix
+ * @param cols number of cols of the new matrix
+ * @returns matrix expression with the same entries as the input matrix but new shape
+ */
+template <typename M>
+auto reshape(M&& m, Eigen::Index rows, Eigen::Index cols)
+{
+    assert(rows * cols == m.rows() * m.cols());
+    assert(rows >= 1);
+    assert(cols >= 1);
+
+    return Eigen::Map<remove_cvref<M>>(m.data(), rows, cols);
+}
+
+/**
+ * @brief overload gtest printer function for eigen matrices.
+ * @note see https://stackoverflow.com/questions/25146997/teach-google-test-how-to-print-eigen-matrix
+ */
+template <class M>
+struct MatrixPrintWrap : public M {
+    friend void PrintTo(const MatrixPrintWrap& m, std::ostream* os)
+    {
+        (*os) << '\n' << m;
+    }
+};
+
+/**
+ * @brief wrap m for gtest printing
+ * returns a reference to the original object, no copying or moving, mind the lifetime!
+ */
+template <class M>
+const MatrixPrintWrap<M>& print_wrap(const M& m)
+{
+    return static_cast<const MatrixPrintWrap<M>&>(m);
+}
+
+} // namespace epi
---
+++ cpp/tests/test_eigen_util.cpp
@@ -0,0 +1,65 @@
+#include <epidemiology/eigen_util.h>
+#include <gtest/gtest.h>
+
+namespace Eigen
+{
+    void PrintTo(const Eigen::VectorXd& m, std::ostream* os)
+    {
+        (*os) << m;
+    }
+}
+
+template<class M>
+struct TestEigenUtilMatrix : public ::testing::Test { };
+using MatrixTypes = ::testing::Types<Eigen::MatrixXd, Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>>;
+TYPED_TEST_SUITE(TestEigenUtilMatrix, MatrixTypes);
+TYPED_TEST(TestEigenUtilMatrix, slice)
+{
+    TypeParam A(5, 4);
+    A << 0, 1, 2, 3,
+         4, 5, 6, 7,
+         8, 9, 10, 11,
+         12, 13, 14, 15,
+         16, 17, 18, 19;
+
+    TypeParam B(3, 2);
+    B << 4, 6, 8, 10, 12, 14;
+
+    ASSERT_EQ(epi::print_wrap(epi::slice(A, {1, 3, 1}, {0, 2, 2})), epi::print_wrap(B));
+}
+
+template<class V>
+struct TestEigenUtilVector : public ::testing::Test { };
+using VectorTypes = ::testing::Types<Eigen::VectorXd, Eigen::RowVectorXd>;
+TYPED_TEST_SUITE(TestEigenUtilVector, VectorTypes);
+TYPED_TEST(TestEigenUtilVector, slice)
+{
+    TypeParam A(12);
+    A << 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11;
+
+    TypeParam B(4);
+    B << 1, 4, 7, 10;
+
+    ASSERT_EQ(epi::print_wrap(epi::slice(A, {1, 4, 3})), epi::print_wrap(B));
+}
+
+TYPED_TEST(TestEigenUtilMatrix, reshape)
+{
+    TypeParam A(2, 3), B(1, 6), C(3, 2), D(6, 1);
+    A << 0, 1, 2, 3, 4, 5;
+    if (TypeParam::IsRowMajor)
+    {
+        B << 0, 1, 2, 3, 4, 5;
+        C << 0, 1, 2, 3, 4, 5;
+        D << 0, 1, 2, 3, 4, 5;
+    }
+    else
+    {
+        B << 0, 3, 1, 4, 2, 5;
+        C << 0, 4, 3, 2, 1, 5;
+        D << 0, 3, 1, 4, 2, 5;        
+    }
+
+    EXPECT_EQ(epi::print_wrap(epi::reshape(A, 1, 6)), epi::print_wrap(B));
+    EXPECT_EQ(epi::print_wrap(epi::reshape(A, 3, 2)), epi::print_wrap(C));
+}
