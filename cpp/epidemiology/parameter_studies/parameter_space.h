3b69cc175336db6dc7f6dc6d8856ded387c05ac2
Martin Joachim Kühn <Martin.Kuehn@dlr.de>
2020-06-03 18:05:58 +0200

    Integration of parameter distribution and sampling methods

--- cpp/epidemiology/parameter_studies/parameter_space.h
+++ cpp/epidemiology/parameter_studies/parameter_space.h
@@ -5,6 +5,8 @@
 #include <epidemiology/secir.h>
 #include <string>
 #include <vector>
+#include <random>
+#include <epidemiology/logging.h>
 
 namespace epi
 {
@@ -12,14 +14,155 @@ namespace epi
 /* TODO: Add more distributions here. */
 typedef enum
 {
-    DIST_UNIFORM
+    DIST_UNIFORM,
+    DIST_NORMAL
 } parameter_distribution;
 
-struct parameter_info {
-    std::string name; /*< The name of this parameter */
-    double min_value; /*< The minumum value of this parameter */
-    double max_value; /*< The maximum value of this parameter */
-    parameter_distribution dist; /*< The statistical distribution of this parameter */
+class ParameterDistribution
+{
+public:
+    ParameterDistribution()
+    {
+        std::random_device random_device;
+        m_random_generator = std::mt19937{random_device()};
+    }
+
+    ParameterDistribution(std::string name, double lower_bound, double upper_bound, parameter_distribution dist)
+    {
+        std::random_device random_device;
+        m_random_generator = std::mt19937{random_device()};
+        m_name             = name;
+        m_lower_bound      = lower_bound;
+        m_upper_bound      = upper_bound;
+        m_dist             = dist;
+    }
+
+    void set_name(std::string name)
+    {
+        m_name = name;
+    }
+
+    void set_lower_bound(double lower_bound)
+    {
+        m_lower_bound = lower_bound;
+    }
+
+    void set_upper_bound(double upper_bound)
+    {
+        m_upper_bound = upper_bound;
+    }
+
+    void set_distribution(parameter_distribution dist)
+    {
+        m_dist = dist;
+    }
+
+    std::string const& get_name() const
+    {
+        return m_name;
+    }
+
+    double get_lower_bound() const
+    {
+        return m_lower_bound;
+    }
+
+    double get_upper_bound() const
+    {
+        return m_upper_bound;
+    }
+
+    parameter_distribution const& get_distribution() const
+    {
+        return m_dist;
+    }
+
+    virtual double get_sample_point()
+    {
+    }
+
+protected:
+    std::string m_name; /*< The name of this parameter */
+    double m_lower_bound; /*< A realistic lower bound on the given parameter */
+    double m_upper_bound; /*< A realistic upper bound on the given parameter */
+    parameter_distribution m_dist; /*< The statistical distribution of this parameter */
+    std::mt19937 m_random_generator;
+};
+
+class ParameterDistributionNormal : ParameterDistribution
+{
+public:
+    void set_mean(double mean)
+    {
+        m_mean = mean;
+    }
+
+    void set_standard_dev(double standard_dev)
+    {
+        m_standard_dev = standard_dev;
+    }
+
+    double get_mean() const
+    {
+        return m_mean;
+    }
+
+    double get_standard_dev() const
+    {
+        return m_standard_dev;
+    }
+
+    /*
+     * @brief gets a sample of a normally distributed variable
+     */
+    double get_sample_point()
+    {
+        if (m_distribution.mean() != m_mean || m_distribution.stddev() != m_standard_dev) {
+            m_distribution = std::normal_distribution<double>{m_mean, m_standard_dev};
+        }
+
+        int i        = 0;
+        int retries  = 10;
+        double rnumb = m_distribution(m_random_generator);
+        while ((rnumb > m_upper_bound || rnumb < m_lower_bound) && i < retries) {
+            rnumb = m_distribution(m_random_generator);
+            i++;
+            if (i == retries) {
+                log_warning("Not successfully sampled within [min,max].");
+                if (rnumb > m_upper_bound) {
+                    rnumb = m_upper_bound;
+                }
+                else {
+                    rnumb = m_lower_bound;
+                }
+            }
+        }
+        return rnumb;
+    }
+
+private:
+    double m_mean; // the mean value of the normal distribution
+    double m_standard_dev; // the standard deviation of the normal distribution
+    std::normal_distribution<double> m_distribution;
+};
+
+class ParameterDistributionUniform : ParameterDistribution
+{
+public:
+    /*
+     * @brief gets a sample of a uniformly distributed variable
+     */
+    double get_sample_point()
+    {
+        if (m_distribution.max() != m_upper_bound || m_distribution.min() != m_lower_bound) {
+            m_distribution = std::uniform_real_distribution<double>{m_lower_bound, m_upper_bound};
+        }
+
+        return m_distribution(m_random_generator);
+    }
+
+private:
+    std::uniform_real_distribution<double> m_distribution;
 };
 
 /* The class parameter_space_t stores ranges of parameters
@@ -50,7 +193,7 @@ public:
 
 private:
     // A vector of all parameters with names and min/max values
-    std::vector<struct parameter_info> parameters;
+    std::vector<ParameterDistribution> parameters;
 };
 
 parameter_space_t::parameter_space_t(std::string& parameter_filename)
