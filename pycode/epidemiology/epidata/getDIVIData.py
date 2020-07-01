eb1a5a9f931c3c3841e26024ba70cd7507e4e5a0
Annette Lutz <Annette.Lutz@dlr.de>
2020-07-01 16:44:30 +0200

    url of DIVI archive changed, adjusted construction of url accordingly

--- pycode/epidemiology/epidata/getDIVIData.py
+++ pycode/epidemiology/epidata/getDIVIData.py
@@ -55,119 +55,51 @@ def get_divi_data(read_data=dd.defaultDict['read_data'],
       start_date = date(2020, 4, 24)
       end_date = date.today()
       delta = timedelta(days=1)
-      data1_end_date = date(2020, 4, 28)
+      data1_end_date = date(2020, 6, 5)
+      data2_start_date = date(2020, 6, 12)
+      data2_end_date = date(2020, 6, 25)
       # start with empty dataframe
       df = pandas.DataFrame()
+      
+      # number at end of url
+      call_number = 3906
 
-      try:
-         # Read urls from file and save in a dictionary
-         url_dict = {}
-         url_dict_file = open("Germany/url_dictionary.txt", "r")
-         for line in url_dict_file:
-            date = line.split(" ")[0]
-            url= line.split(" ")[1]
-            url_dict.update({date: url})
-         url_dict_file.close()
+      while start_date <= end_date:
 
-         # Download data with given urls
-         while start_date <= end_date:
+         call_date = start_date.strftime("%Y-%m-%d")
 
-            call_url = url_dict[start_date.strftime("%Y-%m-%d")]
+         # first links have different upload time
+         if start_date < data1_end_date:
+            call_time = "-09-15"
+         else:
+            call_time = "-12-15"
 
-            df2 = pandas.DataFrame()
-            df2 = gd.loadCsv('file', extension='', apiUrl = call_url)
+         df2 = pandas.DataFrame()
+         
+         if start_date < data2_start_date or start_date > data2_end_date:
+            call_url = "https://www.divi.de/divi-intensivregister-tagesreport-archiv-csv/divi-intensivregister-" + call_date + call_time + "/download"
+         else:
+            call_url = "https://www.divi.de/divi-intensivregister-tagesreport-archiv-csv/divi-intensivregister-" + call_date + call_time + "-2/viewdocument/" + str(call_number)
+            # number in url increases every day by 1
+            call_number += 1
 
-            if (df2.empty != True):
-               # data of first days needs adjustment to following data
-               df2 = adjust_first_data(df2, start_date)
-               df = df.append(df2, ignore_index=True)
-               print("Success: Data of date " + start_date.strftime("%Y-%m-%d") + " has been included to dataframe")
-            else:
-               print("Warning: Data of date " + start_date.strftime("%Y-%m-%d") + " is not included to dataframe")
+         try:
+            df2 = pandas.read_csv(call_url)
+         except OSError as e:
+            exit_string = "ERROR: URL " + url + " could not be opened."
+            sys.exit(exit_string)
+         except:
+            print("Information: URL: ",  call_url, " not knwon")
 
-            start_date += delta
+         if (df2.empty != True):
+            # data of first days needs adjustment to following data
+            df2 = adjust_first_data(df2, start_date)
+            df = df.append(df2, ignore_index=True)
+            print("Success: Data of date " + call_date + " has been included to dataframe")
+         else:
+            print("Warning: Data of date " + call_date + " is not included to dataframe")
 
-      except:
-         # Construct urls
-         # dict which cases where the next data is not +2
-         date_number_dict = {
-             "24-04-2020": 1580,
-             "25-04-2020": 1578,
-             "26-04-2020": 1579,
-             "27-04-2020": 1582,
-             "2020-04-28": 1585,
-             "2020-04-30": 1588,
-             "2020-05-06": 1601,
-             "2020-05-08": 1609,
-             "2020-05-09": 1610,
-             "2020-05-10": 1611,
-             "2020-05-11": 1614,
-             "2020-05-13": 1622,
-             "2020-05-14": 1623,
-             "2020-05-15": 1629,
-             "2020-05-16": 1628,
-             "2020-05-17": 1633,
-             "2020-05-18": 1634,
-             "2020-05-19": 1637,
-             "2020-05-20": 1640,
-             "2020-05-28": 1658,
-             "2020-06-04": 1674,
-             "2020-06-10": 1687,
-             "2020-06-13": 1694,
-             "2020-06-18": 1705,
-             "2020-06-19": 1710,
-             "2020-06-20": 1715,
-             "2020-06-21": 1716,
-             "2020-06-22": 1720,
-         }
-
-         list_call_extension = ["", "-1", "-09-15", "-csv"]
-         list_call_link = ["-divi-intensivregister-tagesreport-", "-divi-intensivregister-"]
-         call_number=0
-
-         while start_date <= end_date:
-
-            # first links have different data order
-            if start_date < data1_end_date:
-               call_date = start_date.strftime("%d-%m-%Y")
-            else:
-               call_date = start_date.strftime("%Y-%m-%d")
-
-            # get strange number of url
-            # usual rule it grows by 2 from day to day
-            # start point and exception is stored in dict
-            try:
-               call_number = date_number_dict[call_date]
-            except KeyError:
-               pass
-
-            df2 = pandas.DataFrame()
-            for call_link in  list_call_link:
-               for call_extension in list_call_extension:
-                  call_url =  'https://www.divi.de/divi-intensivregister-tagesreport-archiv/'\
-                      + str(call_number) + call_link + call_date + call_extension + '/'
-
-                  try:
-                     df2 = gd.loadCsv('file',
-                         extension='',
-                         apiUrl = call_url)
-                     # Write url to url dictionary text file
-                     url_dict_file = open("Germany/url_dictionary.txt", "a")
-                     url_dict_file.write(start_date.strftime("%Y-%m-%d")+" "+call_url+" \n")
-                     url_dict_file.close()
-                  except:
-                     print("Information: URL: ",  call_url, " not knwon")
-
-            if (df2.empty != True):
-               # data of first days needs adjustment to following data
-               df2 = adjust_first_data(df2, start_date)
-               df = df.append(df2, ignore_index=True)
-               print("Success: Data of date " + call_date + " has been included to dataframe")
-            else:
-               print("Warning: Data of date " + call_date + " is not included to dataframe")
-
-            call_number = call_number + 2
-            start_date += delta
+         start_date += delta
 
       # output data to not always download it
       if(df.empty != True):
