10c64419d2d7c59bb4e56e882375a28ef8c571d0
Luca Spataro <luca.spataro@dlr.de>
2020-08-20 18:54:15 +0200

    fix import errors

--- frontend/src/components/TimeMap/TimeMap.js
+++ frontend/src/components/TimeMap/TimeMap.js
@@ -1,7 +1,6 @@
 import {connect} from 'react-redux';
 
 import React from 'react';
-import _ from 'lodash';
 
 import {setSelected} from '../../redux/app';
 import InteractiveHeatMap from '../../common/interactive_heat_map';
--- frontend/src/redux/app.js
+++ frontend/src/redux/app.js
@@ -1,23 +1,10 @@
 import {createSlice} from '@reduxjs/toolkit';
-import {groupBy, renameKey, sumByKey, filterJSObject, stateIdFromCountyId, lastElement} from '../common/utils';
-import {setTimeBounds} from './time';
-
 import axios from 'axios';
 
 export const Datasets = {
   STATES: 'states',
   COUNTIES: 'counties',
   GERMANY: 'germany',
-};
-
-const dataset2datakey = {
-  [Datasets.STATES]: 'ID_State',
-  [Datasets.COUNTIES]: 'ID_County',
-};
-
-const populationKeyMap = {
-  [Datasets.STATES]: 'Bundesland',
-  [Datasets.COUNTIES]: 'Landkreis',
 };
 
 const slice = createSlice({
