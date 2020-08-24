375a1be48fe2b416bf0050ef509c94417bf11b5f
Abele, Daniel <daniel.abele@dlr.de>
2020-08-24 14:21:41 +0200

    remove development artefacts

--- cpp/tests/gtest_helpers.h
+++ cpp/tests/gtest_helpers.h
@@ -5,19 +5,19 @@
 
 //Action equivalent to assignment operator
 //assignee and RHS value are free arguments
-ACTION(Assign) { (arg0) = arg1; }
+ACTION(Assign) { arg0 = arg1; }
 
 //Action equivalent to assignment operator
 //assignee is free argument, RHS value is fixed
-ACTION_P(Assign, x) { (arg0) = x; }
+ACTION_P(Assign, x) { arg0 = x; }
 
 //Action equivalent to addition assignment operator
 //assignee and RHS free arguments
-ACTION(AddAssign) { (arg0) += arg1; }
+ACTION(AddAssign) { arg0 += arg1; }
 
 //Action equivalent to addition assignment operator
 //assignee is free arguemtn, RHS value is fixed
-ACTION_P(AddAssign, x) { (arg0) += x; }
+ACTION_P(AddAssign, x) { arg0 += x; }
 
 //versions that casts away const, necessary for fancy references like Eigen::Ref<Vector> to be assignable
 //make sure arg0 is not originally defined const, otherwise undefined behaviour
