f0e27d8a0b88f688d7e50ab9333ba3265229fc93
Abele, Daniel <daniel.abele@dlr.de>
2020-08-26 09:51:29 +0200

    complete combined ABM header

--- cpp/epidemiology/abm/abm.h
+++ cpp/epidemiology/abm/abm.h
@@ -1,11 +1,13 @@
 #ifndef EPI_ABM_H
 #define EPI_ABM_H
 
+#include "epidemiology/abm/parameters.h"
 #include "epidemiology/abm/simulation.h"
 #include "epidemiology/abm/world.h"
 #include "epidemiology/abm/person.h"
 #include "epidemiology/abm/node.h"
 #include "epidemiology/abm/state.h"
 #include "epidemiology/abm/node_type.h"
+#include "epidemiology/abm/rng.h"
 
 #endif
