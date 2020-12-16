211d23f0307257dc60619805bc1019b3ca7d5705
Koslow, Wadim <wadim.koslow@dlr.de>
2020-12-16 14:20:11 +0100

    Apply 1 suggestion(s) to 1 file(s)

--- pycode/epidemiology/epidata/getRKIData.py
+++ pycode/epidemiology/epidata/getRKIData.py
@@ -331,7 +331,7 @@ def get_rki_data(read_data=dd.defaultDict['read_data'],
 
    # output
    if split_berlin:
-      gd.write_dataframe(gbAllGCounty_cs, directory, "all_county_gender_rki_splited_berlin", out_form)
+      gd.write_dataframe(gbAllGCounty_cs, directory, "all_county_gender_rki_split_berlin", out_form)
    else:
       gd.write_dataframe(gbAllGCounty_cs, directory, "all_county_gender_rki", out_form)
   
