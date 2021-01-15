71c654ddbed38fc1975bbd392829d7cfdbac90e6
Jonas Gilg <jonas.gilg@dlr.de>
2021-01-15 18:14:21 +0100

    Add attribution page.

---
+++ frontend/src/pages/Attribution.js
@@ -0,0 +1,41 @@
+import React, {useState, useEffect} from 'react';
+import {Link} from 'react-router-dom';
+import axios from 'axios';
+
+export default function Attribution() {
+  const [data, setData] = useState({entries: []});
+
+  useEffect(() => {
+    document.title = document.title = `Attribution`;
+    axios('thirdPartyNotice.json').then((r) => {
+      setData({entries: r.data});
+    });
+  }, []);
+
+  return (
+    <div className="attribution">
+      <Link tabIndex="1" titel="Zurück zur Hauptseite" to="/">
+        Zurück
+      </Link>
+      <h2 className="mt-2">
+        <b>Attribution to Third Party Software</b>
+      </h2>
+      <br />
+      <ul>
+        {data.entries.map((item) => (
+          <li key={item.name + item.version} style={{listStyleType: 'none'}}>
+            <h3>
+              <b>{item.name + ' ' + item.version}</b>
+            </h3>
+            <p>Author: {item.author}</p>
+            <p>Repository: {item.repository}</p>
+            <p>Source: {item.source}</p>
+            <p>License: {item.license}</p>
+            <p>{item.licenseText}</p>
+            <hr className="solid" />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
