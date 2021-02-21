3bb849fa79170e5b375e195f2fbc15ba12e4c947
Abele, Daniel <daniel.abele@dlr.de>
2021-02-21 17:30:56 +0100

    add sampled dampings to edges

--- cpp/tests/test_parameter_studies.cpp
+++ cpp/tests/test_parameter_studies.cpp
@@ -89,6 +89,82 @@ TEST(ParameterStudies, sample_from_secir_params)
     EXPECT_EQ(contact_matrix_sample[0].get_dampings().size(), 1);
 }
 
+TEST(ParameterStudies, sample_edges)
+{
+    double t0   = 0;
+    double tmax = 100;
+
+    double tinc    = 5.2, // R_2^(-1)+R_3^(-1)
+        tinfmild   = 6, // 4-14  (=R4^(-1))
+        tserint    = 4.2, // 4-4.4 // R_2^(-1)+0.5*R_3^(-1)
+        thosp2home = 12, // 7-16 (=R5^(-1))
+        thome2hosp = 5, // 2.5-7 (=R6^(-1))
+        thosp2icu  = 2, // 1-3.5 (=R7^(-1))
+        ticu2home  = 8, // 5-16 (=R8^(-1))
+        tinfasy    = 6.2, // (=R9^(-1)=R_3^(-1)+0.5*R_4^(-1))
+        ticu2death = 5; // 3.5-7 (=R5^(-1))
+
+    double cont_freq = 10, // see Polymod study
+        inf_prob = 0.05, carr_infec = 0.67,
+           alpha = 0.09, // 0.01-0.16
+        beta     = 0.25, // 0.05-0.5
+        delta    = 0.3, // 0.15-0.77
+        rho      = 0.2, // 0.1-0.35
+        theta    = 0.25; // 0.15-0.4
+
+    double num_total_t0 = 10000, num_exp_t0 = 100, num_inf_t0 = 50, num_car_t0 = 50, num_hosp_t0 = 20, num_icu_t0 = 10,
+           num_rec_t0 = 10, num_dead_t0 = 0;
+
+    epi::SecirModel<epi::AgeGroup3> model;
+    size_t num_groups = model.parameters.get_num_groups();
+    double fact       = 1.0 / (double)num_groups;
+
+    auto& params = model.parameters;
+    for (size_t i = 0; i < num_groups; i++) {
+        params.times[i].set_incubation(tinc);
+        params.times[i].set_infectious_mild(tinfmild);
+        params.times[i].set_serialinterval(tserint);
+        params.times[i].set_hospitalized_to_home(thosp2home);
+        params.times[i].set_home_to_hospitalized(thome2hosp);
+        params.times[i].set_hospitalized_to_icu(thosp2icu);
+        params.times[i].set_icu_to_home(ticu2home);
+        params.times[i].set_infectious_asymp(tinfasy);
+        params.times[i].set_icu_to_death(ticu2death);
+
+        model.populations[{(epi::AgeGroup3)i, epi::InfectionType::E}] = fact * num_exp_t0;
+        model.populations[{(epi::AgeGroup3)i, epi::InfectionType::C}] = fact * num_car_t0;
+        model.populations[{(epi::AgeGroup3)i, epi::InfectionType::I}] = fact * num_inf_t0;
+        model.populations[{(epi::AgeGroup3)i, epi::InfectionType::H}] = fact * num_hosp_t0;
+        model.populations[{(epi::AgeGroup3)i, epi::InfectionType::U}] = fact * num_icu_t0;
+        model.populations[{(epi::AgeGroup3)i, epi::InfectionType::R}] = fact * num_rec_t0;
+        model.populations[{(epi::AgeGroup3)i, epi::InfectionType::D}] = fact * num_dead_t0;
+        model.populations.set_difference_from_group_total(fact * num_total_t0, (epi::AgeGroup3)i, (epi::AgeGroup3)i,
+                                                          epi::InfectionType::S);
+
+        params.probabilities[i].set_infection_from_contact(inf_prob);
+        params.probabilities[i].set_carrier_infectability(carr_infec);
+        params.probabilities[i].set_asymp_per_infectious(alpha);
+        params.probabilities[i].set_risk_from_symptomatic(beta);
+        params.probabilities[i].set_hospitalized_per_infectious(rho);
+        params.probabilities[i].set_icu_per_hospitalized(theta);
+        params.probabilities[i].set_dead_per_icu(delta);
+    }
+
+    epi::ContactMatrixGroup& contact_matrix = params.get_contact_patterns();
+    contact_matrix[0] = epi::ContactMatrix(Eigen::MatrixXd::Constant(num_groups, num_groups, fact * cont_freq));
+    epi::set_params_distributions_normal(model, t0, tmax, 0.2);
+
+    auto graph = epi::Graph<epi::SecirModel<epi::AgeGroup3>, epi::MigrationParameters>();
+    graph.add_node(0, model);
+    graph.add_node(1, model);
+    graph.add_edge(0, 1, epi::MigrationParameters(Eigen::VectorXd::Constant(Eigen::Index(num_groups * 8), 1.0)));
+
+    auto study = epi::ParameterStudy<epi::SecirModel<epi::AgeGroup3>>(graph, 0.0, 0.0, 0.5, 1);
+    auto results = study.run();
+
+    ASSERT_EQ(results[0].edges()[0].property.get_parameters().get_coefficients()[0].get_dampings().size(), 1);
+}
+
 TEST(ParameterStudies, test_normal_distribution)
 {
 
