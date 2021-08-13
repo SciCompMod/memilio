576cab5cf6e443cd3feb9807c3ef15cb92c8a048
Abele, Daniel <daniel.abele@dlr.de>
2021-08-13 15:00:50 +0200

    add license info to paper1 sim

--- cpp/sims/paper_202011.cpp
+++ cpp/sims/paper_202011.cpp
@@ -1,8 +1,26 @@
 /**
- * Simulation used for:
- * M. J. Kühn et al, 2021: Assessment of effective mitigation and prediction of the spread of SARS-CoV-2 in Germany 
- * using demographic information and spatial resolution
- */
+* Simulation application that was used to produce results of the following publication(s):
+* M. J. Kühn et al, 2021: Assessment of effective mitigation and prediction of the spread of SARS-CoV-2 in Germany 
+* using demographic information and spatial resolution
+* 
+* Copyright (C) 2020-2021 German Aerospace Center (DLR-SC)
+*
+* Authors: Daniel Abele
+*
+* Contact: Martin J. Kuehn <Martin.Kuehn@DLR.de>
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
 
 #include "epidemiology/secir/parameter_studies.h"
 #include "epidemiology/utils/regions.h"
