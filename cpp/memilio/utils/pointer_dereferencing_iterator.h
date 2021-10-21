870b7c36a18d7a49a245fb324f1af02519db5ea2
Abele, Daniel <daniel.abele@dlr.de>
2021-10-21 15:00:33 +0200

    new util transform_iterator

--- cpp/memilio/utils/pointer_dereferencing_iterator.h
+++ cpp/memilio/utils/pointer_dereferencing_iterator.h
@@ -20,117 +20,43 @@
 #ifndef EPI_DEREF_ITERATOR_H
 #define EPI_DEREF_ITERATOR_H
 
+#include "memilio/utils/transform_iterator.h"
+
 namespace mio
 {
 
-    /**
-     * iterator adaptor that makes a range of T* look like a range of T
-     * e.g. std::vector<int*> v;
-     * then *PointerDereferencingIterator<...>(v.begin()) == **v.begin()
-     */
-    template<class PtrIter>
-    class PointerDereferencingIterator
+namespace details
+{
+    //helper functor that dereferences a pointer
+    struct DerefFunctor
     {
-        private:
-        PtrIter m_ptr_iter;
-
-        public:
-        using reference = decltype(*std::declval<typename PtrIter::value_type>());
-        using value_type = typename std::remove_reference<reference>::type;
-        using pointer = value_type*;
-        using difference_type = typename PtrIter::difference_type;
-        using iterator_category = typename PtrIter::iterator_category;
-
-        PointerDereferencingIterator(PtrIter ptr_iter)
-            : m_ptr_iter(ptr_iter)
+        template<class Ptr>
+        auto& operator()(Ptr&& p) const
         {
-
-        }
-
-        reference operator*() const
-        {
-            return **m_ptr_iter;
-        }
-        reference operator[](difference_type d) const
-        {
-            return *m_ptr_iter[d];
-        }
-        pointer operator->() const
-        {
-            return m_ptr_iter.operator*().operator*();
-        }
-
-        PointerDereferencingIterator& operator++()
-        {
-            ++m_ptr_iter;
-            return *this;
-        }
-        PointerDereferencingIterator operator++(int)
-        {
-            return m_ptr_iter++;
-        }
-        PointerDereferencingIterator& operator+=(difference_type d)
-        {
-            m_ptr_iter += d;
-            return *this;
-        }
-        PointerDereferencingIterator operator+(difference_type d)
-        {
-            return m_ptr_iter + d;
-        }     
-        friend PointerDereferencingIterator operator+(difference_type d, const PointerDereferencingIterator& i)
-        {
-            return i + d;
-        }
-
-        PointerDereferencingIterator& operator--()
-        {
-            --m_ptr_iter;
-            return *this;
-        }
-        PointerDereferencingIterator operator--(int)
-        {
-            return m_ptr_iter--;
-        }
-        PointerDereferencingIterator& operator-=(difference_type d)
-        {
-            m_ptr_iter -= d;
-            return *this;
-        }
-        PointerDereferencingIterator operator-(difference_type d)
-        {
-            return m_ptr_iter - d;
-        }
-        difference_type operator-(PointerDereferencingIterator d)
-        {
-            return m_ptr_iter - d.m_ptr_iter;
-        }
-
-        bool operator<(const PointerDereferencingIterator& other) const
-        {
-            return m_ptr_iter < other.m_ptr_iter;
-        }
-        bool operator<=(const PointerDereferencingIterator& other) const
-        {
-            return m_ptr_iter <= other.m_ptr_iter;
-        }
-        bool operator>(const PointerDereferencingIterator& other) const
-        {
-            return m_ptr_iter > other.m_ptr_iter;
-        }
-        bool operator>=(const PointerDereferencingIterator& other) const
-        {
-            return m_ptr_iter >= other.m_ptr_iter;
-        }
-        bool operator!=(const PointerDereferencingIterator& other) const        
-        {
-            return m_ptr_iter != other.m_ptr_iter;
-        }
-        bool operator==(const PointerDereferencingIterator& other) const
-        {
-            return m_ptr_iter == other.m_ptr_iter;
+            return *p;
         }
     };
+
+    //base class for dereferencing iterator
+    template <class PtrIter>
+    using PointerDereferencingIteratorBase =
+        decltype(make_transform_iterator(std::declval<PtrIter>(), std::declval<DerefFunctor>()));
+} // namespace details
+
+/**
+ * iterator adaptor that makes a range of T* look like a range of T
+ * e.g. std::vector<int*> v;
+ * then *PointerDereferencingIterator<...>(v.begin()) == **v.begin()
+ */
+template <class PtrIter>
+class PointerDereferencingIterator : public details::PointerDereferencingIteratorBase<PtrIter>
+{
+public:
+    PointerDereferencingIterator(PtrIter ptr_iter)
+        : details::PointerDereferencingIteratorBase<PtrIter>(ptr_iter)
+    {
+    }
+};
 }
 
 #endif
