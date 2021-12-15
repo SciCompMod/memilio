839db15fe569dd08539794d8498f933eb80a9827
Abele, Daniel <daniel.abele@dlr.de>
2021-12-15 18:52:21 +0100

    include only our lib in coverage

--- cpp/cmake/CodeCoverage.cmake
+++ cpp/cmake/CodeCoverage.cmake
@@ -197,7 +197,7 @@ function(setup_target_for_coverage_lcov)
 
     set(options NO_DEMANGLE)
     set(oneValueArgs BASE_DIRECTORY NAME)
-    set(multiValueArgs EXCLUDE EXECUTABLE EXECUTABLE_ARGS DEPENDENCIES LCOV_ARGS GENHTML_ARGS)
+    set(multiValueArgs EXCLUDE INCLUDE EXECUTABLE EXECUTABLE_ARGS DEPENDENCIES LCOV_ARGS GENHTML_ARGS)
     cmake_parse_arguments(Coverage "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
 
     if(NOT LCOV_PATH)
@@ -225,12 +225,20 @@ function(setup_target_for_coverage_lcov)
     endforeach()
     list(REMOVE_DUPLICATES LCOV_EXCLUDES)
 
+    # Collect excludes (CMake 3.4+: Also compute absolute paths)
+    set(LCOV_INCLUDES "")
+    foreach(INCLUDE ${Coverage_INCLUDE})
+        if(CMAKE_VERSION VERSION_GREATER 3.4)
+            get_filename_component(INCLUDE ${INCLUDE} ABSOLUTE BASE_DIR ${BASEDIR})
+        endif()
+        list(APPEND LCOV_INCLUDES "--include")
+        list(APPEND LCOV_INCLUDES "${INCLUDE}")
+    endforeach()
+
     # Conditional arguments
     if(CPPFILT_PATH AND NOT ${Coverage_NO_DEMANGLE})
       set(GENHTML_EXTRA_ARGS "--demangle-cpp")
     endif()
-
-    message(${LCOV_EXCLUDES})
 
     # Setup target
     add_custom_target(${Coverage_NAME}
@@ -238,13 +246,13 @@ function(setup_target_for_coverage_lcov)
         # Cleanup lcov
         COMMAND ${LCOV_PATH} ${Coverage_LCOV_ARGS} --gcov-tool ${GCOV_PATH} -directory . -b ${BASEDIR} --zerocounters
         # Create baseline to make sure untouched files show up in the report
-        COMMAND ${LCOV_PATH} ${Coverage_LCOV_ARGS} --gcov-tool ${GCOV_PATH} -c -i -d . -b ${BASEDIR} -o ${Coverage_NAME}.base
+        COMMAND ${LCOV_PATH} ${Coverage_LCOV_ARGS} --gcov-tool ${GCOV_PATH} -c -i -d . -b ${BASEDIR} -o ${Coverage_NAME}.base ${LCOV_INCLUDES}
 
         # Run tests
         COMMAND ${Coverage_EXECUTABLE} ${Coverage_EXECUTABLE_ARGS} || exit 0
 
         # Capturing lcov counters and generating report
-        COMMAND ${LCOV_PATH} ${Coverage_LCOV_ARGS} --gcov-tool ${GCOV_PATH} --directory . -b ${BASEDIR} --capture --output-file ${Coverage_NAME}.capture
+        COMMAND ${LCOV_PATH} ${Coverage_LCOV_ARGS} --gcov-tool ${GCOV_PATH} --directory . -b ${BASEDIR} --capture --output-file ${Coverage_NAME}.capture ${LCOV_INCLUDES}
         # add baseline counters
         COMMAND ${LCOV_PATH} ${Coverage_LCOV_ARGS} --gcov-tool ${GCOV_PATH} -a ${Coverage_NAME}.base -a ${Coverage_NAME}.capture --output-file ${Coverage_NAME}.total
         # filter collected data to final coverage report
