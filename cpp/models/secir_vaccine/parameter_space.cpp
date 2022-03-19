3d9a5a273b00372e63cb79b6862854106c359726
Abele, Daniel <daniel.abele@dlr.de>
2022-03-19 12:51:57 +0100

    clean up parameter space for vaccine model

--- cpp/models/secir_vaccine/parameter_space.cpp
+++ cpp/models/secir_vaccine/parameter_space.cpp
@@ -26,67 +26,6 @@ namespace mio
 namespace vaccinated
 {
 
-    void set_params_distributions_normal(SecirModel& model, double t0, double tmax, double dev_rel)
-    {
-        auto set_distribution = [dev_rel](UncertainValue& v, double min_val = 0.001) {
-            v.set_distribution(ParameterDistributionNormal(std::max(min_val, (1 - dev_rel * 2.6) * v),
-                                                           std::max(min_val, (1 + dev_rel * 2.6) * v),
-                                                           std::max(min_val, double(v)), dev_rel * v));
-        };
-
-        set_distribution(model.parameters.get<Seasonality>(), 0.0);
-        set_distribution(model.parameters.get<ICUCapacity>());
-        set_distribution(model.parameters.get<TestAndTraceCapacity>());
-
-        // populations
-        for (auto i = AgeGroup(0); i < model.parameters.get_num_groups(); i++) {
-            for (auto j = Index<InfectionState>(0); j < Index<InfectionState>(InfectionState::Count); j++) {
-
-                // don't touch S and D
-                if (j == InfectionState::Susceptible || j == InfectionState::Dead) {
-                    continue;
-                }
-
-                // variably sized groups
-                set_distribution(model.populations[{i, j}], 0.0);
-            }
-        }
-
-        // times
-        for (auto i = AgeGroup(0); i < model.parameters.get_num_groups(); i++) {
-
-            set_distribution(model.parameters.get<IncubationTime>()[i]);
-            set_distribution(model.parameters.get<SerialInterval>()[i]);
-            set_distribution(model.parameters.get<InfectiousTimeMild>()[i]);
-            set_distribution(model.parameters.get<HospitalizedToHomeTime>()[i]);
-            set_distribution(model.parameters.get<HomeToHospitalizedTime>()[i]);
-            set_distribution(model.parameters.get<InfectiousTimeAsymptomatic>()[i]);
-            set_distribution(model.parameters.get<HospitalizedToICUTime>()[i]);
-            set_distribution(model.parameters.get<ICUToHomeTime>()[i]);
-            set_distribution(model.parameters.get<ICUToDeathTime>()[i]);
-
-            set_distribution(model.parameters.get<InfectionProbabilityFromContact>()[i]);
-            set_distribution(model.parameters.get<RelativeCarrierInfectability>()[i]);
-            set_distribution(model.parameters.get<AsymptoticCasesPerInfectious>()[i]);
-            set_distribution(model.parameters.get<RiskOfInfectionFromSympomatic>()[i]);
-            set_distribution(model.parameters.get<MaxRiskOfInfectionFromSympomatic>()[i]);
-            set_distribution(model.parameters.get<DeathsPerHospitalized>()[i]);
-            set_distribution(model.parameters.get<HospitalizedCasesPerInfectious>()[i]);
-            set_distribution(model.parameters.get<ICUCasesPerHospitalized>()[i]);
-        }
-
-        // dampings
-        auto matrices = std::vector<size_t>();
-        for (size_t i = 0; i < model.parameters.get<ContactPatterns>().get_cont_freq_mat().get_num_matrices(); ++i) {
-            matrices.push_back(i);
-        }
-        auto groups = Eigen::VectorXd::Constant(Eigen::Index(model.parameters.get_num_groups().get()), 1.0);
-        model.parameters.get<ContactPatterns>().get_dampings().emplace_back(
-            mio::UncertainValue(0.5), mio::DampingLevel(0), mio::DampingType(0),
-            mio::SimulationTime(t0 + (tmax - t0) / 2), matrices, groups);
-        set_distribution(model.parameters.get<ContactPatterns>().get_dampings()[0].get_value(), 0.0);
-    }
-
     void draw_sample_demographics(SecirModel& model)
     {
         model.parameters.get<ICUCapacity>().draw_sample();
--- cpp/models/secir_vaccine/parameter_space.h
+++ cpp/models/secir_vaccine/parameter_space.h
@@ -35,31 +35,22 @@ namespace mio
 {
 namespace vaccinated
 {
-    /* Sets alls SecirParams parameters normally distributed,
-*  using the current value and a given standard deviation
-* @param[inout] params SecirParams including contact patterns for alle age groups
-* @param[in] t0 start time
-* @param[in] tmax end time
-* @param[in] dev_rel maximum relative deviation from particular value(s) given in params
-*/
-    void set_params_distributions_normal(SecirModel& model, double t0, double tmax, double dev_rel);
-
     /**
- * draws a sample from the specified distributions for all parameters related to the demographics, e.g. population.
- * @param[inout] model SecirModel including contact patterns for alle age groups
- */
+     * draws a sample from the specified distributions for all parameters related to the demographics, e.g. population.
+     * @param[inout] model SecirModel including contact patterns for alle age groups
+     */
     void draw_sample_demographics(SecirModel& model);
 
     /**
- * draws a sample from the specified distributions for all parameters related to the infection.
- * @param[inout] model SecirModel including contact patterns for alle age groups
- */
+     * draws a sample from the specified distributions for all parameters related to the infection.
+     * @param[inout] model SecirModel including contact patterns for alle age groups
+     */
     void draw_sample_infection(SecirModel& model);
 
     /** Draws a sample from SecirModel parameter distributions and stores sample values
-* as SecirParams parameter values (cf. UncertainValue and SecirParams classes)
-* @param[inout] model SecirModel including contact patterns for alle age groups
-*/
+    * as SecirParams parameter values (cf. UncertainValue and SecirParams classes)
+    * @param[inout] model SecirModel including contact patterns for alle age groups
+    */
     void draw_sample(SecirModel& model);
 
 } // namespace vaccinated
