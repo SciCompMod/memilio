f6c46e7cb447ae372f17128bfe4f3d1a82b97d02
betz_mx <Maximilian.betz@dlr.de>
2022-06-10 16:14:37 +0200

    Change the name and behaviour of the parameters/home/betz_mx/memilio/build/tests/memilio-test

--- cpp/models/seir/model.h
+++ cpp/models/seir/model.h
@@ -45,35 +45,6 @@ namespace oseir
         Model()
             : Base(Po({Index<InfectionState>((size_t)InfectionState::Count)}, 0.), Pa())
         {
-#if !USE_DERIV_FUNC
-            //S to E
-            this->add_flow(
-                std::make_tuple(InfectionState::Susceptible), std::make_tuple(InfectionState::Exposed),
-                [](Pa const& p, Eigen::Ref<const Eigen::VectorXd> pop, Eigen::Ref<const Eigen::VectorXd> y, double t) {
-                    ScalarType cont_freq_eff = params.contact_frequency.get_matrix_at(t)(0, 0);
-
-                    //TODO: We should probably write a static Po::get_total_from function
-                    ScalarType divN =
-                        1.0 / (Po::get_from(y, InfectionState::Susceptible) + Po::get_from(y, InfectionState::Exposed) +
-                               Po::get_from(y, InfectionState::Infected) + Po::get_from(y, InfectionState::Recovered));
-                    return cont_freq_eff * Po::get_from(y, InfectionState::Susceptible) *
-                           Po::get_from(pop, InfectionState::Infected) * divN;
-                });
-
-            //E to I
-            this->add_flow(std::make_tuple(InfectionState::Exposed), std::make_tuple(InfectionState::Infected),
-                           [](Pa const& p, Eigen::Ref<const Eigen::VectorXd> /*pop*/,
-                              Eigen::Ref<const Eigen::VectorXd> y, double /*t*/) {
-                               return p.times.get_incubation_inv() * Po::get_from(y, InfectionState::Exposed);
-                           });
-
-            //I to R
-            this->add_flow(std::make_tuple(InfectionState::Infected), std::make_tuple(InfectionState::Recovered),
-                           [](Pa const& p, Eigen::Ref<const Eigen::VectorXd> /*pop*/,
-                              Eigen::Ref<const Eigen::VectorXd> y, double /*t*/) {
-                               return p.times.get_infectious_inv() * Po::get_from(y, InfectionState::Infected);
-                           });
-#endif
         }
 
 #if USE_DERIV_FUNC
