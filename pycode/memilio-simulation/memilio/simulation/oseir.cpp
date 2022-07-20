af278f82f2dada0a928e4c075f6e4b26491c1390
MaxBetzDLR <Maximilian.betz@dlr.de>
2022-07-20 11:19:11 +0200

    CustomIndexArray binds needed indices

--- pycode/memilio-simulation/memilio/simulation/oseir.cpp
+++ pycode/memilio-simulation/memilio/simulation/oseir.cpp
@@ -48,14 +48,7 @@ pymio::iterable_enum<mio::oseir::InfectionState>(m, "InfectionState")
     .value("Infected", mio::oseir::InfectionState::Infected)
     .value("Recovered", mio::oseir::InfectionState::Recovered);
 
-pymio::bind_Index<mio::oseir::InfectionState>(m, "Index_InfectionState");
-try{
-    pymio::bind_Index<mio::oseir::InfectionState>(m, "Index_InfectionState");
-}catch(...){
-    py::print("Exception Thrown");
-}
 pymio::bind_CustomIndexArray<mio::UncertainValue, mio::oseir::InfectionState>(m, "PopulationArray");
-
 pymio::bind_ParameterSet<mio::oseir::Parameters>(m, "Parameters");
 
 using Populations = mio::Populations<mio::oseir::InfectionState>;
