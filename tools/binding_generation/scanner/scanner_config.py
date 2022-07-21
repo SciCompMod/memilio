f5092e73d6b07b74055f93a63c642c4ad28c2dfc
MaxBetzDLR <Maximilian.betz@dlr.de>
2022-07-21 17:14:22 +0200

    bugs

--- tools/binding_generation/scanner/scanner_config.py
+++ tools/binding_generation/scanner/scanner_config.py
@@ -12,16 +12,16 @@ class ScannerConfig:
     path_database           : str
     namespace               : str
     optional                : dict = field(default_factory=dict)
-    model_class              : str  = field(init = False)
+    model_class             : str  = field(init = False)
     parameterset_name       : str  = field(init = False)
     project_path            : str  = field(init = False)
 
     def __post_init__(self):
         # Predefined Variables
-        self.model_class         = "SecirModel"
-        self.parameterset_name  = "SecirParamsBase"
-        #self.model_class         = "Model"
-        #self.parameterset_name  = "Parameters"
+        #self.model_class         = "SecirModel"
+        #self.parameterset_name  = "SecirParamsBase"
+        self.model_class         = "Model"
+        self.parameterset_name  = "Parameters"
         self.project_path       = check_output(["git", "rev-parse", "--show-toplevel"]).decode()[:-1]
 
 
--- tools/binding_generation/seir2.py
+++ tools/binding_generation/seir2.py
@@ -7,7 +7,7 @@ from scanner.scanner_config import ScannerConfig
 
 # Define ScannerConfig and initialize Scanner
 with open('config.json', 'r') as file:
-    conf = ScannerConfig.schema().loads(file.read(), many=True)[1]
+    conf = ScannerConfig.schema().loads(file.read(), many=True)[0]
 scanner = Scanner(conf)
 
 # Extract results of Scanner into a model data
