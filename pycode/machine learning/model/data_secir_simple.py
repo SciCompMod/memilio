28f0cda388207a6d986c5e13e4eb2a001e62d75d
HenrZu <henrik-zunker@hotmail.de>
2022-08-10 11:46:56 +0200

    rework data generation. Delete datasets

--- pycode/machine learning/model/data_secir_simple.py
+++ pycode/machine learning/model/data_secir_simple.py
@@ -4,6 +4,7 @@ from memilio.simulation.secir import SecirModel, simulate, AgeGroup, Index_Infec
 from memilio.simulation.secir import InfectionState as State
 import numpy as np
 import pandas as pd
+import pickle
 from datetime import date
 from math import ceil
 import random
@@ -18,8 +19,7 @@ import memilio.simulation as mio
 
 import seaborn as sns # plot after normalization
 
-
-def run_secir_simulation():
+def run_secir_simulation(days):
     """! Slightly modified example of Secir Simple.
 
    Here, the initial values are choosen randomly, so the model is no longer deterministic.
@@ -36,20 +36,17 @@ def run_secir_simulation():
     # Define random population number in intervall (0,500000)
     populations = [500000 * random.random()]
 
-    days = 35  # number of days to simulate
-
     # since we defined the number of days, we already know the dimension of our dataset.
     # We aim to save the compartment population for each day.
-    # i.e. for days = 100 (+1 for t0) and len(compartments = 8) -> 101 x  8 array
-    # we also add one column for the damping factor
-    data = np.zeros((days,len(compartments) + 1))
+    # i.e. for days = 35 and len(compartments = 8) -> 35 x  8 array
+    # data = np.zeros((days,len(compartments)))
+    data = []
 
     start_day = 1
     start_month = 1
     start_year = 2019
     dt = 0.1
     num_groups = 1
-    num_compartments = len(compartments)
 
     # Initialize Parameters
     model = SecirModel(1)
@@ -58,11 +55,11 @@ def run_secir_simulation():
 
 
     # Define random values for damping coef and start date
-    damping_factor = random.random()
-    damping_date = ceil(random.random() * days) # scale with days since random is in Interval (0,1)
+    # damping_factor = 1
+    # damping_date = ceil(random.random() * days) # scale with days since random is in Interval (0,1)
 
     # save damping factor in vector. -1 for index correction
-    data[damping_date-1:,-1] = damping_factor
+    # data[damping_date-1:,-1] = damping_factor
 
     # Set parameters
 
@@ -107,94 +104,138 @@ def run_secir_simulation():
         (num_groups, num_groups)) * 1
     model.parameters.ContactPatterns.cont_freq_mat[0].minimum = np.ones(
         (num_groups, num_groups)) * 0
-    #model.parameters.ContactPatterns.cont_freq_mat.add_damping(
-     #   Damping(coeffs=np.r_[damping_factor], t=damping_date, level=0, type=0))
+    # model.parameters.ContactPatterns.cont_freq_mat.add_damping(
+    #     Damping(coeffs=np.r_[damping_factor], t=damping_date, level=0, type=0))
 
     # Apply mathematical constraints to parameters
     model.apply_constraints()
 
     # Run Simulation
-    # store data for t = 0
-    data[0,:-1] = model.populations.get_compartments()
-    # TODO: Do we need to build the whole Model for new, if we only change the number of days?
+    # data[0] = model.populations.get_compartments()
+    data.append(model.populations.get_compartments())
     for day in range(1, days):
         result = simulate(0, day, dt, model)
-
-        # Maybe round data
-        # data[day,:] = np.ceil(result_day)
-        data[day,:-1] = result.get_last_value()
+        # data[day] = result.get_last_value()
+        data.append(result.get_last_value())
 
     return data
 
-
-
-def generate_data(num_runs, path, save_data=True):
+def generate_data(num_runs, path, input_width, label_width, normalize_labels=False, save_data=True):
     """! Generate dataset by calling run_secir_simulation (num_runs)-often
 
    @param num_runs Number of times, the function run_secir_simulation is called.
    @param path Path, where the datasets are stored.
    @param save_data Option to deactivate the save of the dataset. Per default true.
    """
-    data = []
-    damping_factors = []
-    damping_dates = []
+    data = {
+        "inputs" : [],
+        "labels" : []
+    }
+
+    days = input_width + label_width
 
     # show progess in terminal for longer runs
+    # Due to the random structure, theres currently no need to shuffle the data
     bar = Bar('Number of Runs done', max=num_runs)
     for _ in range(0, num_runs):
-        data_run, damping_factor_run, damping_date_run = run_secir_simulation()
-        data.append(data_run)
-        damping_factors.append(damping_factor_run)
-        damping_dates.append(damping_date_run)
+        data_run = run_secir_simulation(days)
+        data["inputs"].append(data_run[:input_width])
+        data["labels"].append(data_run[input_width:])
         bar.next()
     
     bar.finish()
 
     if save_data:
-    
-        columns = ['Susceptible', 'Exposed', 'Carrier',
-                        'Infected', 'Hospitalized', 'ICU', 'Recovered', 'Dead', 'Damping Factor']
 
-        dataset = pd.DataFrame(np.concatenate(data), columns=columns)
-        dampings = pd.DataFrame([damping_factors, damping_dates]).transpose()
-        # short overview over dataset
-        print(dataset.describe().transpose())
+        # cast dfs to tensors
+        data["inputs"] = tf.stack(data["inputs"])
+        data["labels"] = tf.stack(data["labels"])
 
-        # save train dataset as csv file
+        # normalize data 
+        data["inputs"] = normalize(data["inputs"])
+        if normalize_labels:
+            data["labels"] = normalize(data["labels"])
 
-        # check if data directory exists. If necessary create it.
+        # data = splitdata(data["inputs"], data["labels"])
         
-        if not os.path.isdir(path):
+        # check if data directory exists. If necessary create it.
+        if not os.path.isdir(path): 
             os.mkdir(path)
 
-        # save data
-        dataset.to_csv(
-            os.path.join(path, 'data_secir_simple.txt'),
-            sep=' ', index=False,
-            header=True)
+        # save dict to json file
+        with open(os.path.join(path, 'data_secir_simple.pickle'), 'wb') as f:
+            pickle.dump(data, f)
 
+def splitdata(inputs, labels, split_train=0.7, 
+              split_valid=0.2, split_test=0.1):
+    """! Split data in train, valid and test
 
-        dampings.to_csv(
-            os.path.join(path, 'data_secir_simple_dampings.txt'),
-            sep=' ', index=False,
-            header=True)
+   @param inputs input dataset
+   @param labels label dataset
+   @param split_train ratio of train datasets
+   @param split_valid ratio of validation datasets
+   @param split_test ratio of test datasets
+   """
 
+    if split_train + split_valid + split_test != 1:
+        ValueError("summed Split ratios not equal 1! Please adjust the values")
+    elif inputs.shape[0] != labels.shape[0] or inputs.shape[2] != labels.shape[2]:
+        ValueError("Number of batches or features different for input and labels")
 
+    n = inputs.shape[0]
+    n_train = int(n * split_train)
+    n_valid = int(n * split_valid)
+    n_test = int(n * split_test)
 
+    if n_train + n_valid + n_test != n:
+        n_test = n - n_train - n_valid
 
+    inputs_train, inputs_valid, inputs_test = tf.split(inputs, [n_train, n_valid, n_test], 0)
+    labels_train, labels_valid, labels_test = tf.split(labels, [n_train, n_valid, n_test], 0)
 
-        print("Data saved in " + path)
+    data = {
+        "train_inputs" : inputs_train,
+        "train_labels" :labels_train,
+        "valid_inputs" : inputs_valid,
+        "valid_labels" :labels_valid,
+        "test_inputs" : inputs_test,
+        "test_labels" :labels_test
+    }
 
+    return data
 
+def normalize(data):
+    """! # normalize input data with:
+                               (value     − min_value)
+                    data =  _________________________
+                               (max_value − min_value)
+                    
+    alternative is tf.linalg.normalize(data["inputs"], ord='euclidean', axis=None, name=None)
+
+    @param data  dataset
+
+    """
+    return tf.divide(
+            tf.subtract(
+                data, 
+                tf.reduce_min(data)
+            ), 
+            tf.subtract(
+                tf.reduce_max(data), 
+                tf.reduce_min(data)
+            )
+        )
 
 if __name__ == "__main__":
     # TODO: Save contact matrix depending on the damping.
     # In the actual state it might be enough to save the regular one and the damping
 
     path = os.path.dirname(os.path.realpath(__file__))
-    path_data = os.path.join(os.path.dirname(os.path.realpath(path)), 'data35')
+    path_data = os.path.join(os.path.dirname(os.path.realpath(path)), 'data')
 
-    num_runs = 1000
-    generate_data(num_runs, path_data)
+    input_width = 5
+    label_width = 30
+    num_runs = 500
+    generate_data(num_runs, path_data, input_width, label_width)
     
-print('hello') 
+    
