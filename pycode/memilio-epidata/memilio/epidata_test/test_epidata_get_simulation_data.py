4e239837d677d421276d9b18037c134c3d8d298f
patricklnz <93600225+patricklnz@users.noreply.github.com>
2022-08-31 10:07:12 +0200

    153 adjust all arguments for data download tools (#191)
    Adjusting arguments and arguments' order in data download tools

--- pycode/memilio-epidata/memilio/epidata_test/test_epidata_get_simulation_data.py
+++ pycode/memilio-epidata/memilio/epidata_test/test_epidata_get_simulation_data.py
@@ -41,41 +41,48 @@ class TestGetSimulationData(fake_filesystem_unittest.TestCase):
     def test_get_call_sub_functions(self, mock_popul, mock_cases,
                                     mock_divi, mock_vaccination):
 
-        [read_data, file_format, out_folder, no_raw, end_date, impute_dates,
-         make_plot, moving_average, split_berlin, start_date] = [False,
-                                                                 "json_timeasstring", self.path,
-                                                                 False, dd.defaultDict['end_date'],
-                                                                 dd.defaultDict['impute_dates'],
-                                                                 dd.defaultDict['make_plot'],
-                                                                 dd.defaultDict['moving_average'],
-                                                                 dd.defaultDict['split_berlin'],
-                                                                 dd.defaultDict['start_date']]
+        [read_data, file_format, out_folder, no_raw, start_date, end_date,
+         impute_dates, moving_average, make_plot, split_berlin, rep_date] = [
+            False, "json_timeasstring", self.path, False,
+            dd.defaultDict['start_date'],
+            dd.defaultDict['end_date'],
+            dd.defaultDict['impute_dates'],
+            dd.defaultDict['moving_average'],
+            dd.defaultDict['make_plot'],
+            dd.defaultDict['split_berlin'],
+            dd.defaultDict['rep_date']]
 
         gsd.get_simulation_data(
-            read_data, file_format, out_folder, no_raw, end_date, impute_dates,
-            make_plot, moving_average, split_berlin, start_date)
+            read_data=read_data, file_format=file_format,
+            out_folder=out_folder, no_raw=no_raw, start_date=start_date,
+            end_date=end_date, impute_dates=impute_dates,
+            moving_average=moving_average, make_plot=make_plot,
+            split_berlin=split_berlin, rep_date=rep_date)
 
         arg_dict_all = {
             "read_data": dd.defaultDict['read_data'],
             "file_format": dd.defaultDict['file_format'],
             "out_folder": self.path,
             'no_raw': dd.defaultDict["no_raw"]}
-
-        arg_dict_cases = {
-            **arg_dict_all, "make_plot": dd.defaultDict['make_plot'],
+        
+        arg_dict_data_download = {
+            "start_date": dd.defaultDict['start_date'],
+            "end_date": dd.defaultDict['end_date'],
             "impute_dates": dd.defaultDict['impute_dates'],
             "moving_average": dd.defaultDict['moving_average'],
+            "make_plot": dd.defaultDict['make_plot']}
+
+        arg_dict_cases = {
+            **arg_dict_all, **arg_dict_data_download,
+            "rep_date": dd.defaultDict['rep_date'],
             "split_berlin": dd.defaultDict['split_berlin']}
 
         arg_dict_divi = {
-            **arg_dict_all, "end_date": dd.defaultDict['end_date'],
-            "start_date": dd.defaultDict['start_date'],
-            "moving_average": dd.defaultDict['moving_average']}
+            **arg_dict_all, **arg_dict_data_download}
 
         arg_dict_vaccination = {
-            **arg_dict_all,
-            "make_plot": dd.defaultDict['make_plot'],
-            "moving_average": dd.defaultDict['moving_average']}
+            **arg_dict_all, **arg_dict_data_download,
+            "sanitize_data": dd.defaultDict['sanitize_data']}
 
         mock_popul.assert_called()
         mock_popul.assert_called_with(**arg_dict_all)
