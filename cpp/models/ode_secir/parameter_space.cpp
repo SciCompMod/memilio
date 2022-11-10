071cf3d73cfb389fb877b7a7e2312781edeac49b
HenrZu <henrik-zunker@hotmail.de>
2022-11-10 16:51:26 +0100

    sample timeinfected

--- cpp/models/ode_secir/parameter_space.cpp
+++ cpp/models/ode_secir/parameter_space.cpp
@@ -121,9 +121,8 @@ void draw_sample_infection(Model& model)
 
     for (auto i = AgeGroup(0); i < model.parameters.get_num_groups(); i++) {
         //not age dependent
-        model.parameters.get<IncubationTime>()[i]       = model.parameters.get<IncubationTime>()[AgeGroup(0)];
-        model.parameters.get<SerialInterval>()[i]       = model.parameters.get<SerialInterval>()[AgeGroup(0)];
-        model.parameters.get<TimeInfectedSymptoms>()[i] = model.parameters.get<TimeInfectedSymptoms>()[AgeGroup(0)];
+        model.parameters.get<IncubationTime>()[i] = model.parameters.get<IncubationTime>()[AgeGroup(0)];
+        model.parameters.get<SerialInterval>()[i] = model.parameters.get<SerialInterval>()[AgeGroup(0)];
         model.parameters.get<RelativeTransmissionNoSymptoms>()[i] =
             model.parameters.get<RelativeTransmissionNoSymptoms>()[AgeGroup(0)];
         model.parameters.get<RiskOfInfectionFromSymptomatic>()[i] =
@@ -132,6 +131,7 @@ void draw_sample_infection(Model& model)
             model.parameters.get<MaxRiskOfInfectionFromSymptomatic>()[AgeGroup(0)];
 
         //age dependent
+        model.parameters.get<TimeInfectedSymptoms>()[i].draw_sample();
         model.parameters.get<TimeInfectedSevere>()[i].draw_sample();
         model.parameters.get<TimeInfectedCritical>()[i].draw_sample();
 
