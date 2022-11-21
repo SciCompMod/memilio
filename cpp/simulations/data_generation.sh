0f110eceeb12c12a245f6d97de4e148804ea46ab
HenrZu <henrik-zunker@hotmail.de>
2022-11-21 12:28:37 +0100

    add data dir, rm transform

--- cpp/simulations/data_generation.sh
+++ cpp/simulations/data_generation.sh
@@ -3,23 +3,21 @@
 
 # activate virutal_env 
 # The path to the virtual environment has to be adjusted individually
-path_virtual_env="$HOME/Documents/virtualenv/corona/bin/activate"
+path_virtual_env="$HOME/Documents/virtualenv/bin/activate"
 if ! [[ -f $path_virtual_env ]]
 then echo "Set path_virtual_env to an existing python venv." && exit
 else source $path_virtual_env
 fi
-
 # path to memilio dir
-PWD=$(pwd)
 cd ../..
+data_dir=$PWD/data/pydata
 
 # download data
 cd "pycode/memilio-epidata"
 python setup.py install
-python memilio/epidata/getPopulationData.py
-python memilio/epidata/getDIVIData.py -m 7
-python memilio/epidata/getSimulationData.py -m 7
-python memilio/epidata/getCaseData.py -m 7
-python memilio/epidata/transformMobilityData.py
+python memilio/epidata/getPopulationData.py -o $data_dir
+python memilio/epidata/getDIVIData.py -o $data_dir -m 7
+python memilio/epidata/getSimulationData.py -o $data_dir -m 7
+python memilio/epidata/getCaseData.py -o $data_dir -m 7
 
 echo "Generation was succesful." 
