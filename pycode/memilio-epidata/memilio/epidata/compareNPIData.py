96d16449edc176d1cb283969799c273d3335eda0
Martin Joachim Kühn <Martin.Kuehn@DLR.de>
2022-11-21 14:12:46 +0100

    .

--- pycode/memilio-epidata/memilio/epidata/compareNPIData.py
+++ pycode/memilio-epidata/memilio/epidata/compareNPIData.py
@@ -3,18 +3,19 @@ import csv
 import pandas as pd
 import numpy as np
 
-directory = '/home/wend_aa/Documents/PSS/NPIs'
+# directory = '/home/wend_aa/Documents/PSS/NPIs'
+directory = 'c:\\work\\projets\\epidemiology\\code\\memilio\\data/pydata\\Germany/'
 
 #numberofcities = 2
 
 df_npis_old = pd.read_csv(
-    os.path.join(directory, 'old', 'kr_massnahmen_unterkategorien.csv'),
+    os.path.join(directory, 'kr_massnahmen_unterkategorien.csv'),
     sep=',')  # , nrows=numberofcities*1248
 print(df_npis_old)
 numberofcities = int(len(df_npis_old) / 1248)
 print('Number of cities', numberofcities)
 
-list = ['m01a', 'm01b', 'm02a', 'm02b', 'm03', 'm04', 'm05', 'm06', 'm07', 'm08', 'm09',
+codelist = ['m01a', 'm01b', 'm02a', 'm02b', 'm03', 'm04', 'm05', 'm06', 'm07', 'm08', 'm09',
         'm10', 'm11', 'm12', 'm13', 'm14', 'm15', 'm16', 'm17', 'm18', 'm19', 'm20', 'm21']
 
 #list = ['m01a', 'm01b']
@@ -35,46 +36,61 @@ numberofdays = 883
 # create data frame that contains structure of old data
 df = df_npis_old.iloc[:, :6]
 
-for i in range(numberofcities):
-    print('City number', i)
-    counter = 0
-    for code in list:
-        # print(code)
-        df_npis_new = pd.read_csv(
-            os.path.join(directory, 'new',
-                         'kr_massn_unterkat_{}.csv'.format(code)),
-            sep=',')  # , skiprows=1248
-        # print(df_npis_new.iloc[0:numberofdays])
-        #df_npis_new = df_npis_new.iloc[1152:]
-        #print('df_new', df_npis_new)
+start_county = 1
 
-        # initialization of new data frame:
-        if (i == 0 and counter == 0):
-            #print('initialization done')
-            # extract dates from new data
-            dates = df_npis_new.iloc[:numberofdays, 5]
-            # rename dates so that they match dates from old npi dataframe
-            dates_new = []
-            for date in dates:
-                date = ''.join(('d', date.replace('-', '')))
-                dates_new.append(date)
-                # print(date)
+df_local = [pd.DataFrame() for i in range(401)]
+counter_col = 0
+for code in codelist:
+    # print(code)
+    df_npis_new = pd.read_csv(
+        os.path.join(directory, 'new',
+                        'kr_massn_unterkat_{}.csv'.format(code)),
+        sep=',')  # , skiprows=1248
+    # print(df_npis_new.iloc[0:numberofdays])
+    #df_npis_new = df_npis_new.iloc[1152:]
+    #print('df_new', df_npis_new)
+    counties = np.sort(df_npis_new.ags5.unique())
+    if len(df_npis_new) / len(counties) != numberofdays:
+        print('error')
+    if len(counties) != 401:
+        print('error')
 
-            # extend data frame with columns for each date that is included in new data set
-            df = pd.concat([df, pd.DataFrame(columns=dates_new)])
+    # extract dates from new data
+    dates = df_npis_new.iloc[:numberofdays, 5]
+    # rename dates so that they match dates from old npi dataframe
+    dates_new = ['d' + date.replace('-', '') for date in dates]
 
-        # get number of codes for current subcategory, we substract 6 because we have 6 columns for bundesland, kreis, etc.
-        numberofcodes = 0
-        numberofcodes += len(df_npis_new.columns) - 6
-        #print('numberofcodes', numberofcodes)
+    for i in range(0,401):
+        if counter_col == 0:
+            df_local[i] = pd.DataFrame(columns=list(df_npis_new.columns[0:5]) + ['code'] + dates_new)
+        else:
+            print('todo')
 
-        # insert values from new dataset into data frame
-        df.iloc[i*1248 + counter: i*1248 + counter + numberofcodes,
-                6:] = df_npis_new.iloc[0:numberofdays, 6:].T
+        df_npis_new[df_npis_new.ags5 == counties[i]].iloc[:, 6:].T.reset_index()
 
-        #print(df.iloc[i*1248 + counter: i*1248 + counter + numberofcodes])
+    if counter_col == 0:
+        df = df_npis_new.copy()
+    else:
+        # extend data frame with columns for each date that is included in new data set
+        df = pd.concat([df, pd.DataFrame(columns=list(df_npis_new.columns[0:5])+ dates_new)])
 
-        counter += numberofcodes
+    for i in range(start_county, numberofcities):
+        print('County number', i)
+        counter_col = 0
+
+
+
+    # get number of codes for current subcategory, we substract 6 because we have 6 columns for bundesland, kreis, etc.
+    numberofcodes = len(df_npis_new.columns) - 6
+    #print('numberofcodes', numberofcodes)
+
+    # insert values from new dataset into data frame
+    df_local[0].iloc[:, 6:] = df_npis_new.iloc[0:numberofdays, 6:].T
+
+    #print(df.iloc[i*1248 + counter: i*1248 + counter + numberofcodes])
+
+    counter_col += numberofcodes
+    counter_counties += 1
 print(df)
 
 df_dropped = df
@@ -88,17 +104,17 @@ for i in range(numberofcities):
     # df_dropped = df_dropped.drop(
     #   df_dropped.index[range((i+1)*counter, i*counter + 1248)])
     df_dropped = df_dropped.drop(
-        df_dropped.index[range((i+1)*counter, i*counter + 1248)])
+        df_dropped.index[range((i+1)*counter_col, i*counter_col + 1248)])
     #print('df_dropped in loop', df_dropped.iloc[:(i+1)*counter + 3])
     # df_npis_old_dropped = df_npis_old_dropped.drop(df_npis_old_dropped.index[range(
     #   (i+1)*counter, i*counter + 1248)])
     df_npis_old_dropped = df_npis_old_dropped.drop(df_npis_old_dropped.index[range(
-        (i+1)*counter, i*counter + 1248)])
+        (i+1)*counter_col, i*counter_col + 1248)])
 
     # extract all dropped rows from df_npis_old
    # print('indices', (i)*1248, (i+1)*counter)
     df_lost_subcat = df_lost_subcat.drop(
-        df_lost_subcat.index[range(i*(1248-counter), i*(1248-counter) + counter)])
+        df_lost_subcat.index[range(i*(1248-counter_col), i*(1248-counter_col) + counter_col)])
     # print('df_dropped in loop lost subcat',
     #     df_lost_subcat.iloc[:(i)*(1248-counter) + 3])
 
@@ -145,8 +161,8 @@ for i in range(df_npis_old_dropped.shape[0]):
 nodiffcities = []
 for i in range(numberofcities):
     try:
-        if pd.testing.assert_frame_equal(pd.DataFrame(df_npis_old_dropped.iloc[i*counter: (i+1)*counter, :]), pd.DataFrame(df_dropped.iloc[i*counter: (i+1)*counter, :]), check_dtype=False) == None:
-            nodiffcities.append(df_npis_old_dropped.iloc[i*counter]['kreis'])
+        if pd.testing.assert_frame_equal(pd.DataFrame(df_npis_old_dropped.iloc[i*counter_col: (i+1)*counter_col, :]), pd.DataFrame(df_dropped.iloc[i*counter_col: (i+1)*counter_col, :]), check_dtype=False) == None:
+            nodiffcities.append(df_npis_old_dropped.iloc[i*counter_col]['kreis'])
             #print('No difference in column', df_npis_old_dropped.columns[i])
 
     except AssertionError as e:
