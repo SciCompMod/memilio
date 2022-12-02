b9d3bba0409c9c628eec52a67d8daa80d95c8d0b
HenrZu <henrik-zunker@hotmail.de>
2022-12-02 17:36:33 +0100

    naming oseir exampling

--- pycode/examples/simulation/oseir_simple.py
+++ pycode/examples/simulation/oseir_simple.py
@@ -62,7 +62,7 @@ def run_oseir_simulation():
     model.parameters.ContactPatterns.add_damping(
         Damping(coeffs=np.r_[0.9], t=30.0, level=0, type=0))
 
-    # Apply mathematical constraints to parameters
+    # Check logical constraints to parameters
     model.check_constraints()
 
     # Run Simulation
