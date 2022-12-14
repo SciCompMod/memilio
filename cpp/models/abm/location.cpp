0c525112527be32c379beb0cbe7d0f5679d24b63
= <david.kerkmann@uni-duesseldorf.de>
2022-12-14 11:44:33 +0100

    Add initialization of time series.

--- cpp/models/abm/location.cpp
+++ cpp/models/abm/location.cpp
@@ -39,6 +39,7 @@ Location::Location(LocationType type, uint32_t index, uint32_t num_cells)
     , m_cells(std::vector<Cell>(num_cells))
 {
     m_subpopulations_time_series.add_time_point(0);
+    m_subpopulations_time_series.get_last_value().setZero();
 }
 
 InfectionState Location::interact(const Person& person, TimeSpan dt,
@@ -214,11 +215,11 @@ double Location::compute_relative_transmission_risk()
     }
 }
 
-void Location::store_current_population(const TimePoint& t) 
+void Location::store_current_population(const TimePoint& t)
 {
     m_subpopulations_time_series.add_time_point(t.seconds());
     m_subpopulations_time_series.get_last_value().setZero();
-    auto last_idx = m_subpopulations_time_series.get_num_time_points()-2;
+    auto last_idx = m_subpopulations_time_series.get_num_time_points() - 2;
     m_subpopulations_time_series.get_last_value() += m_subpopulations_time_series.get_value(last_idx);
 }
 
