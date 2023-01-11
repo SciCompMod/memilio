8ef18186ee9827eec5029778be35f6bceb6b1d8b
HenrZu <henrik-zunker@hotmail.de>
2023-01-11 16:04:34 +0100

    add sklearn to install req

--- pycode/memilio-surrogatemodel/setup.py
+++ pycode/memilio-surrogatemodel/setup.py
@@ -19,7 +19,8 @@ setup(
         'progress',
         'numpy>=1.22',  # smaller numpy versions cause a security issue
         'tensorflow',
-        'matplotlib', ],
+        'matplotlib',
+        'sklearn', ],
     extras_require={'dev': [
         # smaller pyfakefs versions use deprecated functions for matplotlib versions >=3.4
         'pyfakefs>=4.2.1',
