5d8f555b974881dc8e27fe4ab8e3b13abbc2c91b
Martin Joachim Kühn <Martin.Kuehn@DLR.de>
2023-01-13 18:50:58 +0100

    hdf5 read in

--- tools/plot_germany_heatmap.py
+++ tools/plot_germany_heatmap.py
@@ -46,26 +46,43 @@ def merge_eisenach(map_data : gpd.GeoDataFrame):
     dataframe.
 
     @param map_data GeoPandasDataFrame from county shape file
-    """ 
+    @return Frame with Wartburgkreis and Eisenach merged. 
+    """
     wartburg = map_data.ARS == '16063'
     eisenach = map_data.ARS == '16056'
-    map_data.loc[wartburg, 'geometry'] = [map_data[wartburg].geometry.values[0].union(map_data[eisenach].geometry.values[0])]
+    map_data.loc[wartburg, 'geometry'] = [
+        map_data[wartburg].geometry.values[0].union(
+            map_data[eisenach].geometry.values[0])]
     # remove Eisenach and return
     return map_data.drop(map_data[eisenach].index.values[0])
 
-def get_county_data(file, date, column, filters=None, output='sum', file_format='json'):
-    """ Reads county data from a json or hdf5 file. County data can be time
-    series data where a particular date is extracted or single time data which
-    is used rightaway. If the file contains multiple features per county, a 
+
+def read_data(
+        file, region_spec, column, date, filters=None, output='sum',
+        file_format='json'):
+    """ Reads data from a general json or specific hdf5 file as output by the 
+    MEmilio simulation framework and extracts parts of the data if filters are
+     applied. The input regional data can be a time series data where a 
+    particular date is extracted or single time data which is used rightaway.
+    If the file contains multiple features per region,
     particular columns need to be specified.
 
     @param file Path and filename of file to be read in, relative from current
         directory.
-    @param data Date to be extracted from data frame or empty if single or 
-        no date information is in the input table.
+    @param region_spec Specificier for region, for json, e.g., column name
+        of county IDs for hdf5 level of nesting X where to retrieve
+        fileX.keys(), X could either by empty, such that file.keys() is taken
+        or a list of nested keys [X0, X1, ...] such that 
+        file[X0][X1][...].keys()
     @param column Column with values that will be plotted.
+    @param data Date to be extracted from data frame if it contains a time 
+        series or if single or no date information is in the input table, 
+        provide date=None. For json, pd.date Objects need to be used, for
+        h5, provide an integer from the beginning of the time series.
     @param filters Dictionary with columns and values for filtering rows.
-        If a column or all filters are None then all values are taken.
+        None for a column or all filters means that all values are taken. 
+        For MEmilio h5 Files only 'Group' (AgeGroups from 'Group1' to 'Total') 
+        and 'InfectionState' (from 0 to InfetionState::Count-1) can be filtered.
     @param output [Either 'sum' or 'matrix'] If 'sum' is chosen all selected 
         values for one county will be summed up and only 'column' is returned
         for each county. If 'matrix' is chosen, then also the filter columns
@@ -76,51 +93,126 @@ def get_county_data(file, date, column, filters=None, output='sum', file_format=
     input_file = os.path.join(os.getcwd(), file[1])
     if file_format == 'json':
         df = pd.read_json(input_file + '.' + file_format)
-    elif file_format == 'h5':
-        # todo
-        df = h5py.File(input_file + '.' + file_format, 'r')
-    else:
-        print('Error')
-        # raise gd.DataError("Download of Vaccination Data failed. File is empty.")
+
+        date_str = date.strftime('%Y-%m-%d')
+
+        # If data contains a time series, extract the date, 
+        if date is not None:
+            if 'Date' in df.columns:
+                if df['Date'].nunique() > 1:
+                    df = df[df['Date'] == date_str]
+            else:
+                print('Error.')
+                # raise gd.DataError(Please provide date column.)   
 
 
-    date_str = date.strftime('%Y-%m-%d')
-
-    if 'Date' in df.columns:
-        if df['Date'].nunique() > 1:
-            df = df[df['Date'] == date_str]
-    dffilter = pd.Series([True for i in range(len(df))], index = df.index)
-    if filters != None:
-        for col, vals in filters.items():
-            if vals == None:
-                vals = df[col].unique()
-            dffilter = dffilter & (df[col].isin(vals))
-
-    if output == 'sum':
-        return df[dffilter].groupby(['ID_County']).agg({column : sum}).reset_index()
-    elif output == 'matrix':
+        # Filter data frame according to filter criterion provided 
+        dffilter = pd.Series([True for i in range(len(df))], index = df.index)
         if filters != None:
-            return df[dffilter].loc[:,['ID_County'] +  list(filters.keys()) + [column]]
+            for col, vals in filters.items():
+                if vals == None:
+                    vals = df[col].unique()
+                dffilter = dffilter & (df[col].isin(vals))
+                
+
+        # Aggregated or matrix output
+        if output == 'sum':
+            return df[dffilter].groupby(region_spec).agg({column : sum}).reset_index()
+        elif output == 'matrix':
+            if filters != None:
+                return df[dffilter].loc[:,[region_spec] +  list(filters.keys()) + [column]]
+            else:
+                return df
         else:
+            print('Error')
+            # raise gd.DataError("Chose output form.")
+
+    elif file_format == 'h5':
+        h5file = h5py.File(input_file + '.' + file_format, 'r')
+        # for nested groups, extract the level where the regions to be read in
+        # are stored
+        if region_spec is not None:
+            for elem in region_spec:
+                h5file = h5file[elem]
+
+        regions = list(h5file.keys())
+
+        # set no filtering if filters were set to None
+        if filters == None:
+            filters['Group'] = list(h5file[regions[i]].keys())[:-2] # remove 'Time' and 'Total'
+            filters['InfectionState'] = list(range(h5file[regions[i]]['Group1'].shape[1]))
+
+        InfectionStateList = [j for j in filters['InfectionState']]
+
+        # Create data frame to store results to plot
+        df = pd.DataFrame(columns=['Time', 'Region', 'Group', 'InfectionState', 'Count'])
+        df['Time'] = np.array([date for j in 
+                               range(len(regions) * len(filters['Group']) * len(InfectionStateList))])
+        
+        for i in range(len(regions)):
+            # set region identifier in data frame
+            region_start_idx = i * len(filters['Group']) * len(InfectionStateList)
+            region_end_idx = (i+1) * len(filters['Group']) * len(InfectionStateList)- 1
+            df.loc[region_start_idx:region_end_idx, 'Region'] = regions[i]
+
+            # get date index (allows different time points in regions as long 
+            # the particular point in time is available)
+            date_idx = np.where(np.abs(h5file[regions[i]]['Time'][:] - date)<1e-13)[0]
+            if len(date_idx) == 1:
+                k = 0
+                for group in filters['Group']:
+                    # Add (Age)Group identifier
+                    df.loc[region_start_idx + k*len(InfectionStateList):
+                            region_start_idx+(k+1)*len(InfectionStateList)-1,
+                            'Group'] = group
+
+                    # Add InfectionState identifier
+                    df.loc[region_start_idx + k*len(InfectionStateList):
+                            region_start_idx+(k+1)*len(InfectionStateList)-1,
+                            'InfectionState'] = InfectionStateList
+
+                    # Add counts
+                    df.loc[region_start_idx + k*len(InfectionStateList):
+                            region_start_idx+(k+1)*len(InfectionStateList)-1,
+                            'Count'] = np.array(h5file[regions[i]][group][
+                        date_idx][0][filters['InfectionState']])     
+                    
+                    k += 1               
+            else:
+                print('Error')
+                #raise gd.ValueError("Time point not found for region " + str(regions[i]) + ".")                
+
+        
+        # Aggregated or matrix output
+        if output == 'sum':
+            df.groupby('Region').agg({'Count' : sum}).reset_index()
+        elif output == 'matrix':
             return df
+        else:
+            print('Error')
+            # raise gd.DataError("Chose output form.")
+            
     else:
         print('Error')
-        # raise gd.DataError("Chose output form.")
+        # raise gd.DataError("Data could not be read in.")
 
+    # raise gd.DataError('No data frame can be returned.')
     return pd.DataFrame()
 
 def plot(
     files : list,
-    date : dt.date,
-    column : str = '',
+    region_spec : str,
+    column : str,
+    date,
     filters = None,
+    file_format = 'json',
     relative = True
 ):
 
 
     df = []
     for file in files.items():
-        df.append(get_county_data(file, date=date, column=column, filters=filters))
+        df.append(read_data(file, region_spec=region_spec, column=column, date=date, filters=filters, file_format=file_format))
         df[-1].rename(columns={column : 'Count'}, inplace=True)
 
     # read and filter population data
@@ -249,10 +341,13 @@ def plot(
 
 if __name__ == '__main__':
 
-    files_vacc = {'Reported data' : 'tools/data/raw/all_county_agevacc_vacc_all_dates', 'Sanitized data' : 'tools/data/sanitized/all_county_agevacc_vacc_all_dates'}
+    files_vacc = {'Reported data' : 'tools/raw/test', 'Sanitized data' : 'tools/raw/test_copy'}
     age_groups = ['0-4', '5-14', '15-34', '35-59', '60-79', '80+']
 
     for vacc in ['Vacc_partially']:
         for state in [None]: #[None, 9]:
             # plot(files_vacc, dt.date(2021, 11, 18), column = vacc, filters={'ID_State' : [1], 'Age_RKI' : ['05-11','12-17']})
-            plot(files_vacc, dt.date(2021, 11, 18), column = vacc, filters={'ID_State' : [1], 'Age_RKI' : None})
+            plot(files_vacc, region_spec=None, column=None, date=1, filters={'Group': ['Group1' , 'Group2'], 'InfectionState': [3,4]}, file_format='h5')
+            # plot(files_vacc, region_spec='ID_County', column=vacc, date=dt.date(
+            #     2021, 11, 18), filters={'ID_State': [1], 'Age_RKI': None}, file_format='h5')
+
