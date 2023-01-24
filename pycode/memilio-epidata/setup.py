36e9ea75688136518f32ecc0dac1bfe023905573
patricklnz <patrick.lenz01@icloud.com>
2023-01-24 17:53:55 +0100

    use coverage>=7.0.1

--- pycode/memilio-epidata/setup.py
+++ pycode/memilio-epidata/setup.py
@@ -87,7 +87,8 @@ setup(
         'dev': [
             # smaller pyfakefs versions use deprecated functions for matplotlib versions >=3.4
             'pyfakefs>=4.2.1',
-            'coverage',
+            # coverage 7.0.0 can't find .whl files and breaks CI
+            'coverage>=7.0.1',
             'pylint<=2.11.1',
             'pylint_json2html<=0.3.0',
         ],
