b721a8c3f5fa07ed52981f8f9363b8976fb1c71c
MaxBetzDLR <104758467+MaxBetzDLR@users.noreply.github.com>
2023-02-03 14:24:01 +0100

    Apply suggestions from code review
    Co-authored-by: Martin J. Kühn <62713180+mknaranja@users.noreply.github.com>

--- pycode/memilio-generation/memilio/generation/intermediate_representation.py
+++ pycode/memilio-generation/memilio/generation/intermediate_representation.py
@@ -50,24 +50,24 @@ class IntermediateRepresentation:
         """
         Setter for the attributes of this class.
 
-        @param attribute_name Name of the attribute in Intermediaterepresentation to be set.
-        @param value Value the attribut is set to. Needs to be of right type.
+        @param attribute_name Name of the attribute in IntermediateRepresentation to be set.
+        @param value Value the attribute is set to. Needs to be of right type.
         """
         self.__setattr__(attribute_name, value)
 
     def check_complete_data(self: Self, optional: Dict
                             [str, Union[str, bool]]) -> None:
         """
-        Check for missing data in the IntermediateRepresentation. Called by the Scanne as last step of the data extraction.
+        Check for missing data in the IntermediateRepresentation. Called by the Scanner as last step of the data extraction.
 
         @param optional Dictionary of the optional data from the config.json.
         """
         assert (self.model_class != None), "set a model name"
-        assert (self.namespace != None), "set a model name_space"
+        assert (self.namespace != None), "set a model namespace"
         assert (self.parameterset != None), "set a parameterset"
         if optional.get("parameterset_wrapper"):
             assert (self.parameterset_wrapper !=
-                    None), "No Parameterset_Wrapper found. If None is used in this model set parameterset_wrapper to false in config.json"
+                    None), "No parameterset_wrapper found. If None is used in this model, then set parameterset_wrapper to false in config.json"
         if optional.get("age_group"):
             assert (self.age_group != {
             }), "No AgeGroup found. If None is used in this model set age_group to false in config.json"
