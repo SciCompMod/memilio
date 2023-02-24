0d6364d755ff5214c0067ac6bc8b13d15f5aa436
Abele, Daniel <daniel.abele@dlr.de>
2023-02-24 17:48:41 +0100

    add file license templates

--- cpp/memilio/utils/miompi.h
+++ cpp/memilio/utils/miompi.h
@@ -1,4 +1,22 @@
-
+/* 
+* Copyright (C) 2023 German Aerospace Center (DLR-SC)
+*
+* Authors: Daniel Abele
+*
+* Contact: Martin J. Kuehn <Martin.Kuehn@DLR.de>
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
 #ifndef MIO_UTILS_MPI_H
 #define MIO_UTILS_MPI_H
 
@@ -12,6 +30,10 @@ namespace mio
 {
 namespace mpi
 {
+/**
+* Alias for MPI_Comm, to be used in APIs to avoid
+* having to use #ifdef everywhere.
+*/
 #ifdef MEMILIO_ENABLE_MPI
 using Comm = MPI_Comm;
 #else
