f1bc7039dfba54e5c106edf29667055354616a1f
Ralf Hannemann-Tamas <ralf.ht@gmail.com>
2023-03-10 11:26:28 +0100

    add IWYU pragma: keep

--- cpp/models/ode_seair/model.h
+++ cpp/models/ode_seair/model.h
@@ -22,7 +22,7 @@
 
 #include "memilio/compartments/compartmentalmodel.h"
 #include "memilio/epidemiology/populations.h"
-#include "memilio/epidemiology/contact_matrix.h" //
+#include "memilio/epidemiology/contact_matrix.h" // IWYU pragma: keep
 #include "ode_seair/infection_state.h"
 #include "ode_seair/parameters.h"
 
