0f161edb890ee742571d9fd139402f02d3c7e315
Ralf Hannemann-Tamas <ralf.ht@gmail.com>
2023-03-15 12:54:56 +0100

    finite differences yield same results as forward AD

--- cpp/examples/ad_odeint_example.cpp
+++ cpp/examples/ad_odeint_example.cpp
@@ -12,10 +12,11 @@ GCC_CLANG_DIAGNOSTIC(ignored "-Wdeprecated-declarations")
 GCC_CLANG_DIAGNOSTIC(pop)
 
 
-using ad_type = typename ad::gt1s<double>::type;
+using ad_forward_t = typename ad::gt1s<double>::type;
+using ad_adjoint_t = typename ad::ga1s<double>::type;
 
 /* The type of container used to hold the state vector */
-using value_type = ad_type;
+using value_type = ad_forward_t;
 using time_type = value_type;
 typedef std::vector< value_type > state_type;
 
@@ -40,8 +41,21 @@ int main() {
   state_type x(2);
   x[0] = 1.0; // start at x=1.0, p=0.0
   x[1] = 0.0;
+  ad::derivative(x[0]) = 1.0;
   controlled_stepper_type controlled_stepper;
   boost::numeric::odeint::integrate_adaptive(make_controlled<error_stepper_type>(1e-6,1e-6),
                                              harmonic_oscillator , x , time_type( 0.0) , time_type(10.0) , time_type(0.01) );
+  std::cout << "ad derivative of x is (" << ad::derivative(x[0]) << ", " << ad::derivative(x[1]) << ")\n";
+  const double h = 1e-3;
+  std::vector<double> y = {ad::value(x[0]), ad::value(x[1])};
+  x[0] = 1.0 + h;
+  x[1] = 0.0;
+  boost::numeric::odeint::integrate_adaptive(make_controlled<error_stepper_type>(1e-6,1e-6),
+                                             harmonic_oscillator , x , time_type( 0.0) , time_type(10.0) , time_type(0.01) );
+
+
+  std::cout << "finite differences derivative of x is (" << (ad::value(x[0])-y[0])/h << ", "
+            << (ad::value(x[1])-y[1])/h << ")\n";
+
   return 0;
 }
