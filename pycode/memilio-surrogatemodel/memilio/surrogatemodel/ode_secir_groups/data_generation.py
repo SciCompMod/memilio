a2fb7002826b7b083a0c61eddc7363b3a037ce24
Agatha Schmidt <agatha.schmidt@hotmail.de>
2023-03-20 12:33:22 +0100

    data generation for secir groups

--- pycode/memilio-surrogatemodel/memilio/surrogatemodel/ode_secir_groups/data_generation.py
+++ pycode/memilio-surrogatemodel/memilio/surrogatemodel/ode_secir_groups/data_generation.py
@@ -21,22 +21,21 @@ import copy
 import os
 import pickle
 import random
+import json
 from datetime import date
 
 import numpy as np
-import pandas as pd
 import tensorflow as tf
 from progress.bar import Bar
 from sklearn.preprocessing import FunctionTransformer
 
-from memilio.simulation import (ContactMatrix, Damping, LogLevel,
-                                UncertainContactMatrix, set_log_level)
+from memilio.simulation import (Damping, LogLevel, set_log_level)
 from memilio.simulation.secir import (AgeGroup, Index_InfectionState,
-                                      InfectionState, Model, Simulation,
+                                      InfectionState, Model,
                                       interpolate_simulation_result, simulate)
 
 
-def run_secir_simulation(days):
+def run_secir_simulation(days, damping_day, populations):
     """! Uses an ODE SECIR model allowing for asymptomatic infection with 6 different age groups. The model is not stratified by region. 
     Virus-specific parameters are fixed and initial number of persons in the particular infection states are chosen randomly from defined ranges.
 
@@ -45,61 +44,81 @@ def run_secir_simulation(days):
    """
     set_log_level(LogLevel.Off)
 
-    populations = [50_000]
     start_day = 1
     start_month = 1
     start_year = 2019
     dt = 0.1
-    num_groups = 1
+
+    # Define age Groups
+    groups = ['0-4', '5-14', '15-34', '35-59', '60-79', '80+']
+    num_groups = len(groups)
 
     # Initialize Parameters
-    model = Model(1)
-
-    A0 = AgeGroup(0)
+    model = Model(num_groups)
 
     # Set parameters
-    # Compartment transition duration
-    model.parameters.IncubationTime[A0] = 5.2
-    model.parameters.TimeInfectedSymptoms[A0] = 6.
-    model.parameters.SerialInterval[A0] = 4.2
-    model.parameters.TimeInfectedSevere[A0] = 12.
-    model.parameters.TimeInfectedCritical[A0] = 8.
+    for i in range(num_groups):
+        # Compartment transition duration
+        model.parameters.IncubationTime[AgeGroup(i)] = 5.2
+        model.parameters.TimeInfectedSymptoms[AgeGroup(i)] = 6.
+        model.parameters.SerialInterval[AgeGroup(i)] = 4.2
+        model.parameters.TimeInfectedSevere[AgeGroup(i)] = 12.
+        model.parameters.TimeInfectedCritical[AgeGroup(i)] = 8.
 
-    # Initial number of people in each compartment with random numbers
-    model.populations[A0, Index_InfectionState(
-        InfectionState.Exposed)] = 60 * random.uniform(0.2, 1)
-    model.populations[A0, Index_InfectionState(
-        InfectionState.InfectedNoSymptoms)] = 55 * random.uniform(0.2, 1)
-    model.populations[A0, Index_InfectionState(
-        InfectionState.InfectedSymptoms)] = 50 * random.uniform(0.2, 1)
-    model.populations[A0, Index_InfectionState(
-        InfectionState.InfectedSevere)] = 12 * random.uniform(0.2, 1)
-    model.populations[A0, Index_InfectionState(
-        InfectionState.InfectedCritical)] = 3 * random.uniform(0.2, 1)
-    model.populations[A0, Index_InfectionState(
-        InfectionState.Recovered)] = 50 * random.random()
-    model.populations[A0, Index_InfectionState(InfectionState.Dead)] = 0
-    model.populations.set_difference_from_total(
-        (A0, Index_InfectionState(InfectionState.Susceptible)), populations[0])
+        # Initial number of people in each compartment with random numbers
+        model.populations[AgeGroup(i), Index_InfectionState(
+            InfectionState.Exposed)] = random.uniform(
+            0.00025, 0.0005) * populations[i]
+        model.populations[AgeGroup(i), Index_InfectionState(
+            InfectionState.InfectedNoSymptoms)] = random.uniform(
+            0.0001, 0.00035) * populations[i]
+        model.populations[AgeGroup(i), Index_InfectionState(
+            InfectionState.InfectedSymptoms)] = random.uniform(
+            0.00007, 0.0001) * populations[i]
+        model.populations[AgeGroup(i), Index_InfectionState(
+            InfectionState.InfectedSevere)] = random.uniform(
+            0.00003, 0.00006) * populations[i]
+        model.populations[AgeGroup(i), Index_InfectionState(
+            InfectionState.InfectedCritical)] = random.uniform(
+            0.00001, 0.00002) * populations[i]
+        model.populations[AgeGroup(i), Index_InfectionState(
+            InfectionState.Recovered)] = random.uniform(
+            0.002, 0.008) * populations[i]
+        model.populations[AgeGroup(i),
+                          Index_InfectionState(InfectionState.Dead)] = 0
+        model.populations.set_difference_from_group_total_AgeGroup(
+            (AgeGroup(i), Index_InfectionState(InfectionState.Susceptible)), populations[i])
 
-    # Compartment transition propabilities
-    model.parameters.RelativeTransmissionNoSymptoms[A0] = 0.5
-    model.parameters.TransmissionProbabilityOnContact[A0] = 0.1
-    model.parameters.RecoveredPerInfectedNoSymptoms[A0] = 0.09
-    model.parameters.RiskOfInfectionFromSymptomatic[A0] = 0.25
-    model.parameters.SeverePerInfectedSymptoms[A0] = 0.2
-    model.parameters.CriticalPerSevere[A0] = 0.25
-    model.parameters.DeathsPerCritical[A0] = 0.3
-    # twice the value of RiskOfInfectionFromSymptomatic
-    model.parameters.MaxRiskOfInfectionFromSymptomatic[A0] = 0.5
+        # Compartment transition propabilities
+        model.parameters.RelativeTransmissionNoSymptoms[AgeGroup(i)] = 0.5
+        model.parameters.TransmissionProbabilityOnContact[AgeGroup(i)] = 0.1
+        model.parameters.RecoveredPerInfectedNoSymptoms[AgeGroup(i)] = 0.09
+        model.parameters.RiskOfInfectionFromSymptomatic[AgeGroup(i)] = 0.25
+        model.parameters.SeverePerInfectedSymptoms[AgeGroup(i)] = 0.2
+        model.parameters.CriticalPerSevere[AgeGroup(i)] = 0.25
+        model.parameters.DeathsPerCritical[AgeGroup(i)] = 0.3
+        # twice the value of RiskOfInfectionFromSymptomatic
+        model.parameters.MaxRiskOfInfectionFromSymptomatic[AgeGroup(i)] = 0.5
 
     model.parameters.StartDay = (
         date(start_year, start_month, start_day) - date(start_year, 1, 1)).days
 
-    model.parameters.ContactPatterns.cont_freq_mat[0].baseline = np.ones(
-        (num_groups, num_groups)) * 10
-    model.parameters.ContactPatterns.cont_freq_mat[0].minimum = np.ones(
-        (num_groups, num_groups)) * 0
+    # Load baseline and minimum contact matrix and assign them to the model
+    baseline = getBaselineMatrix()
+    minimum = getMinimumMatrix()
+
+    model.parameters.ContactPatterns.cont_freq_mat[0].baseline = baseline
+    model.parameters.ContactPatterns.cont_freq_mat[0].minimum = minimum
+
+    # Generate a damping matrix and assign it to the model
+    damping = np.ones((num_groups, num_groups)
+                      ) * np.float16(random.uniform(0, 0.5))
+
+    model.parameters.ContactPatterns.cont_freq_mat.add_damping(Damping(
+        coeffs=(damping), t=damping_day, level=0, type=0))
+
+    damped_contact_matrix = model.parameters.ContactPatterns.cont_freq_mat.get_matrix_at(
+        damping_day+1)
 
     # Apply mathematical constraints to parameters
     model.apply_constraints()
@@ -111,11 +130,11 @@ def run_secir_simulation(days):
 
     # Using an array instead of a list to avoid problems with references
     result_array = result.as_ndarray()
-    dataset = []
+
     # Omit first column, as the time points are not of interest here.
     dataset_entries = copy.deepcopy(result_array[1:, :].transpose())
 
-    return dataset_entries.tolist()
+    return dataset_entries.tolist(), damped_contact_matrix
 
 
 def generate_data(
@@ -124,10 +143,10 @@ def generate_data(
     """! Generate data sets of num_runs many equation-based model simulations and transforms the computed results by a log(1+x) transformation.
     Divides the results in input and label data sets and returns them as a dictionary of two TensorFlow Stacks.
 
-    In general, we have 8 different compartments. If we choose, 
+    In general, we have 8 different compartments and 6 age groups.  If we choose, 
     input_width = 5 and label_width = 20, the dataset has 
-    - input with dimension 5 x 8
-    - labels with dimension 20 x 8
+    - input with dimension 5 x 8 x 6
+    - labels with dimension 20 x 8 x 6
 
    @param num_runs Number of times, the function run_secir_simulation is called.
    @param path Path, where the dataset is saved to.
@@ -139,20 +158,34 @@ def generate_data(
    """
     data = {
         "inputs": [],
-        "labels": []
+        "labels": [],
+        "contact_matrix": [],
+        "damping_day": []
     }
 
     # The number of days is the same as the sum of input and label width.
     # Since the first day of the input is day 0, we still need to subtract 1.
     days = input_width + label_width - 1
 
+    # Load population data
+    population = get_population()
+
     # show progess in terminal for longer runs
     # Due to the random structure, theres currently no need to shuffle the data
     bar = Bar('Number of Runs done', max=num_runs)
     for _ in range(0, num_runs):
-        data_run = run_secir_simulation(days)
+
+        # Generate a random damping day
+        # The damping has to take place at least 5 days before the end of the simulation and should not be part of the input
+        damping_day = random.randrange(
+            input_width, (input_width+label_width)-5)
+
+        data_run, damped_contact_matrix = run_secir_simulation(
+            days, damping_day, population[random.randint(0, len(population) - 1)])
         data['inputs'].append(data_run[:input_width])
         data['labels'].append(data_run[input_width:])
+        data['contact_matrix'].append(np.array(damped_contact_matrix))
+        data['damping_day'].append(damping_day)
         bar.next()
     bar.finish()
 
@@ -161,12 +194,12 @@ def generate_data(
         transformer = FunctionTransformer(np.log1p, validate=True)
         inputs = np.asarray(data['inputs']).transpose(2, 0, 1).reshape(8, -1)
         scaled_inputs = transformer.transform(inputs)
-        scaled_inputs = scaled_inputs.transpose().reshape(num_runs, input_width, 8)
+        scaled_inputs = scaled_inputs.transpose().reshape(num_runs, input_width, 48)
         scaled_inputs_list = scaled_inputs.tolist()
 
         labels = np.asarray(data['labels']).transpose(2, 0, 1).reshape(8, -1)
         scaled_labels = transformer.transform(labels)
-        scaled_labels = scaled_labels.transpose().reshape(num_runs, label_width, 8)
+        scaled_labels = scaled_labels.transpose().reshape(num_runs, label_width, 48)
         scaled_labels_list = scaled_labels.tolist()
 
         # cast dfs to tensors
@@ -184,6 +217,74 @@ def generate_data(
     return data
 
 
+def getBaselineMatrix():
+    """! loads the baselinematrix
+    """
+
+    baseline_contact_matrix0 = os.path.join(
+        "./data/contacts/baseline_home.txt")
+    baseline_contact_matrix1 = os.path.join(
+        "./data/contacts/baseline_school_pf_eig.txt")
+    baseline_contact_matrix2 = os.path.join(
+        "./data/contacts/baseline_work.txt")
+    baseline_contact_matrix3 = os.path.join(
+        "./data/contacts/baseline_other.txt")
+
+    baseline = np.loadtxt(baseline_contact_matrix0) \
+        + np.loadtxt(baseline_contact_matrix1) + \
+        np.loadtxt(baseline_contact_matrix2) + \
+        np.loadtxt(baseline_contact_matrix3)
+
+    return baseline
+
+
+def getMinimumMatrix():
+    """! loads the minimum matrix
+    """
+
+    minimum_contact_matrix0 = os.path.join(
+        "./data/contacts/minimum_home.txt")
+    minimum_contact_matrix1 = os.path.join(
+        "./data/contacts/minimum_school_pf_eig.txt")
+    minimum_contact_matrix2 = os.path.join(
+        "./data/contacts/minimum_work.txt")
+    minimum_contact_matrix3 = os.path.join(
+        "./data/contacts/minimum_other.txt")
+
+    minimum = np.loadtxt(minimum_contact_matrix0) \
+        + np.loadtxt(minimum_contact_matrix1) + \
+        np.loadtxt(minimum_contact_matrix2) + \
+        np.loadtxt(minimum_contact_matrix3)
+
+    return minimum
+
+
+def get_population(path="/home/schm_a45/Documents/Code/memilio/memilio/data/pydata/Germany/county_population_dim401.json"):
+
+    with open(path) as f:
+        data = json.load(f)
+    population = []
+    for data_entry in data:
+        population_county = []
+        population_county.append(
+            data_entry['<3 years'] + data_entry['3-5 years'] / 2)
+        population_county.append(data_entry['6-14 years'])
+        population_county.append(
+            data_entry['15-17 years'] + data_entry['18-24 years'] +
+            data_entry['25-29 years'] + data_entry['30-39 years'] / 2)
+        population_county.append(
+            data_entry['30-39 years'] / 2 + data_entry['40-49 years'] +
+            data_entry['50-64 years'] * 2 / 3)
+        population_county.append(
+            data_entry['65-74 years'] + data_entry['>74 years'] * 0.2 +
+            data_entry['50-64 years'] * 1 / 3)
+        population_county.append(
+            data_entry['>74 years'] * 0.8)
+
+        population.append(population_county)
+    return population
+
+
 if __name__ == "__main__":
     # Store data relative to current file two levels higher.
     path = os.path.dirname(os.path.realpath(__file__))
@@ -192,6 +293,6 @@ if __name__ == "__main__":
 
     input_width = 5
     label_width = 30
-    num_runs = 1000
+    num_runs = 10000
     data = generate_data(num_runs, path_data, input_width,
                          label_width)
