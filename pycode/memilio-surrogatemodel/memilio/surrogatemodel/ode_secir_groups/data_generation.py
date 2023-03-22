fixed mistake in data normalization (reshape)

pycode/memilio-surrogatemodel/memilio/surrogatemodel/ode_secir_groups/data_generation.py
@@ -38,7 +38,6 @@ from memilio.simulation.secir import (AgeGroup, Index_InfectionState,
 def run_secir_simulation(days, damping_day, populations):
     """! Uses an ODE SECIR model allowing for asymptomatic infection with 6 different age groups. The model is not stratified by region. 
     Virus-specific parameters are fixed and initial number of persons in the particular infection states are chosen randomly from defined ranges.
-
     @param Days Describes how many days we simulate within a single run.
     @return List containing the populations in each compartment for each day of the simulation.
    """
@@ -142,12 +141,10 @@ def generate_data(
         save_data=True):
     """! Generate data sets of num_runs many equation-based model simulations and transforms the computed results by a log(1+x) transformation.
     Divides the results in input and label data sets and returns them as a dictionary of two TensorFlow Stacks.
-
     In general, we have 8 different compartments and 6 age groups.  If we choose, 
     input_width = 5 and label_width = 20, the dataset has 
     - input with dimension 5 x 8 x 6
     - labels with dimension 20 x 8 x 6
-
    @param num_runs Number of times, the function run_secir_simulation is called.
    @param path Path, where the dataset is saved to.
    @param input_width Int value that defines the number of time series used for the input.
@@ -192,12 +189,12 @@ def generate_data(
     if normalize:
         # logarithmic normalization
         transformer = FunctionTransformer(np.log1p, validate=True)
-        inputs = np.asarray(data['inputs']).transpose(2, 0, 1).reshape(8, -1)
+        inputs = np.asarray(data['inputs']).transpose(2, 0, 1).reshape(48, -1)
         scaled_inputs = transformer.transform(inputs)
         scaled_inputs = scaled_inputs.transpose().reshape(num_runs, input_width, 48)
         scaled_inputs_list = scaled_inputs.tolist()
 
-        labels = np.asarray(data['labels']).transpose(2, 0, 1).reshape(8, -1)
+        labels = np.asarray(data['labels']).transpose(2, 0, 1).reshape(48, -1)
         scaled_labels = transformer.transform(labels)
         scaled_labels = scaled_labels.transpose().reshape(num_runs, label_width, 48)
         scaled_labels_list = scaled_labels.tolist()