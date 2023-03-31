62a968cafefa04214719e7f2a3607e5138e7f8f0
MaxBetzDLR <Maximilian.betz@dlr.de>
2023-03-31 15:59:02 +0200

    Add comment for integrator example

--- cpp/examples/ode_secir.cpp
+++ cpp/examples/ode_secir.cpp
@@ -74,8 +74,19 @@ int main()
 
     model.apply_constraints();
 
-
+    // Using default Integrator
     mio::TimeSeries<double> secir = simulate(t0, tmax, dt, model);
+
+    /*
+    Example of using a different integrator
+    
+    auto integrator = std::make_shared<mio::RKIntegratorCore>();
+    integrator->set_dt_min(0.3);
+    integrator->set_dt_max(1.0);
+    integrator->set_rel_tolerance(1e-4);
+    integrator->set_abs_tolerance(1e-1);
+    mio::TimeSeries<double> secir = simulate(t0, tmax, dt, model, integrator);
+    */
 
     bool print_to_terminal = true;
 
