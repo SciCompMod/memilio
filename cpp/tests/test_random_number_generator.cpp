1109805fb095dbc9b3c4eb1a669566a1472aa21a
Abele, Daniel <daniel.abele@dlr.de>
2023-04-01 17:27:01 +0200

    fix death tests

--- cpp/tests/test_random_number_generator.cpp
+++ cpp/tests/test_random_number_generator.cpp
@@ -24,11 +24,15 @@ TEST(RandomNumberGenerator, blocks)
     ASSERT_THAT(samples2, testing::ElementsAreArray(samples1.begin() + 95, samples1.end()));
 }
 
+#ifndef NDEBUG
 TEST(RandomNumberGenerator, block_error)
 {
     mio::RandomNumberGenerator rng;
     rng.set_block_size(5);
     std::vector<mio::RandomNumberGenerator::result_type> dump;
-    std::generate_n(std::back_inserter(dump), 123, [&rng] { return rng(); });
-    ASSERT_DEBUG_DEATH(rng.forward_to_block(19), ".*");
+    std::generate_n(std::back_inserter(dump), 123, [&rng] {
+        return rng();
+    });
+    ASSERT_DEATH(rng.forward_to_block(19), ".*");
 }
+#endif
