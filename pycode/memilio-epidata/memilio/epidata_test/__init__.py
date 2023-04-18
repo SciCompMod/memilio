f806b5f4460100f89c3e1252f60eccf6fdf5f0df
patricklnz <patrick.lenz01@icloud.com>
2023-04-18 12:55:53 +0200

    disable indicator in unittests

--- pycode/memilio-epidata/memilio/epidata_test/__init__.py
+++ pycode/memilio-epidata/memilio/epidata_test/__init__.py
@@ -1,3 +1,8 @@
 """
 Module epidata_test provides tests for the epidata module.
 """
+
+from memilio import progress_indicator
+
+# Disable Progress Indicators for unittests
+progress_indicator.disable_indicators(True)
