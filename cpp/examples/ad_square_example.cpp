3ed699298fa82d9abee1cc726654d054bf3cef99
Ralf Hannemann-Tamas <r.hannemann-tamas@fz-juelich.de>
2023-05-11 15:29:36 +0200

    add adjoint ad

--- cpp/examples/ad_square_example.cpp
+++ cpp/examples/ad_square_example.cpp
@@ -6,11 +6,30 @@ template<typename FP> FP square(FP x){
 }
 
 int main() {
-  ad::gt1s<double>::type x;
-  ad::value(x) = 3.0;
-  ad::derivative(x) = 1.0;
-  ad::gt1s<double>::type y = square(x);
-  std::cout << "value of square(" << ad::value(x) << ") is " << ad::value(y) << std::endl;
-  std::cout << "derivative of square(" << ad::value(x) << ") is " << ad::derivative(y) << std::endl;
+  std::cout << "Forward AD" << std::endl;
+  ad::gt1s<double>::type t1_x;
+  ad::value(t1_x) = 3.0;
+  ad::derivative(t1_x) = 1.0;
+  ad::gt1s<double>::type t1_y = square(t1_x);
+  std::cout << "value of square(" << ad::value(t1_x) << ") is " << ad::value(t1_y) << std::endl;
+  std::cout << "forward derivative of square(" << ad::value(t1_x) << ") is " << ad::derivative(t1_y) << std::endl;
+
+  std::cout << "Reverse AD" << std::endl;
+  // create tape
+  if(!ad::ga1s<double>::global_tape) ad::ga1s<double>::global_tape = ad::ga1s<double>::tape_t::create();
+  ad::ga1s<double>::global_tape->reset();
+  ad::ga1s<double>::type a1_x;
+  ad::value(a1_x) = 3.0;
+  ad::derivative(a1_x) = 0.0;
+  ad::ga1s<double>::global_tape->register_variable(a1_x);
+  ad::ga1s<double>::type a1_y = square(a1_x);
+  ad::ga1s<double>::global_tape->register_output_variable(a1_y);
+  ad::derivative(a1_y) = 1.0;
+  ad::ga1s<double>::global_tape->interpret_adjoint();
+  std::cout << "value of square(" << ad::value(a1_x) << ") is " << ad::value(t1_y) << std::endl;
+  std::cout << "adjoint derivative is " << ad::derivative(a1_x) << std::endl;
+  ad::ga1s<double>::tape_t::remove(ad::ga1s<double>::global_tape);
+
+
   return 0;
 }
