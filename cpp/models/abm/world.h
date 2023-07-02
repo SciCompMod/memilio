79a7ce314352724cad5ef00491a4e1a7febf0f7d
Ralf Hannemann-Tamas <r.hannemann-tamas@fz-juelich.de>
2023-07-02 16:06:43 +0200

    finish abm world

--- cpp/models/abm/world.h
+++ cpp/models/abm/world.h
@@ -26,12 +26,12 @@
 #include "abm/person.h"
 #include "abm/lockdown_rules.h" // IWYU pragma: keep
 #include "abm/trip_list.h"
-#include "abm/mask_type.h"
+#include "abm/mask_type.h" // IWYU pragma: keep
 #include "abm/migration_rules.h"
 #include "memilio/utils/random_number_generator.h"
 #include "memilio/utils/stl_util.h"
-#include "abm/infection.h"
-#include "abm/vaccine.h"
+#include "abm/infection.h" // IWYU pragma: keep
+#include "abm/vaccine.h" // IWYU pragma: keep
 
 #include "abm/testing_strategy.h"
 #include "memilio/utils/pointer_dereferencing_iterator.h"
@@ -39,6 +39,7 @@
 
 #include <vector>
 #include <memory>
+
 
 namespace mio
 {
@@ -182,38 +183,72 @@ public:
      * @param[in] person The Person.
      * @return Reference to the assigned Location.
      */
-    Location<FP>& find_location(LocationType type, const Person<FP>& person);
+    Location<FP>& find_location(LocationType type, const Person<FP>& person)
+    {
+        auto index = person.get_assigned_location_index(type);
+        assert(index != INVALID_LOCATION_INDEX && "unexpected error.");
+        return get_individualized_location({index, type});
+    }
 
     /** 
      * @brief Get the number of Persons in one #InfectionState at all Location%s of a type.
      * @param[in] s Specified #InfectionState.
      * @param[in] type Specified #LocationType.
      */
-    size_t get_subpopulation_combined(TimePoint t, InfectionState s, LocationType type) const;
+    size_t get_subpopulation_combined(TimePoint t, InfectionState s, LocationType type) const
+    {
+        return std::accumulate(m_locations.begin(), m_locations.end(), (size_t)0,
+                               [t, s, type](size_t running_sum, const std::unique_ptr<Location<FP>>& loc) {
+                                   return loc->get_type() == type ? running_sum + loc->get_subpopulation(t, s)
+                                                                  : running_sum;
+                               });
+    }
 
     /** 
      * @brief Get the MigrationParameters.
      * @return Reference to the MigrationParameters.
      */
-    MigrationParameters<FP>& get_migration_parameters();
+    MigrationParameters<FP>& get_migration_parameters()
+    {
+        return m_migration_parameters;
+    }
 
-    const MigrationParameters<FP>& get_migration_parameters() const;
+
+    const MigrationParameters<FP>& get_migration_parameters() const
+    {
+        return m_migration_parameters;
+    }
 
     /** 
      * @brief Get the GlobalInfectionParameters.
      * @return Reference to the GlobalInfectionParameters.
      */
-    GlobalInfectionParameters<FP>& get_global_infection_parameters();
+    GlobalInfectionParameters<FP>& get_global_infection_parameters()
+    {
+        return m_infection_parameters;
+    }
 
-    const GlobalInfectionParameters<FP>& get_global_infection_parameters() const;
+
+    const GlobalInfectionParameters<FP>& get_global_infection_parameters() const
+    {
+        return m_infection_parameters;
+    }
 
     /**
      * @brief Get the migration data.
      * @return Reference to the list of Trip%s that the Person%s make.
      */
-    TripList& get_trip_list();
+    TripList& get_trip_list()
+    {
+        return m_trip_list;
+    }
 
-    const TripList& get_trip_list() const;
+
+    const TripList& get_trip_list() const
+    {
+        return m_trip_list;
+    }
+
 
     /** 
      * @brief Decide if migration rules (like go to school/work) are used or not;
@@ -221,16 +256,56 @@ public:
      * @param[in] param If true uses the migration rules for migration to school/work etc., else only the rules 
      * regarding hospitalization/ICU/quarantine.
      */
-    void use_migration_rules(bool param);
-    bool use_migration_rules() const;
+    void use_migration_rules(bool param)
+    {
+        m_use_migration_rules = param;
+        // Set up global migration rules for all agents
+        // check if a person has to go to the hospital, ICU or home due to quarantine/recovery
+        if (m_use_migration_rules) {
+            m_migration_rules = {
+                                 std::make_pair(&return_home_when_recovered<FP>,
+                                                std::vector<LocationType>{
+                                                                          LocationType::Home,
+                                                                          LocationType::Hospital}), //assumption: if there is an ICU, there is also an hospital
+                                 std::make_pair(&go_to_hospital<FP>, std::vector<LocationType>{LocationType::Home, LocationType::Hospital}),
+                                 std::make_pair(&go_to_icu<FP>, std::vector<LocationType>{LocationType::Hospital, LocationType::ICU}),
+                                 std::make_pair(&go_to_school<FP>, std::vector<LocationType>{LocationType::School, LocationType::Home}),
+                                 std::make_pair(&go_to_work<FP>, std::vector<LocationType>{LocationType::Home, LocationType::Work}),
+                                 std::make_pair(&go_to_shop<FP>, std::vector<LocationType>{LocationType::Home, LocationType::BasicsShop}),
+                                 std::make_pair(&go_to_event<FP>, std::vector<LocationType>{LocationType::Home, LocationType::SocialEvent}),
+                                 std::make_pair(&go_to_quarantine<FP>, std::vector<LocationType>{LocationType::Home})};
+        }
+        else {
+            m_migration_rules = {
+                                 std::make_pair(&return_home_when_recovered<FP>,
+                                                std::vector<LocationType>{
+                                                                          LocationType::Home,
+                                                                          LocationType::Hospital}), //assumption: if there is an ICU, there is also an hospital
+                                 std::make_pair(&go_to_hospital<FP>, std::vector<LocationType>{LocationType::Home, LocationType::Hospital}),
+                                 std::make_pair(&go_to_icu<FP>, std::vector<LocationType>{LocationType::Hospital, LocationType::ICU}),
+                                 std::make_pair(&go_to_quarantine<FP>, std::vector<LocationType>{LocationType::Home})};
+        }
+    }
+
+    bool use_migration_rules() const
+    {
+        return m_use_migration_rules;
+    }
+
 
     /** 
      * @brief Get the TestingStrategy.
      * @return Reference to the list of TestingScheme%s that are checked for testing.
      */
-    TestingStrategy<FP>& get_testing_strategy();
+    TestingStrategy<FP>& get_testing_strategy()
+    {
+        return m_testing_strategy;
+    }
 
-    const TestingStrategy<FP>& get_testing_strategy() const;
+    const TestingStrategy<FP>& get_testing_strategy() const
+    {
+        return m_testing_strategy;
+    }
 
 private:
     /**
