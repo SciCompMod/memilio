23b7eb98b943dbf56ec7ee3c94d4458149581d13
Rene Schmieding <rene.schmieding@outlook.com>
2023-07-13 13:27:44 +0200

    Review suggestions and documentation

--- cpp/memilio/compartments/compartmentalmodel.h
+++ cpp/memilio/compartments/compartmentalmodel.h
@@ -17,8 +17,8 @@
 * See the License for the specific language governing permissions and
 * limitations under the License.
 */
-#ifndef COMPARTMENTALMODEL_H
-#define COMPARTMENTALMODEL_H
+#ifndef MIO_COMPARTMENTALMODEL_H
+#define MIO_COMPARTMENTALMODEL_H
 
 #include "memilio/config.h"
 #include "memilio/math/eigen.h"
@@ -44,33 +44,37 @@ using check_constraints_expr_t = decltype(std::declval<T>().check_constraints())
 template <class T>
 using apply_constraints_expr_t = decltype(std::declval<T>().apply_constraints());
 
-// empty struct used to pass parameter packs via function arguments
+// Empty struct used to pass parameter packs via function arguments.
 template <class... Ts>
 struct typelist {
 };
 
-// function declaration used to remove Omittand from the type list of a tuple
-template <class Omittand, class... Tags>
-decltype(std::tuple_cat(std::declval<typename std::conditional<std::is_same<Omittand, Tags>::value, std::tuple<>,
+// The following functions are not defined anywhere. Their use is to provide type conversions via their return type.
+
+// Function declaration used to remove OmittedTag from the type list of a tuple.
+// First a list of tuples is generated for each Tag in Tags, where the tuple is either of type tuple<Tag>, or if
+// Tag == OmittedTag, of type tuple<>. This list is then concatonated, effectively removing OmittedTag.
+template <class OmittedTag, class... Tags>
+decltype(std::tuple_cat(std::declval<typename std::conditional<std::is_same<OmittedTag, Tags>::value, std::tuple<>,
                                                                std::tuple<Tags>>::type>()...))
     filter_tuple(std::tuple<Tags...>);
 
-// function declaration used to replace type T by std::tuple
+// Function declaration used to replace type T by std::tuple.
 template <template <class...> class T, class... Args>
 std::tuple<Args...> as_tuple(T<Args...>);
 
-// function declaration used to replace std::tuple by type T
+// Function declaration used to replace std::tuple by type T.
 template <template <class...> class T, class... Args>
 T<Args...> as_index(std::tuple<Args...>);
 
-// remove all occurrences of Omittand from the types in a std::tuple<types...>
-template <class Omittand, class Tuple>
-using filtered_tuple_t = decltype(filter_tuple<Omittand>(std::declval<Tuple>()));
+// Remove all occurrences of OmittedTag from the types in a std::tuple<types...>.
+template <class OmittedTag, class Tuple>
+using filtered_tuple_t = decltype(filter_tuple<OmittedTag>(std::declval<Tuple>()));
 
-// remove all occurrences of Omittand from the types in an Index = IndexTemplate<types...>
-template <class Omittand, template <class...> class IndexTemplate, class Index>
-using filtered_index_t = decltype(
-    as_index<IndexTemplate>(std::declval<filtered_tuple_t<Omittand, decltype(as_tuple(std::declval<Index>()))>>()));
+// Remove all occurrences of OmittedTag from the types in an Index = IndexTemplate<types...>.
+template <class OmittedTag, template <class...> class IndexTemplate, class Index>
+using filtered_index_t = decltype(as_index<IndexTemplate>(
+    std::declval<filtered_tuple_t<OmittedTag, decltype(as_tuple(std::declval<Index>()))>>()));
 } //namespace details
 
 /**
@@ -117,11 +121,11 @@ public:
     {
     }
 
-    CompartmentalModel(const CompartmentalModel&) = default;
-    CompartmentalModel(CompartmentalModel&&)      = default;
+    CompartmentalModel(const CompartmentalModel&)            = default;
+    CompartmentalModel(CompartmentalModel&&)                 = default;
     CompartmentalModel& operator=(const CompartmentalModel&) = default;
-    CompartmentalModel& operator=(CompartmentalModel&&) = default;
-    virtual ~CompartmentalModel()                       = default;
+    CompartmentalModel& operator=(CompartmentalModel&&)      = default;
+    virtual ~CompartmentalModel()                            = default;
 
     //REMARK: Not pure virtual for easier java/python bindings
     virtual void get_derivatives(Eigen::Ref<const Eigen::VectorXd>, Eigen::Ref<const Eigen::VectorXd> /*y*/,
@@ -206,17 +210,20 @@ public:
 template <class Comp, class Pop, class Params, class... Flows>
 struct CompartmentalModel<Comp, Pop, Params, TypeChart<Flows...>> : public CompartmentalModel<Comp, Pop, Params> {
     using PopIndex = typename Pop::Index;
-    // PopIndex without Comp, used as argument type for get_flow_index
+    // FlowIndex is the same as PopIndex without the category Comp. It is used as argument type for get_flow_index,
+    // since a flow is used to determine only the compartment (i.e. Index<Comp>) for the population of the model.
+    // The remaining indices (those contained in FlowIndex) must still be provided to get an entry of population.
+    // This approach only works with exactly one category of type Comp in PopIndex (hence the assertion below).
     using FlowIndex = details::filtered_index_t<Comp, Index, PopIndex>;
-    // Enforce that Comp is a unique Category of PopIndex, since we use source/target of Flows to provide Comp indices
-    static_assert(FlowIndex::size == Pop::Index::size - 1,
-                  "Compartments must be used exactly once as population index.");
+    // Enforce that Comp is a unique Category of PopIndex, since we use Flows (via their source/target) to provide
+    // Comp indices for the population.
+    static_assert(FlowIndex::size == PopIndex::size - 1, "Compartments must be used exactly once as population index.");
 
 public:
     using Base      = CompartmentalModel<Comp, Pop, Params>;
     using FlowChart = TypeChart<Flows...>;
     /**
-     * @brief default constructor, forwarding args to Base constructor
+     * @brief Default constructor, forwarding args to Base constructor.
      */
     template <class... Args>
     CompartmentalModel<Comp, Pop, Params, FlowChart>(Args... args)
@@ -233,20 +240,20 @@ public:
                            Eigen::Ref<Eigen::VectorXd> /*flows*/) const {};
 
     /**
-     * @brief compute the right-hand-side of the ODE dydt = f(y, t) from flow values
+     * @brief Compute the right-hand-side of the ODE dydt = f(y, t) from flow values.
      *
-     * This function is generated at compile time depending on the TypeChart and Pop tparams of the model.
+     * This function is generated at compile time depending on the template parameters Flows and Pop of the model.
      *
-     * @param flows the current flow values (as calculated by get_flows) as a flat array
-     * @param dydt a reference to the calculated output
+     * @param[in] flows The current flow values (as calculated by get_flows) as a flat array.
+     * @param[out] dydt A reference to the calculated output.
      */
     void get_derivatives(Eigen::Ref<const Eigen::VectorXd> flows, Eigen::Ref<Eigen::VectorXd> dydt) const
     {
         dydt.setZero();
         for (size_t i = 0; i < this->populations.numel() / static_cast<size_t>(Comp::Count); i++) {
             // see get_flow_index for more info on these index calculations.
-            // the Comp-index is added in get_rhs_impl; the flat Pop::Index is
-            //   flat_pop_index = i % m_comp_factor + (i / m_comp_factor) * m_comp_factor * Comp::Count;
+            // the Comp-index is added in get_rhs_impl; the flat PopIndex is
+            //   flat_pop_index = (i % m_comp_factor) + (i / m_comp_factor) * m_comp_factor * Comp::Count;
             // mind the integer division.
             get_rhs_impl<0>(flows, dydt,
                             i + (i / m_comp_factor) * m_comp_factor * (static_cast<size_t>(Comp::Count) - 1),
@@ -258,12 +265,12 @@ public:
      * @brief Compute the right-hand-side f(y, t) of the ODE and store it in dydt.
      *
      * This function uses get_flow(..., flows) and get_derivatives(flows, dydt) to provide the
-     * same interface as a CompartmentalModel without flows
+     * same interface as a CompartmentalModel without flows.
      *
-     * @param pop the current population of the model as a flat array
-     * @param y the current state of the model as a flat array
-     * @param t the current time
-     * @param dydt a reference to the calculated output
+     * @param[in] pop The current population of the model as a flat array.
+     * @param[in] y The current state of the model as a flat array.
+     * @param[in] t The current time.
+     * @param[out] dydt A reference to the calculated output.
      */
     void get_derivatives(Eigen::Ref<const Eigen::VectorXd> pop, Eigen::Ref<const Eigen::VectorXd> y, double t,
                          Eigen::Ref<Eigen::VectorXd> dydt) const override final
@@ -274,9 +281,9 @@ public:
     }
 
     /**
-     * @brief initial values for flows.
-     * This can be used as initial conditions in an ODE solver. By default, this is a zero vector
-     * @return the initial flows
+     * @brief Initial values for flows.
+     * This can be used as initial conditions in an ODE solver. By default, this is a zero vector.
+     * @return The initial flows.
      */
     Eigen::VectorXd get_initial_flows() const
     {
@@ -285,7 +292,7 @@ public:
     }
 
     /**
-     * @brief a flat index into an array of flows (as computed by get_flows), given the indices of each category
+     * @brief S flat index into an array of flows (as computed by get_flows), given the indices of each category.
      *
      * Calculation of flat indices (as in this->populations.get_flat_index) use the formula
      * flat_index = ((((I_{1}) * D_{2} + I_{2}) * D_{3} + I_{3}) ... ) * D_{n} + I_{n}
@@ -304,10 +311,10 @@ public:
      * Then, we add the flow position I_{flow} via flow_index = flat_index * D_{flow} + I_{flow}, where
      * D_{flow} is the number of flows.
      *
-     * @param indices the custom indices for each category
-     * @tparam Source the source of a flow
-     * @tparam Target the target of a flow
-     * @return a flat index into a data structure storing flow values
+     * @param[in] indices The custom indices for each category.
+     * @tparam Source the source of a flow.
+     * @tparam Target the target of a flow.
+     * @return A flat index into a data structure storing flow values.
      */
     template <Comp Source, Comp Target>
     size_t get_flow_index(const FlowIndex& indices) const
@@ -317,10 +324,10 @@ public:
     }
 
     /**
-     * @brief a flat index into an array of flows (as computed by get_flows), if the only used category in Pop is Comp
-     * @tparam Source the source of a flow
-     * @tparam Target the target of a flow
-     * @return a flat index into a data structure storing flow values
+     * @brief A flat index into an array of flows (as computed by get_flows), if the only used category in Pop is Comp.
+     * @tparam Source the source of a flow.
+     * @tparam Target the target of a flow.
+     * @return A flat index into a data structure storing flow values.
      */
     template <Comp Source, Comp Target>
     constexpr size_t get_flow_index() const
@@ -330,26 +337,36 @@ public:
     }
 
 private:
-    size_t m_comp_factor;
-    FlowIndex m_flow_index_dimensions;
-    mutable Eigen::VectorXd m_flow_values; // avoid allocation in get_derivatives (using get_flows)
+    size_t m_comp_factor; ///< The factor used for the category Comp in this->population.get_flat_index.
+    FlowIndex m_flow_index_dimensions; ///< The dimensions of a FlowIndex.
+    mutable Eigen::VectorXd m_flow_values; ///< Cache to avoid allocation in get_derivatives (using get_flows).
 
-    // turn a PopIndex into a FlowIndex
+    /**
+     * @brief Reduce a PopIndex to a FlowIndex.
+     * This function should only be called by `FlowIndex reduce_index(const PopIndex&)`.
+     */
     template <class... Categories>
-    FlowIndex reduce_index(const PopIndex& i, details::typelist<Categories...>) const
+    FlowIndex reduce_index(const PopIndex& i, mio::Tag<mio::Index<Categories...>>) const
     {
         return Index<Categories...>{get<Categories>(i)...};
     }
 
-    // turn a PopIndex into a FlowIndex
+    /**
+     * @brief Reduce a PopIndex to a FlowIndex.
+     * @param[in] i Any PopIndex.
+     * @return The FlowIndex gained from i by omitting the compartment.
+     */
     FlowIndex reduce_index(const PopIndex& i) const
     {
-        // since FlowIndex may not be trivially constructible, we use the typelist T to determine its Categories
-        using T = decltype(details::as_index<details::typelist>(details::as_tuple(std::declval<FlowIndex>())));
-        return reduce_index(i, T());
+        // since FlowIndex may not be trivially constructible, we use mio::Tag<FlowIndex> to determine its Categories
+        return reduce_index(i, mio::Tag<FlowIndex>());
     }
 
-    // compute only the factor used with "Comp" in this->population.get_flat_index
+    /**
+     * @brief Recursively compute the factor used with the Category "Comp" in this->population.get_flat_index.
+     * @tparam I Recursion index, do not change default value.
+     * @return Factor used to add a compartment index to a flat index.
+     */
     template <size_t I = PopIndex::size - 1>
     inline std::enable_if_t<(I > details::IndexPosition<Comp, PopIndex>::value), size_t> comp_factor()
     {
@@ -358,32 +375,41 @@ private:
         return static_cast<size_t>(get<I>(this->populations.size())) * comp_factor<I - 1>();
     }
 
-    // compute only the factor used with "Comp" in this->population.get_flat_index
+    /// @brief Ends recursion of `size_t comp_factor()`.
     template <size_t I = PopIndex::size - 1>
     inline std::enable_if_t<(I <= details::IndexPosition<Comp, PopIndex>::value), size_t> comp_factor()
     {
         return 1;
     }
 
-    // compute the derivatives/rhs of the model equations for a given Flow and non-"Comp" (flat) indices.
-    // the recursion is resolved at compile time depending on the FlowChart.
-    template <size_t flow_id>
-    inline std::enable_if_t<flow_id != sizeof...(Flows)>
+    /**
+     * @brief Compute the derivatives of the compartments.
+     * Compute the derivatives/rhs of the model equations for a given flow and non-"Comp" (flat) indices.
+     * Uses recursion, which is resolved at compile time depending on the FlowChart.
+     * @param[in] flows Current change in flows, as computed by get_flows.
+     * @param[out] rhs The derivatives of the model equations.
+     * @param[in] flat_pop Flattened PopIndex without contribution from the compartment index.
+     * @param[in] flat_flow Flattened FlowIndex.
+     * @tparam FlowID The index of a flow in FlowChart.
+     */
+    template <size_t FlowID>
+    inline std::enable_if_t<FlowID != sizeof...(Flows)>
     get_rhs_impl(Eigen::Ref<const Eigen::VectorXd> flows, Eigen::Ref<Eigen::VectorXd> rhs, const size_t& flat_pop,
                  const size_t& flat_flow) const
     {
-        constexpr size_t source = static_cast<size_t>(FlowChart().template get<flow_id>().source);
-        constexpr size_t target = static_cast<size_t>(FlowChart().template get<flow_id>().target);
+        constexpr size_t source = static_cast<size_t>(FlowChart().template get<FlowID>().source);
+        constexpr size_t target = static_cast<size_t>(FlowChart().template get<FlowID>().target);
         // see get_flow_index for more info on these index calculations.
         // flat_flow and flat_pop already contain index information for all non-Comp Categories, so
-        // we only need to add flow_id and source/target respectively (with the correct factors).
-        rhs[flat_pop + source * m_comp_factor] -= flows[flat_flow + flow_id];
-        rhs[flat_pop + target * m_comp_factor] += flows[flat_flow + flow_id];
-        get_rhs_impl<flow_id + 1>(flows, rhs, flat_pop, flat_flow);
+        // we only need to add FlowID and source/target respectively (with the correct factors).
+        rhs[flat_pop + source * m_comp_factor] -= flows[flat_flow + FlowID];
+        rhs[flat_pop + target * m_comp_factor] += flows[flat_flow + FlowID];
+        get_rhs_impl<FlowID + 1>(flows, rhs, flat_pop, flat_flow);
     }
 
-    template <size_t flow_id>
-    inline std::enable_if_t<flow_id == sizeof...(Flows)>
+    /// @brief Ends recursion of `void get_rhs_impl(...)`.
+    template <size_t FlowID>
+    inline std::enable_if_t<FlowID == sizeof...(Flows)>
     get_rhs_impl(Eigen::Ref<const Eigen::VectorXd>, Eigen::Ref<Eigen::VectorXd>, const size_t&, const size_t&) const
     {
         // end recursion
@@ -426,4 +452,4 @@ using is_compartment_model = std::integral_constant<bool, (is_expression_valid<e
 
 } // namespace mio
 
-#endif // COMPARTMENTALMODEL_H
+#endif // MIO_COMPARTMENTALMODEL_H
