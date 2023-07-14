30bb03d5a2b221ab6edd3147d54b9658e2e53fb1
HenrZu <henrik-zunker@hotmail.de>
2023-07-14 10:09:00 +0200

    mainly formatting

--- cpp/models/ode_seir/reproduction_nb.cpp
+++ cpp/models/ode_seir/reproduction_nb.cpp
@@ -2,14 +2,19 @@
 #include "ode_seir/parameters.h"
 #include "parameters.h"
 
-double getReproductionNumber(Eigen::Index timept, double coeffStoE, double TimeInfected, mio::TimeSeries<ScalarType> y){//Computes the reproduction number at a certain time (actually only needs number of susceptibles from the TimeSeries)
-            return y.get_value(timept)[(Eigen::Index)mio::oseir::InfectionState::Susceptible]*TimeInfected*coeffStoE;
-    }
+ScalarType getReproductionNumber(Eigen::Index timept, ScalarType coeffStoE, ScalarType TimeInfected,
+                                 mio::TimeSeries<ScalarType> y)
+{ //Computes the reproduction number at a certain time (actually only needs number of susceptibles from the TimeSeries)
+    return y.get_value(timept)[(Eigen::Index)mio::oseir::InfectionState::Susceptible] * TimeInfected * coeffStoE;
+}
 
-    Eigen::VectorXd getReproductionNumbers(double coeffStoE, double TimeInfected, mio::TimeSeries<ScalarType> y){//Computes the reproduction numbers at all times
-            Eigen::VectorXd temp(y.get_num_time_points());
-            for(int i = 0; i < y.get_num_time_points(); i++){
-                temp[i] = coeffStoE*TimeInfected*y.get_value((Eigen::Index)i)[(Eigen::Index)mio::oseir::InfectionState::Susceptible];
-            }
-            return temp;
+Eigen::VectorXd getReproductionNumbers(ScalarType coeffStoE, ScalarType TimeInfected, mio::TimeSeries<ScalarType> y)
+{
+    auto num_time_points = y.get_num_time_points();
+    Eigen::VectorXd temp(num_time_points);
+    for (int i = 0; i < num_time_points; i++) {
+        temp[i] = coeffStoE * TimeInfected *
+                  y.get_value((Eigen::Index)i)[(Eigen::Index)mio::oseir::InfectionState::Susceptible];
     }
+    return temp;
+}
--- cpp/tests/test_reproduction_nb_seir.cpp
+++ cpp/tests/test_reproduction_nb_seir.cpp
@@ -14,11 +14,11 @@
 #include "memilio/math/euler.h"
 #include "memilio/compartments/simulation.h"
 
-TEST(ReproductionNumberTest, SingleNumberCalculation){
-
+TEST(ReproductionNumberTest, AllNumbersCalculation)
+{
     double t0   = 0;
     double tmax = 1;
-    double dt   =  0.001;
+    double dt   = 0.001;
 
     mio::oseir::Model model;
 
@@ -31,8 +31,7 @@ TEST(ReproductionNumberTest, SingleNumberCalculation){
         model.populations[{mio::Index<mio::oseir::InfectionState>(mio::oseir::InfectionState::Exposed)}] -
         model.populations[{mio::Index<mio::oseir::InfectionState>(mio::oseir::InfectionState::Infected)}] -
         model.populations[{mio::Index<mio::oseir::InfectionState>(mio::oseir::InfectionState::Recovered)}];
-    // suscetible now set with every other update
-    // params.nb_sus_t0   = params.nb_total_t0 - params.nb_exp_t0 - params.nb_inf_t0 - params.nb_rec_t0;
+
     model.parameters.set<mio::oseir::TimeExposed>(5.2);
     model.parameters.set<mio::oseir::TimeInfected>(6);
     model.parameters.set<mio::oseir::TransmissionProbabilityOnContact>(0.04);
@@ -40,59 +39,18 @@ TEST(ReproductionNumberTest, SingleNumberCalculation){
 
     model.check_constraints();
 
-    double checkReproductionNumber = 2.328; //Correct result for these input values
+    double coeffStoE     = 4e-05;
+    double& TimeInfected = model.parameters.get<mio::oseir::TimeInfected>();
 
+    Eigen::VectorXd checkReproductionNumbers(7);
+    checkReproductionNumbers << 2.328, 2.3279906878991881, 2.3279487809434576, 2.3277601483151549, 2.3269102025388899,
+        2.323058005241374, 2.318540062468307;
 
-    double coeffStoE =  4e-05;       
-    Eigen::Index timept = 0;
-    double TimeInfected = 6;
-    mio::TimeSeries<double> result = simulate(t0, tmax, dt, model);
-    EXPECT_NEAR(getReproductionNumber(timept, coeffStoE, TimeInfected, result), checkReproductionNumber, 1e-12)<<"Reproduction number at time 0 is wrong";
-}
-
-TEST(ReproductionNumberTest, AllNumbersCalculation){
-    double t0   = 0;
-    double tmax = 1;
-    double dt   =  0.001;
-
-    mio::oseir::Model model;
-
-     double total_population                                                                            = 10000;
-    model.populations[{mio::Index<mio::oseir::InfectionState>(mio::oseir::InfectionState::Exposed)}]   = 100;
-    model.populations[{mio::Index<mio::oseir::InfectionState>(mio::oseir::InfectionState::Infected)}]  = 100;
-    model.populations[{mio::Index<mio::oseir::InfectionState>(mio::oseir::InfectionState::Recovered)}] = 100;
-    model.populations[{mio::Index<mio::oseir::InfectionState>(mio::oseir::InfectionState::Susceptible)}] =
-        total_population -
-        model.populations[{mio::Index<mio::oseir::InfectionState>(mio::oseir::InfectionState::Exposed)}] -
-        model.populations[{mio::Index<mio::oseir::InfectionState>(mio::oseir::InfectionState::Infected)}] -
-        model.populations[{mio::Index<mio::oseir::InfectionState>(mio::oseir::InfectionState::Recovered)}];
-    // suscetible now set with every other update
-    // params.nb_sus_t0   = params.nb_total_t0 - params.nb_exp_t0 - params.nb_inf_t0 - params.nb_rec_t0;
-    model.parameters.set<mio::oseir::TimeExposed>(5.2);
-    model.parameters.set<mio::oseir::TimeInfected>(6);
-    model.parameters.set<mio::oseir::TransmissionProbabilityOnContact>(0.04);
-    model.parameters.get<mio::oseir::ContactPatterns>().get_baseline()(0, 0) = 10;
-
-    model.check_constraints();
-
-    double coeffStoE = 4e-05;
-    double TimeInfected = 6;
-
-    Eigen::VectorXd checkReproductionNumbers(7); 
-    checkReproductionNumbers << 2.328,
-2.3279906878991881,
-2.3279487809434576,
-2.3277601483151549,
-2.3269102025388899,
-2.323058005241374,
-2.318540062468307;
-
-    model.parameters.get<mio::oseir::TimeInfected>()=6;
-    
     mio::TimeSeries<double> result = simulate(t0, tmax, dt, model);
 
-    for(Eigen::Index i = 0; i < getReproductionNumbers(coeffStoE, TimeInfected, result).size(); i++){
-        EXPECT_NEAR(getReproductionNumbers(coeffStoE, TimeInfected, result)[i], checkReproductionNumbers[i], 1e-12)<<"Reproduction numbers are wrong at time: "<<i;
+    auto reproduction_numbers = getReproductionNumbers(coeffStoE, TimeInfected, result);
+
+    for (Eigen::Index i = 0; i < reproduction_numbers.size(); i++) {
+        EXPECT_NEAR(reproduction_numbers[i], checkReproductionNumbers[i], 1e-12);
     }
-
 }
