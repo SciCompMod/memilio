d95e71562fbd8b03eb86987d10b16b5eb7a7f2c0
Sascha Korf <51127093+xsaschako@users.noreply.github.com>
2023-07-24 07:23:11 +0200

    thing
    Signed-off-by: Sascha Korf <51127093+xsaschako@users.noreply.github.com>

--- cpp/simulations/abm_braunschweig.cpp
+++ cpp/simulations/abm_braunschweig.cpp
@@ -541,7 +541,7 @@ mio::abm::Simulation create_sampled_simulation(const mio::abm::TimePoint& t0)
     auto world = mio::abm::World(infection_params);
 
     // Create the world object from statistical data.
-    create_world_from_data(world, "C:/Users/korf_sa/Documents/rep/data/mobility/bs.csv");
+    create_world_from_data(world, "");
     world.use_migration_rules(false);
 
     // Assign an infection state to each person.
