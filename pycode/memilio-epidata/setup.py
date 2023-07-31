44292e9844e0d90f27285e109ccd8aac667df009
patricklnz <patrick.lenz01@icloud.com>
2023-07-31 11:58:13 +0200

    wrong python-magic version

--- pycode/memilio-epidata/setup.py
+++ pycode/memilio-epidata/setup.py
@@ -83,7 +83,7 @@ setup(
         'pyxlsb',
         'wget',
         'twill',
-        'python-magic==0.4.13'  # fails for other version
+        'python-magic==0.4.14'  # fails for other version
     ],
     extras_require={
         'dev': [
