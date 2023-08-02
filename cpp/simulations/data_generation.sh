0267b669269c721c01651113948bc2581f087b4d
Henrik Zunker <henrik.zunker@dlr.de>
2023-08-02 15:12:28 +0200

    move to new branch

---
+++ cpp/simulations/data_generation.sh
@@ -0,0 +1,42 @@
+#! /bin/bash
+# Run all scripts in memilio-epidata which are necessary to run the simulation files.
+
+# activate virtual_env 
+# The path to the virtual environment has to be adjusted individually
+# Otherwise the script can be called with -PATH_ENV Path argument, e.g.
+# sh data_generation.sh -PATH_ENV "HOME/Documents/virtualenv/bin/activate"
+path_virtual_env= "HOME/Documents/virtualenv/bin/activate" 
+
+# Use/check for input argument
+while [[ $# -gt 0 ]]; do
+    key="$1"
+    case $key in
+        -PATH_ENV)
+        path_virtual_env="$2"
+        shift
+        shift
+        ;;
+        *)
+        echo "Unknown parameter: $1"
+        exit 1
+        ;;
+    esac
+done
+
+if ! [[ -f $path_virtual_env ]]
+then echo "Set path_virtual_env to an existing python venv." && exit
+else source $path_virtual_env
+fi
+
+# path to memilio dir
+cd ../..
+data_dir=$PWD/data/pydata
+mobility_dir=$PWD/data/mobility/
+
+# download data
+cd "pycode/memilio-epidata"
+python setup.py install
+python memilio/epidata/getSimulationData.py -o $data_dir -m 7
+python memilio/epidata/transformMobilityData.py -o $mobility_dir
+
+echo "Generation was succesful." 
