3034356dd5a1b3451fc99017e422e7ec772032f7
HenrZu <henrik-zunker@hotmail.de>
2023-08-02 14:38:04 +0200

    add option for script + Doc

--- cpp/simulations/data_generation.sh
+++ cpp/simulations/data_generation.sh
@@ -1,13 +1,33 @@
 #! /bin/bash
 # Run all scripts in memilio-epidata which are necessary to run the simulation files.
 
-# activate virutal_env 
+# activate virtual_env 
 # The path to the virtual environment has to be adjusted individually
-path_virtual_env="$HOME/Documents/virtualenv/bin/activate"
+# Otherwise the script can be called with -PATH_ENV Path argument, e.g.
+# sh data_generation.sh -PATH_ENV "HOME/Documents/virtualenv/bin/activate"
+path_virtual_env= "HOME/Documents/virtualenv/bin/activate" 
+
+# Use/ check for input argument
+while [[ $# -gt 0 ]]; do
+    key="$1"
+    case $key in
+        -PATH_ENV)
+        path_virtual_env="$2"
+        shift
+        shift
+        ;;
+        *)
+        echo "Unknown parameter: $1"
+        exit 1
+        ;;
+    esac
+done
+
 if ! [[ -f $path_virtual_env ]]
 then echo "Set path_virtual_env to an existing python venv." && exit
 else source $path_virtual_env
 fi
+
 # path to memilio dir
 cd ../..
 data_dir=$PWD/data/pydata
