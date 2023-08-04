4342ad7ec3c05425d504cbfe5ddd6446ef0364dc
Abele, Daniel <daniel.abele@dlr.de>
2023-08-04 17:20:23 +0200

    review: comment for manual openmp reduction

--- cpp/models/abm/simulation.cpp
+++ cpp/models/abm/simulation.cpp
@@ -66,14 +66,19 @@ void Simulation::store_result_at(TimePoint t)
 {
     m_result.add_time_point(t.days());
     m_result.get_last_value().setZero();
+
+    //Use a manual parallel reduction to sum up the subpopulations
+    //The reduction clause of `omp parallel for` doesn't work well for `Eigen::VectorXd`
     PRAGMA_OMP(parallel)
     {
+        //thread local sum of subpopulations, computed in parallel
         Eigen::VectorXd sum = Eigen::VectorXd::Zero(m_result.get_num_elements());
         PRAGMA_OMP(for)
         for (auto i = size_t(0); i < m_world.get_locations().size(); ++i) {
             auto&& location = m_world.get_locations()[i];
             sum += location.get_subpopulations().get_last_value().cast<ScalarType>();
         }
+        //synchronized total sum
         PRAGMA_OMP(critical)
         {
             m_result.get_last_value() += sum;
