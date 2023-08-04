40795fe045d2ded37f48497cea1ce9ecf1a39603
Abele, Daniel <daniel.abele@dlr.de>
2023-08-04 17:26:45 +0200

    review: document openmp pragma macro

--- cpp/memilio/utils/mioomp.h
+++ cpp/memilio/utils/mioomp.h
@@ -7,10 +7,21 @@
 #ifdef MEMILIO_ENABLE_OPENMP
 #include "omp.h"
 
+/**
+* Macro for OpenMP directives.
+* OpenMP is enabled.
+* `PRAGMA_OMP(parallel for)` evaluates to `#pragma omp parallel for`
+*/
 #define PRAGMA_OMP(x) _Pragma(QUOTE(omp x))
 
 #else
 
+/**
+* Macro for OpenMP directives. 
+* Evaluates to nothing because OpenMP is disabled.
+* Unknown pragmas are ignored by the compiler, but the compiler emits warnings, so
+* we remove the #pragma completely.
+*/
 #define PRAGMA_OMP(x)
 
 #endif
