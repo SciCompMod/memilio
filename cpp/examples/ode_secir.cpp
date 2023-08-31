d57f9417f53b20e25279fd09d91b36cac900508b
Paul Johannssen <paul.johannssen@dlr.de>
2023-08-31 09:06:49 +0200

    changes

--- cpp/examples/ode_secir.cpp
+++ cpp/examples/ode_secir.cpp
@@ -17,7 +17,6 @@
 * See the License for the specific language governing permissions and
 * limitations under the License.
 */
-#include "ode_secir/infection_state.h"
 #include "ode_secir/model.h"
 #include "memilio/compartments/simulation.h"
 #include "memilio/utils/logging.h"
