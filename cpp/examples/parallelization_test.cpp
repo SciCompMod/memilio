5681e971f88836e36339723b5d5174cd9a8223df
“BenoBurth” <142229168+BenoBurth@users.noreply.github.com>
2023-09-12 13:19:48 +0200

    increase vector size

--- cpp/examples/parallelization_test.cpp
+++ cpp/examples/parallelization_test.cpp
@@ -2,16 +2,17 @@
 #include <vector>
 #include <omp.h>
 
-int main() {
-    const int vectorSize = 1000000; // Größe der Vektoren anpassen
+int main()
+{
+    const int vectorSize = 100000000; // Größe der Vektoren anpassen
 
     std::vector<double> vectorA(vectorSize);
     std::vector<double> vectorB(vectorSize);
     std::vector<double> vectorC(vectorSize);
     std::vector<double> vectorD(vectorSize);
 
-    // Zufällige Werte für die Vektoren generieren
-    #pragma omp parallel for
+// Zufällige Werte für die Vektoren generieren
+#pragma omp parallel for
     for (int i = 0; i < vectorSize; ++i) {
         vectorA[i] = static_cast<double>(rand()) / RAND_MAX;
         vectorB[i] = static_cast<double>(rand()) / RAND_MAX;
@@ -19,8 +20,8 @@ int main() {
         vectorD[i] = 0;
     }
 
-    // Vektortriade parallel berechnen
-    #pragma omp parallel for
+// Vektortriade parallel berechnen
+#pragma omp parallel for
     for (int i = 0; i < vectorSize; ++i) {
         vectorD[i] = vectorA[i] + vectorB[i] - vectorC[i];
     }
