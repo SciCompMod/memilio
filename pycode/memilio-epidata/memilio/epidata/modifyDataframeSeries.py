4db355b17b14afb82a9a599e53b5160abcb8ecb6
patricklnz <patrick.lenz01@icloud.com>
2023-09-19 15:31:07 +0200

    pre commit

--- pycode/memilio-epidata/memilio/epidata/modifyDataframeSeries.py
+++ pycode/memilio-epidata/memilio/epidata/modifyDataframeSeries.py
@@ -100,7 +100,8 @@ def impute_and_reduce_df(
     # loop over all items in columns that are given to group by (i.e. regions/ages/gender)
     for ids in unique_ids_comb:
         # filter df
-        df_local = df_old[np.all([df_old[col] == id_ for col, id_ in zip(group_by, ids)], axis=0)]
+        df_local = df_old[np.all(
+            [df_old[col] == id_ for col, id_ in zip(group_by, ids)], axis=0)]
 
         # create missing dates
         df_local.index = df_local.Date
