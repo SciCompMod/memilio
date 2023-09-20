a5232294ba83969f28d62f50d149a7ce5ccf4a00
alexgit0 <95716945+alexgit0@users.noreply.github.com>
2023-09-20 09:37:55 +0200

    add loop for secir example

--- cpp/examples/ode_secir.cpp
+++ cpp/examples/ode_secir.cpp
@@ -26,7 +26,6 @@
 int main()
 {
 
-    for(int iter = 0; iter < 10000; iter++) {
     LIKWID_MARKER_INIT;
 
     LIKWID_MARKER_START("setup");  
@@ -124,6 +123,6 @@ int main()
         printf("number total: %f",
                res_j[0] + res_j[1] + res_j[2] + res_j[3] + res_j[4] + res_j[5] + res_j[6] + res_j[7]);
     }
-    }
+    
 }
 }
