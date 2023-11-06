d857f8e7366341e733295e1d7d92e057ab12a7ed
Rene Schmieding <reneSchm@users.noreply.github.com>
2023-11-06 11:17:27 +0100

    add comments/documentation

--- cpp/memilio/utils/multi_index_range.h
+++ cpp/memilio/utils/multi_index_range.h
@@ -17,21 +17,33 @@
 * See the License for the specific language governing permissions and
 * limitations under the License.
 */
-#ifndef MIO_UTILS_MULTI_INDEX_H_
-#define MIO_UTILS_MULTI_INDEX_H_
+#ifndef MIO_UTILS_MULTI_INDEX_RANGE_H_
+#define MIO_UTILS_MULTI_INDEX_RANGE_H_
 
 #include "memilio/utils/index.h"
 
 namespace mio
 {
 
+/**
+ * @brief A Range that can be used to iterate over a MultiIndex.
+ * @tparam MultiIndex A type mio::Index<Categories...> for some set of categories.
+ */
 template <class MultiIndex>
 class MultiIndexRange
 {
 public:
+    /**
+     * @brief Construct a Range that can be used to iterate over all MultiIndices in the given dimensions.
+     * The range for each Index i in the MultiIndex is determined by [0, d_i), where d_i is the dimension of i.
+     * @param[in] dimensions A MultIndex that contains the dimension for each Category.
+     */
     MultiIndexRange(const MultiIndex& dimensions)
         : m_dimensions(dimensions)
         , m_end([](const MultiIndex& dims) {
+            // set end to the first invalid index that is reached by increments of 1,
+            // i.e. 0 everywhere except for the most significant index (position 0),
+            // which is set to its dimension
             auto end         = MultiIndex::Zero();
             mio::get<0>(end) = mio::get<0>(dims);
             return end;
@@ -39,23 +51,33 @@ public:
     {
     }
 
+    /**
+     * @brief Iterator for MultiIndices.
+     */
     class MultiIndexIterator
     {
     public:
         using value_type = MultiIndex;
         using reference  = const MultiIndex&;
 
+        /**
+         * @brief Iterator for MultiIndices.
+         * @param index Initial value for the iterator position.
+         * @param dimensions A reference to the dimensions of the MultiIndex.
+         */
         explicit MultiIndexIterator(value_type index, reference dimensions)
             : m_index(index)
             , m_dims(dimensions)
         {
         }
 
+        /// Dereference operator.
         reference operator*() const
         {
             return m_index;
         }
 
+        /// Pre-increment operator.
         MultiIndexIterator& operator++()
         {
             if constexpr (MultiIndex::size > 0) {
@@ -64,6 +86,7 @@ public:
             return *this;
         }
 
+        /// Post-increment operator.
         MultiIndexIterator operator++(int)
         {
             auto retval = *this;
@@ -71,23 +94,31 @@ public:
             return retval;
         }
 
+        /// Equality operator.
         bool operator==(MultiIndexIterator& other) const
         {
             return m_index == other.m_index;
         }
 
+        /// Inequality operator.
         bool operator!=(MultiIndexIterator& other) const
         {
             return !(*this == other);
         }
 
     private:
+        /**
+         * @brief Implementation of ++. Increments m_index.
+         * @tparam I position in the MultiIndex.
+         */
         template <size_t I = MultiIndex::size - 1>
         inline void increment_index()
         {
             assert(mio::get<I>(m_dims).get() > 0 && "All dimensions must be positive.");
             assert(mio::get<I>(m_index) < mio::get<I>(m_dims) && "Index out of bounds.");
+
             if constexpr (I > 0) {
+                // increment first, then do a carry-over if necessary
                 ++mio::get<I>(m_index);
                 if (mio::get<I>(m_index) == mio::get<I>(m_dims)) {
                     mio::get<I>(m_index) = mio::get<I>(m_index).Zero();
@@ -96,16 +127,26 @@ public:
             }
             else {
                 ++mio::get<0>(m_index);
+                // no carry check for the most significant index
             }
         }
-        value_type m_index;
-        reference m_dims;
+        value_type m_index; ///< Index used for iteration.
+        reference m_dims; ///< Read only reference to range dimensions.
     };
 
+    /**
+     * @brief STL iterator for a MultiIndexRange.
+     * @return Returns the first index for the given dimensions, i.e. 0.
+     */
     MultiIndexIterator begin() const
     {
         return MultiIndexIterator(MultiIndex::Zero(), m_dimensions);
     }
+
+    /**
+     * @brief STL iterator for a MultiIndexRange.
+     * @return Returns the first index outside of the given dimensions.
+     */
     MultiIndexIterator end() const
     {
         return MultiIndexIterator(m_end, m_dimensions);
@@ -118,4 +159,4 @@ private:
 
 } // namespace mio
 
-#endif
+#endif // MIO_UTILS_MULTI_INDEX_RANGE_H_
