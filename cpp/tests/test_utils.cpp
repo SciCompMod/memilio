ae25cd27887c085c7a99e1f957de835e0ec2a4ef
Rene Schmieding <reneSchm@users.noreply.github.com>
2023-11-13 13:36:12 +0100

    add a test for extend_index

--- cpp/tests/test_utils.cpp
+++ cpp/tests/test_utils.cpp
@@ -55,6 +55,32 @@ TEST(TestUtils, reduce_index)
     EXPECT_EQ(result_j, reference_j);
 }
 
+TEST(TestUtils, extend_index)
+{
+    // the superindex
+    mio::Index<CategoryTag<1>, CategoryTag<2>, CategoryTag<3>> reference_i{CategoryTag<1>(1), CategoryTag<2>(2),
+                                                                           CategoryTag<3>(3)};
+    // some subindices
+    mio::Index<CategoryTag<1>, CategoryTag<2>> i12{CategoryTag<1>(1), CategoryTag<2>(2)};
+    mio::Index<CategoryTag<2>, CategoryTag<3>> i23{CategoryTag<2>(2), CategoryTag<3>(3)};
+    mio::Index<CategoryTag<1>, CategoryTag<3>> i13{CategoryTag<1>(1), CategoryTag<3>(3)};
+    // test extension results
+    auto result_i12 = mio::extend_index<decltype(reference_i)>(i12, 3);
+    EXPECT_EQ(result_i12, reference_i);
+    auto result_i23 = mio::extend_index<decltype(reference_i)>(i23, 1);
+    EXPECT_EQ(result_i23, reference_i);
+    auto result_i13 = mio::extend_index<decltype(reference_i)>(i13, 2);
+    EXPECT_EQ(result_i13, reference_i);
+
+    // test extension of an index with non-unique categories
+    mio::Index<CategoryTag<1>, CategoryTag<1>, CategoryTag<1>> j{CategoryTag<1>(1), CategoryTag<1>(2),
+                                                                 CategoryTag<1>(3)};
+    mio::Index<CategoryTag<1>, CategoryTag<1>, CategoryTag<1>, CategoryTag<1>> reference_j{
+        CategoryTag<1>(1), CategoryTag<1>(1), CategoryTag<1>(1), CategoryTag<1>(1)};
+    auto result_j = mio::extend_index<decltype(reference_j)>(j);
+    EXPECT_EQ(result_j, reference_j);
+}
+
 TEST(TestUtils, IndexRange)
 {
     using I = mio::Index<CategoryTag<1>, CategoryTag<2>, CategoryTag<3>>;
@@ -68,11 +94,11 @@ TEST(TestUtils, IndexRange)
     EXPECT_EQ(*range.end(), reference_end);
     // test increments
     auto iterator = range.begin();
-    for (size_t i = 0; i < 2 * 3 * 5; i++) {
+    for (size_t reference_flat_index = 0; reference_flat_index < 2 * 3 * 5; reference_flat_index++) {
         // manually compute flatten_index(*iterator, dims)
         size_t flat_index =
             (3 * 5) * mio::get<0>(*iterator).get() + (5) * mio::get<1>(*iterator).get() + mio::get<2>(*iterator).get();
-        ASSERT_EQ(flat_index, i);
+        EXPECT_EQ(flat_index, reference_flat_index);
         iterator++;
     }
 }
