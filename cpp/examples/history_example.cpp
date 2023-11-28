e4cfc70c4f88a44e06a045528e89a8ceb5e22e9e
Sascha Korf <51127093+xsaschako@users.noreply.github.com>
2023-11-28 17:39:07 +0100

    Update history_example.cpp to include the correct header file
    Signed-off-by: Sascha Korf <51127093+xsaschako@users.noreply.github.com>

--- cpp/examples/history_example.cpp
+++ cpp/examples/history_example.cpp
@@ -1,37 +1,20 @@
-#include "history.h"
+#include "memilio/io/history.h"
 #include <iostream>
 #include <vector>
 
 // Define a Logger
-struct MyLogger {
+struct MyLogger : mio::LogAlways {
     using Type = int;
-
-    Type log(const int& input)
+    static Type log(const int& input)
     {
         return input * 2;
-    }
-
-    bool should_log(const int& input)
-    {
-        return input % 2 == 0;
-    }
-};
-
-// Define a Writer
-struct MyWriter {
-    using Data = std::vector<int>;
-
-    template <class Logger>
-    static void add_record(const typename Logger::Type& t, Data& data)
-    {
-        data.push_back(t);
     }
 };
 
 int main()
 {
     // Create a History object
-    History<MyWriter, MyLogger> history;
+    mio::History<mio::DataWriterToMemory, MyLogger> history;
 
     // Log some data
     for (int i = 0; i < 10; ++i) {
