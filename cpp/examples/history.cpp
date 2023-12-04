33930fe61a420ce0e00565ce4cb76130454442dd
Sascha Korf <51127093+xsaschako@users.noreply.github.com>
2023-12-04 13:57:22 +0100

    Enhance history.cpp Documentation

--- cpp/examples/history.cpp
+++ cpp/examples/history.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 #include <vector>
 
-// Define a Logger
+// We define a Logger that logs the current input multiplied by 2. For that we have to return the input * 2.
 struct MyLogger : mio::LogAlways {
     using Type = int;
     static Type log(const int& input)
@@ -13,21 +13,21 @@ struct MyLogger : mio::LogAlways {
 
 int main()
 {
-    // Create a History object with the predefined Writer DataWriterToMemory and our above defined Logger MyLogger.
-    // The Writer DataWriterToMemory uses an std::vector to store the data.
+    // We construct our History object to store data defined by the Logger MyLogger. To specify how we write the data we use the Writer DataWriterToMemory.
+    // That Writer writes the data to a a tuple of std::vector<Logger::Type>, in this case std::tuple<std::vector<int>> as we use MyLogger.
     mio::History<mio::DataWriterToMemory, MyLogger> history;
 
-    // Log some data.
+    // For example purposes we "log" the numbers 0-9. As we multiply it by 2 in the logger, the History stores 0,2,4,... .
     for (int i = 0; i < 10; ++i) {
-        history.log(i); // The data specified by the Logger is stored in the History object using the Writer.
+        history.log(i); 
     }
 
-    // Get the log data.
-    // For this one has to specify which from which Logger the data should be retrieved.
-    // Also the data type of this is an std::vector<int> as the Writer has a tuple of std::vector<x> where x is specified by the Type of the logger, in this case int.
-    std::vector<int> logData = std::get<0>(history.get_log()); // The first logger is MyLogger
+    // If we want to to access the logged data we can use the get_log() function. 
+    // As the History object stores a std::vector<Logger::Type> in a std::tuple for 
+    // each logger, we have to use std::get<0>(history.get_log()) to access the data of the first (and only) logger which contains the records of MyLogger.
+    std::vector<int> logData = std::get<0>(history.get_log()); 
 
-    // Print the logged data.
+    // Finally we can print the logged data.
     std::cout << "Logged data from MyLogger:" << std::endl;
     for (const auto& data : logData) {
         std::cout << data << std::endl;
