8019f93c85f0967499912da035360cce60bc69e2
Ralf Hannemann-Tamas <r.hannemann-tamas@fz-juelich.de>
2024-01-16 12:39:46 +0100

    insert template parameter FP and move function definitions into header

--- cpp/models/abm/testing_strategy.h
+++ cpp/models/abm/testing_strategy.h
@@ -27,7 +27,7 @@
 #include "abm/time.h"
 #include "memilio/utils/random_number_generator.h"
 #include <bitset>
-#include <unordered_set>
+#include <unordered_set> // IWYU pragma: keep
 
 namespace mio
 {
@@ -89,67 +89,18 @@ public:
      * @param[in] p Person to be checked.
      * @param[in] t TimePoint when to evaluate the TestingCriteria.
      */
-<<<<<<< HEAD
     template<typename FP=double>
-    bool evaluate(const Person<FP>& p, const Location<FP>& l, TimePoint t) const
+    bool evaluate(const Person<FP>& p, TimePoint t) const
     {
-        return has_requested_age(p) && is_requested_location_type(l) && has_requested_infection_state(p, t);
+        // An empty vector of ages or none bitset of #InfectionStates% means that no condition on the corresponding property is set.
+        return (m_ages.none() || m_ages[static_cast<size_t>(p.get_age())]) &&
+               (m_infection_states.none() || m_infection_states[static_cast<size_t>(p.get_infection_state(t))]);
     }
-
-private:
-    /**
-     * @brief Check if a Person has the required age to get tested.
-     * @param[in] p Person to be checked.
-     */
-    template<typename FP=double>
-    bool has_requested_age(const Person<FP>& p) const
-    {
-        if (m_ages.empty()) {
-            return true; // no condition on the age
-        }
-        return std::find(m_ages.begin(), m_ages.end(), p.get_age()) != m_ages.end();
-    }
-
-    /**
-     * @brief Check if a Location is in the set of Location%s that are allowed for testing.
-     * @param[in] l Location to be checked.
-     */
-    template<typename FP=double>
-    bool is_requested_location_type(const Location<FP>& l) const
-    {
-        if (m_location_types.empty()) {
-            return true; // no condition on the location
-        }
-        return std::find(m_location_types.begin(), m_location_types.end(), l.get_type()) != m_location_types.end();
-    }
-
-    /**
-     * @brief Check if a Person has the required InfectionState to get tested.
-     * @param[in] p Person to be checked.
-     * @param[in] t TimePoint when to check.
-     */
-    template<typename FP=double>
-    bool has_requested_infection_state(const Person<FP>& p, TimePoint t) const
-    {
-        if (m_infection_states.empty()) {
-            return true; // no condition on infection state
-        }
-        return std::find(m_infection_states.begin(), m_infection_states.end(), p.get_infection_state(t)) !=
-               m_infection_states.end();
-    }
-
-    std::vector<AgeGroup> m_ages; ///< Set of #AgeGroup%s that are either allowed or required to be tested.
-    std::vector<LocationType> m_location_types; /**< Set of #LocationState%s that are either allowed or required to be 
-    tested.*/
-    std::vector<InfectionState> m_infection_states; /**< Set of #InfectionState%s that are either allowed or required to
-=======
-    bool evaluate(const Person& p, TimePoint t) const;
 
 private:
     std::bitset<MAX_NUM_AGE_GROUPS> m_ages; ///< Set of #AgeGroup%s that are either allowed or required to be tested.
     std::bitset<(size_t)InfectionState::Count>
         m_infection_states; /**< BitSet of #InfectionState%s that are either allowed or required to
->>>>>>> upstream/main
     be tested.*/
 };
 
@@ -170,11 +121,8 @@ public:
      * @param test_type The type of test to be performed.
      * @param probability Probability of the test to be performed if a testing rule applies.
      */
-<<<<<<< HEAD
-    TestingScheme(const std::vector<TestingCriteria>& testing_criteria,
-                  TimeSpan minimal_time_since_last_test,
-                  TimePoint start_date, TimePoint end_date,
-                  const GenericTest<FP>& test_type, ScalarType probability)
+    TestingScheme(const TestingCriteria& testing_criteria, TimeSpan minimal_time_since_last_test,
+                  TimePoint start_date, TimePoint end_date, const GenericTest<FP>& test_type, double probability)
         : m_testing_criteria(testing_criteria)
         , m_minimal_time_since_last_test(minimal_time_since_last_test)
         , m_start_date(start_date)
@@ -183,10 +131,6 @@ public:
         , m_probability(probability)
     {
     }
-=======
-    TestingScheme(const TestingCriteria& testing_criteria, TimeSpan minimal_time_since_last_test, TimePoint start_date,
-                  TimePoint end_date, const GenericTest& test_type, ScalarType probability);
->>>>>>> upstream/main
 
     /**
      * @brief Compares two TestingScheme%s for functional equality.
@@ -202,31 +146,8 @@ public:
         //To be adjusted and also TestType should be static.
     }
 
-    /**
-<<<<<<< HEAD
-     * @brief Add a TestingCriteria to the set of TestingCriteria that are checked for testing.
-     * @param[in] criteria TestingCriteria to be added.
-     */
-    void add_testing_criteria(const TestingCriteria criteria)
-    {
-        if (std::find(m_testing_criteria.begin(), m_testing_criteria.end(), criteria) == m_testing_criteria.end()) {
-            m_testing_criteria.push_back(criteria);
-        }
-    }
 
     /**
-     * @brief Remove a TestingCriteria from the set of TestingCriteria that are checked for testing.
-     * @param[in] criteria TestingCriteria to be removed.
-     */
-    void remove_testing_criteria(const TestingCriteria criteria)
-    {
-        auto last = std::remove(m_testing_criteria.begin(), m_testing_criteria.end(), criteria);
-        m_testing_criteria.erase(last, m_testing_criteria.end());
-    }
-
-    /**
-=======
->>>>>>> upstream/main
      * @brief Get the activity status of the scheme.
      * @return Whether the TestingScheme is currently active.
      */
@@ -244,7 +165,6 @@ public:
         m_is_active = (m_start_date <= t && t <= m_end_date);
     }
 
-
     /**
      * @brief Runs the TestingScheme and potentially tests a Person.
      * @param[inout] rng Person::RandomNumberGenerator for the Person being tested.
@@ -252,26 +172,19 @@ public:
      * @param[in] t TimePoint when to run the scheme.
      * @return If the person is allowed to enter the Location by the scheme.
      */
-<<<<<<< HEAD
-    bool run_scheme(typename Person<FP>::RandomNumberGenerator& rng,
-                    Person<FP>& person, const Location<FP>& location, TimePoint t) const
+    bool run_scheme(typename Person<FP>::RandomNumberGenerator& rng, Person<FP>& person,
+                    TimePoint t) const
     {
         if (person.get_time_since_negative_test() > m_minimal_time_since_last_test) {
-            double random = UniformDistribution<double>::get_instance()(rng);
-            if (random < m_probability) {
-                if (std::any_of(m_testing_criteria.begin(), m_testing_criteria.end(),
-                                [&person, &location, t](TestingCriteria tr) {
-                                    return tr.evaluate(person, location, t);
-                                })) {
+            if (m_testing_criteria.evaluate(person, t)) {
+                double random = UniformDistribution<double>::get_instance()(rng);
+                if (random < m_probability) {
                     return !person.get_tested(rng, t, m_test_type.get_default());
                 }
             }
         }
         return true;
     }
-=======
-    bool run_scheme(Person::RandomNumberGenerator& rng, Person& person, TimePoint t) const;
->>>>>>> upstream/main
 
 private:
     TestingCriteria m_testing_criteria; ///< TestingCriteria of the scheme.
@@ -295,30 +208,34 @@ public:
      * @param[in] testing_schemes Vector of TestingSchemes that are checked for testing.
      */
     TestingStrategy() = default;
-<<<<<<< HEAD
-    explicit TestingStrategy(const std::vector<TestingScheme<FP>>& testing_schemes)
-        : m_testing_schemes(testing_schemes)
+    explicit TestingStrategy(const std::unordered_map<LocationId, std::vector<TestingScheme<FP>>>& location_to_schemes_map)
+        : m_location_to_schemes_map(location_to_schemes_map.begin(), location_to_schemes_map.end())
     {
     }
-=======
-    explicit TestingStrategy(const std::unordered_map<LocationId, std::vector<TestingScheme>>& location_to_schemes_map);
->>>>>>> upstream/main
 
     /**
      * @brief Add a TestingScheme to the set of schemes that are checked for testing at a certain Location.
      * @param[in] loc_id LocationId key for TestingScheme to be added.
      * @param[in] scheme TestingScheme to be added.
      */
-<<<<<<< HEAD
-    void add_testing_scheme(const TestingScheme<FP>& scheme)
+    void add_testing_scheme(const LocationId& loc_id, const TestingScheme<FP>& scheme)
     {
-        if (std::find(m_testing_schemes.begin(), m_testing_schemes.end(), scheme) == m_testing_schemes.end()) {
-            m_testing_schemes.push_back(scheme);
+        auto iter_schemes =
+            std::find_if(m_location_to_schemes_map.begin(), m_location_to_schemes_map.end(), [loc_id](auto& p) {
+                return p.first == loc_id;
+            });
+        if (iter_schemes == m_location_to_schemes_map.end()) {
+            //no schemes for this location yet, add a new list with one scheme
+            m_location_to_schemes_map.emplace_back(loc_id, std::vector<TestingScheme<FP>>(1, scheme));
+        }
+        else {
+            //add scheme to existing vector if the scheme doesn't exist yet
+            auto& schemes = iter_schemes->second;
+            if (std::find(schemes.begin(), schemes.end(), scheme) == schemes.end()) {
+                schemes.push_back(scheme);
+            }
         }
     }
-=======
-    void add_testing_scheme(const LocationId& loc_id, const TestingScheme& scheme);
->>>>>>> upstream/main
 
     /**
      * @brief Add a TestingScheme to the set of schemes that are checked for testing at a certain LocationType.
@@ -327,7 +244,7 @@ public:
      * @param[in] loc_type LocationId key for TestingScheme to be added.
      * @param[in] scheme TestingScheme to be added.
      */
-    void add_testing_scheme(const LocationType& loc_type, const TestingScheme& scheme)
+    void add_testing_scheme(const LocationType& loc_type, const TestingScheme<FP>& scheme)
     {
         add_testing_scheme(LocationId{INVALID_LOCATION_INDEX, loc_type}, scheme);
     }
@@ -337,13 +254,23 @@ public:
      * @param[in] loc_id LocationId key for TestingScheme to be remove.
      * @param[in] scheme TestingScheme to be removed.
      */
-<<<<<<< HEAD
-    void remove_testing_scheme(const TestingScheme<FP>& scheme)
+    void remove_testing_scheme(const LocationId& loc_id, const TestingScheme<FP>& scheme)
     {
-        auto last = std::remove(m_testing_schemes.begin(), m_testing_schemes.end(), scheme);
-        m_testing_schemes.erase(last, m_testing_schemes.end());
-=======
-    void remove_testing_scheme(const LocationId& loc_id, const TestingScheme& scheme);
+        auto iter_schemes =
+            std::find_if(m_location_to_schemes_map.begin(), m_location_to_schemes_map.end(), [loc_id](auto& p) {
+                return p.first == loc_id;
+            });
+        if (iter_schemes != m_location_to_schemes_map.end()) {
+            //remove the scheme from the list
+            auto& schemes_vector = iter_schemes->second;
+            auto last            = std::remove(schemes_vector.begin(), schemes_vector.end(), scheme);
+            schemes_vector.erase(last, schemes_vector.end());
+            //delete the list of schemes for this location if no schemes left
+            if (schemes_vector.empty()) {
+                m_location_to_schemes_map.erase(iter_schemes);
+            }
+        }
+    }
 
     /**
      * @brief Remove a TestingScheme from the set of schemes that are checked for testing at a certain Location.
@@ -352,10 +279,9 @@ public:
      * @param[in] loc_type LocationType key for TestingScheme to be remove.
      * @param[in] scheme TestingScheme to be removed.
      */
-    void remove_testing_scheme(const LocationType& loc_type, const TestingScheme& scheme)
+    void remove_testing_scheme(const LocationType& loc_type, const TestingScheme<FP>& scheme)
     {
         remove_testing_scheme(LocationId{INVALID_LOCATION_INDEX, loc_type}, scheme);
->>>>>>> upstream/main
     }
 
     /**
@@ -365,11 +291,12 @@ public:
      */
     void update_activity_status(const TimePoint t)
     {
-        for (auto& ts : m_testing_schemes) {
-            ts.update_activity_status(t);
+        for (auto& [_, testing_schemes] : m_location_to_schemes_map) {
+            for (auto& scheme : testing_schemes) {
+                scheme.update_activity_status(t);
+            }
         }
     }
-
 
     /**
      * @brief Runs the TestingStrategy and potentially tests a Person.
@@ -379,32 +306,38 @@ public:
      * @param[in] t TimePoint when to run the strategy.
      * @return If the Person is allowed to enter the Location.
      */
-<<<<<<< HEAD
     bool run_strategy(typename Person<FP>::RandomNumberGenerator& rng,
-                      Person<FP>& person, const Location<FP>& location, TimePoint t) const
+                      Person<FP>& person, const Location<FP>& location, TimePoint t)
     {
         // Person who is in quarantine but not yet home should go home. Otherwise they can't because they test positive.
         if (location.get_type() == mio::abm::LocationType::Home && person.is_in_quarantine()) {
             return true;
         }
-        return std::all_of(m_testing_schemes.begin(), m_testing_schemes.end(),
-                           [&person, &location, &rng, t](const TestingScheme<FP>& ts) {
-                               if (ts.is_active()) {
-                                   return ts.run_scheme(rng, person, location, t);
-                               }
-                               return true;
-                           });
+
+        //lookup schemes for this specific location as well as the location type
+        //lookup in std::vector instead of std::map should be much faster unless for large numbers of schemes
+        for (auto loc_key : {LocationId{location.get_index(), location.get_type()},
+                             LocationId{INVALID_LOCATION_INDEX, location.get_type()}}) {
+            auto iter_schemes =
+                std::find_if(m_location_to_schemes_map.begin(), m_location_to_schemes_map.end(), [loc_key](auto& p) {
+                    return p.first == loc_key;
+                });
+            if (iter_schemes != m_location_to_schemes_map.end()) {
+                //apply all testing schemes that are found
+                auto& schemes = iter_schemes->second;
+                if (!std::all_of(schemes.begin(), schemes.end(), [&rng, &person, t](TestingScheme<FP>& ts) {
+                        return !ts.is_active() || ts.run_scheme(rng, person, t);
+                    })) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 
 private:
-    std::vector<TestingScheme<FP>> m_testing_schemes; ///< Set of schemes that are checked for testing.
-=======
-    bool run_strategy(Person::RandomNumberGenerator& rng, Person& person, const Location& location, TimePoint t);
-
-private:
-    std::vector<std::pair<LocationId, std::vector<TestingScheme>>>
+    std::vector<std::pair<LocationId, std::vector<TestingScheme<FP> > > >
         m_location_to_schemes_map; ///< Set of schemes that are checked for testing.
->>>>>>> upstream/main
 };
 
 } // namespace abm
