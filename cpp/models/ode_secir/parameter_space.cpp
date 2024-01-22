2571d6ba709f64ce133a84bcdab1451bf96e1e4a
Ralf Hannemann-Tamas <r.hannemann-tamas@fz-juelich.de>
2024-01-22 11:14:36 +0100

    remove body (is already in header)

--- cpp/models/ode_secir/parameter_space.cpp
+++ cpp/models/ode_secir/parameter_space.cpp
@@ -20,17 +20,4 @@
 #include "ode_secir/parameter_space.h" // IWYU pragma: keep
 
 
-<<<<<<< HEAD
-=======
-void set_params_distributions_normal(Model& model, double t0, double tmax, double dev_rel)
-{
-    auto set_distribution = [dev_rel](UncertainValue& v, double min_val = 0.001) {
-        v.set_distribution(ParameterDistributionNormal(
-            //add add limits for nonsense big values. Also mscv has a problem with a few doubles so this fixes it
-            std::min(std::max(min_val, (1 - dev_rel * 2.6) * v), 0.1 * std::numeric_limits<double>::max()),
-            std::min(std::max(min_val, (1 + dev_rel * 2.6) * v), 0.5 * std::numeric_limits<double>::max()),
-            std::min(std::max(min_val, double(v)), 0.3 * std::numeric_limits<double>::max()),
-            std::min(std::max(min_val, dev_rel * v), std::numeric_limits<double>::max())));
-    };
->>>>>>> upstream/main
 
