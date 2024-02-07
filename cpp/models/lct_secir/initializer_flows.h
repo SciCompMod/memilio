7ffa12d2880dc01bb78ca548f44262e6b45834ab
lenaploetzke <lena.ploetzke@gmx.de>
2024-02-07 15:41:51 +0100

    added initializer from flows

---
+++ cpp/models/lct_secir/initializer_flows.h
@@ -0,0 +1,103 @@
+/* 
+* Copyright (C) 2020-2024 MEmilio
+*
+* Authors: Lena Ploetzke
+*
+* Contact: Martin J. Kuehn <Martin.Kuehn@DLR.de>
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+#ifndef LCTSECIR_INITIALIZER_H
+#define LCTSECIR_INITIALIZER_H
+
+#include "memilio/config.h"
+#include "lct_secir/infection_state.h"
+#include "memilio/math/eigen.h"
+#include "memilio/utils/time_series.h"
+#include "lct_secir/parameters.h"
+
+namespace mio
+{
+namespace lsecir
+{
+
+/**
+ * @brief Class that can be used to compute an initialization vector out of flows for a LCT Model.
+ */
+class Initializer
+{
+public:
+    /**
+     * @brief Constructs a new Initializer object.
+     *
+     * @param[in] flows Initalizing TimeSeries with flows fitting to these defined in InfectionTransition. 
+     *      Timesteps should be equidistant.
+     * @param[in, out] infectionState_init InfectionState%s for the Initializer, specifies number of Subcompartments for each infection state.
+     * @param[in, out] parameters_init Specifies Parameters necessary for the Initializer.
+     */
+    Initializer(TimeSeries<ScalarType>&& flows, InfectionState infectionState_init = InfectionState(),
+                Parameters&& parameters_init = Parameters())
+        : parameters(parameters_init)
+        , infectionState(infectionState_init)
+        , m_flows(std::move(flows))
+    {
+        m_dt = m_flows.get_time(1) - m_flows.get_time(0);
+    }
+
+    /**
+     * @brief Core function of Initializer.
+     *
+     * Computes a vector that can be used for the initalization of an LCT model with the number of persons for each subcompartment.
+     *
+     * @param[in] total_population The total size of the considered population.
+     * @param[in] deaths Number of deceased people from the disease at time 0.
+     * @param[in] total_confirmed_cases Total number of confirmed cases at time 0.
+     * @return Vector with a possible initialization for an LCT model computed out of the flows. 
+     *      A subcompartment is set to -1 if calculation was not possible.
+     */
+    Eigen::VectorXd compute_initializationvector(ScalarType total_population, ScalarType deaths,
+                                                 ScalarType total_confirmed_cases) const;
+
+    /**
+     * @brief Checks constraints of the Initializer inclusive check for parameters.
+     * @return Returns true if one (or more) constraint(s) are not satisfied, otherwise false. 
+     */
+    bool check_constraints() const;
+
+    Parameters parameters{}; ///< Parameters with the Initalizers Parameters.
+    InfectionState infectionState; ///< InfectionState specifies number of subcompartments.
+
+private:
+    /**
+     * @brief Computes a vector with the number of people in each compartment for one InfectionStateBase.
+     *
+     * With this function, partial result of compute_initializationvector are achieved.
+     *
+     * @param[in] base The InfectionStateBase for which the partial result should be calculated.
+     * @param[in] idx_incoming_flow Index of the flow which is relevant for the calculation, so the flow to the InfectionStateBase base.
+     * @param[in] transition_rate Specifies the transition rate of the InfectionsStateBase. Is equal to 1 / (expected Time in base).
+     * @return Vector with a possible initialization for the subcompartments of base. 
+     *      Subcompartment is set to -1 if calculation was not possible.
+     */
+    Eigen::VectorXd compute_compartment(InfectionStateBase base, Eigen::Index idx_incoming_flow,
+                                        ScalarType transition_rate) const;
+
+    TimeSeries<ScalarType> m_flows; ///< TimeSeries with the flows which are used to calculate the initial vector.
+    ScalarType m_dt{}; ///< Step size of the times in m_flows and time step for the approximation of the integral.
+};
+
+} // namespace lsecir
+} // namespace mio
+
+#endif
---
+++ cpp/tests/test_lct_initializer_flows.cpp
@@ -0,0 +1,201 @@
+/* 
+* Copyright (C) 2020-2023 German Aerospace Center (DLR-SC)
+*
+* Authors: Lena Ploetzke
+*
+* Contact: Martin J. Kuehn <Martin.Kuehn@DLR.de>
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+#include "lct_secir/infection_state.h"
+#include "lct_secir/initializer_flows.h"
+#include "lct_secir/parameters.h"
+#include "memilio/config.h"
+#include "memilio/utils/time_series.h"
+#include "memilio/epidemiology/uncertain_matrix.h"
+#include "memilio/utils/compiler_diagnostics.h"
+#include "memilio/math/eigen.h"
+
+#include <vector>
+#include <gtest/gtest.h>
+// Test compares a calculation of an initial vector using data for flows with a previous result.
+TEST(TestInitializer, compareWithPrevious)
+{
+    ScalarType dt = 0.5;
+
+    // Define number of subcompartments.
+    std::vector<int> SubcompartmentNumbers((int)mio::lsecir::InfectionStateBase::Count, 1);
+    SubcompartmentNumbers[(int)mio::lsecir::InfectionStateBase::Exposed]            = 2;
+    SubcompartmentNumbers[(int)mio::lsecir::InfectionStateBase::InfectedNoSymptoms] = 3;
+    SubcompartmentNumbers[(int)mio::lsecir::InfectionStateBase::InfectedSymptoms]   = 2;
+    SubcompartmentNumbers[(int)mio::lsecir::InfectionStateBase::InfectedSevere]     = 3;
+    SubcompartmentNumbers[(int)mio::lsecir::InfectionStateBase::InfectedCritical]   = 2;
+    mio::lsecir::InfectionState InfState(SubcompartmentNumbers);
+
+    // Previous result.
+    Eigen::VectorXd compare(InfState.get_count());
+    compare << 82810889.00545, 850.70432, 970.04980, 315.32890, 391.51799, 391.39351, 565.45854, 580.79267, 85.97421,
+        86.02738, 80.26791, 189.53449, 167.57963, 329757.36512, 9710;
+
+    // Define parameters.
+    mio::lsecir::Parameters parameters_lct;
+    parameters_lct.get<mio::lsecir::TimeExposed>()                      = 3.1;
+    parameters_lct.get<mio::lsecir::TimeInfectedNoSymptoms>()           = 3.1;
+    parameters_lct.get<mio::lsecir::TimeInfectedSymptoms>()             = 6.1;
+    parameters_lct.get<mio::lsecir::TimeInfectedSevere>()               = 11.1;
+    parameters_lct.get<mio::lsecir::TimeInfectedCritical>()             = 17.1;
+    parameters_lct.get<mio::lsecir::TransmissionProbabilityOnContact>() = 0.01;
+    mio::ContactMatrixGroup contact_matrix                              = mio::ContactMatrixGroup(1, 1);
+    parameters_lct.get<mio::lsecir::ContactPatterns>()                  = mio::UncertainContactMatrix(contact_matrix);
+
+    parameters_lct.get<mio::lsecir::RelativeTransmissionNoSymptoms>() = 1;
+    parameters_lct.get<mio::lsecir::RiskOfInfectionFromSymptomatic>() = 1;
+    parameters_lct.get<mio::lsecir::Seasonality>()                    = 0;
+    parameters_lct.get<mio::lsecir::StartDay>()                       = 0;
+    parameters_lct.get<mio::lsecir::RecoveredPerInfectedNoSymptoms>() = 0.1;
+    parameters_lct.get<mio::lsecir::SeverePerInfectedSymptoms>()      = 0.1;
+    parameters_lct.get<mio::lsecir::CriticalPerSevere>()              = 0.1;
+    parameters_lct.get<mio::lsecir::DeathsPerCritical>()              = 0.1;
+
+    ScalarType total_confirmed_cases = 341223;
+    ScalarType deaths                = 9710;
+    ScalarType total_population      = 83155031.0;
+
+    // Add time points for initialization of transitions.
+    mio::TimeSeries<ScalarType> init((int)mio::lsecir::InfectionTransition::Count);
+    mio::TimeSeries<ScalarType>::Vector vec_init((int)mio::lsecir::InfectionTransition::Count);
+    vec_init[(int)mio::lsecir::InfectionTransition::SusceptibleToExposed]                 = 25.0;
+    vec_init[(int)mio::lsecir::InfectionTransition::ExposedToInfectedNoSymptoms]          = 15.0;
+    vec_init[(int)mio::lsecir::InfectionTransition::InfectedNoSymptomsToInfectedSymptoms] = 8.0;
+    vec_init[(int)mio::lsecir::InfectionTransition::InfectedNoSymptomsToRecovered]        = 4.0;
+    vec_init[(int)mio::lsecir::InfectionTransition::InfectedSymptomsToInfectedSevere]     = 1.0;
+    vec_init[(int)mio::lsecir::InfectionTransition::InfectedSymptomsToRecovered]          = 4.0;
+    vec_init[(int)mio::lsecir::InfectionTransition::InfectedSevereToInfectedCritical]     = 1.0;
+    vec_init[(int)mio::lsecir::InfectionTransition::InfectedSevereToRecovered]            = 1.0;
+    vec_init[(int)mio::lsecir::InfectionTransition::InfectedCriticalToDead]               = 1.0;
+    vec_init[(int)mio::lsecir::InfectionTransition::InfectedCriticalToRecovered]          = 1.0;
+    // Add initial time point to time series.
+    init.add_time_point(-130, vec_init);
+    // Add further time points until time 0.
+    while (init.get_last_time() < 0) {
+        vec_init *= 1.01;
+        init.add_time_point(init.get_last_time() + dt, vec_init);
+    }
+
+    // Calculate initial vector and compare with previous reult.
+    mio::lsecir::Initializer initializer(std::move(init), InfState, std::move(parameters_lct));
+    auto init_compartments = initializer.compute_initializationvector(total_population, deaths, total_confirmed_cases);
+
+    for (int i = 0; i < InfState.get_count(); i++) {
+        EXPECT_NEAR(init_compartments[i], compare[i], 1e-4) << "at subcompartment number " << i;
+    }
+}
+
+// Check if the constraints of the Initializer are validated as expected.
+TEST(TestInitializer, testConstraints)
+{
+    // Deactivate temporarily log output for next tests.
+    mio::set_log_level(mio::LogLevel::off);
+
+    ScalarType dt = 0.5;
+
+    // Define number of subcompartments.
+    std::vector<int> SubcompartmentNumbers((int)mio::lsecir::InfectionStateBase::Count, 1);
+    SubcompartmentNumbers[(int)mio::lsecir::InfectionStateBase::Exposed]            = 2;
+    SubcompartmentNumbers[(int)mio::lsecir::InfectionStateBase::InfectedNoSymptoms] = 3;
+    SubcompartmentNumbers[(int)mio::lsecir::InfectionStateBase::InfectedSymptoms]   = 2;
+    SubcompartmentNumbers[(int)mio::lsecir::InfectionStateBase::InfectedSevere]     = 3;
+    SubcompartmentNumbers[(int)mio::lsecir::InfectionStateBase::InfectedCritical]   = 2;
+    mio::lsecir::InfectionState InfState(SubcompartmentNumbers);
+
+    // Check wrong size of initial flows.
+    mio::TimeSeries<ScalarType> init_wrong_size((int)mio::lsecir::InfectionTransition::Count - 1);
+    Eigen::VectorXd vec_wrong_size = Eigen::VectorXd::Ones((int)mio::lsecir::InfectionTransition::Count - 1);
+    init_wrong_size.add_time_point(-10, vec_wrong_size);
+    init_wrong_size.add_time_point(-9, vec_wrong_size);
+
+    mio::lsecir::Initializer initializer_init_wrong_size(std::move(init_wrong_size), InfState);
+
+    bool constraint_check = initializer_init_wrong_size.check_constraints();
+    EXPECT_TRUE(constraint_check);
+
+    // Check if last time of initial flows is not zero.
+    mio::TimeSeries<ScalarType> init_wrong((int)mio::lsecir::InfectionTransition::Count);
+    Eigen::VectorXd vec_init = Eigen::VectorXd::Ones((int)mio::lsecir::InfectionTransition::Count);
+    init_wrong.add_time_point(-10, vec_init);
+    init_wrong.add_time_point(-9, vec_init);
+
+    mio::TimeSeries<ScalarType> init_copy(init_wrong);
+    mio::lsecir::Initializer initializer_init_wrong_last_time(std::move(init_copy), InfState);
+
+    constraint_check = initializer_init_wrong_last_time.check_constraints();
+    EXPECT_TRUE(constraint_check);
+
+    // Check if time steps of initial flows are not equidistant.
+    init_wrong.add_time_point(init_wrong.get_last_time() + 1., vec_init);
+    while (init_wrong.get_last_time() < 0) {
+        init_wrong.add_time_point(init_wrong.get_last_time() + dt, vec_init);
+    }
+
+    mio::lsecir::Initializer initializer_init_wrong_equidistant(std::move(init_wrong), InfState);
+
+    constraint_check = initializer_init_wrong_equidistant.check_constraints();
+    EXPECT_TRUE(constraint_check);
+
+    // Check large step size.
+    mio::TimeSeries<ScalarType> init_wrong_step((int)mio::lsecir::InfectionTransition::Count);
+    init_wrong_step.add_time_point(-10, vec_init);
+    init_wrong_step.add_time_point(init_wrong_step.get_last_time() + 2., vec_init);
+    while (init_wrong_step.get_last_time() < 0) {
+        init_wrong_step.add_time_point(init_wrong_step.get_last_time() + dt, vec_init);
+    }
+
+    mio::lsecir::Initializer initializer_init_wrong_step(std::move(init_wrong_step), InfState);
+
+    constraint_check = initializer_init_wrong_step.check_constraints();
+    EXPECT_TRUE(constraint_check);
+
+    // Check with correct flows.
+    mio::TimeSeries<ScalarType> init_right((int)mio::lsecir::InfectionTransition::Count);
+    init_right.add_time_point(-10, vec_init);
+    while (init_right.get_last_time() < 0) {
+        init_right.add_time_point(init_right.get_last_time() + dt, vec_init);
+    }
+
+    mio::lsecir::Initializer initializer_right(std::move(init_right), InfState);
+
+    constraint_check = initializer_right.check_constraints();
+    EXPECT_FALSE(constraint_check);
+
+    // Check with too short time period of initial data. The time period above was long enough.
+    mio::TimeSeries<ScalarType> init_short((int)mio::lsecir::InfectionTransition::Count);
+    init_short.add_time_point(-1., vec_init);
+    while (init_short.get_last_time() < 0) {
+        init_short.add_time_point(init_short.get_last_time() + dt, vec_init);
+    }
+
+    mio::lsecir::Initializer initializer_init_short(std::move(init_short), InfState);
+    ScalarType total_confirmed_cases = 341223;
+    ScalarType deaths                = 9710;
+    ScalarType total_population      = 83155031.0;
+    auto initialconditions =
+        initializer_init_short.compute_initializationvector(total_population, deaths, total_confirmed_cases);
+
+    for (int i = 2; i < InfState.get_count() - 2; i++) {
+        EXPECT_EQ(-1, initialconditions[i]);
+    }
+
+    // Reactive log output.
+    mio::set_log_level(mio::LogLevel::warn);
+}
