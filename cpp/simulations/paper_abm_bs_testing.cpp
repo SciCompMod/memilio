706d8808a23977bd8657080f90d89a5bfa1e08b4
DavidKerkmann <44698825+DavidKerkmann@users.noreply.github.com>
2024-02-26 13:08:49 +0100

    Add location contact matrices.
    Signed-off-by: DavidKerkmann <44698825+DavidKerkmann@users.noreply.github.com>

--- cpp/simulations/paper_abm_bs_testing.cpp
+++ cpp/simulations/paper_abm_bs_testing.cpp
@@ -468,34 +468,211 @@ void set_parameters(mio::abm::Parameters& params)
 void set_local_parameters(mio::abm::World& world)
 {
     const int n_age_groups = world.parameters.get_num_groups();
-    // const Eigen::MatrixXd contacts_home{
-    //     {0.4413, 0.4504, 1.2383, 0.8033, 0.0494, 0.0017}, {0.0485, 0.7616, 0.6532, 1.1614, 0.0256, 0.0013},
-    //     {0.1800, 0.1795, 0.8806, 0.6413, 0.0429, 0.0032}, {0.0495, 0.2639, 0.5189, 0.8277, 0.0679, 0.0014},
-    //     {0.0087, 0.0394, 0.1417, 0.3834, 0.7064, 0.0447}, {0.0292, 0.0648, 0.1248, 0.4179, 0.3497, 0.1544}};
 
-    // Eigen::MatrixXi a{
-    //     // construct a 2x2 matrix
-    //     {1, 2}, // first row
-    //     {3, 4} // second row
-    // };
+    // setting this up in matrix-form would be much nicer,
+    // but we somehow can't construct Eigen object with initializer lists
+    /* baseline_home
+        0.4413 0.4504 1.2383 0.8033 0.0494 0.0017
+        0.0485 0.7616 0.6532 1.1614 0.0256 0.0013
+        0.1800 0.1795 0.8806 0.6413 0.0429 0.0032
+        0.0495 0.2639 0.5189 0.8277 0.0679 0.0014
+        0.0087 0.0394 0.1417 0.3834 0.7064 0.0447
+        0.0292 0.0648 0.1248 0.4179 0.3497 0.1544
+    */
+    mio::CustomIndexArray<ScalarType, mio::AgeGroup, mio::AgeGroup> contacts_home(
+        {mio::AgeGroup(n_age_groups), mio::AgeGroup(n_age_groups)}, 0.);
+    contacts_home[{age_group_0_to_4, age_group_0_to_4}]     = 0.4413;
+    contacts_home[{age_group_0_to_4, age_group_5_to_14}]    = 0.0504;
+    contacts_home[{age_group_0_to_4, age_group_15_to_34}]   = 1.2383;
+    contacts_home[{age_group_0_to_4, age_group_35_to_59}]   = 0.8033;
+    contacts_home[{age_group_0_to_4, age_group_60_to_79}]   = 0.0494;
+    contacts_home[{age_group_0_to_4, age_group_80_plus}]    = 0.0017;
+    contacts_home[{age_group_5_to_14, age_group_0_to_4}]    = 0.0485;
+    contacts_home[{age_group_5_to_14, age_group_5_to_14}]   = 0.7616;
+    contacts_home[{age_group_5_to_14, age_group_15_to_34}]  = 0.6532;
+    contacts_home[{age_group_5_to_14, age_group_35_to_59}]  = 1.1614;
+    contacts_home[{age_group_5_to_14, age_group_60_to_79}]  = 0.0256;
+    contacts_home[{age_group_5_to_14, age_group_80_plus}]   = 0.0013;
+    contacts_home[{age_group_15_to_34, age_group_0_to_4}]   = 0.1800;
+    contacts_home[{age_group_15_to_34, age_group_5_to_14}]  = 0.1795;
+    contacts_home[{age_group_15_to_34, age_group_15_to_34}] = 0.8806;
+    contacts_home[{age_group_15_to_34, age_group_35_to_59}] = 0.6413;
+    contacts_home[{age_group_15_to_34, age_group_60_to_79}] = 0.0429;
+    contacts_home[{age_group_15_to_34, age_group_80_plus}]  = 0.0032;
+    contacts_home[{age_group_35_to_59, age_group_0_to_4}]   = 0.0495;
+    contacts_home[{age_group_35_to_59, age_group_5_to_14}]  = 0.2639;
+    contacts_home[{age_group_35_to_59, age_group_15_to_34}] = 0.5189;
+    contacts_home[{age_group_35_to_59, age_group_35_to_59}] = 0.8277;
+    contacts_home[{age_group_35_to_59, age_group_60_to_79}] = 0.0679;
+    contacts_home[{age_group_35_to_59, age_group_80_plus}]  = 0.0014;
+    contacts_home[{age_group_60_to_79, age_group_0_to_4}]   = 0.0087;
+    contacts_home[{age_group_60_to_79, age_group_5_to_14}]  = 0.0394;
+    contacts_home[{age_group_60_to_79, age_group_15_to_34}] = 0.1417;
+    contacts_home[{age_group_60_to_79, age_group_35_to_59}] = 0.3834;
+    contacts_home[{age_group_60_to_79, age_group_60_to_79}] = 0.7064;
+    contacts_home[{age_group_60_to_79, age_group_80_plus}]  = 0.0447;
+    contacts_home[{age_group_80_plus, age_group_0_to_4}]    = 0.0292;
+    contacts_home[{age_group_80_plus, age_group_5_to_14}]   = 0.0648;
+    contacts_home[{age_group_80_plus, age_group_15_to_34}]  = 0.1248;
+    contacts_home[{age_group_80_plus, age_group_35_to_59}]  = 0.4179;
+    contacts_home[{age_group_80_plus, age_group_60_to_79}]  = 0.3497;
+    contacts_home[{age_group_80_plus, age_group_80_plus}]   = 0.1544;
 
-    Eigen::Matrix<int, 2, 3> b{
-        {2, 3, 4},
-        {5, 6, 7},
-    };
+    /* baseline_school
+        1.1165 0.2741 0.2235 0.1028 0.0007 0.0000
+        0.1627 1.9412 0.2431 0.1780 0.0130 0.0000
+        0.0148 0.1646 1.1266 0.0923 0.0074 0.0000
+        0.0367 0.1843 0.3265 0.0502 0.0021 0.0005
+        0.0004 0.0370 0.0115 0.0014 0.0039 0.0000
+        0.0000 0.0000 0.0000 0.0000 0.0000 0.0000
+    */
+    mio::CustomIndexArray<ScalarType, mio::AgeGroup, mio::AgeGroup> contacts_school(
+        {mio::AgeGroup(n_age_groups), mio::AgeGroup(n_age_groups)}, 0.);
+    contacts_school[{age_group_0_to_4, age_group_0_to_4}]     = 1.1165;
+    contacts_school[{age_group_0_to_4, age_group_5_to_14}]    = 0.2741;
+    contacts_school[{age_group_0_to_4, age_group_15_to_34}]   = 0.2235;
+    contacts_school[{age_group_0_to_4, age_group_35_to_59}]   = 0.1028;
+    contacts_school[{age_group_0_to_4, age_group_60_to_79}]   = 0.0007;
+    contacts_school[{age_group_0_to_4, age_group_80_plus}]    = 0.0000;
+    contacts_school[{age_group_5_to_14, age_group_0_to_4}]    = 0.1627;
+    contacts_school[{age_group_5_to_14, age_group_5_to_14}]   = 1.9412;
+    contacts_school[{age_group_5_to_14, age_group_15_to_34}]  = 0.2431;
+    contacts_school[{age_group_5_to_14, age_group_35_to_59}]  = 0.1780;
+    contacts_school[{age_group_5_to_14, age_group_60_to_79}]  = 0.0130;
+    contacts_school[{age_group_5_to_14, age_group_80_plus}]   = 0.0000;
+    contacts_school[{age_group_15_to_34, age_group_0_to_4}]   = 0.0148;
+    contacts_school[{age_group_15_to_34, age_group_5_to_14}]  = 0.1646;
+    contacts_school[{age_group_15_to_34, age_group_15_to_34}] = 1.1266;
+    contacts_school[{age_group_15_to_34, age_group_35_to_59}] = 0.0923;
+    contacts_school[{age_group_15_to_34, age_group_60_to_79}] = 0.0074;
+    contacts_school[{age_group_15_to_34, age_group_80_plus}]  = 0.0000;
+    contacts_school[{age_group_35_to_59, age_group_0_to_4}]   = 0.0367;
+    contacts_school[{age_group_35_to_59, age_group_5_to_14}]  = 0.1843;
+    contacts_school[{age_group_35_to_59, age_group_15_to_34}] = 0.3265;
+    contacts_school[{age_group_35_to_59, age_group_35_to_59}] = 0.0502;
+    contacts_school[{age_group_35_to_59, age_group_60_to_79}] = 0.0021;
+    contacts_school[{age_group_35_to_59, age_group_80_plus}]  = 0.0005;
+    contacts_school[{age_group_60_to_79, age_group_0_to_4}]   = 0.0004;
+    contacts_school[{age_group_60_to_79, age_group_5_to_14}]  = 0.0370;
+    contacts_school[{age_group_60_to_79, age_group_15_to_34}] = 0.0115;
+    contacts_school[{age_group_60_to_79, age_group_35_to_59}] = 0.0014;
+    contacts_school[{age_group_60_to_79, age_group_60_to_79}] = 0.0039;
+    contacts_school[{age_group_60_to_79, age_group_80_plus}]  = 0.0000;
+    contacts_school[{age_group_80_plus, age_group_0_to_4}]    = 0.0000;
+    contacts_school[{age_group_80_plus, age_group_5_to_14}]   = 0.0000;
+    contacts_school[{age_group_80_plus, age_group_15_to_34}]  = 0.0000;
+    contacts_school[{age_group_80_plus, age_group_35_to_59}]  = 0.0000;
+    contacts_school[{age_group_80_plus, age_group_60_to_79}]  = 0.0000;
+    contacts_school[{age_group_80_plus, age_group_80_plus}]   = 0.0000;
 
-    const Eigen::Matrix<double, 1, 1> asb{1, 1.0};
+    /* baseline_work
+        0.0000 0.0000 0.0000 0.0000 0.0000 0.0000
+        0.0000 0.0000 0.0000 0.0000 0.0000 0.0000
+        0.0000 0.0127 1.7570 1.6050 0.0133 0.0000
+        0.0000 0.0020 1.0311 2.3166 0.0098 0.0000
+        0.0000 0.0002 0.0194 0.0325 0.0003 0.0000
+        0.0000 0.0000 0.0000 0.0000 0.0000 0.0000
+    */
+    mio::CustomIndexArray<ScalarType, mio::AgeGroup, mio::AgeGroup> contacts_work(
+        {mio::AgeGroup(n_age_groups), mio::AgeGroup(n_age_groups)}, 0.);
+    contacts_work[{age_group_0_to_4, age_group_0_to_4}]     = 0.0000;
+    contacts_work[{age_group_0_to_4, age_group_5_to_14}]    = 0.0000;
+    contacts_work[{age_group_0_to_4, age_group_15_to_34}]   = 0.0000;
+    contacts_work[{age_group_0_to_4, age_group_35_to_59}]   = 0.0000;
+    contacts_work[{age_group_0_to_4, age_group_60_to_79}]   = 0.0000;
+    contacts_work[{age_group_0_to_4, age_group_80_plus}]    = 0.0000;
+    contacts_work[{age_group_5_to_14, age_group_0_to_4}]    = 0.0000;
+    contacts_work[{age_group_5_to_14, age_group_5_to_14}]   = 0.0000;
+    contacts_work[{age_group_5_to_14, age_group_15_to_34}]  = 0.0000;
+    contacts_work[{age_group_5_to_14, age_group_35_to_59}]  = 0.0000;
+    contacts_work[{age_group_5_to_14, age_group_60_to_79}]  = 0.0000;
+    contacts_work[{age_group_5_to_14, age_group_80_plus}]   = 0.0000;
+    contacts_work[{age_group_15_to_34, age_group_0_to_4}]   = 0.0000;
+    contacts_work[{age_group_15_to_34, age_group_5_to_14}]  = 0.0127;
+    contacts_work[{age_group_15_to_34, age_group_15_to_34}] = 1.7570;
+    contacts_work[{age_group_15_to_34, age_group_35_to_59}] = 1.6050;
+    contacts_work[{age_group_15_to_34, age_group_60_to_79}] = 0.0133;
+    contacts_work[{age_group_15_to_34, age_group_80_plus}]  = 0.0000;
+    contacts_work[{age_group_35_to_59, age_group_0_to_4}]   = 0.0000;
+    contacts_work[{age_group_35_to_59, age_group_5_to_14}]  = 0.0020;
+    contacts_work[{age_group_35_to_59, age_group_15_to_34}] = 1.0311;
+    contacts_work[{age_group_35_to_59, age_group_35_to_59}] = 2.3166;
+    contacts_work[{age_group_35_to_59, age_group_60_to_79}] = 0.0098;
+    contacts_work[{age_group_35_to_59, age_group_80_plus}]  = 0.0000;
+    contacts_work[{age_group_60_to_79, age_group_0_to_4}]   = 0.0000;
+    contacts_work[{age_group_60_to_79, age_group_5_to_14}]  = 0.0002;
+    contacts_work[{age_group_60_to_79, age_group_15_to_34}] = 0.0194;
+    contacts_work[{age_group_60_to_79, age_group_35_to_59}] = 0.0325;
+    contacts_work[{age_group_60_to_79, age_group_60_to_79}] = 0.0003;
+    contacts_work[{age_group_60_to_79, age_group_80_plus}]  = 0.0000;
+    contacts_work[{age_group_80_plus, age_group_0_to_4}]    = 0.0000;
+    contacts_work[{age_group_80_plus, age_group_5_to_14}]   = 0.0000;
+    contacts_work[{age_group_80_plus, age_group_15_to_34}]  = 0.0000;
+    contacts_work[{age_group_80_plus, age_group_35_to_59}]  = 0.0000;
+    contacts_work[{age_group_80_plus, age_group_60_to_79}]  = 0.0000;
+    contacts_work[{age_group_80_plus, age_group_80_plus}]   = 0.0000;
+
+    /* baseline_other
+        0.5170 0.3997 0.7957 0.9958 0.3239 0.0428
+        0.0632 0.9121 0.3254 0.4731 0.2355 0.0148
+        0.0336 0.1604 1.7529 0.8622 0.1440 0.0077
+        0.0204 0.1444 0.5738 1.2127 0.3433 0.0178
+        0.0371 0.0393 0.4171 0.9666 0.7495 0.0257
+        0.0791 0.0800 0.3480 0.5588 0.2769 0.0180
+
+    */
+    mio::CustomIndexArray<ScalarType, mio::AgeGroup, mio::AgeGroup> contacts_other(
+        {mio::AgeGroup(n_age_groups), mio::AgeGroup(n_age_groups)}, 0.);
+    contacts_other[{age_group_0_to_4, age_group_0_to_4}]     = 0.5170;
+    contacts_other[{age_group_0_to_4, age_group_5_to_14}]    = 0.3997;
+    contacts_other[{age_group_0_to_4, age_group_15_to_34}]   = 0.7957;
+    contacts_other[{age_group_0_to_4, age_group_35_to_59}]   = 0.9958;
+    contacts_other[{age_group_0_to_4, age_group_60_to_79}]   = 0.3239;
+    contacts_other[{age_group_0_to_4, age_group_80_plus}]    = 0.0428;
+    contacts_other[{age_group_5_to_14, age_group_0_to_4}]    = 0.0632;
+    contacts_other[{age_group_5_to_14, age_group_5_to_14}]   = 0.9121;
+    contacts_other[{age_group_5_to_14, age_group_15_to_34}]  = 0.3254;
+    contacts_other[{age_group_5_to_14, age_group_35_to_59}]  = 0.4731;
+    contacts_other[{age_group_5_to_14, age_group_60_to_79}]  = 0.2355;
+    contacts_other[{age_group_5_to_14, age_group_80_plus}]   = 0.0148;
+    contacts_other[{age_group_15_to_34, age_group_0_to_4}]   = 0.0336;
+    contacts_other[{age_group_15_to_34, age_group_5_to_14}]  = 0.1604;
+    contacts_other[{age_group_15_to_34, age_group_15_to_34}] = 1.7529;
+    contacts_other[{age_group_15_to_34, age_group_35_to_59}] = 0.8622;
+    contacts_other[{age_group_15_to_34, age_group_60_to_79}] = 0.1440;
+    contacts_other[{age_group_15_to_34, age_group_80_plus}]  = 0.0077;
+    contacts_other[{age_group_35_to_59, age_group_0_to_4}]   = 0.0204;
+    contacts_other[{age_group_35_to_59, age_group_5_to_14}]  = 0.1444;
+    contacts_other[{age_group_35_to_59, age_group_15_to_34}] = 0.5738;
+    contacts_other[{age_group_35_to_59, age_group_35_to_59}] = 1.2127;
+    contacts_other[{age_group_35_to_59, age_group_60_to_79}] = 0.3433;
+    contacts_other[{age_group_35_to_59, age_group_80_plus}]  = 0.0178;
+    contacts_other[{age_group_60_to_79, age_group_0_to_4}]   = 0.0371;
+    contacts_other[{age_group_60_to_79, age_group_5_to_14}]  = 0.0393;
+    contacts_other[{age_group_60_to_79, age_group_15_to_34}] = 0.4171;
+    contacts_other[{age_group_60_to_79, age_group_35_to_59}] = 0.9666;
+    contacts_other[{age_group_60_to_79, age_group_60_to_79}] = 0.7495;
+    contacts_other[{age_group_60_to_79, age_group_80_plus}]  = 0.0257;
+    contacts_other[{age_group_80_plus, age_group_0_to_4}]    = 0.0791;
+    contacts_other[{age_group_80_plus, age_group_5_to_14}]   = 0.0800;
+    contacts_other[{age_group_80_plus, age_group_15_to_34}]  = 0.3480;
+    contacts_other[{age_group_80_plus, age_group_35_to_59}]  = 0.5588;
+    contacts_other[{age_group_80_plus, age_group_60_to_79}]  = 0.2769;
+    contacts_other[{age_group_80_plus, age_group_80_plus}]   = 0.0180;
 
     for (auto& loc : world.get_locations()) {
         switch (loc.get_type()) {
         case mio::abm::LocationType::Home:
-            loc.get_infection_parameters().get<mio::abm::ContactRates>().array() = contacts_home;
+            loc.get_infection_parameters().get<mio::abm::ContactRates>() = contacts_home;
             break;
         case mio::abm::LocationType::School:
+            loc.get_infection_parameters().get<mio::abm::ContactRates>() = contacts_work;
             break;
         case mio::abm::LocationType::Work:
+            loc.get_infection_parameters().get<mio::abm::ContactRates>() = contacts_work;
             break;
         default:
+            loc.get_infection_parameters().get<mio::abm::ContactRates>() = contacts_other;
             break;
         }
     }
