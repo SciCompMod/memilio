ac1c59f1fd50aebe9c3cb583578ba0c036622370
HenrZu <69154294+HenrZu@users.noreply.github.com>
2024-04-03 12:32:51 +0200

    print results generation

--- pycode/memilio-generation/memilio/generation_test/test_oseir_generation.py
+++ pycode/memilio-generation/memilio/generation_test/test_oseir_generation.py
@@ -82,6 +82,10 @@ class TestOseirGeneration(unittest.TestCase):
         with open(os.path.join(irdata.target_folder, "test_oseir.py")) as result:
             self.assertEqual(result.read(), self.expected_test_oseir_py)
         with open(os.path.join(irdata.target_folder, "test_oseir.cpp")) as result:
+            print("------- read -------------")
+            print(result.read())
+            print("------- expected -------------")
+            print(self.expected_test_oseir_cpp)
             self.assertEqual(result.read(), self.expected_test_oseir_cpp)
 
     def test_wrong_model_name(self):
