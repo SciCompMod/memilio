4f620092c5528bbec62e5913d00bb3466f391cbb
Agatha Schmidt <agatha.schmidt@hotmail.de>
2024-04-10 20:42:28 +0200

    add GNN activation-optimizer plots

--- pycode/machine-learning/model/GNN/grid_search_analysis_2024.py
+++ pycode/machine-learning/model/GNN/grid_search_analysis_2024.py
@@ -1,15 +1,12 @@
 import pandas as pd 
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib.lines import Line2D
-import matplotlib as mpl
 import re
 
-import matplotlib.gridspec as gridspec
 
 
 
-df_XENET = pd.read_csv('/home/schm_a45/Documents/Code/memilio/memilio/pycode/machine-learning/dataframe_hyperparameters_we/dataframe_XENEtConv_Baseline_noCV.csv')
+#df_XENET = pd.read_csv('/home/schm_a45/Documents/Code/memilio/memilio/pycode/machine-learning/dataframe_hyperparameters_we/dataframe_XENEtConv_Baseline_noCV.csv')
 df_part1 = pd.read_csv('/home/schm_a45/Documents/Code/memilio/memilio/pycode/machine-learning/dataframe_gridsearch_2024/dataframe_grid_search_networkarchitecture_GNN_type1.csv')
 df_part2 = pd.read_csv('/home/schm_a45/Documents/Code/memilio/memilio/pycode/machine-learning/dataframe_gridsearch_2024/dataframe_grid_searc_networkarchitecture_GNN_type1_part2.csv')
 df_combined = pd.concat([df_part1 ,df_part2], ignore_index = True )
@@ -81,7 +78,7 @@ def heatmaps(df):
 
     for ax, df_heatmap, name  in zip(axs.flat, [df_1 ,df_2, df_3, df_4, df_5], layers):
         
-        im = ax.imshow(df_heatmap.values, cmap ='Blues_r' )
+        im = ax.imshow(df_heatmap.values, cmap ='summer' )
 
         # Show all ticks and label them with the respective list entries
         ax.set_xticks(np.arange(len(df_heatmap.columns)), labels=df_heatmap.columns)
@@ -110,7 +107,7 @@ def heatmaps(df):
     
     #plt.rcParams.update({'font.size': 25})
     plt.show()
-    plt.savefig("GNN1_layers_channels.png")
+    plt.savefig("GNN1_layers_channels_summer.png")
 
 
 def barplot_baseline(df, df_GSC):
@@ -231,7 +228,7 @@ def boxplot_layers(df_combined):
     df = df_combined
     plt.figure().clf()   
 
-    df_1 = pd.DataFrame(data =  df.loc[(df['layer'] == 'GCNConv')][['number_of_layers', 'channels', 'kfold_test']])
+    df_1 = pd.DataFrame(data =  df.loc[(df['layer'] == 'GCNConv')][['optimizer', 'activation', 'kfold_test']])
     df_1= df_1.pivot(index='number_of_layers', columns='channels', values='kfold_test')
     
     df_2 = pd.DataFrame(data =  df.loc[(df['layer'] == 'ARMAConv')][['number_of_layers', 'channels', 'kfold_test']])
@@ -266,3 +263,106 @@ def boxplot_layers(df_combined):
     fig.delaxes(ax6)
 
     plt.savefig("boxplot_GNN_layers.png")
+
+
+def heatmaps_act_opt(df):
+    
+
+    df_1 = pd.DataFrame(data =  df.loc[(df['layer'] == 'GCNConv') & (df['number_of_layers']==2)& (df['channels']==512)][['optimizer', 'activation', 'kfold_test']])
+    df_1= df_1.pivot(index='optimizer', columns='activation', values='kfold_test')
+    
+    df_2 = pd.DataFrame(data =  df.loc[(df['layer'] == 'ARMAConv')&(df['number_of_layers']==3)& (df['channels']==128)][['optimizer', 'activation', 'kfold_test']])
+    df_2= df_2.pivot(index='optimizer', columns='activation', values='kfold_test')
+    
+    df_3 = pd.DataFrame(data =  df.loc[(df['layer'] == 'APPNPConv')&(df['number_of_layers']==3)& (df['channels']==512)][['optimizer', 'activation', 'kfold_test']])
+    df_3= df_3.pivot(index='optimizer', columns='activation', values='kfold_test')
+    
+    df_4 = pd.DataFrame(data =  df.loc[(df['layer'] == 'GATConv')&(df['number_of_layers']==3)& (df['channels']==1024)][['optimizer', 'activation', 'kfold_test']])
+    df_4= df_4.pivot(index='optimizer', columns='activation', values='kfold_test')
+        
+    df_5 = pd.DataFrame(data =  df.loc[(df['layer'] == 'GCSConv')&(df['number_of_layers']==3)& (df['channels']==512)][['optimizer', 'activation', 'kfold_test']])
+    df_5= df_5.pivot(index='optimizer', columns='activation', values='kfold_test')
+
+
+    plt.figure().clf() 
+    layers = df['layer'].unique()
+
+
+
+    fig, axs = plt.subplots(nrows = 3, ncols = 2, sharex=False, figsize = (7,9), constrained_layout = True)
+
+
+
+    for ax, df_heatmap, name  in zip(axs.flat, [df_1 ,df_2, df_3, df_4, df_5], layers):
+        
+        im = ax.imshow(df_heatmap.values, cmap ='Blues_r' )
+
+        # Show all ticks and label them with the respective list entries
+        ax.set_xticks(np.arange(len(df_heatmap.columns)), labels=df_heatmap.columns)
+        ax.set_yticks(np.arange(len(df_heatmap.index)), labels=df_heatmap.index)
+
+        ax.set_ylabel('optimizer')
+        ax.set_xlabel('activation')
+
+        # Rotate the tick labels and set their alignment.
+        plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
+                rotation_mode="anchor")
+
+        # Loop over data dimensions and create text annotations.
+        for i in range(len(df_heatmap.index)):
+            for j in range(len(df_heatmap.columns)):
+                text = ax.text(j, i, np.around(df_heatmap.values, decimals=2)[i, j],
+                            ha="center", va="center", color="k")
+                
+        ax.set_title('Model = '+name) 
+
+
+
+    fig.colorbar(im, ax = axs, shrink=0.75, label = 'Test MAPE')
+    
+    fig.delaxes(axs[2][1])
+    
+    #plt.rcParams.update({'font.size': 25})
+    plt.show()
+    plt.savefig("GNN1_optimizer_activation.png")
+
+
+def ARMA_heatmap():
+    df = pd.read_csv('/home/schm_a45/Documents/code3/memilio/pycode/machine-learning/dataframe_gridsearch_2024/ARMAConv_act_opt_run2.csv')        
+        
+    optimizer = []
+    for i in df['optimizer']:
+        optimizer.append(re.sub('[^a-zA-Z]+', '', i.split(' ')[0].split('.')[-1]))
+
+    df['optimizer'] = optimizer
+    df_heatmap1 = pd.DataFrame(data = df[['activation', 'optimizer', 'kfold_test']])
+    df_heatmap1= df_heatmap1.pivot(index='activation', columns='optimizer', values='kfold_test').transpose()
+    #df_heatmap1 = df_heatmap1[['elu', 'linear', 'relu']]
+
+    fig, ax = plt.subplots()
+    im = ax.imshow(df_heatmap1.values , cmap = 'summer_r')
+
+    # Show all ticks and label them with the respective list entries
+    ax.set_xticks(np.arange(len(df_heatmap1.columns)), labels=df_heatmap1.columns)
+    ax.set_yticks(np.arange(len(df_heatmap1.index)), labels=df_heatmap1.index)
+
+    # Rotate the tick labels and set their alignment.
+    plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
+            rotation_mode="anchor")
+
+    # Loop over data dimensions and create text annotations.
+    for i in range(len(df_heatmap1.index)):
+        for j in range(len(df_heatmap1.columns)):
+            text = ax.text(j, i, np.around(df_heatmap1.values, decimals=2)[i, j],
+                        ha="center", va="center", color="black")
+
+
+    cbar_kw = {}        
+    cbar = ax.figure.colorbar(im, ax=ax, **cbar_kw)
+    cbar.ax.set_ylabel('MAPE', rotation=-90, va="bottom")        
+
+
+    #ax.set_title("Activation function and optimizer for best ARMAConv architechture")
+    fig.tight_layout()
+    plt.show()
+    plt.savefig("ARMA_heatmap_activation_optimizer_run2.png")
