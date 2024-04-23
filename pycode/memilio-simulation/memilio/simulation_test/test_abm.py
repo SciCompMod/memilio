fbad78f822b754f861a715a5b54cdf28d99021b3
Khoa Nguyen <4763945+khoanguyen-dev@users.noreply.github.com>
2024-04-23 16:12:51 +0200

    Modify TestingScheme in Pycode to include TestParameters instead of TestType

--- pycode/memilio-simulation/memilio/simulation_test/test_abm.py
+++ pycode/memilio-simulation/memilio/simulation_test/test_abm.py
@@ -58,7 +58,7 @@ class TestAbm(unittest.TestCase):
         testing_crit = abm.TestingCriteria(
             testing_ages, testing_inf_states)
         testing_scheme = abm.TestingScheme(testing_crit, abm.days(
-            1), t0, t0 + abm.days(1), abm.AntigenTest(), 1.0)
+            1), t0, t0 + abm.days(1), abm.TestParameters(1.0,1.0), 1.0)
         # initially false, will only active once simulation starts
         self.assertEqual(testing_scheme.active, False)
 
