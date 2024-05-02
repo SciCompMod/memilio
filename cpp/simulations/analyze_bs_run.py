35a402f157ff970652108698ca42d396db51fe8b
Khoa Nguyen <4763945+khoanguyen-dev@users.noreply.github.com>
2024-05-02 14:25:11 +0200

    Change plot_infection_states_individual() to show single y-axis + change plot_infection_per_location_type() to show multiple subplots

--- cpp/simulations/analyze_bs_run.py
+++ cpp/simulations/analyze_bs_run.py
@@ -54,7 +54,7 @@ def main(path, n_runs):
             plot_infection_per_location_type(df)
         if file.startswith("infection_per_age_group.txt"):
             df = pd.read_csv(file_path, delim_whitespace=True)
-            #plot_infection_per_age_group(df)
+            # plot_infection_per_age_group(df)
         # if file.startswith("run_"):
             # convert to numpy array
         #    df_np = df.to_numpy()
@@ -64,15 +64,28 @@ def main(path, n_runs):
 
 
 def plot_infection_per_location_type(df):
-    df.plot(x='Time', y=['Home', 'Work', 'School', 'SocialEvent', 'BasicsShop', 'Hospital',
-            'ICU', 'Car', 'PublicTransport', 'TransportWithoutContact', 'Cemetery'], figsize=(10, 6))
+    # df.plot.area(x='Time', y=['Home', 'Work', 'School', 'SocialEvent', 'BasicsShop'], stacked=True, figsize=(10, 6))
+
+    fig, axs = plt.subplots(5, 2, constrained_layout=True)
+    df.plot(x='Time', y='Home', color='tab:blue', ax=axs[0, 0])
+    df.plot(x='Time', y='Work', color='tab:green', ax=axs[0, 1])
+    df.plot(x='Time', y='School', color='tab:red', ax=axs[1, 0])
+    df.plot(x='Time', y='SocialEvent', color='tab:orange', ax=axs[1, 1])
+    df.plot(x='Time', y='BasicsShop', color='tab:purple', ax=axs[2, 0])
+    df.plot(x='Time', y='Hospital', color='tab:brown', ax=axs[2, 1])
+    df.plot(x='Time', y='ICU', color='tab:pink', ax=axs[3, 0])
+    df.plot(x='Time', y='Car', color='tab:gray', ax=axs[3, 1])
+    df.plot(x='Time', y='PublicTransport', color='tab:olive', ax=axs[4, 0])
+    df.plot(x='Time', y='Cemetery', color='tab:cyan', ax=axs[4, 1])
     plt.show()
 
 
 def plot_infection_per_age_group(df):
     df.plot(x='Time', y=['0_to_4', '5_to_14', '15_to_34',
             '35_to_59', '60_to_79', '80_plus'], figsize=(10, 6))
+
     plt.show()
+
 
 def plot_results(path):
     # median / 50-percentile
@@ -131,67 +144,47 @@ def plot_infection_states(x, y50, y25, y75):
     plt.legend(legend_plot)
 
     for i in states_plot:
-       plt.fill_between(x, y50[:, i], y25[:, i],
-                        alpha=0.5, color=color_plot[i])
-       plt.fill_between(x, y50[:, i], y75[:, i],
-                        alpha=0.5, color=color_plot[i])
+        plt.fill_between(x, y50[:, i], y25[:, i],
+                         alpha=0.5, color=color_plot[i])
+        plt.fill_between(x, y50[:, i], y75[:, i],
+                         alpha=0.5, color=color_plot[i])
 
 
 def plot_infection_states_individual(x, y50, y25, y75, y_real):
     # plt.figure('Infection_states_dead')
     # # plt.plot(x, y50[:,[5,7]])
-    x_real = np.linspace(0, y_real.shape[0]-1,y_real.shape[0]) 
+    x_real = np.linspace(0, y_real.shape[0]-1, y_real.shape[0])
     # plt.plot(x_real, y_real[:,[2]])
     # plt.legend(['I_Crit', 'Dead'])
 
     fig, ax = plt.subplots(3, 1)
 
     # Severe
-    color = 'tab:red'
     ax[0].set_xlabel('time (s)')
-    ax[0].set_ylabel('y50', color=color)
-    ax[0].plot(x, y50[:, [4]], color=color)
-    ax[0].tick_params(axis='y', labelcolor=color)
-    # instantiate a second axes that shares the same x-axis
-    ax2 = ax[0].twinx()
-    color = 'tab:blue'
-    # we already handled the x-label with ax1
-    ax2.set_ylabel('y_real', color=color)
-    ax2.plot(x_real, y_real[:, [6]], color=color)
-    ax2.tick_params(axis='y', labelcolor=color)
-    plt.title('Severe')
+    ax[0].plot(x, y50[:, [4]], color='tab:red', label='y50')
+    ax[0].plot(x_real, y_real[:, [6]], color='tab:blue', label='y_real')
+    ax[0].tick_params(axis='y')
+    ax[0].title.set_text('Severe')
+    ax[0].legend()
     fig.tight_layout()  # otherwise the right y-label is slightly clipped
 
     # Critical
-    color = 'tab:red'
     ax[1].set_xlabel('time (s)')
-    ax[1].set_ylabel('y50', color=color)
-    ax[1].plot(x, y50[:, [5]], color=color)
-    ax[1].tick_params(axis='y', labelcolor=color)
-    # instantiate a second axes that shares the same x-axis
-    ax2 = ax[1].twinx()
-    color = 'tab:blue'
-    # we already handled the x-label with ax1
-    ax2.set_ylabel('y_real', color=color)
-    ax2.plot(x_real, y_real[:, [7]], color=color)
-    ax2.tick_params(axis='y', labelcolor=color)
-    plt.title('Critical')
+    ax[1].plot(x, y50[:, [5]], color='tab:red', label='y50')
+    ax[1].plot(x_real, y_real[:, [7]], color='tab:blue', label='y_real')
+    ax[1].tick_params(axis='y')
+    ax[1].title.set_text('Critical')
+    ax[1].legend()
     fig.tight_layout()  # otherwise the right y-label is slightly clipped
 
     # Dead
-    color = 'tab:red'
     ax[2].set_xlabel('time (s)')
-    ax[2].set_ylabel('y50', color=color)
-    ax[2].plot(x, y50[:, [7]], color=color)
-    ax[2].tick_params(axis='y', labelcolor=color)
-    # instantiate a second axes that shares the same x-axis
-    ax2 = ax[2].twinx()
-    color = 'tab:blue'
-    ax2.set_ylabel('y_real', color=color)  # we already handled the x-label with ax1
-    ax2.plot(x_real, y_real[:, [9]], color=color)
-    ax2.tick_params(axis='y', labelcolor=color)
-    plt.title('Dead')
+    ax[2].plot(x, y50[:, [7]], color='tab:red', label='y50')
+    ax[2].plot(x_real, y_real[:, [9]], color='tab:blue', label='y_real')
+    ax[2].tick_params(axis='y')
+    ax[2].title.set_text('Dead')
     fig.tight_layout()  # otherwise the right y-label is slightly clipped
+    ax[2].legend()
     plt.show()
 
 
@@ -220,15 +213,15 @@ def plot_infections_per_age_group(path):
 def plot_mean_and_std(Y):
 
     x_plot = Y[:, 0, 0]
-    compartments = Y[:,1:,1:]
+    compartments = Y[:, 1:, 1:]
     # average value
-    compartments_avg = np.mean(compartments,axis=2)
-    #plot average
+    compartments_avg = np.mean(compartments, axis=2)
+    # plot average
     for i in range(compartments_avg.shape[1]):
-        plt.plot(x_plot,compartments_avg[:,i])
-    
-    #plt.plot(x_plot,compartments_avg)
-    #legend
+        plt.plot(x_plot, compartments_avg[:, i])
+
+    # plt.plot(x_plot,compartments_avg)
+    # legend
     plt.legend(['S', 'E', 'I_NS', 'I_Sy', 'I_Sev', 'I_Crit', 'R', 'D'])
     plt.show()
     # standard deviation
@@ -239,8 +232,8 @@ def plot_mean_and_std(Y):
 
 
 if __name__ == "__main__":
-    #path to results
-    path = "/Users/David/Documents/HZI/memilio/data/results"
+    # path to results
+    path = "./results"
     if (len(sys.argv) > 1):
         n_runs = sys.argv[1]
     else:
