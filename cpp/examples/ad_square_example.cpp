194abe7b1f5f5c3314cb1f39de43a4d5aaedb7b7
Ralf Hannemann-Tamas <r.hannemann-tamas@fz-juelich.de>
2024-05-02 14:26:20 +0200

    improve comments

--- cpp/examples/ad_square_example.cpp
+++ cpp/examples/ad_square_example.cpp
@@ -29,30 +29,35 @@ FP square(FP x)
 
 int main()
 {
+    /**** Forward AD ****/
     std::cout << "Forward AD" << std::endl;
-    ad::gt1s<double>::type t1_x;
-    ad::value(t1_x)             = 3.0;
-    ad::derivative(t1_x)        = 1.0;
+    ad::gt1s<double>::type t1_x; // tangent-linear AD type
+    ad::value(t1_x)             = 3.0; // set value (this corresponds to the non-AD part)
+    ad::derivative(t1_x)        = 1.0; // set tangent-linear derivative seed
     ad::gt1s<double>::type t1_y = square(t1_x);
     std::cout << "value of square(" << ad::value(t1_x) << ") is " << ad::value(t1_y) << std::endl;
     std::cout << "forward derivative of square(" << ad::value(t1_x) << ") is " << ad::derivative(t1_y) << std::endl;
 
+    /**** Reverse AD ****/
     std::cout << "Reverse AD" << std::endl;
-    // create tape
+    // create tape (allocation)
     if (!ad::ga1s<double>::global_tape)
         ad::ga1s<double>::global_tape = ad::ga1s<double>::tape_t::create();
+    // clear tape
     ad::ga1s<double>::global_tape->reset();
-    ad::ga1s<double>::type a1_x;
-    ad::value(a1_x)      = 3.0;
-    ad::derivative(a1_x) = 0.0;
-    ad::ga1s<double>::global_tape->register_variable(a1_x);
+
+    ad::ga1s<double>::type a1_x; // reverse-mode AD type
+    ad::value(a1_x)      = 3.0; // set value (this corresponds to the non-AD part)
+    ad::derivative(a1_x) = 0.0; // for reverse-mode the seed has to be set to zero
+    ad::ga1s<double>::global_tape->register_variable(a1_x); // register input variable
     ad::ga1s<double>::type a1_y = square(a1_x);
     std::cout << "value of square(" << ad::value(a1_x) << ") is " << ad::value(t1_y) << std::endl;
     ad::ga1s<double>::global_tape->register_output_variable(a1_y);
     ad::derivative(a1_y) = 1.0;
-    ad::ga1s<double>::global_tape->interpret_adjoint();
-    std::cout << "adjoint derivative is " << ad::derivative(a1_x) << std::endl;
-    ad::ga1s<double>::tape_t::remove(ad::ga1s<double>::global_tape);
+    ad::ga1s<double>::global_tape
+        ->interpret_adjoint(); // compute reverse-mode derivatives by evaluatin the tape backwards
+    std::cout << "adjoint derivative is " << ad::derivative(a1_x) << std::endl; // access reverse-derivatives
+    ad::ga1s<double>::tape_t::remove(ad::ga1s<double>::global_tape); // deallocate tape
 
     return 0;
 }
