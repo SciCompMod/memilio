f0191b6422d0ca3941504fbbc94c503df8492426
Paul Johannssen <138109402+johapau@users.noreply.github.com>
2024-05-02 10:42:42 +0200

    905 add age group resolution to seir and sir model (#911)
    - Integration of age groups into the seir and sir models
    - New examples using the age groups
    
    Co-authored-by: HenrZu <69154294+HenrZu@users.noreply.github.com>
    Co-authored-by: jubicker <113909589+jubicker@users.noreply.github.com>

--- cpp/models/ode_sir/model.h
+++ cpp/models/ode_sir/model.h
@@ -22,6 +22,7 @@
 #define ODESIR_MODEL_H
 
 #include "memilio/compartments/compartmentalmodel.h"
+#include "memilio/epidemiology/age_group.h"
 #include "memilio/epidemiology/populations.h"
 #include "memilio/epidemiology/contact_matrix.h"
 #include "ode_sir/infection_state.h"
@@ -36,30 +37,48 @@ namespace osir
     * define the model *
     ********************/
 
-class Model : public CompartmentalModel<InfectionState, Populations<InfectionState>, Parameters>
+class Model : public CompartmentalModel<InfectionState, Populations<AgeGroup, InfectionState>, Parameters>
 {
-    using Base = CompartmentalModel<InfectionState, mio::Populations<InfectionState>, Parameters>;
+    using Base = CompartmentalModel<InfectionState, mio::Populations<AgeGroup, InfectionState>, Parameters>;
 
 public:
-    Model()
-        : Base(Populations({InfectionState::Count}, 0.), ParameterSet())
+    Model(int num_agegroups)
+        : Base(Populations({AgeGroup(num_agegroups), InfectionState::Count}), ParameterSet(AgeGroup(num_agegroups)))
     {
     }
 
     void get_derivatives(Eigen::Ref<const Eigen::VectorXd> pop, Eigen::Ref<const Eigen::VectorXd> y, double t,
                          Eigen::Ref<Eigen::VectorXd> dydt) const override
     {
-        auto& params     = this->parameters;
-        double coeffStoI = params.get<ContactPatterns>().get_matrix_at(t)(0, 0) *
-                           params.get<TransmissionProbabilityOnContact>() / populations.get_total();
+        auto params                              = this->parameters;
+        AgeGroup n_agegroups                     = params.get_num_groups();
+        ContactMatrixGroup const& contact_matrix = params.get<ContactPatterns>();
 
-        dydt[(size_t)InfectionState::Susceptible] =
-            -coeffStoI * y[(size_t)InfectionState::Susceptible] * pop[(size_t)InfectionState::Infected];
-        dydt[(size_t)InfectionState::Infected] =
-            coeffStoI * y[(size_t)InfectionState::Susceptible] * pop[(size_t)InfectionState::Infected] -
-            (1.0 / params.get<TimeInfected>()) * y[(size_t)InfectionState::Infected];
-        dydt[(size_t)InfectionState::Recovered] =
-            (1.0 / params.get<TimeInfected>()) * y[(size_t)InfectionState::Infected];
+        for (auto i = AgeGroup(0); i < n_agegroups; i++) {
+
+            size_t Si = this->populations.get_flat_index({i, InfectionState::Susceptible});
+            size_t Ii = this->populations.get_flat_index({i, InfectionState::Infected});
+            size_t Ri = this->populations.get_flat_index({i, InfectionState::Recovered});
+
+            for (auto j = AgeGroup(0); j < n_agegroups; j++) {
+
+                size_t Sj = this->populations.get_flat_index({j, InfectionState::Susceptible});
+                size_t Ij = this->populations.get_flat_index({j, InfectionState::Infected});
+                size_t Rj = this->populations.get_flat_index({j, InfectionState::Recovered});
+
+                double Nj = pop[Sj] + pop[Ij] + pop[Rj];
+
+                double coeffStoI = contact_matrix.get_matrix_at(t)(static_cast<Eigen::Index>((size_t)i),
+                                                                   static_cast<Eigen::Index>((size_t)j)) *
+                                   params.get<TransmissionProbabilityOnContact>()[i] / Nj;
+
+                dydt[Si] += -coeffStoI * y[Si] * pop[Ij];
+                dydt[Ii] += coeffStoI * y[Si] * pop[Ij];
+            }
+            dydt[Ii] -= (1.0 / params.get<TimeInfected>()[i]) * y[Ii];
+            dydt[Ri] = (1.0 / params.get<TimeInfected>()[i]) * y[Ii];
+
+        }
     }
 };
 
