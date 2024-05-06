dd1a09afa64893fc0e2ca945818c86947a163ec2
Ralf Hannemann-Tamas <ralf.ht@gmail.com>
2024-05-06 10:52:48 +0200

    improve doc

--- cpp/examples/ad_square_example.cpp
+++ cpp/examples/ad_square_example.cpp
@@ -21,6 +21,45 @@
 #include "ad/ad.hpp"
 #include <iostream>
 
+/*  This example computes the derivative of f(x) = x^2 in two different ways:
+ *  1. Using the forward mode of algorithmic differentiation (AD)
+ *  2. Using the reverse mode of algorithmic differentiation 
+ *  
+ *  Assume, a subroutine is given that computes y = f(x), where x is an n-dimenstional input vector
+ *  and y is an m-dimensional output vector. 
+ *  
+ *  Forward mode AD (also known as tangent-linear mode)
+ *  computes Jacobian-vector products of f(x). I.e., if J(x) denotes the Jacobian of f(x) and
+ *  v is an n-dimensional seed vector, the forward mode computes the matrix-vector product
+ *  J(x) * v. To this end, The data type of x is set to a forward mode AD data type that additionally to the
+ *  original value, also stores derivative information. The original value is accessed by the command
+ *  ad::value(x). The derivative information is accessed by ad::derivative(x) which is accordingly
+ *  used to set the seed vector v, i.e., ad::derivative(x) = v.
+ *  After the conmputation the Jacobian-vector product is accessed by the command ad::derivative(y).
+ *  
+ *  Reverse mode (also known as adjoint mode, or back progpagation) computes vector-Jacobian products of f(x).
+ *  I..e, if if J(x) denotes the Jacobian of f(x) and
+ *  w is an m-dimensional seed vector (which is a row vector), the revose mode computes the  vector-matrix product
+ *  w * J(x). The data type of x is set to a reverse mode AD data type that additionally to the
+ *  original value, also stores derivative information. The original value is accessed by the command
+ *  ad::value(x). The derivative information is accessed by ad::derivative(x).
+ *  While the forward mode computes derivatives in one sweep, the reverse mode computes is divided
+ *  into two sweeps. A forward sweep for the computation, where intermediate results are stored on a tape. After
+ *  the forward sweep the seed of the compuation is set by the commad ad::derivative(y) = w.
+ *  And a reverse sweep, where the actual derivative compuation has to be performed. The derivative information of
+ *  x has to be initalized to zero (ad::derivative(x) = 0), before the tape can be intepreted backwards. Then, 
+ *  the tape can interpreted backwards
+ *  and the vector-Jacobian product can be accessed the command ad::derivative(x).
+ *  
+ *  For an concise introduction to AD we recommend the article: 
+ *  Verma, Arun. "An introduction to automatic differentiation." Current Science (2000): 804-807.
+ */
+
+/**
+ * @brief square computes the square of the input parameter
+ * @param x input parameter
+ * @return the square of x.
+ */
 template <typename FP>
 FP square(FP x)
 {
@@ -29,7 +68,7 @@ FP square(FP x)
 
 int main()
 {
-    /**** Forward AD ****/
+    /**** 1. Forward AD ****/
     std::cout << "Forward AD" << std::endl;
     ad::gt1s<double>::type t1_x; // tangent-linear AD type
     ad::value(t1_x)             = 3.0; // set value (this corresponds to the non-AD part)
@@ -38,7 +77,7 @@ int main()
     std::cout << "value of square(" << ad::value(t1_x) << ") is " << ad::value(t1_y) << std::endl;
     std::cout << "forward derivative of square(" << ad::value(t1_x) << ") is " << ad::derivative(t1_y) << std::endl;
 
-    /**** Reverse AD ****/
+    /**** 2. Reverse AD ****/
     std::cout << "Reverse AD" << std::endl;
     // create tape (allocation)
     if (!ad::ga1s<double>::global_tape)
