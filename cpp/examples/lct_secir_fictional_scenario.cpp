823727d6f54890c0e497c9756dfd1fa1a5a6c8c1
lenaploetzke <70579874+lenaploetzke@users.noreply.github.com>
2024-05-15 09:59:48 +0200

    added simulation for IDE model

--- cpp/examples/lct_secir_fictional_scenario.cpp
+++ cpp/examples/lct_secir_fictional_scenario.cpp
@@ -131,102 +131,92 @@ mio::TimeSeries<ScalarType> get_initial_flows()
     return init;
 }
 
-/*mio::IOResult<void> simulate_ide_model(ScalarType R0, int num_subcompartments = 3, std::string save_dir = "",
-                                       ScalarType tmax = 10)
+mio::IOResult<void> simulate_ide_model(ScalarType R0, std::string save_dir = "", ScalarType tmax = 10)
 {
-    ScalarType dt_flows         = 0.1;
-    ScalarType total_population = 83155031.0;
-    //--------- IDE model ------------
     // Initialize model.
-    mio::isecir::Model model_ide(std::move(init), total_population, deaths, total_confirmed_cases);
+    mio::isecir::Model model_ide(std::move(get_initial_flows()), simulation_parameter["total_population"],
+                                 simulation_parameter["deaths"], simulation_parameter["total_confirmed_cases"]);
 
     // Set working parameters.
     // Set TransitionDistributions.
     mio::ConstantFunction initialfunc(0);
     mio::StateAgeFunctionWrapper delaydistributioninit(initialfunc);
-    std::vector<mio::StateAgeFunctionWrapper> vec_delaydistrib(num_transitions, delaydistributioninit);
+    std::vector<mio::StateAgeFunctionWrapper> vec_delaydistrib((int)mio::isecir::InfectionTransition::Count,
+                                                               delaydistributioninit);
 
-    mio::ExponentialDecay expdecayInfectedSevereToInfectedCritical(1. / 9.36);
+    mio::ExponentialSurvivalFunction expInfectedSevereToInfectedCritical(1. / 9.36);
     vec_delaydistrib[(int)mio::isecir::InfectionTransition::InfectedSevereToInfectedCritical].set_state_age_function(
-        expdecayInfectedSevereToInfectedCritical);
+        expInfectedSevereToInfectedCritical);
 
     mio::LognormSurvivalFunction lognInfectedSevereToRecovered(0.76, -0.45, 9.41);
     vec_delaydistrib[(int)mio::isecir::InfectionTransition::InfectedSevereToRecovered].set_state_age_function(
         lognInfectedSevereToRecovered);
 
-    mio::ExponentialDecay expdecayInfectedCriticalToDeath(1. / 14.88, 1);
+    mio::ExponentialSurvivalFunction expInfectedCriticalToDeath(1. / 14.88, 1);
     vec_delaydistrib[(int)mio::isecir::InfectionTransition::InfectedCriticalToDead].set_state_age_function(
-        expdecayInfectedCriticalToDeath);
-    expdecayInfectedCriticalToDeath.set_parameter(1 / 16.92);
+        expInfectedCriticalToDeath);
+    expInfectedCriticalToDeath.set_distribution_parameter(1 / 16.92);
     vec_delaydistrib[(int)mio::isecir::InfectionTransition::InfectedCriticalToRecovered].set_state_age_function(
-        expdecayInfectedCriticalToDeath);
+        expInfectedCriticalToDeath);
 
-    mio::GammaSurvivalFunction erlangExposedToInfectedNoSymptoms(
-        vec_subcompartments[(int)mio::lsecir::InfectionStateBase::Exposed], 0,
-        3.335 / vec_subcompartments[(int)mio::lsecir::InfectionStateBase::Exposed]);
+    mio::GammaSurvivalFunction erlangExposedToInfectedNoSymptoms(num_subcompartments, 0, 3.335 / num_subcompartments);
     vec_delaydistrib[(int)mio::isecir::InfectionTransition::ExposedToInfectedNoSymptoms].set_state_age_function(
         erlangExposedToInfectedNoSymptoms);
 
-    mio::GammaSurvivalFunction erlangInfectedNoSymptomsToInfectedSymptoms(
-        vec_subcompartments[(int)mio::lsecir::InfectionStateBase::InfectedNoSymptoms], 0,
-        1.865 / vec_subcompartments[(int)mio::lsecir::InfectionStateBase::InfectedNoSymptoms]);
+    mio::GammaSurvivalFunction erlangInfectedNoSymptomsToInfectedSymptoms(num_subcompartments, 0,
+                                                                          1.865 / num_subcompartments);
     vec_delaydistrib[(int)mio::isecir::InfectionTransition::InfectedNoSymptomsToInfectedSymptoms]
         .set_state_age_function(erlangInfectedNoSymptomsToInfectedSymptoms);
-    erlangInfectedNoSymptomsToInfectedSymptoms.set_scale(
-        8.865 / vec_subcompartments[(int)mio::lsecir::InfectionStateBase::InfectedNoSymptoms]);
+    erlangInfectedNoSymptomsToInfectedSymptoms.set_scale(8.865 / num_subcompartments);
     vec_delaydistrib[(int)mio::isecir::InfectionTransition::InfectedNoSymptomsToRecovered].set_state_age_function(
         erlangInfectedNoSymptomsToInfectedSymptoms);
 
-    mio::GammaSurvivalFunction erlangInfectedSymptomsToInfectedSevere(
-        vec_subcompartments[(int)mio::lsecir::InfectionStateBase::InfectedSymptoms], 0,
-        6.30662 / vec_subcompartments[(int)mio::lsecir::InfectionStateBase::InfectedSymptoms]);
+    mio::GammaSurvivalFunction erlangInfectedSymptomsToInfectedSevere(num_subcompartments, 0,
+                                                                      6.30662 / num_subcompartments);
     vec_delaydistrib[(int)mio::isecir::InfectionTransition::InfectedSymptomsToInfectedSevere].set_state_age_function(
         erlangInfectedSymptomsToInfectedSevere);
-    erlangInfectedSymptomsToInfectedSevere.set_scale(
-        7. / vec_subcompartments[(int)mio::lsecir::InfectionStateBase::InfectedSymptoms]);
+    erlangInfectedSymptomsToInfectedSevere.set_scale(7. / num_subcompartments);
     vec_delaydistrib[(int)mio::isecir::InfectionTransition::InfectedSymptomsToRecovered].set_state_age_function(
         erlangInfectedSymptomsToInfectedSevere);
 
     model_ide.parameters.set<mio::isecir::TransitionDistributions>(vec_delaydistrib);
 
     // Set other parameters.
-    std::vector<ScalarType> vec_prob((int)mio::isecir::InfectionTransition::Count, 0.5);
-    vec_prob[Eigen::Index(mio::isecir::InfectionTransition::SusceptibleToExposed)]        = 1;
-    vec_prob[Eigen::Index(mio::isecir::InfectionTransition::ExposedToInfectedNoSymptoms)] = 1;
+    std::vector<ScalarType> vec_prob((int)mio::isecir::InfectionTransition::Count, 1.);
     vec_prob[Eigen::Index(mio::isecir::InfectionTransition::InfectedNoSymptomsToInfectedSymptoms)] =
-        1 - parameters_lct.get<mio::lsecir::RecoveredPerInfectedNoSymptoms>();
+        1 - simulation_parameter["RecoveredPerInfectedNoSymptoms"];
     vec_prob[Eigen::Index(mio::isecir::InfectionTransition::InfectedNoSymptomsToRecovered)] =
-        parameters_lct.get<mio::lsecir::RecoveredPerInfectedNoSymptoms>();
+        simulation_parameter["RecoveredPerInfectedNoSymptoms"];
     vec_prob[Eigen::Index(mio::isecir::InfectionTransition::InfectedSymptomsToInfectedSevere)] =
-        parameters_lct.get<mio::lsecir::SeverePerInfectedSymptoms>();
+        simulation_parameter["SeverePerInfectedSymptoms"];
     vec_prob[Eigen::Index(mio::isecir::InfectionTransition::InfectedSymptomsToRecovered)] =
-        1 - parameters_lct.get<mio::lsecir::SeverePerInfectedSymptoms>();
+        1 - simulation_parameter["SeverePerInfectedSymptoms"];
     vec_prob[Eigen::Index(mio::isecir::InfectionTransition::InfectedSevereToInfectedCritical)] =
-        parameters_lct.get<mio::lsecir::CriticalPerSevere>();
+        simulation_parameter["CriticalPerSevere"];
     vec_prob[Eigen::Index(mio::isecir::InfectionTransition::InfectedSevereToRecovered)] =
-        1 - parameters_lct.get<mio::lsecir::CriticalPerSevere>();
+        1 - simulation_parameter["CriticalPerSevere"];
     vec_prob[Eigen::Index(mio::isecir::InfectionTransition::InfectedCriticalToDead)] =
-        parameters_lct.get<mio::lsecir::DeathsPerCritical>();
+        simulation_parameter["DeathsPerCritical"];
     vec_prob[Eigen::Index(mio::isecir::InfectionTransition::InfectedCriticalToRecovered)] =
-        1 - parameters_lct.get<mio::lsecir::DeathsPerCritical>();
+        1 - simulation_parameter["DeathsPerCritical"];
 
     model_ide.parameters.set<mio::isecir::TransitionProbabilities>(vec_prob);
 
-    model_ide.parameters.get<mio::isecir::ContactPatterns>() = mio::UncertainContactMatrix(contact_matrix);
+    model_ide.parameters.get<mio::isecir::ContactPatterns>() = get_contact_matrix(R0);
 
-    mio::ConstantFunction constfunc(parameters_lct.get<mio::lsecir::TransmissionProbabilityOnContact>());
+    mio::ConstantFunction constfunc(simulation_parameter["TransmissionProbabilityOnContact"]);
     mio::StateAgeFunctionWrapper StateAgeFunctionWrapperide(constfunc);
     model_ide.parameters.set<mio::isecir::TransmissionProbabilityOnContact>(StateAgeFunctionWrapperide);
-    StateAgeFunctionWrapperide.set_parameter(parameters_lct.get<mio::lsecir::RelativeTransmissionNoSymptoms>());
+    StateAgeFunctionWrapperide.set_distribution_parameter(simulation_parameter["RelativeTransmissionNoSymptoms"]);
     model_ide.parameters.set<mio::isecir::RelativeTransmissionNoSymptoms>(StateAgeFunctionWrapperide);
-    StateAgeFunctionWrapperide.set_parameter(parameters_lct.get<mio::lsecir::RiskOfInfectionFromSymptomatic>());
+    StateAgeFunctionWrapperide.set_distribution_parameter(simulation_parameter["RiskOfInfectionFromSymptomatic"]);
     model_ide.parameters.set<mio::isecir::RiskOfInfectionFromSymptomatic>(StateAgeFunctionWrapperide);
 
     model_ide.set_tol_for_support_max(1e-6);
-    model_ide.check_constraints(dt_flows);
+    model_ide.check_constraints(simulation_parameter["dt_flows"]);
 
     // Simulate.
-    mio::isecir::Simulation sim(model_ide, 0, dt_flows);
+    mio::isecir::Simulation sim(model_ide, simulation_parameter["dt_flows"]);
     sim.advance(tmax);
 
     if (!save_dir.empty()) {
@@ -239,7 +229,8 @@ mio::TimeSeries<ScalarType> get_initial_flows()
         filename_ide                           = filename_ide + ".h5";
         mio::IOResult<void> save_result_status = mio::save_result({sim.get_result()}, {0}, 1, filename_ide);
     }
-}*/
+    return mio::success();
+}
 
 /** 
 * @brief Perform a fictive simulation with realistic parameters and contacts, such that the reproduction number 
@@ -298,7 +289,7 @@ mio::IOResult<void> simulate_lct_model(ScalarType R0, std::string save_dir = "",
 
     // Perform simulation.
     mio::TimeSeries<ScalarType> result = mio::lsecir::simulate(
-        0, tmax, 0.5, model,
+        0, tmax, 0.1, model,
         std::make_shared<mio::ControlledStepperWrapper<boost::numeric::odeint::runge_kutta_cash_karp54>>(1e-10, 1e-5, 0,
                                                                                                          0.1));
     // Calculate result without division in subcompartments.
@@ -331,10 +322,22 @@ int main()
         return -1;
     }
 
+    result = simulate_ide_model(R0, save_dir, 150);
+    if (!result) {
+        printf("%s\n", result.error().formatted_message().c_str());
+        return -1;
+    }
+
     R0       = 4.;
     save_dir = "../../data/simulation_lct/riseR04long/";
 
     result = simulate_lct_model(R0, save_dir, 75);
+    if (!result) {
+        printf("%s\n", result.error().formatted_message().c_str());
+        return -1;
+    }
+
+    result = simulate_ide_model(R0, save_dir, 75);
     if (!result) {
         printf("%s\n", result.error().formatted_message().c_str());
         return -1;
