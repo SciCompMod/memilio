754d8716a7a40294da72580c8129b5fff9d1fea6
Sascha Korf <51127093+xsaschako@users.noreply.github.com>
2024-06-18 14:22:00 +0200

    enable openmp again, ups
    Signed-off-by: Sascha Korf <51127093+xsaschako@users.noreply.github.com>

--- cpp/models/abm/world.cpp
+++ cpp/models/abm/world.cpp
@@ -65,7 +65,7 @@ void World::evolve(TimePoint t, TimeSpan dt)
 
 void World::interaction(TimePoint t, TimeSpan dt)
 {
-    // PRAGMA_OMP(parallel for)
+    PRAGMA_OMP(parallel for)
     for (auto i = size_t(0); i < m_persons.size(); ++i) {
         auto&& person     = m_persons[i];
         auto personal_rng = Person::RandomNumberGenerator(m_rng, *person);
@@ -75,7 +75,7 @@ void World::interaction(TimePoint t, TimeSpan dt)
 
 void World::migration(TimePoint t, TimeSpan dt)
 {
-    // PRAGMA_OMP(parallel for)
+    PRAGMA_OMP(parallel for)
     for (auto i = size_t(0); i < m_persons.size(); ++i) {
         auto&& person     = m_persons[i];
         auto personal_rng = Person::RandomNumberGenerator(m_rng, *person);
@@ -158,7 +158,7 @@ void World::migration(TimePoint t, TimeSpan dt)
 void World::begin_step(TimePoint t, TimeSpan dt)
 {
     m_testing_strategy.update_activity_status(t);
-    // PRAGMA_OMP(parallel for)
+    PRAGMA_OMP(parallel for)
     for (auto i = size_t(0); i < m_locations.size(); ++i) {
         auto&& location = m_locations[i];
         location->adjust_contact_rates(parameters.get_num_groups());
