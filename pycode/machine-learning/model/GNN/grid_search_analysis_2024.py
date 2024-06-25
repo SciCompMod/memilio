538702106948219a13a22e3208629a4bb49f8536
Agatha Schmidt <agatha.schmidt@hotmail.de>
2024-06-25 10:03:41 +0200

    grid search for graohODE with CV and without CV as well as figures

--- pycode/machine-learning/model/GNN/grid_search_analysis_2024.py
+++ pycode/machine-learning/model/GNN/grid_search_analysis_2024.py
@@ -1,11 +1,16 @@
 import pandas as pd 
 import numpy as np
 import matplotlib.pyplot as plt
+import pickle
 import re
+from matplotlib.colors import ListedColormap
+from sklearn.model_selection import KFold
+import seaborn as sns
+from matplotlib.lines import Line2D
 
 
-
-
+df_2024 = pd.read_csv('/home/schm_a45/Documents/Code/memilio/memilio/pycode/machine-learning/dataframe_hyperparameters_2024/dataframe_hyperparameter_tuning_30days_oldrange_noCV.csv')
+df_ARMA = pd.read_csv('/home/schm_a45/Documents/Code/memilio/memilio/pycode/machine-learning/dataframe_hyperparameters_2024/dataframe_ARMAConv_30days_oldrange_noCV.csv')
 #df_XENET = pd.read_csv('/home/schm_a45/Documents/Code/memilio/memilio/pycode/machine-learning/dataframe_hyperparameters_we/dataframe_XENEtConv_Baseline_noCV.csv')
 df_part1 = pd.read_csv('/home/schm_a45/Documents/Code/memilio/memilio/pycode/machine-learning/dataframe_gridsearch_2024/dataframe_grid_search_networkarchitecture_GNN_type1.csv')
 df_part2 = pd.read_csv('/home/schm_a45/Documents/Code/memilio/memilio/pycode/machine-learning/dataframe_gridsearch_2024/dataframe_grid_searc_networkarchitecture_GNN_type1_part2.csv')
@@ -364,3 +369,383 @@ def ARMA_heatmap():
     fig.tight_layout()
     plt.show()
     plt.savefig("ARMA_heatmap_activation_optimizer_run2.png")
+
+
+def grid_search_2014(df_2024):
+    df = df_2024
+    layers = []
+    for i in df['layer']:
+        layers.append(re.sub('[^a-zA-Z]+', '', i.split('.')[-1]))
+
+    df['layer'] = layers
+    df['kfold_test'] = df['kfold_test'].astype('float')
+
+    df_1 = pd.DataFrame(data =  df.loc[(df['layer'] == 'GCNConv')][['number_of_layers', 'number_of_neurons', 'kfold_test']])
+    df_1= df_1.pivot(index='number_of_layers', columns='number_of_neurons', values='kfold_test')
+    
+    df_2 = pd.DataFrame(data =  df.loc[(df['layer'] == 'ARMAConv')][['number_of_layers', 'number_of_neurons', 'kfold_test']])
+    df_2= df_2.pivot(index='number_of_layers', columns='number_of_neurons', values='kfold_test')
+    
+    df_3 = pd.DataFrame(data =  df.loc[(df['layer'] == 'APPNPConv')][['number_of_layers', 'number_of_neurons', 'kfold_test']])
+    df_3= df_3.pivot(index='number_of_layers', columns='number_of_neurons', values='kfold_test')
+    
+    df_4 = pd.DataFrame(data =  df.loc[(df['layer'] == 'GATConv')][['number_of_layers', 'number_of_neurons', 'kfold_test']])
+    df_4= df_4.pivot(index='number_of_layers', columns='number_of_neurons', values='kfold_test')
+
+
+    plt.figure().clf() 
+    layers = df['layer'].unique()
+
+
+    fig, axs = plt.subplots(nrows = 2, ncols = 2, sharex=False, figsize = (7,7), constrained_layout = True)
+
+    for ax, df_heatmap, name  in zip(axs.flat, [df_1 ,df_2, df_3, df_4], layers):
+        
+        im = ax.imshow(df_heatmap.values, cmap ='summer_r' )
+
+        # Show all ticks and label them with the respective list entries
+        ax.set_xticks(np.arange(len(df_heatmap.columns)), labels=df_heatmap.columns)
+        ax.set_yticks(np.arange(len(df_heatmap.index)), labels=df_heatmap.index)
+
+        ax.set_ylabel('number of layers')
+        ax.set_xlabel('channels')
+
+        # Rotate the tick labels and set their alignment.
+        plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
+                rotation_mode="anchor")
+
+        # Loop over data dimensions and create text annotations.
+        for i in range(len(df_heatmap.index)):
+            for j in range(len(df_heatmap.columns)):
+                text = ax.text(j, i, np.around(df_heatmap.values, decimals=2)[i, j],
+                            ha="center", va="center", color="k")
+                
+        ax.set_title('Model = '+name) 
+
+
+
+    fig.colorbar(im, ax = axs, shrink=0.75, label = 'MAPE')
+    
+    #plt.rcParams.update({'font.size': 25})
+    plt.show()
+    plt.savefig("GNN1_layers_channels_2024.png")
+
+def heatmap_ARMAConv():
+    df_ARMA_1 = pd.read_csv('/home/schm_a45/Documents/Code/memilio/memilio/pycode/machine-learning/dataframe_hyperparameters_2024/dataframe_ARMAConv_30days_oldrange_noCV.csv')
+    df_ARMA_2 = pd.read_csv('/home/schm_a45/Documents/Code/memilio/memilio/pycode/machine-learning/dataframe_hyperparameters_2024/dataframe_ARMAConv_30days_oldrange_noCV_part2.csv')
+    df = pd.concat([df_ARMA_1, df_ARMA_2])
+
+    df_plot = pd.DataFrame(data =  df[['number_of_layers', 'number_of_neurons', 'kfold_test']])
+    df_plot = df_plot.pivot(index='number_of_layers', columns='number_of_neurons', values='kfold_test')
+    
+    fig, ax = plt.subplots()
+    im = ax.imshow(df_plot.values, cmap ='summer_r')
+
+    # Show all ticks and label them with the respective list entries
+    ax.set_xticks(np.arange(len(df_plot.columns)), labels=df_plot.columns)
+    ax.set_yticks(np.arange(len(df_plot.index)), labels=df_plot.index)
+    ax.set_ylabel('number of layers')
+    ax.set_xlabel('channels')
+
+    # Rotate the tick labels and set their alignment.
+    plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
+            rotation_mode="anchor")
+
+    # Loop over data dimensions and create text annotations.
+    for i in range(len(df_plot.index)):
+        for j in range(len(df_plot.columns)):
+            text = ax.text(j, i, np.around(df_plot.values, decimals=3)[i, j],
+                        ha="center", va="center", color="black", fontsize = 14 )
+
+    cbar_kw = {}        
+    cbar = ax.figure.colorbar(im, ax=ax, **cbar_kw)
+    cbar.ax.set_ylabel('MAPE', rotation=-90, va="bottom")    
+
+    ax.set_title("")
+    fig.tight_layout()
+    plt.show()
+    plt.savefig("ARNMAConv_enhanced_gridsearch.png")
+
+
+def plot_data_differentsplits():
+    
+        file = open('/home/schm_a45/Documents/Code/memilio/memilio/pycode/machine-learning/data_GNN_nodamp_400pop_30days_2024_oldrange/data_secir_age_groups.pickle', 'rb')
+        data_secir = pickle.load(file)
+
+        kf = KFold(n_splits=5)
+        train_idxs = []
+        test_idxs = []
+        for i, (train_index, test_index) in enumerate(kf.split(data_secir['labels'][0])):
+
+            train_idxs.append(train_index)
+            test_idxs.append(test_index)
+
+        first_test_split = data_secir['labels'][0][test_idxs[0]]
+        last_test_split = data_secir['labels'][0][test_idxs[4]]
+
+        first_array = np.asarray(first_test_split.transpose().reshape(48,-1))
+        all_comp_first = []
+        index = [0,1,2,3,4,5,6,7]
+        for i in index :
+            comparray = first_array[i::8]
+            all_comp_first.append(comparray.flatten())
+
+        last_array = np.asarray(last_test_split.transpose().reshape(48,-1))
+        all_comp_last = []
+        index = [0,1,2,3,4,5,6,7]
+        for i in index :
+            comparray = last_array[i::8]
+            all_comp_last.append(comparray.flatten())
+
+        infectionstates = ['Susceptible','Exposed', 'InfectedNoSymptoms', 'InfectedSymptoms', 'InfectedSevere', 'InfectedCritical', 'Receovered', 'Dead']
+
+        #index = [str(compartment).split('.')[1] for compartment in  infectionstates ]
+       
+        fig, ((ax1, ax2), (ax3, ax4), (ax5, ax6), (ax7, ax8)) = plt.subplots(nrows=4, ncols=2, sharey=False, figsize=(7, 9))
+        axes = [ax1, ax2, ax3, ax4, ax5, ax6, ax7, ax8]
+
+        #for ax, compartment, d, dw1, dw2 in zip(axes, index, np.asarray(sum_all).transpose(), np.asarray(sum_all_w).transpose(), np.asarray(sum_all_w2).transpose()):
+        for ax, compartment, data in zip(axes, infectionstates, all_comp_last):
+          
+                ax.hist(data, bins = 30)
+                ax.set_title(compartment, fontsize = 10)
+                handles, labels = ax.get_legend_handles_labels()
+
+        fig.legend(handles, labels, loc='lower center', bbox_to_anchor=(0.55, 0.001), frameon=False)
+        plt.tight_layout()
+                 
+        plt.savefig("GNN_hist_compartments_las.png")
+
+
+def heatmap_ARMAConv_withCV():
+    df_ARMA = pd.read_csv('/home/schm_a45/Documents/Code/memilio/memilio/pycode/machine-learning/dataframe_hyperparameters_2024/dataframe_ARMAConv_30days_oldrange_withCV.csv')
+  
+    df_plot = pd.DataFrame(data =  df_ARMA[['number_of_layers', 'number_of_neurons', 'kfold_test']])
+    df_plot = df_plot.pivot(index='number_of_layers', columns='number_of_neurons', values='kfold_test')
+    
+    fig, ax = plt.subplots()
+    im = ax.imshow(df_plot.values, cmap ='summer_r')
+
+    # Show all ticks and label them with the respective list entries
+    ax.set_xticks(np.arange(len(df_plot.columns)), labels=df_plot.columns)
+    ax.set_yticks(np.arange(len(df_plot.index)), labels=df_plot.index)
+    ax.set_ylabel('number of layers')
+    ax.set_xlabel('channels')
+
+    # Rotate the tick labels and set their alignment.
+    plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
+            rotation_mode="anchor")
+
+    # Loop over data dimensions and create text annotations.
+    for i in range(len(df_plot.index)):
+        for j in range(len(df_plot.columns)):
+            text = ax.text(j, i, np.around(df_plot.values, decimals=3)[i, j],
+                        ha="center", va="center", color="black", fontsize = 14 )
+
+    cbar_kw = {}        
+    cbar = ax.figure.colorbar(im, ax=ax, **cbar_kw)
+    cbar.ax.set_ylabel('MAPE', rotation=-90, va="bottom")    
+
+    ax.set_title("")
+    fig.tight_layout()
+    plt.show()
+    plt.savefig("ARNMAConv_withCV.png")
+
+
+def scatterplot_ARMA():
+    # Read dataframe from CSV
+    df_ARMA = pd.read_csv('/home/schm_a45/Documents/Code/memilio/memilio/pycode/machine-learning/dataframe_hyperparameters_2024/dataframe_ARMAConv_30days_oldrange_withCV.csv')
+
+    # Extract relevant columns
+    df_plot = pd.DataFrame(data=df_ARMA[['number_of_layers', 'number_of_neurons', 'all_testscores']])
+
+    # Convert string representations of arrays to actual arrays
+    df_plot['all_testscores'] = df_plot['all_testscores'].apply(lambda x: np.array(eval(x)))
+
+    # Explode the dataframe based on the 'all_testscores' column
+    df_expanded = df_plot.explode('all_testscores')
+    df_expanded.reset_index(drop=True, inplace=True)
+
+    # Create a new column 'summary' by concatenating 'number_of_layers' and 'number_of_neurons'
+    df_expanded['summary'] = df_expanded.apply(lambda row: f"{row['number_of_layers']}, {row['number_of_neurons']}", axis=1)
+
+    # Define positions
+    array = ['1st split', '2nd split', '3d split', '4th split', '5th split']
+    df_expanded['position'] = np.tile(array, 9)
+
+    # Define your own custom colors
+    custom_colors = ['red', 'blue', 'green', 'orange', 'purple']
+
+    # Create a ListedColormap using the custom colors
+    custom_cmap = ListedColormap(custom_colors)
+
+    # Plot the scatter plot with color coding based on 'position'
+    plt.figure(figsize=(6, 5))  # Adjust figure size as needed
+    for i, position in enumerate(array):
+        plt.scatter(df_expanded[df_expanded['position'] == position]['summary'], df_expanded[df_expanded['position'] == position]['all_testscores'], c=custom_colors[i], label=position, cmap=custom_cmap)
+
+    plt.legend()
+    plt.ylabel('MAPE')
+    plt.xlabel('model architecture')  # Fixed x-label
+    plt.xticks(rotation=45)
+    plt.tight_layout()  # Adjust layout to prevent label overlap
+    plt.savefig("ARMAConv_CV_testscores.png")
+
+
+def ARMA_op_act_heatmap_cv_3_512():
+    df_1 = pd.read_csv('/home/schm_a45/Documents/code3/memilio/pycode/machine-learning/dataframe_gridsearch_2024/ARMAConv_act_opt_2024_withCV.csv')        
+    df_2 = pd.read_csv('/home/schm_a45/Documents/code3/memilio/pycode/machine-learning/dataframe_gridsearch_2024/ARMAConv_act_opt_2024_withCV_part2.csv')
+
+    df = pd.concat([df_1, df_2], axis = 0)
+    optimizer = []
+    for i in df['optimizer']:
+        optimizer.append(re.sub('[^a-zA-Z]+', '', i.split(' ')[0].split('.')[-1]))
+
+    df['optimizer'] = optimizer
+
+    df_heatmap1 = pd.DataFrame(data = df[['activation', 'optimizer', 'kfold_test']])
+    df_heatmap1= df_heatmap1.pivot(index='activation', columns='optimizer', values='kfold_test').transpose()
+    #df_heatmap1 = df_heatmap1[['elu', 'linear', 'relu']]
+
+    fig, ax = plt.subplots()
+    im = ax.imshow(df_heatmap1.values , cmap = 'summer_r')
+
+    # Show all ticks and label them with the respective list entries
+    ax.set_xticks(np.arange(len(df_heatmap1.columns)), labels=df_heatmap1.columns)
+    ax.set_yticks(np.arange(len(df_heatmap1.index)), labels=df_heatmap1.index)
+
+    # Rotate the tick labels and set their alignment.
+    plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
+            rotation_mode="anchor")
+
+    # Loop over data dimensions and create text annotations.
+    for i in range(len(df_heatmap1.index)):
+        for j in range(len(df_heatmap1.columns)):
+            text = ax.text(j, i, np.around(df_heatmap1.values, decimals=2)[i, j],
+                        ha="center", va="center", color="black", fontsize = 15)
+
+
+    cbar_kw = {}        
+    cbar = ax.figure.colorbar(im, ax=ax, **cbar_kw)
+    cbar.ax.set_ylabel('MAPE', rotation=-90, va="bottom")        
+
+
+    #ax.set_title("Activation function and optimizer for best ARMAConv architechture")
+    fig.tight_layout()
+    plt.show()
+    plt.savefig("ARMA_heatmap_activation_optimizer_CV_3_512.png")
+
+
+def ARMA_op_act_heatmap_nocv_4_512():
+    df = pd.read_csv('/home/schm_a45/Documents/code3/memilio/pycode/machine-learning/dataframe_gridsearch_2024/ARMAConv_act_opt_2024_nocv_4_512.csv')        
+
+    optimizer = []
+    for i in df['optimizer']:
+        optimizer.append(re.sub('[^a-zA-Z]+', '', i.split(' ')[0].split('.')[-1]))
+
+    df['optimizer'] = optimizer
+
+    df_heatmap1 = pd.DataFrame(data = df[['activation', 'optimizer', 'kfold_test']])
+    df_heatmap1= df_heatmap1.pivot(index='activation', columns='optimizer', values='kfold_test').transpose()
+    #df_heatmap1 = df_heatmap1[['elu', 'linear', 'relu']]
+
+    fig, ax = plt.subplots()
+    im = ax.imshow(df_heatmap1.values , cmap = 'summer_r')
+
+    # Show all ticks and label them with the respective list entries
+    ax.set_xticks(np.arange(len(df_heatmap1.columns)), labels=df_heatmap1.columns)
+    ax.set_yticks(np.arange(len(df_heatmap1.index)), labels=df_heatmap1.index)
+
+    # Rotate the tick labels and set their alignment.
+    plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
+            rotation_mode="anchor")
+
+    # Loop over data dimensions and create text annotations.
+    for i in range(len(df_heatmap1.index)):
+        for j in range(len(df_heatmap1.columns)):
+            text = ax.text(j, i, np.around(df_heatmap1.values, decimals=2)[i, j],
+                        ha="center", va="center", color="black", fontsize = 15)
+
+
+    cbar_kw = {}        
+    cbar = ax.figure.colorbar(im, ax=ax, **cbar_kw)
+    cbar.ax.set_ylabel('MAPE', rotation=-90, va="bottom")        
+
+
+    #ax.set_title("Activation function and optimizer for best ARMAConv architechture")
+    fig.tight_layout()
+    plt.show()
+    plt.savefig("ARMA_heatmap_activation_optimizer_noCV_4_512.png")
+
+def plot_ARMA_modifications_scatter():
+    df = pd.read_csv('/home/schm_a45/Documents/Code/memilio/memilio/pycode/machine-learning/dataframe_hyperparameters_2024/dataframe_ARMAConv_4_512_30days_oldrange_withCV_hyperparameters.csv')
+        # Extract relevant columns
+    df_plot = pd.DataFrame(data=df[['configuration', 'all_testscores']])
+
+    # Convert string representations of arrays to actual arrays
+    df_plot['all_testscores'] = df_plot['all_testscores'].apply(lambda x: np.array(eval(x)))
+    df_plot = df_plot[:5]
+
+    # Explode the dataframe based on the 'all_testscores' column
+    df_expanded = df_plot.explode('all_testscores')
+    df_expanded.reset_index(drop=True, inplace=True)
+
+    # Define positions
+    array = ['1st split', '2nd split', '3d split', '4th split', '5th split']
+    df_expanded['position'] = np.tile(array, len(df_plot))
+
+    # Define your own custom colors
+    custom_colors = ['red', 'blue', 'green', 'orange', 'purple']
+
+    # Create a ListedColormap using the custom colors
+    custom_cmap = ListedColormap(custom_colors)
+
+    # Plot the scatter plot with color coding based on 'position'
+    plt.figure(figsize=(6, 5))  # Adjust figure size as needed
+    for i, position in enumerate(array):
+        plt.scatter(df_expanded[df_expanded['position'] == position]['configuration'], df_expanded[df_expanded['position'] == position]['all_testscores'], c=custom_colors[i], label=position, cmap=custom_cmap)
+
+
+    plt.legend()
+    plt.ylabel('MAPE')
+    plt.xlabel('model architecture')  # Fixed x-label
+    plt.xticks(rotation=45)
+    plt.tight_layout()  # Adjust layout to prevent label overlap
+    plt.savefig("ARMAConv_CV_testscores_configurations.png")
+
+
+def plot_ARMA_modifications_barplot():
+
+
+# Read the data from CSV
+    df = pd.read_csv('/home/schm_a45/Documents/Code/memilio/memilio/pycode/machine-learning/dataframe_hyperparameters_2024/dataframe_ARMAConv_4_512_30days_oldrange_withCV_hyperparameters.csv')
+
+    # Convert kfold_test values to float
+    df['kfold_test'] = df['kfold_test'].astype(float)
+
+    # Select relevant columns and first 5 rows
+    df_plot = df[['kfold_test', 'configuration']][:5]
+
+    # Append a row with entries ['4.24', 'baseline']
+    df_plot.loc[len(df_plot)] = ['4.24', 'baseline']
+
+    # Extract values for the bars and convert to float
+    values = df_plot['kfold_test'].astype(float)
+
+    # Create the bar plot
+    fig, ax = plt.subplots(layout='constrained')
+    bars = ax.barh(df_plot['configuration'], values)
+
+    # Add annotations to the bars
+    for i, bar in enumerate(bars):
+        ax.text(bar.get_width() - 0.6, bar.get_y() + bar.get_height()/2, f'{values[i]:.2f}',
+                va='center', ha='left', fontsize=10, color='white', fontweight='bold')
+
+    # Customize plot
+    #ax.legend(['test MAPE'], loc='upper right', fontsize=8, bbox_to_anchor=(1.2, 1))
+    ax.set_ylabel('Model Configuration')
+    ax.set_xlabel('Test MAPE')
+
+    # Save plot
+    plt.savefig("ARMAConv_configurations_bar.png", bbox_inches='tight')
+    plt.show()
