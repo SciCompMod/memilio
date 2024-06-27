e31cee610161d6f9a6836f25d310c4a3cea2c752
reneSchm <49305466+reneSchm@users.noreply.github.com>
2024-06-27 15:24:01 +0200

    fix a use after free warning with newer compilers

--- cpp/memilio/epidemiology/damping.h
+++ cpp/memilio/epidemiology/damping.h
@@ -519,7 +519,7 @@ private:
     static void inclusive_exclusive_sum_rec(Iter b, Iter e, Matrix& sum)
     {
         if (b != e) {
-            sum = sum + std::get<Matrix>(*b) - (sum.array() * std::get<Matrix>(*b).array()).matrix();
+            sum = (sum + std::get<Matrix>(*b) - (sum.array() * std::get<Matrix>(*b).array()).matrix()).eval();
             inclusive_exclusive_sum_rec(++b, e, sum);
         }
     }
@@ -527,8 +527,7 @@ private:
     static Matrix inclusive_exclusive_sum(const std::vector<Tuple>& v)
     {
         assert(!v.empty());
-        auto& m  = std::get<Matrix>(v.front());
-        auto sum = m.eval();
+        Matrix sum = std::get<Matrix>(v.front());
         inclusive_exclusive_sum_rec(v.begin() + 1, v.end(), sum);
         return sum;
     }
