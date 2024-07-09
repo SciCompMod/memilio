852d68e9fd6ebc347431b0938ea9eff55395dec6
reneSchm <49305466+reneSchm@users.noreply.github.com>
2024-07-09 15:38:05 +0200

    remove unrelated code

--- cpp/examples/cli.cpp
+++ cpp/examples/cli.cpp
@@ -1,15 +1,5 @@
 #include "memilio/io/cli.h"
-#include "memilio/io/io.h"
-#include "memilio/io/json_serializer.h"
 
-#include <cmath>
-#include <cstddef>
-#include <cstdio>
-#include <cstring>
-#include <fstream>
-#include <iostream>
-#include <string>
-#include <system_error>
 #include <vector>
 
 struct Name {
@@ -66,75 +56,8 @@ struct Greeting {
     }
 };
 
-using HourlyContactMatrix = std::array<Eigen::MatrixXd, 24>;
-
-mio::IOResult<HourlyContactMatrix> read_hourly_contact_matrix(const std::string& csv_file)
-{
-    std::ifstream file(csv_file);
-    if (!file.good()) {
-        return mio::failure(mio::StatusCode::FileNotFound, "Could not open " + csv_file + ".");
-    }
-
-    std::string reader;
-
-    std::vector<double> matrix_entries;
-    int current_hour = 0;
-    int t, row, col;
-    double val;
-
-    HourlyContactMatrix hcm;
-
-    // skip first line
-    std::getline(file, reader);
-    // possible EOF here
-    // read csv
-    while (std::getline(file, reader)) {
-        int status = sscanf(reader.c_str(), "%i,%i,%i,%lf\n", &t, &row, &col, &val);
-
-        if (status != 4) {
-            return mio::failure(mio::StatusCode::InvalidFileFormat,
-                                "Unexpected format while reading " + csv_file + ". Line reads \"" + reader + "\"");
-        }
-
-        printf("%i,%i,%i,%lf\n", t, row, col, val);
-
-        if (t > current_hour) {
-            size_t n          = std::round(std::sqrt(matrix_entries.size()));
-            hcm[current_hour] = Eigen::MatrixXd(n, n);
-            for (size_t i = 0; i < matrix_entries.size(); i++) {
-                hcm[current_hour].data()[i] = matrix_entries[i];
-            }
-            matrix_entries.clear();
-            ++current_hour;
-        }
-
-        matrix_entries.push_back(val);
-    }
-
-    size_t n          = std::round(std::sqrt(matrix_entries.size()));
-    hcm[current_hour] = Eigen::MatrixXd(n, n);
-    for (size_t i = 0; i < matrix_entries.size(); i++) {
-        hcm[current_hour].data()[i] = matrix_entries[i];
-    }
-
-    return mio::success(hcm);
-}
-
 int main(int argc, char** argv)
 {
-    std::string filename = "/home/schm_r6/Documents/24h_networks_csv/office_5_5.csv";
-
-    auto res = read_hourly_contact_matrix(filename);
-
-    if (!res) {
-        std::cout << res.error().formatted_message();
-        return res.error().code().value();
-    }
-
-    std::cout << mio::serialize_json(res.value()).value() << "\n";
-
-    return 0;
-
     if (argc == 1) { // Print this if no arguments were given
         std::cout << "This is a small example on how to use the command line interface. "
                      "Use \"-h\" to show the help dialogue.\n";
