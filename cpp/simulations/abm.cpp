574695778f447d4d77c959305ed10c8c794098ed
Khoa Nguyen <4763945+khoanguyen-dev@users.noreply.github.com>
2024-08-05 15:25:46 +0200

    Add data from studies

--- cpp/simulations/abm.cpp
+++ cpp/simulations/abm.cpp
@@ -470,11 +470,12 @@ void set_parameters(mio::abm::Parameters params)
 
     params.set<mio::abm::IncubationPeriod>({{mio::abm::VirusVariant::Count, mio::AgeGroup(num_age_groups)}, 4.});
 
-    // Set protection level from high viral load. Information based on: https://doi.org/10.1093/cid/ciaa886
-     params.get<mio::abm::HighViralLoadProtectionFactor>()[{mio::abm::ExposureType::NaturalInfection, age_group_0_to_4, mio::abm::VirusVariant::Wildtype}] =
+    // Set protection level from high viral load. Information based on: https://doi.org/10.1186/s12916-021-02220-0 and https://doi.org/10.1371/journal.pone.0243597
+    params.get<mio::abm::HighViralLoadProtectionFactor>()[{mio::abm::ExposureType::NaturalInfection, age_group_0_to_4,
+                                                           mio::abm::VirusVariant::Wildtype}] =
         [](ScalarType days) -> ScalarType {
         return mio::linear_interpolation_of_data_set<ScalarType, ScalarType>(
-            {{0, 0.863}, {1, 0.969}, {7, 0.029}, {10, 0.002}, {14, 0.0014}, {21, 0}}, days);
+            {{0, 0}, {5, 0.3}, {10, 0.6}, {15, 0.8}, {20, 1}}, days);
     };
 
     //0-4
