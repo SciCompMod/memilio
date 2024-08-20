3949c667814f16d5c1b1796956108da00fc7e072
Sascha Korf <51127093+xsaschako@users.noreply.github.com>
2024-08-20 12:51:08 +0200

    cluster
    Signed-off-by: Sascha Korf <51127093+xsaschako@users.noreply.github.com>

--- cpp/simulations/paper_abm_bs_testing.cpp
+++ cpp/simulations/paper_abm_bs_testing.cpp
@@ -2324,8 +2324,8 @@ int main(int argc, char** argv)
     rank      = 0;
 #endif
 
-    // std::string input_dir = "/p/project1/loki/memilio/memilio/data";
-    std::string input_dir = "/Users/saschakorf/Documents/Arbeit.nosynch/memilio/memilio/data";
+    std::string input_dir = "/p/project1/loki/memilio/memilio/data";
+    // std::string input_dir = "/Users/saschakorf/Documents/Arbeit.nosynch/memilio/memilio/data";
     // std::string input_dir = "/Users/david/Documents/HZI/memilio/data";
     // std::string input_dir       = "C:/Users/korf_sa/Documents/rep/data";
 
