c8009811b55dcaf18e99e2d21172cf222579aaee
Sascha Korf <51127093+xsaschako@users.noreply.github.com>
2024-08-24 22:59:49 +0200

    prep cluster run
    Signed-off-by: Sascha Korf <51127093+xsaschako@users.noreply.github.com>

--- cpp/simulations/paper_abm_bs_testing.cpp
+++ cpp/simulations/paper_abm_bs_testing.cpp
@@ -2352,8 +2352,8 @@ int main(int argc, char** argv)
     rng.seed(seeds);
     rng.synchronize();
 
-    // std::string input_dir = "/p/project1/loki/memilio/memilio/data";
-    std::string input_dir = "/Users/saschakorf/Documents/Arbeit.nosynch/memilio/memilio/data";
+    std::string input_dir = "/p/project1/loki/memilio/memilio/data";
+    // std::string input_dir = "/Users/saschakorf/Documents/Arbeit.nosynch/memilio/memilio/data";
     // std::string input_dir = "/Users/david/Documents/HZI/memilio/data";
     // std::string input_dir       = "C:/Users/korf_sa/Documents/rep/data";
 
@@ -2405,7 +2405,7 @@ int main(int argc, char** argv)
         std::vector<std::pair<double, double>> grid_boundaries = {{1.5, 2.2}, {2.0, 3.6}, {0.4, 0.7}};
         // std::vector<double> grid_boundaries = {2.3, 2.6, 0.55};
         // std::vector<int> points_per_dim = {11, 11, 7, 11};
-        std::vector<int> points_per_dim = {2, 4, 2};
+        std::vector<int> points_per_dim = {11, 11, 11};
         auto grid                       = grid_points(grid_boundaries, points_per_dim);
         if (rank == 0) {
             auto created = create_result_folders(result_dir, 0, run_grid_search);
