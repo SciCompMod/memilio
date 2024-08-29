fb8cb3a4d1da62ef498e3fc68333e315b2959542
Sascha Korf <51127093+xsaschako@users.noreply.github.com>
2024-08-29 06:56:25 +0200

    grid 1
    Signed-off-by: Sascha Korf <51127093+xsaschako@users.noreply.github.com>

--- cpp/simulations/paper_abm_bs_testing.cpp
+++ cpp/simulations/paper_abm_bs_testing.cpp
@@ -2455,10 +2455,10 @@ int main(int argc, char** argv)
         //     {1.4, 2.0}, {2.5, 5.5}, {0.35, 0.8}, {0.02, 0.045}, {3, 15}};
         // std::vector<int> points_per_dim = {6, 6, 6, 6, 6};
 
-        std::vector<double> grid_boundaries = {1.76, 3.7, 0.53, 0.035, 15.0};
+        // std::vector<double> grid_boundaries = {1.76, 3.7, 0.53, 0.035, 15.0};
         // std::vector<double> grid_boundaries = {1.76, 3.7, 0.53, 0.03, 7.8};
-        // std::vector<double> grid_boundaries = {1.88, 3.7, 0.35, 0.035, 12.6};
-        std::vector<int> points_per_dim = {4, 4, 4, 4, 4};
+        std::vector<double> grid_boundaries = {1.88, 3.7, 0.35, 0.035, 12.6};
+        std::vector<int> points_per_dim     = {4, 4, 4, 4, 4};
 
         // std::vector<std::pair<double, double>> grid_boundaries = {
         // {1.79, 1.83}, {3.28, 3.29}, {0.52, 0.56}, {0.03, 0.04}, {1.0, 15.0}};
