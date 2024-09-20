1bf16dd7f7396361356f5c85508eb706ef828542
Sascha Korf <51127093+xsaschako@users.noreply.github.com>
2024-09-21 00:01:02 +0200

    grid_Search128_par1
    Signed-off-by: Sascha Korf <51127093+xsaschako@users.noreply.github.com>

--- cpp/simulations/paper_abm_bs_testing.cpp
+++ cpp/simulations/paper_abm_bs_testing.cpp
@@ -2255,6 +2255,9 @@ mio::IOResult<void> run(const fs::path& input_dir, const fs::path& result_dir, s
             }
         }
 
+        rmse_results_per_grid_point.at(par_i) /= num_runs;
+        printf("RMSE: %f\n, par_i: %d\n", rmse_results_per_grid_point.at(par_i), par_i);
+
 #ifdef MEMILIO_ENABLE_MPI
 
         //gather results
@@ -2482,10 +2485,10 @@ int main(int argc, char** argv)
         // std::vector<std::vector<double>> parameters = {{1.52}, {4.9}, {0.71}, {0.02}, {3.0}, {10.0}, {0.5}};
         // std::vector<std::vector<double>> parameters = {
         //     {1.76}, {3.7}, {0.53}, {0.035}, {15.0}, {2, 5, 8, 11, 14}, {0.0, 0.25, 0.5, 0.75, 1.0}};
-        // std::vector<std::vector<double>> parameters = {{1.596}, {4.171}, {0.7275}, {0.02472}, {4.83}, {10.0}, {0.5}};
+        std::vector<std::vector<double>> parameters = {{1.596}, {4.171}, {0.7275}, {0.02472}, {4.83}, {10.0}, {0.5}};
         // std::vector<std::vector<double>> parameters = {{1.52}, {4.3}, {0.85}, {0.024}, {4.6}, {10.0}, {0.5}};
-        std::vector<std::vector<double>> parameters = {{1.596}, {4.171}, {0.7125}, {0.02472}, {4.83}, {10.0}, {0.5}};
-        auto every_combination                      = every_combination_of_parameters(parameters);
+        // std::vector<std::vector<double>> parameters = {{1.596}, {4.171}, {0.7125}, {0.02472}, {4.83}, {10.0}, {0.5}};
+        auto every_combination = every_combination_of_parameters(parameters);
         if (rank == 0) {
             auto created = create_result_folders(result_dir, every_combination.size(), run_grid_search);
             if (!created) {
