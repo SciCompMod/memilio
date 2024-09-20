89ada50b90527e93601ba8f3e6c9726731f552ea
Sascha Korf <51127093+xsaschako@users.noreply.github.com>
2024-09-21 01:27:49 +0200

    scenario1
    Signed-off-by: Sascha Korf <51127093+xsaschako@users.noreply.github.com>

--- cpp/simulations/paper_abm_bs_testing.cpp
+++ cpp/simulations/paper_abm_bs_testing.cpp
@@ -2487,15 +2487,15 @@ int main(int argc, char** argv)
 
         // std::vector<std::vector<double>> parameters = {{1.596}, {4.171}, {0.7125}, {0.02472}, {4.83}, {10.0}, {0.5}};
 
-        // std::vector<std::vector<double>> parameters = {{1.76}, {3.7},  {0.53}, {0.035, 0.07, 0.105},
-        //                                                {15.0}, {10.0}, {0.5}};
+        std::vector<std::vector<double>> parameters = {{1.596}, {4.171}, {0.7125}, {0.02472, 0.04944, 0.07416},
+                                                       {4.83},  {10.0},  {0.5}};
 
         // std::vector<std::vector<double>> parameters = {
         //     {1.596}, {4.171}, {0.7125}, {0.012, 0.024, 0.036, 0.048, 0.06}, {2.0, 5.0, 8.0, 11.0, 14.0}, {10.0}, {0.5}};
         // std::vector<std::vector<double>> parameters = {
         //     {1.596}, {4.171}, {0.7125}, {0.012, 0.024, 0.036, 0.048, 0.06}, {4.83}, {2, 5, 8, 11, 14}, {0.5}};
-        std::vector<std::vector<double>> parameters = {
-            {1.596}, {4.171}, {0.7125}, {0.02472}, {4.83}, {2, 5, 8, 11, 14}, {0.0, 0.25, 0.5, 0.75, 1.0}};
+        // std::vector<std::vector<double>> parameters = {
+        //     {1.596}, {4.171}, {0.7125}, {0.02472}, {4.83}, {2, 5, 8, 11, 14}, {0.0, 0.25, 0.5, 0.75, 1.0}};
 
         auto every_combination = every_combination_of_parameters(parameters);
         if (rank == 0) {
