8087b1cc56847a28dec6dac76a9fa2e96662260e
lenaploetzke <70579874+lenaploetzke@users.noreply.github.com>
2024-10-07 09:45:05 +0200

    cleanup runandplot

--- tools/run_and_plot.py
+++ tools/run_and_plot.py
@@ -2,7 +2,7 @@ import subprocess
 import os
 import pandas as pd
 
-from plot_lct_fictional_noage import plot_new_infections, get_maxpeak_incidence, plot_maxpeak_incidence, plot_subcompartments3D, plot_day_peak_incidence
+from plot_lct_fictional_noage import *
 
 
 def run_simulation(R0s, simulationdays, save_dir=""):
@@ -30,17 +30,25 @@ def get_file_name(R0, subcompartment, data_dir, boolsubcomp=False):
 
 
 def main():
-    data_dir = "../data/simulation_lct_noage/riseR0long/"
-    # subcompartments = 5
-    R0s = list([2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0])
-    simulationdays = [140, 100, 90, 80, 60, 60, 60, 60, 60]
-    # run_simulation(R0s, simulationdays, data_dir)
-    plot_maxpeak_incidence(lambda R0, subcompartment: get_file_name(R0, subcompartment, data_dir=data_dir),
-                           R0s, list([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]), filename_plot="compare_peak_size")
-    plot_day_peak_incidence(lambda R0, subcompartment: get_file_name(R0, subcompartment, data_dir=data_dir),
-                            R0s, list([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]), filename_plot="compare_peak_days")
-    # plot_subcompartments3D(get_file_name(
-    #     2, 50, "../data/simulation_lct_noage/riseR0short/", True), 50, 2, 4, filename_plot="a")
+    case = 1
+    if case == 0:
+        data_dir = "../data/simulation_lct_noage/riseR0long/"
+        R0s = list([2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0])
+        simulationdays = [140, 100, 90, 80, 60, 60, 60, 60, 60]
+        run_simulation(R0s, simulationdays, data_dir)
+        # plot_maxpeak_incidence(lambda R0, subcompartment: get_file_name(R0, subcompartment, data_dir=data_dir),
+        #                     R0s, list([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]), filename_plot="compare_peak_size")
+        # plot_day_peak_incidence(lambda R0, subcompartment: get_file_name(R0, subcompartment, data_dir=data_dir),
+        #                         R0s, list([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]), filename_plot="compare_peak_days")
+    elif case == 1:
+        folder = "../data/simulation_lct_noage/riseR0shortequalTETC/"
+        plot_subcompartments3D(get_file_name(
+            2, 50, "../data/simulation_lct_noage/riseR0shortequalTETC/", True), 50, 2, 4, filename_plot="new_infections_rise2.0_equalTETC")
+        plot_new_infections([os.path.join(folder, "fictional_lct_2.0_1"), os.path.join(folder, "fictional_lct_2.0_3"),
+                             os.path.join(folder, "fictional_lct_2.0_10"),
+                             os.path.join(folder, "fictional_lct_2.0_50")],
+                            20000, legendplot=list(["ODE", "LCT3", "LCT10", "LCT50"]),
+                            filename_plot="new_infections_lct_rise2.0equalTETC")
 
 
 if __name__ == "__main__":
