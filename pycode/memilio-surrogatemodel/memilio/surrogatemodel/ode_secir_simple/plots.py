b0f1b126a3fe10a38a8c5790e4e86b7c9057bc1c
Agatha Schmidt <agatha.schmidt@hotmail.de>
2024-10-28 09:37:40 +0100

    heatmap for grid search of models without age groups

--- pycode/memilio-surrogatemodel/memilio/surrogatemodel/ode_secir_simple/plots.py
+++ pycode/memilio-surrogatemodel/memilio/surrogatemodel/ode_secir_simple/plots.py
@@ -257,17 +257,75 @@ def boxplot_inputs_single(file):
     plt.savefig("boxplot_input_compartments_noagegroups_I_based.png")
 
 
-# lineplots_compartments(data['inputs'], data['labels'], 100)
-# SINGLE_lineplot_compartments(data['inputs'], data['labels'], 100)
+def heatmap_gridsearch_results(df_gridsearch, savename):
+    df = df_gridsearch
 
-# boxplot_inputs()
+    plt.figure().clf()
+    df_heatmap1 = pd.DataFrame(data=df.loc[(df['model'] == 'Dense')][[
+                               'number_of_hidden_layers', 'number_of_neurons', 'kfold_val']])
+    df_heatmap1 = df_heatmap1.pivot(
+        index='number_of_hidden_layers', columns='number_of_neurons', values='kfold_val')
 
-# file = open('/home/schm_a45/Documents/Code/memilio_test/memilio/pycode/memilio-surrogatemodel/memilio/data_paper/data_secir_simple_30days_I_based_10k.pickle', 'rb')
-# boxplot_inputs_single(file)
+    df_heatmap2 = pd.DataFrame(data=df.loc[(df['model'] == 'CNN')][[
+                               'number_of_hidden_layers', 'number_of_neurons', 'kfold_val']])
+    df_heatmap2 = df_heatmap2.pivot(
+        index='number_of_hidden_layers', columns='number_of_neurons', values='kfold_val')
+
+    df_heatmap3 = pd.DataFrame(data=df.loc[(df['model'] == 'LSTM')][[
+                               'number_of_hidden_layers', 'number_of_neurons', 'kfold_val']])
+    df_heatmap3 = df_heatmap3.pivot(
+        index='number_of_hidden_layers', columns='number_of_neurons', values='kfold_val')
+
+    fig, axs = plt.subplots(nrows=2, ncols=2, sharex=False,
+                            figsize=(20, 20), constrained_layout=True)
+
+    for ax, df_heatmap, name in zip(axs.flat, [df_heatmap1, df_heatmap2, df_heatmap3], ['MLP', 'CNN', 'LSTM']):
+
+        im = ax.imshow(df_heatmap.values, cmap='RdYlGn_r')
+        plt.rcParams.update({'font.size': 30})
+        # Show all ticks and label them with the respective list entries
+        ax.set_xticks(np.arange(len(df_heatmap.columns)),
+                      labels=df_heatmap.columns, fontsize=25)
+        ax.set_yticks(np.arange(len(df_heatmap.index)),
+                      labels=df_heatmap.index, fontsize=25)
+
+        ax.set_ylabel('number of hidden layers', fontsize=25)
+        ax.set_xlabel('number of neurons per layer', fontsize=25)
+
+        # Rotate the tick labels and set their alignment.
+        plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
+                 rotation_mode="anchor")
+
+        # Loop over data dimensions and create text annotations.
+        for i in range(len(df_heatmap.index)):
+            for j in range(len(df_heatmap.columns)):
+                text = ax.text(j, i, np.around(df_heatmap.values, decimals=2)[i, j],
+                               ha="center", va="center", color="k", fontsize=25)
+
+        ax.set_title('Model = '+name, fontsize=30)
+
+    fig.colorbar(im, ax=axs, shrink=0.75, label='Validation MAPE')
+    fig.delaxes(axs[1][1])
+    plt.show()
+    plt.savefig(savename)
+
+    # lineplots_compartments(data['inputs'], data['labels'], 100)
+    # SINGLE_lineplot_compartments(data['inputs'], data['labels'], 100)
+
+    # boxplot_inputs()
+
+    # boxplot_inputs_single(file)
+
+    # SINGLE_lineplot_compartments_log_and_nolog(
+    #    data['inputs'], data['labels'], 100, 95)
 
 
-SINGLE_lineplot_compartments_log_and_nolog(
-    data['inputs'], data['labels'], 100, 95)
+filename = 'dataframe_30days_I_based_10k_nodamp'
 
-# plt.savefig("/localdata1/gnn_paper_2024/images/no_agegroups/compartment_lines_noagegroups_30days_10k_paper_no" +
-# str(i) + ".png")
+path = os.path.dirname(os.path.realpath(__file__))
+path_data = os.path.join(os.path.dirname(os.path.realpath(
+    os.path.dirname(os.path.realpath(path)))), 'secir_simple_grid_search_paper')
+filepath = os.path.join(path_data, filename)
+df = pd.DataFrame(data=pd.read_csv(filepath))
+savename = "heatmap_secir_simple_noagegroups_30days_I_based_10k_paper.png"
+heatmap_gridsearch_results(df, savename)
