f4b820bd3113a1888560505d0d58acb80d289c9c
DavidKerkmann <44698825+DavidKerkmann@users.noreply.github.com>
2024-11-27 18:45:32 +0100

    Fix makro for test

--- cpp/tests/test_abm_model.cpp
+++ cpp/tests/test_abm_model.cpp
@@ -430,7 +430,7 @@ TEST_F(TestModel, evolveMobilityTrips)
     EXPECT_EQ(model.get_number_persons(home_id), 3);
 }
 
-#ifdef MEMILIO_ENABLE_OPENMP // Test can fail with parallel execution of mobility, as the capacity is not taken into account correctly at the moment
+#ifndef MEMILIO_ENABLE_OPENMP // Test can fail with parallel execution of mobility, as the capacity is not taken into account correctly at the moment
 /**
  * @brief Test that a location correctly enforces its capacity constraint.
  */
