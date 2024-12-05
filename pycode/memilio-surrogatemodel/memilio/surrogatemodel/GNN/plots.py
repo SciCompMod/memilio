d39adf24af3abf467a6a34a66676d2b416a04b38
Agatha Schmidt <agatha.schmidt@hotmail.de>
2024-12-05 10:24:15 +0100

    single colorbar for all plots in heatmap

--- pycode/memilio-surrogatemodel/memilio/surrogatemodel/GNN/plots.py
+++ pycode/memilio-surrogatemodel/memilio/surrogatemodel/GNN/plots.py
@@ -7,42 +7,76 @@ import pickle
 import re
 import matplotlib.gridspec as gridspec
 from matplotlib.colors import ListedColormap
+from matplotlib.colors import Normalize
+from matplotlib.colorbar import Colorbar
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import re
+from matplotlib.colors import Normalize
 
 
-def heatmap_gridsearch_results(df_gridsearch, savename):
+def heatmap_gridsearch_results(df_gridsearch, savename, vmin=None, vmax=None):
     df = df_gridsearch
     layers = []
     for i in df['layer']:
         layers.append(re.sub('[^a-zA-Z]+', '', i.split('.')[-1]))
-
     df['layer'] = layers
-    plt.figure().clf()
+
+    # Create pivot tables for each layer type
     df_heatmap1 = pd.DataFrame(data=df.loc[(df['layer'] == 'ARMAConv')][[
-                               'number_of_layers', 'number_of_neurons', 'kfold_val']])
+                               'number_of_layers', 'number_of_neurons', 'kfold_test']])
     df_heatmap1 = df_heatmap1.pivot(
-        index='number_of_layers', columns='number_of_neurons', values='kfold_val')
+        index='number_of_layers', columns='number_of_neurons', values='kfold_test')
 
     df_heatmap2 = pd.DataFrame(data=df.loc[(df['layer'] == 'GCNConv')][[
-        'number_of_layers', 'number_of_neurons', 'kfold_val']])
+        'number_of_layers', 'number_of_neurons', 'kfold_test']])
     df_heatmap2 = df_heatmap2.pivot(
-        index='number_of_layers', columns='number_of_neurons', values='kfold_val')
+        index='number_of_layers', columns='number_of_neurons', values='kfold_test')
 
     df_heatmap3 = pd.DataFrame(data=df.loc[(df['layer'] == 'APPNPConv')][[
-                               'number_of_layers', 'number_of_neurons', 'kfold_val']])
+                               'number_of_layers', 'number_of_neurons', 'kfold_test']])
     df_heatmap3 = df_heatmap3.pivot(
-        index='number_of_layers', columns='number_of_neurons', values='kfold_val')
+        index='number_of_layers', columns='number_of_neurons', values='kfold_test')
 
     df_heatmap4 = pd.DataFrame(data=df.loc[(df['layer'] == 'GATConv')][[
-                               'number_of_layers', 'number_of_neurons', 'kfold_val']])
+                               'number_of_layers', 'number_of_neurons', 'kfold_test']])
     df_heatmap4 = df_heatmap4.pivot(
-        index='number_of_layers', columns='number_of_neurons', values='kfold_val')
+        index='number_of_layers', columns='number_of_neurons', values='kfold_test')
 
+    # Combine all data to find global min and max
+    all_values = np.concatenate([
+        df_heatmap1.values.flatten(),
+        df_heatmap2.values.flatten(),
+        df_heatmap3.values.flatten(),
+        df_heatmap4.values.flatten()
+    ])
+
+    # Set clipping range (if not provided, use data range)
+    if vmin is None:
+        vmin = np.nanmin(all_values)
+    if vmax is None:
+        vmax = np.nanmax(all_values)
+
+    # Clip data to the specified range for color normalization
+    norm = Normalize(vmin=vmin, vmax=vmax)
+
+    # Create subplots
     fig, axs = plt.subplots(nrows=2, ncols=2, sharex=False,
                             figsize=(20, 20), constrained_layout=True)
 
-    for ax, df_heatmap, name in zip(axs.flat, [df_heatmap1, df_heatmap2, df_heatmap3, df_heatmap4], ['ARMAConv', 'GCNConv', 'GATConv', 'APPNPConv']):
-        # for ax, df_heatmap, name in zip(axs.flat, [df_heatmap1, df_heatmap2, df_heatmap3], ['ARMAConv', 'GCNConv', 'APPNPConv']):
-        im = ax.imshow(df_heatmap.values, cmap='RdYlGn_r')
+    # Generate heatmaps
+    for ax, df_heatmap, name in zip(
+        axs.flat, [df_heatmap1, df_heatmap2, df_heatmap3, df_heatmap4],
+        ['ARMAConv', 'GCNConv', 'GATConv', 'APPNPConv']
+    ):
+        # Clip values for color normalization
+        clipped_data = np.clip(df_heatmap.values, vmin, vmax)
+
+        # Create heatmap with clipped data for colormap
+        im = ax.imshow(clipped_data, cmap='RdYlGn_r', norm=norm)
+
         plt.rcParams.update({'font.size': 30})
         # Show all ticks and label them with the respective list entries
         ax.set_xticks(np.arange(len(df_heatmap.columns)),
@@ -50,8 +84,8 @@ def heatmap_gridsearch_results(df_gridsearch, savename):
         ax.set_yticks(np.arange(len(df_heatmap.index)),
                       labels=df_heatmap.index, fontsize=25)
 
-        ax.set_ylabel('number of hidden layers', fontsize=25)
-        ax.set_xlabel('number of neurons per layer', fontsize=25)
+        ax.set_ylabel('Number of Hidden Layers', fontsize=25)
+        ax.set_xlabel('Number of Neurons per Layer', fontsize=25)
 
         # Rotate the tick labels and set their alignment.
         plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
@@ -60,15 +94,22 @@ def heatmap_gridsearch_results(df_gridsearch, savename):
         # Loop over data dimensions and create text annotations.
         for i in range(len(df_heatmap.index)):
             for j in range(len(df_heatmap.columns)):
-                text = ax.text(j, i, np.around(df_heatmap.values, decimals=2)[i, j],
+                # Get the real value (unclipped) for annotation
+                real_value = df_heatmap.values[i, j]
+
+                # Annotate with the real value
+                text = ax.text(j, i, np.around(real_value, decimals=2),
                                ha="center", va="center", color="k", fontsize=25)
 
         ax.set_title('Model = '+name, fontsize=30)
 
-    fig.colorbar(im, ax=axs, shrink=0.75, label='Validation MAPE')
-    # fig.delaxes(axs[1][1])
+    # Add a single colorbar for all heatmaps
+    cbar = fig.colorbar(im, ax=axs, location='right',
+                        shrink=0.75, label='Validation MAPE')
+
+    # Save and show the figure
     plt.show()
-    plt.savefig(savename)
+    plt.savefig(savename, bbox_inches='tight')
 
 
 def ARMA_days_scatter(df_plot):
@@ -112,7 +153,7 @@ def ARMA_days_scatter(df_plot):
 
 # HEATMAP
 filename = 'GNN_gridsearch_withCV_nodeswithvariance.csv'
-filename = 'GNN_gridsearch_withCV_equalnodes.csv'
+# filename = 'GNN_gridsearch_withCV_equalnodes.csv'
 # filename = 'gridserach_secir_groups_30days_I_based_Germany_10k_nodamp.csv'
 path = os.path.dirname(os.path.realpath(__file__))
 path_data = os.path.join(os.path.dirname(os.path.realpath(
@@ -121,4 +162,4 @@ filepath = os.path.join(path_data, filename)
 df = pd.DataFrame(data=pd.read_csv(filepath))
 savename = "GNN_gridsearch_nodeswithvariance.png"
 # savename = "GNN_gridsearch_equalnodes.png"
-heatmap_gridsearch_results(df, savename)
+heatmap_gridsearch_results(df, savename, vmin=None, vmax=110)
