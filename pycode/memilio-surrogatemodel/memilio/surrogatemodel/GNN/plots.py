fc87b4908c4c5719e9804c7c4c6bf62a74bd4e7c
Agatha Schmidt <agatha.schmidt@hotmail.de>
2024-12-12 13:13:37 +0100

    plot only ARMAConv , add parameters instead of hard coded column names

--- pycode/memilio-surrogatemodel/memilio/surrogatemodel/GNN/plots.py
+++ pycode/memilio-surrogatemodel/memilio/surrogatemodel/GNN/plots.py
@@ -17,7 +17,17 @@ import re
 from matplotlib.colors import Normalize
 
 
-def heatmap_gridsearch_results(df_gridsearch, savename, vmin=None, vmax=None):
+def heatmap_gridsearch_results(df_gridsearch, dimension1, dimension2, dimension3, savename, vmin=None, vmax=None):
+    """! Creates a heatmap for all four GNN layer types. 
+
+    @param df_gridsearch
+    @param dimension1 
+    @param dimension2 
+    @param dimension3 
+    @param savename
+    @param vmin
+    @return vmax
+    """
     df = df_gridsearch
     layers = []
     for i in df['layer']:
@@ -26,24 +36,24 @@ def heatmap_gridsearch_results(df_gridsearch, savename, vmin=None, vmax=None):
 
     # Create pivot tables for each layer type
     df_heatmap1 = pd.DataFrame(data=df.loc[(df['layer'] == 'ARMAConv')][[
-                               'number_of_layers', 'number_of_neurons', 'kfold_test']])
+                               dimension1, dimension2, dimension3]])
     df_heatmap1 = df_heatmap1.pivot(
-        index='number_of_layers', columns='number_of_neurons', values='kfold_test')
+        index=dimension1,  columns=dimension2, values=dimension3)
 
     df_heatmap2 = pd.DataFrame(data=df.loc[(df['layer'] == 'GCNConv')][[
-        'number_of_layers', 'number_of_neurons', 'kfold_test']])
+        dimension1, dimension2, dimension3]])
     df_heatmap2 = df_heatmap2.pivot(
-        index='number_of_layers', columns='number_of_neurons', values='kfold_test')
+        index=dimension1, columns=dimension2, values=dimension3)
 
     df_heatmap3 = pd.DataFrame(data=df.loc[(df['layer'] == 'APPNPConv')][[
-                               'number_of_layers', 'number_of_neurons', 'kfold_test']])
+                               dimension1, dimension2, dimension3]])
     df_heatmap3 = df_heatmap3.pivot(
-        index='number_of_layers', columns='number_of_neurons', values='kfold_test')
+        index=dimension1, columns=dimension2, values=dimension3)
 
     df_heatmap4 = pd.DataFrame(data=df.loc[(df['layer'] == 'GATConv')][[
-                               'number_of_layers', 'number_of_neurons', 'kfold_test']])
+                               dimension1, dimension2, dimension3]])
     df_heatmap4 = df_heatmap4.pivot(
-        index='number_of_layers', columns='number_of_neurons', values='kfold_test')
+        index=dimension1, columns=dimension2, values=dimension3)
 
     # Combine all data to find global min and max
     all_values = np.concatenate([
@@ -112,9 +122,16 @@ def heatmap_gridsearch_results(df_gridsearch, savename, vmin=None, vmax=None):
     plt.savefig(savename, bbox_inches='tight')
 
 
-def ARMA_days_scatter(df_plot):
+def ARMA_days_scatter(df):
+    layers = []
+    for i in df['layer']:
+        layers.append(re.sub('[^a-zA-Z]+', '', i.split('.')[-1]))
+    df['layer'] = layers
+    df_plot = df.loc[(df['layer'] == 'ARMAConv')]
+
     df_plot = df[['number_of_layers',
-                  'number_of_neurons', 'all_testscores']][:12]
+                  'number_of_neurons', 'all_testscores']]
+
     df_plot['all_testscores'] = df_plot['all_testscores'].apply(
         lambda x: np.asarray(ast.literal_eval(x)))
 
@@ -137,7 +154,7 @@ def ARMA_days_scatter(df_plot):
     custom_cmap = ListedColormap(custom_colors)
 
     # Plot the scatter plot with color coding based on 'position'
-    plt.figure(figsize=(6, 5))  # Adjust figure size as needed
+    plt.figure(figsize=(20, 8))  # Adjust figure size as needed
     for i, position in enumerate(array):
         plt.scatter(df_expanded[df_expanded['position'] == position]['summary'], df_expanded[df_expanded['position']
                     == position]['all_testscores'], c=custom_colors[i], label=position, cmap=custom_cmap)
@@ -146,20 +163,152 @@ def ARMA_days_scatter(df_plot):
     plt.ylabel('MAPE')
     plt.xlabel('Number of Days')
     # Set the x-ticks and labels explicitly
-    plt.xticks(ticks=df_plot['summary'], labels=df_plot['summary'])
+    plt.xticks(ticks=df_expanded['summary'],
+               labels=df_expanded['summary'], rotation=90)
     plt.tight_layout()  # Adjust layout to prevent label overlap
     plt.savefig("ARMAConv_CV_testscores_days_equalnodes.png")
 
 
-# HEATMAP
-filename = 'GNN_gridsearch_withCV_nodeswithvariance.csv'
-# filename = 'GNN_gridsearch_withCV_equalnodes.csv'
-# filename = 'gridserach_secir_groups_30days_I_based_Germany_10k_nodamp.csv'
+def heatmap_ARMAConv(df_gridsearch, savename, dimension1, dimension2, dimension3, vmin=None, vmax=None):
+    """! Creates a heatmap for all four GNN layer types. 
+
+    @param df_gridsearch
+    @param dimension1 
+    @param dimension2 
+    @param dimension3 
+    @param savename
+    @param vmin
+    @return vmax
+    """
+    df = df_gridsearch
+
+    # layers = []
+    # for i in df['layer']:
+    #     layers.append(re.sub('[^a-zA-Z]+', '', i.split('.')[-1]))
+    # df['layer'] = layers
+    # df = df.loc[df['layer'] == 'ARMAConv']
+
+    # Create pivot tables for each layer type
+    df_heatmap = pd.DataFrame(
+        data=df[[dimension1, dimension2, dimension3]])
+    df_heatmap = df_heatmap.pivot(
+        index=dimension1,  columns=dimension2, values=dimension3)
+
+    # Create subplots
+    fig, ax = plt.subplots(figsize=(10, 10), constrained_layout=True)
+
+    # Create heatmap with clipped data for colormap
+    im = ax.imshow(df_heatmap, cmap='RdYlGn_r')
+
+    plt.rcParams.update({'font.size': 30})
+    # Show all ticks and label them with the respective list entries
+    ax.set_xticks(np.arange(len(df_heatmap.columns)),
+                  labels=df_heatmap.columns, fontsize=25)
+    ax.set_yticks(np.arange(len(df_heatmap.index)),
+                  labels=df_heatmap.index, fontsize=25)
+
+    ax.set_ylabel('Number of parallel GCS stacks', fontsize=25)
+    ax.set_xlabel('Number of iterations per stack ', fontsize=25)
+
+    # Rotate the tick labels and set their alignment.
+    plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
+             rotation_mode="anchor")
+
+    # Loop over data dimensions and create text annotations.
+    for i in range(len(df_heatmap.index)):
+        for j in range(len(df_heatmap.columns)):
+            # Get the real value (unclipped) for annotation
+            real_value = df_heatmap.values[i, j]
+
+            # Annotate with the real value
+            text = ax.text(j, i, np.around(real_value, decimals=2),
+                           ha="center", va="center", color="k", fontsize=25)
+
+    ax.set_title('Model = ARMAConv', fontsize=30)
+
+    # Add a single colorbar for all heatmaps
+    cbar = fig.colorbar(im, ax=ax, location='right',
+                        shrink=0.75, label='Validation MAPE')
+
+    # Save and show the figure
+    plt.show()
+    plt.savefig(savename, bbox_inches='tight')
+
+
+# HEATMAP FOUR MODELS
+
 path = os.path.dirname(os.path.realpath(__file__))
 path_data = os.path.join(os.path.dirname(os.path.realpath(
     os.path.dirname(os.path.realpath(path)))), 'model_evaluations_paper')
-filepath = os.path.join(path_data, filename)
-df = pd.DataFrame(data=pd.read_csv(filepath))
+
+
+# TEST MAPE
+dimension1 = 'number_of_layers'
+dimension2 = 'number_of_neurons'
+dimension3 = 'kfold_test'
+# dimension3 = 'training_time'
+# equal nodes
+filename = 'GNN_gridsearch_withCV_equalnodes.csv'  # filename 4x3 all models
+df = pd.DataFrame(data=pd.read_csv(os.path.join(path_data, filename)))
+df_2 = pd.DataFrame(data=pd.read_csv(os.path.join(
+    path_data, 'GNN_gridsearch_ARMA_samenodes_2.csv')))  # etxneded grid search ARMAConv
+df_3 = pd.DataFrame(data=pd.read_csv(os.path.join(
+    path_data, 'GNN_gridsearch_ARMA_equalnodes_fillNAN.csv')))  # fill NAN from ARMAConv
+df_all = pd.concat([df, df_2, df_3])
+savename = "GNN_gridsearch_equalnodes.png"
+
+heatmap_gridsearch_results(
+    df_all, dimension1, dimension2, dimension3, savename, vmin=None, vmax=70)
+
+# nodes with variance
+filename = 'GNN_gridsearch_withCV_nodeswithvariance.csv'  # 3x4 heatmaps
+df = pd.DataFrame(data=pd.read_csv(os.path.join(path_data, filename)))
+df_2 = pd.DataFrame(data=pd.read_csv(os.path.join(
+    path_data, 'GNN_gridsearch_ARMA_nodeswithvariance_2.csv')))  # extended grid search for ARMAConv
+df_3 = pd.DataFrame(data=pd.read_csv(os.path.join(
+    path_data, 'GNN_gridsearch_ARMA_nodeswithvariance_part3.csv')))
+df_all = pd.concat([df, df_2, df_3])
 savename = "GNN_gridsearch_nodeswithvariance.png"
-# savename = "GNN_gridsearch_equalnodes.png"
-heatmap_gridsearch_results(df, savename, vmin=None, vmax=110)
+
+heatmap_gridsearch_results(
+    df_all, dimension1, dimension2, dimension3, savename, vmin=None, vmax=70)
+
+# HEATMAP FOR ARMAConv HYPERPARAMETERS
+dimension1 = 'order'
+dimension2 = 'iterations'
+dimension3 = 'kfold_test'
+
+# equal nodes
+
+filename1 = 'GNN_gridsearch_ARMAConv_hyperparameters_4thrun.csv'
+filename2 = 'GNN_gridsearch_ARMAConv_hyperparameters_transposed.csv'
+filename3 = 'GNN_gridsearch_ARMAConv_hyperparameters_5thrun.csv'
+filename4 = 'GNN_gridsearch_ARMAConv_hyperparameters_transposed_part2.csv'
+
+df1 = pd.DataFrame(data=pd.read_csv(os.path.join(path_data, filename1)))
+df2 = pd.DataFrame(data=pd.read_csv(os.path.join(path_data, filename2)))
+df3 = pd.DataFrame(data=pd.read_csv(os.path.join(path_data, filename3)))
+df4 = pd.DataFrame(data=pd.read_csv(os.path.join(path_data, filename4)))
+# df4 = pd.DataFrame(data=pd.read_csv(os.path.join(path_data, filename4)))
+df_all = pd.concat([df1, df2, df3, df4])
+
+
+savename = 'ARMAConv_hyper_equalnodes.png'
+
+heatmap_ARMAConv(df_all, savename, dimension1, dimension2, dimension3,
+                 vmin=None, vmax=None)
+
+
+# nodes with variance
+
+filename1 = 'GNN_gridsearch_ARMAConv_hyperparameters_nodeswithvariance.csv'
+filename2 = 'GNN_gridsearch_ARMAConv_hyperparameters_transposed.csv'
+filename3 = 'GNN_gridsearch_ARMAConv_hyperparameters_transposed_part2.csv'
+
+df1 = pd.DataFrame(data=pd.read_csv(os.path.join(path_data, filename1)))
+df2 = pd.DataFrame(data=pd.read_csv(os.path.join(path_data, filename2)))
+df3 = pd.DataFrame(data=pd.read_csv(os.path.join(path_data, filename3)))
+savename = 'ARMAConv_hyper_nodeswithvariance.png'
+df_all = pd.concat([df1, df2, df_3])
+heatmap_ARMAConv(df_all, dimension1, dimension2, dimension3, savename,
+                 vmin=None, vmax=None)
