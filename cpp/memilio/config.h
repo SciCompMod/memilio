754675b9f1552739205c06652c9901e7f0e13277
lenaploetzke <70579874+lenaploetzke@users.noreply.github.com>
2024-12-13 10:28:01 +0100

    1154 integrator is handled as adaptive if using small fixed step sizes (#1155)
    Add a tolerance in OdeIntegrator when detecting whether the current IntegratorCore is adaptive. 
    
    Co-authored-by: reneSchm <49305466+reneSchm@users.noreply.github.com>

--- cpp/memilio/config.h
+++ cpp/memilio/config.h
@@ -30,6 +30,17 @@
 
 using ScalarType = double;
 
+namespace ad
+{
+namespace internal
+{
+// Forward declaration used for support of ad types in Limits.
+// These templates are called AD_TAPE_REAL and DATA_HANDLER internally in AD.
+template <class FP, class DataHandler>
+struct active_type;
+} // namespace internal
+} // namespace ad
+
 namespace mio
 {
 /**
@@ -65,6 +76,15 @@ struct Limits<double> {
     static constexpr double zero_tolerance()
     {
         return 1e-12;
+    }
+};
+
+template <class FP, class DataHandler>
+struct Limits<ad::internal::active_type<FP, DataHandler>> {
+    /// @brief Returns the limit under which an AD value may be rounded down to zero.
+    static constexpr FP zero_tolerance()
+    {
+        return Limits<FP>::zero_tolerance();
     }
 };
 /** @} */
