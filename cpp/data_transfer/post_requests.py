213f72ac2106dcfb9b263973f926ce16e551d4ca
HenrZu <69154294+HenrZu@users.noreply.github.com>
2024-12-19 16:03:21 +0100

    make post function more modular

--- cpp/data_transfer/post_requests.py
+++ cpp/data_transfer/post_requests.py
@@ -2,6 +2,7 @@ import requests
 import datetime
 import json
 from itertools import combinations
+import memilio.epidata.geoModificationGermany as gMG
 
 header = {'Authorization': "Bearer anythingAsPasswordIsFineCurrently"}
 
@@ -82,9 +83,7 @@ def delete_everything_from_db():
     print(requests.get(url + "scenarios/", headers=header).json())
 
 
-def post_to_db():
-    # Define all necessary data and post.
-
+def post_to_db_compartments():
     compartment_data = [
         {"name": "MildInfections",
          "description": "MildInfections",
@@ -107,12 +106,11 @@ def post_to_db():
     for compartment in compartment_data:
         post_response = requests.post(
             url + "compartments/", json=compartment, headers=header)
+        if (post_response.status_code != 200):
+            print(post_response.status_code)
 
-        # post_response_json = post_response.json()
-        # print(post_response_json)
 
-        print(post_response.status_code)
-
+def post_to_db_groups():
     group_data = [
         {"name": "age_0", "description": "0-4", "category": "age"},
         {"name": "age_1", "description": "5-14", "category": "age"},
@@ -125,8 +123,11 @@ def post_to_db():
     for group in group_data:
         post_response = requests.post(
             url + "groups/", json=group, headers=header)
-        print(post_response.status_code)
+        if (post_response.status_code != 200):
+            print(post_response.status_code)
 
+
+def post_to_db_interventions():
     intervention_data = [
         {
             "name": "School closure",
@@ -158,8 +159,11 @@ def post_to_db():
     for intervention in intervention_data:
         post_response = requests.post(
             url + "interventions/templates/", json=intervention, headers=header)
-        print(post_response.status_code)
+        if (post_response.status_code != 200):
+            print(post_response.status_code)
 
+
+def post_to_db_parameters():
     parameter_data = [
         {"name": "TimeExposed",
          "description": "TimeExposed"},
@@ -208,8 +212,11 @@ def post_to_db():
     for parameter in parameter_data:
         post_response = requests.post(
             url + "parameterdefinitions/", json=parameter, headers=header)
-        print(post_response.status_code)
+        if (post_response.status_code != 200):
+            print(post_response.status_code)
 
+
+def post_to_db_model():
     get_compartments = requests.get(url + "compartments/", headers=header)
     compartments = [compartment["id"]
                     for compartment in get_compartments.json()]
@@ -231,411 +238,17 @@ def post_to_db():
 
     post_response = requests.post(
         url + "models/", json=model_data, headers=header)
-    print(post_response.status_code)
+    if (post_response.status_code != 200):
+        print(post_response.status_code)
 
-    nodes_init = [
-        "00000",
-        "01001",
-        "01002",
-        "01003",
-        "01004",
-        "01051",
-        "01053",
-        "01054",
-        "01055",
-        "01056",
-        "01057",
-        "01058",
-        "01059",
-        "01060",
-        "01061",
-        "01062",
-        "02000",
-        "03101",
-        "03102",
-        "03103",
-        "03151",
-        "03153",
-        "03154",
-        "03155",
-        "03157",
-        "03158",
-        "03159",
-        "03241",
-        "03251",
-        "03252",
-        "03254",
-        "03255",
-        "03256",
-        "03257",
-        "03351",
-        "03352",
-        "03353",
-        "03354",
-        "03355",
-        "03356",
-        "03357",
-        "03358",
-        "03359",
-        "03360",
-        "03361",
-        "03401",
-        "03402",
-        "03403",
-        "03404",
-        "03405",
-        "03451",
-        "03452",
-        "03453",
-        "03454",
-        "03455",
-        "03456",
-        "03457",
-        "03458",
-        "03459",
-        "03460",
-        "03461",
-        "03462",
-        "04011",
-        "04012",
-        "05111",
-        "05112",
-        "05113",
-        "05114",
-        "05116",
-        "05117",
-        "05119",
-        "05120",
-        "05122",
-        "05124",
-        "05154",
-        "05158",
-        "05162",
-        "05166",
-        "05170",
-        "05314",
-        "05315",
-        "05316",
-        "05334",
-        "05358",
-        "05362",
-        "05366",
-        "05370",
-        "05374",
-        "05378",
-        "05382",
-        "05512",
-        "05513",
-        "05515",
-        "05554",
-        "05558",
-        "05562",
-        "05566",
-        "05570",
-        "05711",
-        "05754",
-        "05758",
-        "05762",
-        "05766",
-        "05770",
-        "05774",
-        "05911",
-        "05913",
-        "05914",
-        "05915",
-        "05916",
-        "05954",
-        "05958",
-        "05962",
-        "05966",
-        "05970",
-        "05974",
-        "05978",
-        "06411",
-        "06412",
-        "06413",
-        "06414",
-        "06431",
-        "06432",
-        "06433",
-        "06434",
-        "06435",
-        "06436",
-        "06437",
-        "06438",
-        "06439",
-        "06440",
-        "06531",
-        "06532",
-        "06533",
-        "06534",
-        "06535",
-        "06611",
-        "06631",
-        "06632",
-        "06633",
-        "06634",
-        "06635",
-        "06636",
-        "07111",
-        "07131",
-        "07132",
-        "07133",
-        "07134",
-        "07135",
-        "07137",
-        "07138",
-        "07140",
-        "07141",
-        "07143",
-        "07211",
-        "07231",
-        "07232",
-        "07233",
-        "07235",
-        "07311",
-        "07312",
-        "07313",
-        "07314",
-        "07315",
-        "07316",
-        "07317",
-        "07318",
-        "07319",
-        "07320",
-        "07331",
-        "07332",
-        "07333",
-        "07334",
-        "07335",
-        "07336",
-        "07337",
-        "07338",
-        "07339",
-        "07340",
-        "08111",
-        "08115",
-        "08116",
-        "08117",
-        "08118",
-        "08119",
-        "08121",
-        "08125",
-        "08126",
-        "08127",
-        "08128",
-        "08135",
-        "08136",
-        "08211",
-        "08212",
-        "08215",
-        "08216",
-        "08221",
-        "08222",
-        "08225",
-        "08226",
-        "08231",
-        "08235",
-        "08236",
-        "08237",
-        "08311",
-        "08315",
-        "08316",
-        "08317",
-        "08325",
-        "08326",
-        "08327",
-        "08335",
-        "08336",
-        "08337",
-        "08415",
-        "08416",
-        "08417",
-        "08421",
-        "08425",
-        "08426",
-        "08435",
-        "08436",
-        "08437",
-        "09161",
-        "09162",
-        "09163",
-        "09171",
-        "09172",
-        "09173",
-        "09174",
-        "09175",
-        "09176",
-        "09177",
-        "09178",
-        "09179",
-        "09180",
-        "09181",
-        "09182",
-        "09183",
-        "09184",
-        "09185",
-        "09186",
-        "09187",
-        "09188",
-        "09189",
-        "09190",
-        "09261",
-        "09262",
-        "09263",
-        "09271",
-        "09272",
-        "09273",
-        "09274",
-        "09275",
-        "09276",
-        "09277",
-        "09278",
-        "09279",
-        "09361",
-        "09362",
-        "09363",
-        "09371",
-        "09372",
-        "09373",
-        "09374",
-        "09375",
-        "09376",
-        "09377",
-        "09461",
-        "09462",
-        "09463",
-        "09464",
-        "09471",
-        "09472",
-        "09473",
-        "09474",
-        "09475",
-        "09476",
-        "09477",
-        "09478",
-        "09479",
-        "09561",
-        "09562",
-        "09563",
-        "09564",
-        "09565",
-        "09571",
-        "09572",
-        "09573",
-        "09574",
-        "09575",
-        "09576",
-        "09577",
-        "09661",
-        "09662",
-        "09663",
-        "09671",
-        "09672",
-        "09673",
-        "09674",
-        "09675",
-        "09676",
-        "09677",
-        "09678",
-        "09679",
-        "09761",
-        "09762",
-        "09763",
-        "09764",
-        "09771",
-        "09772",
-        "09773",
-        "09774",
-        "09775",
-        "09776",
-        "09777",
-        "09778",
-        "09779",
-        "09780",
-        "10041",
-        "10042",
-        "10043",
-        "10044",
-        "10045",
-        "10046",
-        "11000",
-        "12051",
-        "12052",
-        "12053",
-        "12054",
-        "12060",
-        "12061",
-        "12062",
-        "12063",
-        "12064",
-        "12065",
-        "12066",
-        "12067",
-        "12068",
-        "12069",
-        "12070",
-        "12071",
-        "12072",
-        "12073",
-        "13003",
-        "13004",
-        "13071",
-        "13072",
-        "13073",
-        "13074",
-        "13075",
-        "13076",
-        "14511",
-        "14521",
-        "14522",
-        "14523",
-        "14524",
-        "14612",
-        "14625",
-        "14626",
-        "14627",
-        "14628",
-        "14713",
-        "14729",
-        "14730",
-        "15001",
-        "15002",
-        "15003",
-        "15081",
-        "15082",
-        "15083",
-        "15084",
-        "15085",
-        "15086",
-        "15087",
-        "15088",
-        "15089",
-        "15090",
-        "15091",
-        "16051",
-        "16052",
-        "16053",
-        "16054",
-        "16055",
-        "16061",
-        "16062",
-        "16063",
-        "16064",
-        "16065",
-        "16066",
-        "16067",
-        "16068",
-        "16069",
-        "16070",
-        "16071",
-        "16072",
-        "16073",
-        "16074",
-        "16075",
-        "16076",
-        "16077"
-    ]
+
+def post_to_db_nodes():
+    nodes_init = gMG.get_county_ids()
+    nodes_init.insert(0, '00000')
+    # every county ID should have 5 digits
+    nodes_init = [str(countyID).zfill(5) for countyID in nodes_init]
+    # transform nodes_init to strings
+    nodes_init = [str(countyID) for countyID in nodes_init]
 
     node_data = [{"nuts": f"{node}",
                   "name": f"{node}"} for node in nodes_init]
@@ -643,8 +256,11 @@ def post_to_db():
     for node in node_data:
         post_response = requests.post(
             url + "nodes/", json=node, headers=header)
-        print(post_response.status_code)
+        if (post_response.status_code != 200):
+            print(post_response.status_code)
 
+
+def post_to_db_nodelist():
     get_nodes = requests.get(url + "nodes/", headers=header)
     nodes = [node["id"] for node in get_nodes.json()]
 
@@ -656,9 +272,11 @@ def post_to_db():
 
     post_response = requests.post(
         url + "nodelists/", json=nodelist_data, headers=header)
-    print(post_response.status_code)
+    if (post_response.status_code != 200):
+        print(post_response.status_code)
 
-    # Scenario
+
+def post_to_db_scenarios():
     # Define start and end date of simulation
     start_date = datetime.datetime.now().strftime("%Y-%m-%d")
     end_date = (datetime.datetime.now() +
@@ -716,7 +334,8 @@ def post_to_db():
         "ReducTimeInfectedMild": [[1., 1., 1., 1., 1., 1.], [1., 1., 1., 1., 1., 1.]],
         "Seasonality": [[0.1, 0.1, 0.1, 0.1, 0.1, 0.1], [0.3, 0.3, 0.3, 0.3, 0.3, 0.3]]}
 
-    # get_parameters = requests.get(url + "parameterdefinitions/", headers=header)
+    get_parameters = requests.get(
+        url + "parameterdefinitions/", headers=header)
 
     modelparameters_entry = []
     for parameter in get_parameters.json():
@@ -832,15 +451,22 @@ def post_to_db():
             "linkedInterventions": intervention_entry
         })
 
-    # with open("scenarios.json", "w") as outfile:
-    #     json.dump(scenario_data, outfile)
-
     for scenario in scenario_data:
         post_response = requests.post(
             url + "scenarios/", json=scenario, headers=header)
         if (post_response.status_code != 200):
             print(post_response.status_code)
-    print()
+
+
+def post_to_db():
+    # Define all necessary data and post.
+    post_to_db_compartments()
+    post_to_db_groups()
+    post_to_db_interventions()
+    post_to_db_parameters()
+    post_to_db_model()
+    post_to_db_nodes()
+    post_to_db_nodelist()
 
 
 def main():
