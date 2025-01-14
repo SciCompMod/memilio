c74474fe68517d618523beff48fa97d6f46ff894
Gerstein <gers_ca@fe-store02.maas>
2025-01-14 14:29:09 +0100

    new likwid example

--- cpp/examples/likwid_test.cpp
+++ cpp/examples/likwid_test.cpp
@@ -1,12 +1,20 @@
-#include <vector>
+#include <cstddef> 
+
+double work(double* a, size_t n) {
+    double s = 0;
+    for (size_t j=0; j<n; j++){
+        s=s+a[j]*a[j];
+    }
+    return s;
+}
 
 int main()
 {
     const size_t n=1e9;
-    double s, a[n];
-    for(size_t i=0; i<1e5; i++){
-        for(size_t j=0; j<n; j++){
-            s=s+a[j]*a[j];
-        }
+    double s = 0;
+    double a[n];
+     
+    for(size_t i=0; i<1e7; i++){
+        s += work(a, n);
     }
 }
