a6b726985a9a536d04cec70c30e274d8d8edcefd
Daniel Richter <danielrichter01@gmx.de>
2025-01-21 13:11:02 +0100

    Dictionary for scanner.py, not all strings replaced!

--- pycode/memilio-generation/memilio/generation/scanner.py
+++ pycode/memilio-generation/memilio/generation/scanner.py
@@ -31,6 +31,8 @@ from clang.cindex import *
 from typing_extensions import Self
 
 from memilio.generation import IntermediateRepresentation, utility
+import json
+from types import SimpleNamespace
 
 
 if TYPE_CHECKING:
@@ -51,6 +53,11 @@ class Scanner:
         utility.try_set_libclang_path(
             self.config.optional.get("libclang_library_path"))
 
+        file_path = os.path.abspath("dict.json")
+        with open(file_path, "r") as file:
+            self.dict = json.load(
+                file, object_hook=lambda d: SimpleNamespace(**d))
+
     def extract_results(self: Self, root_cursor: Cursor) -> IntermediateRepresentation:
         """! Extract the information of the abstract syntax tree and save them in the dataclass intermed_repr.
         Call find_node to visit all nodes of abstract syntax tree and finalize to finish the extraction.
@@ -58,7 +65,7 @@ class Scanner:
         @param root_cursor Represents the root node of the abstract syntax tree as a Cursor object from libclang.
         @return Information extracted from the model saved as an IntermediateRepresentation.
         """
-        if self.config.model_class != "Model":
+        if self.config.model_class != self.dict.model:
             raise AssertionError("set a model name")
 
         intermed_repr = IntermediateRepresentation()
@@ -82,7 +89,7 @@ class Scanner:
 
         vor_parameter_space = str(path_vor_parameter_space)
 
-        new_path_source_file = vor_parameter_space + "/parameter_space.cpp"
+        new_path_source_file = vor_parameter_space + self.dict.parameterspace
 
         if (os.path.isfile(new_path_source_file)):
 
@@ -135,7 +142,7 @@ class Scanner:
         @param node Current node represented as a Cursor object.
         @param intermed_repr Dataclass used for saving the extracted model features.
         """
-        if node.spelling.strip() != "":
+        if node.spelling.strip() != self.dict.empty:
             intermed_repr.enum_populations[node.spelling] = []
 
     def check_enum_const(
@@ -166,7 +173,7 @@ class Scanner:
             self.check_model_includes(node, intermed_repr)
             self.check_age_group(node, intermed_repr)
 
-        elif (node.spelling == "Simulation"):
+        elif (node.spelling == self.dict.simulation):
             intermed_repr.simulation = True
 
         elif (intermed_repr.has_age_group and self.config.parameterset + "<FP>" in [base.spelling for base in node.get_children()]):
@@ -189,10 +196,10 @@ class Scanner:
             base_name = base.spelling
             base_type = base.type
 
-            if "FlowModel" in base_name:
+            if self.dict.flowmodel in base_name:
                 intermed_repr.is_flowmodel = True
 
-            if "CompartmentalModel" in base_name and "mio" in node.semantic_parent.spelling:
+            if self.dict.compartmentalmodel in base_name and "mio" in node.semantic_parent.spelling:
                 intermed_repr.is_compartmentalmodel = True
 
             intermed_repr.model_base.append(
@@ -225,19 +232,19 @@ class Scanner:
         intermed_repr.include_list.append(filepaths[1])
 
         intermed_repr.include_list.append(
-            filepaths[1].replace("model.h", "") + "infection_state.h")
+            filepaths[1].replace(self.dict.modelfile, self.dict.empty) + self.dict.infectionstatefile)
 
         for file in os.listdir(filepaths[0]):
-            if file == "parameter_space.h":
+            if file == self.dict.parameterspacefile:
                 intermed_repr.include_list.append(filepaths[1].replace(
-                    "model.h", "") +
-                    "parameter_space.h")
-            elif file == "analyze_result.h":
+                    self.dict.modelfile, self.dict.empty) +
+                    self.dict.parameterspacefile)
+            elif file == self.dict.analyzeresultfile:
                 model_has_analyze_results = True
 
         if model_has_analyze_results:
             intermed_repr.include_list.append(
-                filepaths[1].replace("model.h", "") + "analyze_result.h")
+                filepaths[1].replace(self.dict.modelfile, self.dict.empty) + self.dict.analyzeresultfile)
         else:
             intermed_repr.include_list.append("memilio/data/analyze_result.h")
 
@@ -255,7 +262,7 @@ class Scanner:
             if base.kind != CursorKind.CXX_BASE_SPECIFIER:
                 continue
             for base_template_arg in base.get_children():
-                if (base_template_arg.kind == CursorKind.TYPE_REF and "AgeGroup" in base_template_arg.spelling):
+                if (base_template_arg.kind == CursorKind.TYPE_REF and self.dict.agegroup in base_template_arg.spelling):
                     intermed_repr.has_age_group = True
                     for child in base_template_arg.get_definition().get_children():
                         if child.kind == CursorKind.CXX_BASE_SPECIFIER:
@@ -275,8 +282,8 @@ class Scanner:
         @param node Current node represented as a Cursor object.
         @param intermed_repr Dataclass used for saving the extracted model features.
         """
-        if intermed_repr.model_class == "Model":
-            if node.spelling.startswith("Model") and ('<' in node.spelling and '>' in node.spelling):
+        if intermed_repr.model_class == self.dict.model:
+            if node.spelling.startswith(self.dict.model) and ('<' in node.spelling and '>' in node.spelling):
                 init = {"type": [], "name": []}
                 for arg in node.get_arguments():
                     tokens = []
@@ -314,7 +321,7 @@ class Scanner:
 
         population_groups = []
         for value in intermed_repr.model_base[0:]:
-            if "FlowModel" in value[0]:
+            if self.dict.flowmodel in value[0]:
                 start = value[0].find("Populations<")
                 end = value[0].find(">", start)
 
