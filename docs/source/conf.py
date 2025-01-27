9ec1f0db499db492ff95b0f26b157ad82b7d82f2
Kilian Volmer <kvolmer@uni-bonn.de>
2025-01-27 11:18:32 +0100

    CHG: Add doxygen file generation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,6 +9,15 @@ author = ''
 release = ''
 version = '1.3.0'
 
+import subprocess, os
+
+read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
+
+if read_the_docs_build:
+
+     subprocess.call('cd ..; doxygen', shell=True)
+
+
 import sys
 
 sys.path.append("../ext/breathe")
