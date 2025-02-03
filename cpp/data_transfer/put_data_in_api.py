394bfcfd69e79f163e2c0ef2abfe407bee0e7f5f
Jadebeck, Johann Fredrik <j.jadebeck@fz-juelich.de>
2025-02-03 21:27:37 +0100

    update delay and improve logging for put of scenarios

--- cpp/data_transfer/put_data_in_api.py
+++ cpp/data_transfer/put_data_in_api.py
@@ -4,6 +4,7 @@ from zipfile import ZipFile
 
 import logging
 import time
+
 log = logging.getLogger(__file__)
 
 header = {'Authorization': "Bearer anythingAsPasswordIsFineCurrently"}
@@ -17,8 +18,8 @@ def write_zip(path_to_data, zipped_name, percentiles=['p50'], case_data=False):
     @param[in] percentiles. List[str]
     """
     log.info(path_to_data)
-    zipfile = os.path.join(path_to_data, zipped_name +  ".zip")
-    with ZipFile(zipfile,"w") as zip_object:
+    zipfile = os.path.join(path_to_data, zipped_name + ".zip")
+    with ZipFile(zipfile, "w") as zip_object:
         for percentile in percentiles:
             if not case_data:
                 real_path_to_data = os.path.join(path_to_data, zipped_name, percentile)
@@ -36,32 +37,45 @@ def put_scenario(scenario_id, zip_file, url):
     fileobj = open(zip_file, 'rb')
     put_response = requests.put(url + "scenarios/" + scenario_id, headers=header,
                                 files={"file": (zip_file, fileobj)})
-    log.info(f'Put HTTP response code for scenario {scenario_id} was {put_response.status_code}')
+    print(f'Put HTTP response code for scenario {scenario_id} was {put_response.status_code}')
+
 
 def put_scenarios(path_to_scenario_results, url, delay=10):
-    scenarios = requests.get(url + "scenarios/", headers=header).json()
+    print(f'Uploading scenarios to {url} from {path_to_scenario_results}')
+    scenarios = requests.get(url + "scenarios/", headers=header)
+    print(f'scenarios response code was {scenarios.status_code}')
+    print(f'scenarios response was {scenarios.content}')
+    scenarios = scenarios.json()
     for scenario in scenarios:
-        real_path_to_scenario_results = path_to_scenario_results #+ f"{scenario['name']}_{scenario['id']}/"
-        if not os.path.exists(real_path_to_scenario_results+ f"/{scenario['name']}_{scenario['id']}/"):
+        real_path_to_scenario_results = path_to_scenario_results  # + f"{scenario['name']}_{scenario['id']}/"
+        scenario_path = real_path_to_scenario_results + f"/{scenario['name']}_{scenario['id']}/"
+        if not os.path.exists(scenario_path):
+            log.error(f'Path {scenario_path} does not exist')
             continue
-        if scenario["name"]=="casedata":
-            percentiles=['p50']
-            zip_file = write_zip(path_to_data=real_path_to_scenario_results, zipped_name=f"{scenario['name']}_{scenario['id']}", percentiles=percentiles, case_data=True)
+        if scenario["name"] == "casedata":
+            percentiles = ['p50']
+            zip_file = write_zip(path_to_data=real_path_to_scenario_results,
+                                 zipped_name=f"{scenario['name']}_{scenario['id']}", percentiles=percentiles,
+                                 case_data=True)
         else:
-            percentiles=['p25', 'p50', 'p75']
-            zip_file = write_zip(path_to_data=real_path_to_scenario_results, zipped_name=f"{scenario['name']}_{scenario['id']}", percentiles=percentiles, case_data=False)
+            percentiles = ['p25', 'p50', 'p75']
+            zip_file = write_zip(path_to_data=real_path_to_scenario_results,
+                                 zipped_name=f"{scenario['name']}_{scenario['id']}", percentiles=percentiles,
+                                 case_data=False)
         # parse id
         # path_to_data = os.path.join(path_to_scenario_results)
-        
+
+        print(f'uploading {scenario["id"]}')
         put_scenario(scenario['id'], zip_file=zip_file, url=url)
+        print(f'uploaded {scenario["id"]}')
+        print(f'waiting for  {delay} seconds before uploading next scenario')
         time.sleep(delay)
 
-        
 
 def main():
-    cwd = os.getcwd()
-    url = "http://localhost:8123/"
-    put_scenarios(os.path.join(cwd, "test_results_osecirvvs/"), url)
+    url = 'https://zam10063.zam.kfa-juelich.de/api-new/'
+    put_scenarios(os.path.join('/home/jadebeck/', "results_osecirvvs/"), url)
+
 
 if __name__ == '__main__':
     main()
