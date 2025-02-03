c9522ad3743e9e046aea7b00f72275b39f0cb5ac
Daniel Richter <danielrichter01@gmx.de>
2025-02-03 08:17:30 +0100

    test changes, instead of using an temporary file for the target folder we use the generation-test folder

--- pycode/memilio-generation/memilio/generation_test/test_oseir_generation.py
+++ pycode/memilio-generation/memilio/generation_test/test_oseir_generation.py
@@ -76,7 +76,8 @@ class TestOseirGeneration(unittest.TestCase):
             "/cpp/models/ode_seir/model.cpp"
 
         # Could be any target folder
-        conf.target_folder = self.test_dir.name
+        conf.target_folder = self.project_path + \
+            "pycode/memilio-generation/memilio/generation-test"
         self.scanner = Scanner(conf)
         self.ast = AST(conf)
 
