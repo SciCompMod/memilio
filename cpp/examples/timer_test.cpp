0ee71ffc2ac0f6ef808947dceadaee063ca3eefe
reneSchm <49305466+reneSchm@users.noreply.github.com>
2025-03-14 20:08:37 +0100

    refactor timer structure

--- cpp/examples/timer_test.cpp
+++ cpp/examples/timer_test.cpp
@@ -1,4 +1,5 @@
 #include "memilio/utils/mioomp.h"
+#include "memilio/utils/stl_util.h"
 
 #include <chrono>
 #include <ctime>
@@ -7,136 +8,98 @@
 #include <map>
 #include <mutex>
 #include <string>
+#include <string_view>
 #include <thread>
 #include <utility>
+#include <vector>
 
-// #define DEBUG(cout_args) std::cerr << cout_args << std::endl << std::flush;
-#define DEBUG(cout_args)
+// clang-format off
+#define CONST_LITERAL(literal) []() constexpr { return literal; }
+// clang-format on
 
-//////////////////////
-// src: https://stackoverflow.com/questions/15858141/conveniently-declaring-compile-time-strings-in-c/15863804#15863804
-//////////////////////
-
-// helper function
-constexpr unsigned c_strlen(char const* str, unsigned count = 0)
+namespace mio
 {
-    return ('\0' == str[0]) ? count : c_strlen(str + 1, count + 1);
+
+namespace details
+{
+
+constexpr std::size_t constexpr_strlen(const char* string_literal)
+{
+    // returns length of string, does not count terminating 0 char
+    return string_literal[0] == '\0' ? 0 : constexpr_strlen(string_literal + 1) + 1;
 }
 
-// destination "template string" type
-template <char... chars>
-struct exploded_string {
-    inline static constexpr char data[] = {chars...};
-    static void print()
-    {
-        char const str[] = {chars...};
-        std::cout.write(str, sizeof(str));
-    }
-};
-
-// struct to explode a `char const*` to an `exploded_string` type
-template <typename StrProvider, unsigned len, char... chars>
-struct explode_impl {
-    using result = typename explode_impl<StrProvider, len - 1, StrProvider::str()[len - 1], chars...>::result;
-};
-
-// recursion end
-template <typename StrProvider, char... chars>
-struct explode_impl<StrProvider, 0, chars...> {
-    using result = exploded_string<chars...>;
-};
-
-// syntactical sugar
-template <typename StrProvider>
-using explode = typename explode_impl<StrProvider, c_strlen(StrProvider::str())>::result;
-
-//////////////////////
-//////////////////////
-//////////////////////
+} // namespace details
 
 namespace timing
 {
 
-using ClockType    = std::chrono::steady_clock;
-using TimeType     = ClockType::time_point;
-using DurationType = ClockType::duration;
+#ifdef MEMILIO_ENABLE_OPENMP
 
-// TODO: add thread id?
-using TimerRegistration = std::pair<const char*, const DurationType*>;
+using TimeType     = decltype(omp_get_wtime());
+using DurationType = decltype(omp_get_wtime());
 
-inline double printable_time(DurationType time)
+inline double time_in_seconds(DurationType time)
+{
+    return time;
+}
+
+inline TimeType get_time_now()
+{
+    return omp_get_wtime();
+}
+
+#else
+
+using TimeType     = std::chrono::steady_clock::time_point;
+using DurationType = std::chrono::steady_clock::duration;
+
+inline double time_in_seconds(DurationType time)
 {
     return std::chrono::duration_cast<std::chrono::duration<double>>(time).count();
 }
 
-template <const char Name[]>
-struct my_str_provider {
-    constexpr static char const* str()
+inline TimeType get_time_now()
+{
+    return std::chrono::steady_clock::now();
+}
+
+#endif
+
+class BasicTimer
+{
+public:
+    void start()
     {
-        return Name;
+        m_start_time = get_time_now();
     }
+
+    void stop()
+    {
+        const TimeType stop_time = get_time_now();
+        m_elapsed_time += stop_time - m_start_time;
+    }
+
+    DurationType get_elapsed_time() const
+    {
+        return m_elapsed_time;
+    }
+
+private:
+    TimeType m_start_time;
+    DurationType m_elapsed_time;
 };
 
-template <char... Chars>
-class Timer2;
-
-template <char... Chars>
-Timer2<Chars...> timer_type(exploded_string<Chars...>);
-
-template <const char Name[]>
-using timer_from_name = decltype(timer_type(explode<my_str_provider<Name>>{}));
+struct TimerRegistration {
+    std::string name;
+    BasicTimer& timer;
+    int thread_id;
+};
 
 // holds raw pointers to names and durations of the thread_local Timer instances
 class TimerRegistrar
 {
 public:
-    void add_timer(const TimerRegistration& registration)
-    {
-        m_registration_lock.lock();
-        m_registry.push_back(registration);
-        m_registration_lock.unlock();
-    }
-
-    // preregistration does not seem to affect timing basically at all. it removes the registration from timers started
-    // before a preregistered timer, but a timer does not measure its own registration
-    //
-    // TODO: does this need to be in a omp parallel to correctly preregister?
-    template <const char*... Names>
-    void preregister_timers()
-    {
-        (timer_from_name<Names>::get_instance(), ...);
-    }
-
-    // questionable whether this should be allowed to be used over print_on_exit.
-    void print_timers(std::ostream& out = std::cout)
-    {
-        PRAGMA_OMP(single)
-        {
-            const auto indent = "  ";
-            out << "All Timers: " << m_registry.size() << "\n";
-            for (const auto& entry : m_registry) {
-                out << indent << entry.first << ": " << std::scientific << printable_time(*entry.second) << "\n";
-            }
-            // dedupe list entries from parallel execution
-            std::map<std::string, DurationType> deduper;
-            for (const auto& entry : m_registry) {
-                deduper[entry.first] = DurationType::zero(); // is this neccessary?
-            }
-            for (const auto& entry : m_registry) {
-                deduper[entry.first] += *entry.second;
-            }
-            out << "Unique Timers (accumulated): " << deduper.size() << "\n";
-            for (const auto& entry : deduper) {
-                out << indent << entry.first << ": " << std::scientific << printable_time(entry.second) << "\n";
-            }
-        }
-    }
-
-    void print_on_exit() // requires dtor (i.e. get_instance scheme)
-    {
-        m_print_on_death = true;
-    }
-
     // can either use get_instance scheme, or nake all members static. the latter does not use ctor/dtor
     //
     // get_instance scheme has the benefit of guaranteeing correct destruction order, see
@@ -149,11 +112,48 @@ public:
         return t;
     }
 
-private:
-    TimerRegistrar()
+    void add_timer(TimerRegistration&& registration)
     {
-        DEBUG("### Registrar at your service")
+        m_registration_lock.lock();
+        m_register.emplace_back(registration);
+        m_registration_lock.unlock();
     }
+
+    const auto& get_register()
+    {
+        return m_register;
+    }
+
+    // questionable whether this should be allowed to be used over print_on_exit.
+    void print_timers(std::ostream& out = std::cout)
+    {
+        PRAGMA_OMP(single)
+        {
+            const auto indent = "  ";
+            out << "All Timers: " << m_register.size() << "\n";
+            for (const auto& entry : m_register) {
+                out << indent << entry.name << ": " << std::scientific
+                    << time_in_seconds(entry.timer.get_elapsed_time()) << "\n";
+            }
+            // dedupe list entries from parallel execution
+            std::map<std::string, DurationType> deduper;
+            for (const auto& entry : m_register) {
+                deduper[entry.name] += entry.timer.get_elapsed_time();
+            }
+            out << "Unique Timers (accumulated): " << deduper.size() << "\n";
+            for (const auto& entry : deduper) {
+                out << indent << entry.first << ": " << std::scientific << time_in_seconds(entry.second) << "\n";
+            }
+        }
+    }
+
+    void print_on_exit() // requires dtor (i.e. get_instance scheme)
+    {
+        m_print_on_death = true;
+    }
+
+private:
+    TimerRegistrar() = default;
 
     ~TimerRegistrar()
     {
@@ -163,218 +163,138 @@ private:
                 std::cout << "Final Timer Summary\n";
                 print_timers();
             }
-            DEBUG("### Registrar retired")
         }
     }
 
-    std::list<TimerRegistration> m_registry;
+    std::list<TimerRegistration> m_register;
     std::mutex m_registration_lock;
     bool m_print_on_death;
 };
 
-// since it is thread_local and typed by its name, no collisions can occur between different timers or threads
-//
-// though one can still double start or stop the timer, creating bogus results. this can be fixed by making everything
-// protected and using e.g. ScopedTimer with inheritance
-//
-// BUG: timers can have same name, but different type, since their type differs by pointer...
-// alternatives: Timer2, enums, C++20
-template <const char Name[]>
-class Timer
+// second attempt, templates dont look nice at all, but are indep. of the char[] used, only using its content
+template <class Tag, char... Chars>
+class Timer : public BasicTimer
 {
 public:
+    static constexpr char Name[] = {Chars..., '\0'};
+
     static Timer& get_instance()
     {
+        // create and return a timer
+        // the constructor is only called the first time this function is used (for each thread)
         thread_local static Timer t;
         return t;
     }
 
-    void start()
-    {
-        m_start_time = ClockType::now();
-    }
-
-    void stop()
-    {
-        const TimeType stop_time = ClockType::now();
-        m_elapsed_time += stop_time - m_start_time;
-    }
-
 private:
     Timer()
-        : m_start_time()
-        , m_elapsed_time(DurationType::zero())
     {
-        register_self();
-        DEBUG("--- Timer registration : " << Name)
+        TimerRegistrar::get_instance().add_timer({Name, *this, mio::get_omp_thread_id()});
     }
-
-    ~Timer()
-    {
-        DEBUG("--- Timer destruction  : " << Name)
-    }
-
-    void register_self() // inline into ctor?
-    {
-        TimerRegistrar::get_instance().add_timer({Name, &m_elapsed_time});
-    }
-
-    TimeType m_start_time;
-    DurationType m_elapsed_time;
 };
 
-// second attempt, templates dont look nice at all, but are indep. of the char[] used, only using its content
-template <char... Chars>
-class Timer2
+namespace details
 {
-public:
-    static constexpr auto Name = exploded_string<Chars...>::data;
 
-    static Timer2& get_instance()
-    {
-        thread_local static Timer2 t;
-        return t;
-    }
-
-    void start()
-    {
-        m_start_time = ClockType::now();
-    }
-
-    void stop()
-    {
-        const TimeType stop_time = ClockType::now();
-        m_elapsed_time += stop_time - m_start_time;
-        DEBUG("    Elapsed: " << printable_time(m_elapsed_time))
-    }
-
-private:
-    Timer2()
-        : m_start_time()
-        , m_elapsed_time(DurationType::zero())
-    {
-        register_self();
-        DEBUG("--- Timer registration : " << Name)
-    }
-
-    ~Timer2()
-    {
-        DEBUG("--- Timer destruction  : " << Name)
-    }
-
-    void register_self() // inline into ctor?
-    {
-        TimerRegistrar::get_instance().add_timer({Name, &m_elapsed_time});
-    }
-
-    TimeType m_start_time;
-    DurationType m_elapsed_time;
-};
-
-// specialization allowing disabling timers by changing their name. good method?
-// alternative : constexpr ifs? keeps stuff in place, but bloates Timer2
-template <>
-class Timer2<'@', 'd', 'i', 's', 'a', 'b', 'l', 'e'>
+template <class Tag, typename Identifier, std::size_t... I>
+constexpr auto& get_timer_by_name(Identifier timer_name, std::index_sequence<I...>)
 {
-public:
-    static constexpr auto Name = "@disable";
+    (void)timer_name; // if the name is "", i.e. empty, the Identifier shows up as unused
+    return Timer<Tag, timer_name()[I]...>::get_instance();
+}
 
-    static Timer2& get_instance()
-    {
-        thread_local static Timer2 t;
-        return t;
-    }
+} // namespace details
 
-    void start()
-    {
-    }
+template <class Tag = void, class Identifier>
+constexpr auto& get_timer(Identifier timer_name)
+{
+    static_assert(std::is_same_v<decltype(std::declval<Identifier>()()), const char*>, "message");
+    return details::get_timer_by_name<Tag>(timer_name,
+                                           std::make_index_sequence<mio::details::constexpr_strlen(timer_name())>{});
+}
 
-    void stop()
-    {
-    }
-};
-
-// starts on creating, stops at end of scope. can be called repeatedly
-//
-// potential misuse by starting this too late in a scope
-template <const char Name[]>
+template <class Tag = void>
 class ScopedTimer
 {
 public:
-    ScopedTimer()
+    template <class Identifier>
+    ScopedTimer(Identifier timer_name)
+        : m_timer(timing::get_timer<Tag>(timer_name))
     {
-        timer_from_name<Name>::get_instance().start();
-        DEBUG("... ScopedTimer start: " << Name)
+        m_timer.start();
     }
+
+    ScopedTimer(timing::BasicTimer& timer)
+        : m_timer(timer)
+    {
+        m_timer.start();
+    }
+
     ~ScopedTimer()
     {
-        DEBUG("... ScopedTimer stop:  " << Name)
-        timer_from_name<Name>::get_instance().stop();
+        m_timer.stop();
     }
+
+private:
+    timing::BasicTimer& m_timer;
 };
 
 } // namespace timing
 
+using timing::ScopedTimer;
+
+} // namespace mio
+
 int main()
 {
-    DEBUG("*** enter main")
+    mio::ScopedTimer timer_hw(CONST_LITERAL("Hello World!"));
 
-    static constexpr char timer_name_hw[]                           = "@disable";
-    static constexpr char timer_name_ms[]                           = "main scope";
-    static constexpr char timer_name_loop_inner[]                   = "Loop Inner";
-    static constexpr char timer_name_loop_inner_prereg[]            = "Loop Inner Prereg";
-    static constexpr char timer_name_loop_outer[]                   = "Loop Outer no Inner";
-    static constexpr char timer_name_loop_outer_with_inner[]        = "Loop Outer with Inner";
-    static constexpr char timer_name_loop_outer_with_inner_prereg[] = "Loop Outer with Inner Prereg";
-    timing::ScopedTimer<timer_name_ms> timer_ms;
-
-    timing::TimerRegistrar::get_instance().preregister_timers<timer_name_loop_inner_prereg>();
-    timing::TimerRegistrar::get_instance().print_on_exit();
+    // timing::TimerRegistrar::get_instance().preregister_timers<timer_name_loop_inner_prereg>();
+    mio::timing::TimerRegistrar::get_instance().print_on_exit();
 
     volatile int ctr = 0;
-    int N            = 10000;
+    int N            = 1000;
     (void)ctr;
     const auto load = [&] {
-        ++ctr;
-        // std::this_thread::sleep_for(std::chrono::milliseconds(3));
+        // ++ctr;
+        std::this_thread::sleep_for(std::chrono::milliseconds(3));
     };
 
-    DEBUG("*** main post setup")
-    std::cout << "Num threads: " << mio::get_num_threads() << "\n";
+    std::cout << "Num threads: " << mio::get_omp_num_threads() << "\n";
 
     {
-        timing::ScopedTimer<timer_name_hw> timer_hw;
+        mio::ScopedTimer timer_ms(CONST_LITERAL("Main"));
 
         {
-            timing::ScopedTimer<timer_name_loop_outer_with_inner> timer_loop_outer_with_inner;
+            mio::ScopedTimer timer_out(CONST_LITERAL("outside loop with inner timer"));
 
             PRAGMA_OMP(parallel for)
             for (int i = 0; i < N; i++) {
-                timing::ScopedTimer<timer_name_loop_inner> timer_loop_inner;
+                mio::ScopedTimer timer_in(CONST_LITERAL("inside loop"));
                 load();
             }
         }
         {
-            timing::ScopedTimer<timer_name_loop_outer_with_inner_prereg> timer_loop_outer_with_inner;
+            mio::ScopedTimer timer_out(CONST_LITERAL(""));
 
             PRAGMA_OMP(parallel for)
             for (int i = 0; i < N; i++) {
-                timing::ScopedTimer<timer_name_loop_inner_prereg> timer_loop_inner;
                 load();
             }
         }
 
         {
-            timing::ScopedTimer<timer_name_loop_outer> timer_loop_outer;
+            auto& timer_out = mio::timing::get_timer(CONST_LITERAL("outside loop manual timer"));
+            timer_out.start();
 
             PRAGMA_OMP(parallel for)
             for (int i = 0; i < N; i++) {
                 load();
             }
+
+            timer_out.stop();
         }
     }
 
-    timing::TimerRegistrar::get_instance().print_timers();
-
-    DEBUG("*** exit main")
+    mio::timing::TimerRegistrar::get_instance().print_timers();
 }
