82539c637fdcc0d61292ebd2f37696c137d10e9d
jubicker <julia.bicker@dlr.de>
2025-03-20 13:18:19 +0100

    documentation AbstractParameterDistribution

--- cpp/memilio/utils/parameter_distribution_wrapper.h
+++ cpp/memilio/utils/parameter_distribution_wrapper.h
@@ -31,9 +31,18 @@
 namespace mio
 {
 
+/**
+ * @brief This class represents an arbitrary ParameterDistribution.
+ * @see mio::ParameterDistribution
+ * This class can for instance be used for model parameters that should have an arbitrary distribution.
+ */
 class AbstractParameterDistribution
 {
 public:
+    /**
+     * The implementation handed to the constructor should have get_sample function 
+     * overloaded with mio::RandomNumberGenerator and mio::abm::PersonalRandomNumberGenerator as input arguments
+     */
     template <class Impl>
     AbstractParameterDistribution(Impl&& dist)
         : m_dist(std::make_shared<Impl>(std::move(dist)))
@@ -94,21 +103,6 @@ public:
 
     AbstractParameterDistribution& operator=(const AbstractParameterDistribution& other) = default;
 
-    double get(RandomNumberGenerator& rng) const
-    {
-        return sample_impl1(m_dist.get(), rng);
-    }
-
-    double get(abm::PersonalRandomNumberGenerator& rng) const
-    {
-        return sample_impl2(m_dist.get(), rng);
-    }
-
-    std::vector<double> params() const
-    {
-        return static_cast<ParameterDistribution*>(m_dist.get())->params();
-    }
-
     bool operator<(const AbstractParameterDistribution& other) const
     {
         return static_cast<ParameterDistribution*>(m_dist.get())
@@ -116,7 +110,33 @@ public:
     }
 
     /**
-     * serialize a AbstractParameterDistribution.
+     * @brief Returns a value sampled with the given distribution.
+     * @param[in] rng RandomNumberGenerator used for sampling. 
+     */
+    double get(RandomNumberGenerator& rng) const
+    {
+        return sample_impl1(m_dist.get(), rng);
+    }
+
+    /**
+     * @brief Returns a value sampled with the given distribution.
+     * @param[in] rng abm::PersonalRandomNumberGenerator used for sampling. 
+     */
+    double get(abm::PersonalRandomNumberGenerator& rng) const
+    {
+        return sample_impl2(m_dist.get(), rng);
+    }
+
+    /**
+     * @brief Get the parameters of the given distribution.
+     */
+    std::vector<double> params() const
+    {
+        return static_cast<ParameterDistribution*>(m_dist.get())->params();
+    }
+
+    /**
+     * serialize an AbstractParameterDistribution.
      * @see mio::serialize
      */
     template <class IOContext>
@@ -126,9 +146,14 @@ public:
     }
 
 private:
-    std::shared_ptr<void> m_dist;
-    double (*sample_impl1)(void*, RandomNumberGenerator&);
-    double (*sample_impl2)(void*, abm::PersonalRandomNumberGenerator&);
+    std::shared_ptr<void> m_dist; ///< Underlying distribtuion.
+    double (*sample_impl1)(
+        void*,
+        RandomNumberGenerator&); ///< Sample function of the distribution which gets a RandomNumberGenerator as rng.
+    double (*sample_impl2)(
+        void*,
+        abm::
+            PersonalRandomNumberGenerator&); ///< Sample function of the distribution which gets a abm::PersonalRandomNumberGenerator as rng.
 };
 
 /**
@@ -165,113 +190,6 @@ IOResult<AbstractParameterDistribution> deserialize_internal(IOContext& io, Tag<
         }
         else {
             return failure(StatusCode::InvalidValue, "Type of ParameterDistribution in AbstractParameterDistribution" +
-                                                         type.value() + " not valid.");
-        }
-    }
-    return failure(type.error());
-}
-
-class ParameterDistributionWrapper
-{
-public:
-    ParameterDistributionWrapper()
-        : m_dist(nullptr)
-    {
-    }
-
-    ParameterDistributionWrapper(ParameterDistribution& dist)
-        : m_dist(std::unique_ptr<ParameterDistribution>(dist.clone()))
-    {
-    }
-
-    ParameterDistributionWrapper(const ParameterDistributionWrapper& other)
-    {
-        m_dist = (other.m_dist == nullptr) ? nullptr : std::unique_ptr<ParameterDistribution>(other.m_dist->clone());
-    }
-
-    ParameterDistributionWrapper(ParameterDistributionWrapper&& other)
-    {
-        m_dist = (other.m_dist == nullptr) ? nullptr : std::unique_ptr<ParameterDistribution>(other.m_dist->clone());
-    }
-
-    ParameterDistributionWrapper& operator=(ParameterDistributionWrapper const& other)
-    {
-        m_dist = (other.m_dist == nullptr) ? nullptr : std::unique_ptr<ParameterDistribution>(other.m_dist->clone());
-        return *this;
-    }
-
-    ParameterDistributionWrapper& operator=(ParameterDistributionWrapper&& other)
-    {
-        m_dist = (other.m_dist == nullptr) ? nullptr : std::unique_ptr<ParameterDistribution>(other.m_dist->clone());
-        return *this;
-    };
-
-    ~ParameterDistributionWrapper() = default;
-
-    std::vector<double> params() const
-    {
-        if (m_dist == nullptr) {
-            log_error("Distribution is not defined. Parameters cannot be deduced.");
-        }
-        return m_dist->params();
-    }
-
-    template <class RNG>
-    double get(RNG& rng)
-    {
-        if (m_dist == nullptr) {
-            log_error("Distribution is not defined. Value cannot be sampled.");
-        }
-        return m_dist->get_sample(rng);
-    }
-
-    /**
-     * serialize this.
-     * @see mio::serialize
-     */
-    template <class IOContext>
-    void serialize(IOContext& io) const
-    {
-        m_dist->serialize(io);
-    }
-
-private:
-    std::unique_ptr<ParameterDistribution> m_dist;
-};
-
-/**
- * deserialize a ParameterDistributionWrapper.
- * @see mio::deserialize
- */
-template <class IOContext>
-IOResult<ParameterDistributionWrapper> deserialize_internal(IOContext& io, Tag<ParameterDistributionWrapper>)
-{
-
-    auto obj  = io.expect_object("ParameterDistribution");
-    auto type = obj.expect_element("Type", Tag<std::string>{});
-    if (type) {
-        if (type.value() == "Uniform") {
-            BOOST_OUTCOME_TRY(auto&& r, ParameterDistributionUniform::deserialize_elements(io, obj));
-            return ParameterDistributionWrapper(r);
-        }
-        else if (type.value() == "Normal") {
-            BOOST_OUTCOME_TRY(auto&& r, ParameterDistributionNormal::deserialize_elements(io, obj));
-            return ParameterDistributionWrapper(r);
-        }
-        else if (type.value() == "LogNormal") {
-            BOOST_OUTCOME_TRY(auto&& r, ParameterDistributionLogNormal::deserialize_elements(io, obj));
-            return ParameterDistributionWrapper(r);
-        }
-        else if (type.value() == "Exponential") {
-            BOOST_OUTCOME_TRY(auto&& r, ParameterDistributionExponential::deserialize_elements(io, obj));
-            return ParameterDistributionWrapper(r);
-        }
-        else if (type.value() == "Constant") {
-            BOOST_OUTCOME_TRY(auto&& r, ParameterDistributionConstant::deserialize_elements(io, obj));
-            return ParameterDistributionWrapper(r);
-        }
-        else {
-            return failure(StatusCode::InvalidValue, "Type of ParameterDistribution in ParameterDistributionWrapper" +
                                                          type.value() + " not valid.");
         }
     }
