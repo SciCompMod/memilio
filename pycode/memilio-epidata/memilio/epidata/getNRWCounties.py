1291acdb23a1da67aedd705d43d02838a52cac8b
Carlotta Gerstein <100771374+charlie0614@users.noreply.github.com>
2025-03-28 16:10:41 +0100

    format python file

--- pycode/memilio-epidata/memilio/epidata/getNRWCounties.py
+++ pycode/memilio-epidata/memilio/epidata/getNRWCounties.py
@@ -6,6 +6,7 @@ import pandas as pd
 from memilio.epidata import geoModificationGermany as geoger
 from memilio.epidata import getDataIntoPandasDataFrame as gd
 
+
 def main():
     """! Main program entry."""
 
@@ -26,7 +27,7 @@ def main():
     stateIDs = geoger.get_state_ids()
     # get state ID to county ID map
     stateID_to_countyID = geoger.get_stateid_to_countyids_map()
-    
+
     # iterate over state_to_county map and replace IDs by numbering 0, ..., n
     state_indices = []
     county_indices = []
@@ -34,19 +35,19 @@ def main():
         state_indices.append(stateIDs.index(state))
         county_indices.append(
             np.array([countyIDs.index(county) for county in counties]))
-        
-    mobility_matrix_nrw = mobility_matrix.loc[county_indices[4], county_indices[4]]
+
+    mobility_matrix_nrw = mobility_matrix.loc[county_indices[4],
+                                              county_indices[4]]
 
     gd.write_dataframe(
         mobility_matrix_nrw, directory_mobility, mobility_file + '_nrw', 'txt',
         param_dict={'sep': ' ', 'header': None, 'index': False})
-        
-    population = pd.read_json(os.path.join(directory_population + population_file + '.json'))
+
+    population = pd.read_json(os.path.join(
+        directory_population + population_file + '.json'))
     population_nrw = population.loc[county_indices[4]]
-    gd.write_dataframe(population_nrw, directory_population, population_file + '_nrw', 'json')
-
-
-    
+    gd.write_dataframe(population_nrw, directory_population,
+                       population_file + '_nrw', 'json')
 
 
 if __name__ == "__main__":
