678291793bee45f5464a248079ea91df34aecacc
Carlotta Gerstein <100771374+charlie0614@users.noreply.github.com>
2025-03-31 13:45:13 +0200

    commit likwid examples

--- shellscripts/likwid_equationbased.sh
+++ shellscripts/likwid_equationbased.sh
@@ -20,6 +20,9 @@ echo Running $1 on node $SLURM_JOB_NODELIST with $warm_up_runs warm up runs and 
 cd ../cpp/build
 rm -rf CMakeCache.txt CMakeFiles/
 cmake -DCMAKE_BUILD_TYPE="Release" -DMEMILIO_ENABLE_OPENMP=ON -DMEMILIO_ENABLE_WARNINGS_AS_ERRORS=OFF ..
-cmake --build . --target ode_seir_mobility_timing
+cmake --build . --target ode_metapop_timing
 
-srun --cpu-bind=cores --cpus-per-task=1 --cpu-freq=2200000-2200000 likwid-perfctr -C 0 -g MEM_DP -m ./bin/ode_seir_mobility_timing $warm_up_runs $runs $regions
+perf record -C 0 likwid-pin ./bin/ode_metapop_timing $warm_up_runs $runs $regions
+perf report 
+# srun --cpu-bind=cores --cpus-per-task=1 --cpu-freq=2200000-2200000 valgrind --tool=massif --detailed-freq=2 ./bin/ode_metapop_timing $warm_up_runs $runs $regions
+# srun --cpu-bind=cores --cpus-per-task=1 --cpu-freq=2200000-2200000 likwid-perfctr -C 0 -g MEM_DP -m ./bin/ode_metapop_timing $warm_up_runs $runs $regions
--- shellscripts/likwid_graphbased.sh
+++ shellscripts/likwid_graphbased.sh
@@ -10,16 +10,18 @@
 #SBATCH --job-name=likwid_mobilitymodels
 
 warm_up_runs=0
-runs=50
-regions=400
+runs=1
+regions=80
 
 module purge
-module load PrgEnv/gcc12-openmpi
+module load PrgEnv/gcc13-openmpi
 
 echo Running $1 on node $SLURM_JOB_NODELIST with $warm_up_runs warm up runs and $runs runs.
 cd ../cpp/build
 rm -rf CMakeCache.txt CMakeFiles/
-cmake -DCMAKE_BUILD_TYPE="Release" -DMEMILIO_ENABLE_OPENMP=ON -DMEMILIO_ENABLE_WARNINGS_AS_ERRORS=OFF ..
-cmake --build . --target graph_timing
+CXX="g++ -fverbose-asm" cmake -DCMAKE_BUILD_TYPE="Release" -DMEMILIO_ENABLE_OPENMP=ON -DMEMILIO_ENABLE_WARNINGS_AS_ERRORS=OFF ..
+# cmake --build . --target graph_timing
 
+# perf record ./bin/graph_timing $warm_up_runs $runs $regions
+# perf report 
 srun --cpu-bind=cores --cpus-per-task=1 --cpu-freq=2200000-2200000 likwid-perfctr -C 0 -g MEM_DP -m ./bin/graph_timing $warm_up_runs $runs $regions
