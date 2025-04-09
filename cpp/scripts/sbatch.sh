fa120f62083daa348321626bc9443ac67c2f06aa
litz2 <litz2@jpblt-s01-01.jupiter.internal>
2025-04-09 13:28:43 +0200

    Update

---
+++ cpp/scripts/sbatch.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH --account=training2508
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cores=1
+#SBATCH --threads-per-core=1
+#SBATCH --gres=gpu:4
+#SBATCH --gpus-per-task=1
+#SBATCH --output=slurm-%j-acc.out
+#SBATCH --error=slurm-%j-acc-err
+#SBATCH --time=00:15:00
+#SBATCH --partition=all
+#SBATCH --reservation gpuhack25-day3
+
+module load NVHPC CMake CUDA NVHPC Eigen HDF5 OpenMPI
+
+export OMP_NUM_THREADS=1
+
+# srun --cpus-per-task=$OMP_NUM_THREADS nsys profile --trace cuda,openacc,nvtx ./../build/bin/ide_secir_example
+
+
+srun ./../build/bin/ide_secir_example
+
+srun nsys profile --trace cuda,openacc,nvtx ./../build/bin/ide_secir_example
