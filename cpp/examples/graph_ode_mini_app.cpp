1c876c8fd8bf82f8919ab3a04d7d43841a019fd5
Kilian Volmer <13285635+kilianvolmer@users.noreply.github.com>
2025-04-09 17:21:19 +0200

    Change mini example to references

--- cpp/examples/graph_ode_mini_app.cpp
+++ cpp/examples/graph_ode_mini_app.cpp
@@ -17,11 +17,11 @@ class Node
     std::vector<int> population;
 };
 
-void exchange_person(Node from, Node to){
-    for(size_t i = 0; i < from.population.size(); i++){
-        if (from.population[i] > 1){
-            from.population[i] -= 1;
-            to.population[i] += 1;
+void exchange_person(std::vector<int>& from, std::vector<int>& to){
+    for(size_t i = 0; i < from.size(); i++){
+        if (from[i] > 1){
+            from[i] -= 1;
+            to[i] += 1;
         }
     }
 }
@@ -30,27 +30,43 @@ int main(){
     //Initialise nodes
     std::vector<Node> nodes(4000);
 
-    for (size_t time = 0; time < 28; time ++){
+    for (size_t time = 0; time < 1; time ++){
         
-        nvtxRangePushA("Exchange");
         //Infect people (not relevant)
         for(size_t i = 0; i < nodes.size(); i++){
-            for(size_t j = 0; j < nodes[i].population.size()-1; j++){
+            for(size_t j = 0; j < nodes.size(); j++){
                 if(nodes[i].population[j] > 1 && rand() < 0.5 && (i+1)%4 == 0){
                     nodes[i].population[j] -= 1;
                     nodes[i].population[j+1] += 1;
                 }
             }
         }
-        nvtxRangePop();
 
+        nvtxRangePushA("Exchange");
         //Exchange people (to be optimized)
         for(size_t i = 0; i < nodes.size(); i++){
+            #pragma acc parallel loop
             for(size_t j = 0; j < nodes.size(); j++){
-                exchange_person(nodes[i], nodes[j]);
+                exchange_person(nodes[i].population, nodes[j].population);
             }
         }
-    }
+        nvtxRangePop();
 
+    }
+    // for(size_t j = 0; j < 100; j++){
+    //     std::vector<double> a(1000000, 1.0);
+    //     std::vector<double> b(1000000, 1.0);
+    //     std::vector<double> c(1000000);
+    //     #pragma acc 
+    //     for (size_t i = 0; i < a.size(); i++){
+    //         c[i] = a[i] + b[i];
+    //     }
+
+    //     double result = 0;
+    //     for (size_t i = 0; i < c.size(); i++){
+    //         result += c[i];
+    //     }
+    // }
+//37 mit OpenACC, 38 ohne
 
 }
