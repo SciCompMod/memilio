33d931797da455e894db462a40e4ea803a0616a6
reneSchm <49305466+reneSchm@users.noreply.github.com>
2025-04-09 16:13:20 +0200

    [wip] new old integrator

---
+++ cpp/examples/gpu_integrator.cpp
@@ -0,0 +1,154 @@
+#define R123_NO_CUDA_DEVICE_RANDOM 1
+// #define CUDA_TEST_SIZE 1024
+
+#include <cuda_runtime.h>
+
+#include "gpu_integrator.h"
+
+// #include "memilio/math/integrator.h"
+// #include "memilio/math/adapt_rk.h"
+
+#include <iostream>
+#include <memory>
+#include <cmath>
+
+uint64_t seed                  = 147634;
+// Numerical Recipes, ranqd1
+const uint64_t rand_modulus    = (uint64_t(1) << 32);
+const uint64_t rand_multiplier = 1664525;
+const uint64_t rand_increment  = 1013904223;
+
+uint64_t randc() {
+    seed = (rand_multiplier * seed + rand_increment) & (rand_modulus - 1);
+    return seed; 
+}
+
+double uniform_rand(double min, double max) {
+    auto val = randc() / ((double)rand_modulus) * (max - min) + min;
+    return val;
+}
+
+std::vector<double> t_offset, t_scale;
+// std::vector<double> amplitude;
+std::vector<std::vector<double>> amplitude_lincomb;
+
+void set_params(size_t size, size_t band_width, double min, double max) {
+    t_offset = std::vector<double>(size, 0.);
+    t_scale =  std::vector<double>(size, 0.);
+    // amplitude = std::vector<double>(size, 0.); (void)band_width;
+    amplitude_lincomb = std::vector<std::vector<double>>(size, std::vector<double>(size, 0.));
+
+
+    for (size_t i = 0; i < size; i++) {
+        t_offset[i] = uniform_rand(min, max);
+        t_scale[i] = uniform_rand(min, max);
+        // amplitude[i] = uniform_rand(min, max);
+        
+        for (int j = - ((int)band_width / 2); j < (((int)band_width + 1) / 2); j++) {
+            if ((int)i + j >= 0 && i + j < size)
+                amplitude_lincomb[i][i + j] = uniform_rand(min, max);
+        } 
+    }
+}
+
+
+void rhs(const std::vector<double>& x, double t, std::vector<double>& dxdt) {
+    for (size_t i = 0; i < x.size(); i++) {
+        // dxdt[i] = amplitude[i] * std::sin(t * t_scale[i] + t_offset[i]);
+        
+        dxdt[i] = 0;
+        for (size_t j = 0; j < x.size(); j++) {
+            dxdt[i] += amplitude_lincomb[i][j] * std::sin(t * t_scale[j] + t_offset[j]);
+        }
+    }
+}
+
+namespace mio {
+    void log_debug(std::string_view s) {
+        std::cout << s << "\n";
+    }
+}
+
+int main()
+{   
+    // using namespace mio;
+    // set_log_level(LogLevel::off);
+    
+    mio::log_debug("Enter Main");
+
+    const int size = 30;
+
+    // // Guard the CUDA test with proper CUDA error handling
+    // // cudaError_t cudaStatus = cudaSetDevice(0);
+    // // if (cudaStatus != cudaSuccess) {
+    // //     std::cerr << "CUDA initialization failed: " << cudaGetErrorString(cudaStatus) << std::endl;
+    // //     std::cout << "CUDA test failed! Continuing without CUDA." << std::endl;
+    // // }
+    // // else {
+    // //     std::cout << "CUDA initialization succeeded." <<  std::endl;
+        
+    // //     cudaDeviceReset();
+    // // } 
+
+    // // TODO: nvidia-x-markers??
+
+    set_params(size, 1, -3.0, 3.0);
+    mio::log_debug("Params Set");
+
+    print(t_offset);
+    print(t_scale);
+    print(amplitude_lincomb);
+    std::cout << "\n";
+
+    // // std::cout << amplitude_lincomb << "\n";
+
+    const double abs_tol = 1e-3, rel_tol = 1e-8, min_dt = 1e-2, max_dt = 1e+2;
+    // // auto core = std::make_shared<mio::ControlledStepperWrapper<double, boost::numeric::odeint::runge_kutta_cash_karp54>>(abs_tol, rel_tol, min_dt, max_dt);
+    // auto core = std::make_shared<mio::RKIntegratorCore<double>>(abs_tol, rel_tol, min_dt, max_dt);
+
+    Monstrosity stepper{abs_tol, rel_tol, min_dt, max_dt, std::vector<double>(size), std::vector<double>(size), std::vector<std::vector<double>>(tableau().entries_low.size(), std::vector<double>(size)), tableau()};
+
+    mio::log_debug("Core Set");
+
+    // OdeIntegrator<double> integrator(core);
+
+    mio::log_debug("Integrator Set");
+
+    // TimeSeries<double> results(0, Eigen::VectorXd::Zero(size));
+
+    // const double tmax = 100 * M_PI;
+    // double dt = 0.1;
+
+    double dt = 0.1;
+    double t = 0.0;
+    std::vector<double> y(size, 0.0);
+    std::vector<double> y2(size, 0.0);
+    
+    print(y);
+    print(y2);
+    std::cout << "\n";
+
+    mio::log_debug("Results Set");    
+    mio::log_debug("Integrating...");
+
+
+    while (t < 100 * M_PI) {
+        std::cout << "-- integrator t:" << t << " dt:" << dt << "\n";
+        stepper.step(&rhs, y, t, dt, y2);
+        for (size_t i = 0; i < y.size(); i++) {
+            y[i] = y2[i];
+            y2[i] = 0;
+        }
+    }
+    // integrator.advance(rhs, tmax, dt, results);
+    
+    mio::log_debug("Integration Finished");
+
+    // if (size < 5)
+    //     results.print_table();
+    // else
+    //     std::cout << "Num time steps: " << results.get_num_time_points() << "\n"; 
+
+    mio::log_debug("Exit Main");
+    // return 0;
+}
---
+++ cpp/examples/gpu_integrator.h
@@ -0,0 +1,185 @@
+#include <cassert>
+#include <cmath>
+#include <iostream>
+#include <vector>
+
+struct tableau{
+    tableau() : entries_high(6), entries_low(6), entries(5) {
+        entries_low[0]  = 25 / 216.0;
+        entries_low[1]  = 0.0;
+        entries_low[2]  = 1408 / 2565.0;
+        entries_low[3]  = 2197 / 4104.0;
+        entries_low[4]  = -0.2;
+        entries_low[5]  = 0.0;
+        entries_high[0] = 16 / 135.0;
+        entries_high[1] = 0.0;
+        entries_high[2] = 6656 / 12825.0;
+        entries_high[3] = 28561 / 56430.0;
+        entries_high[4] = -9 / 50.0;
+        entries_high[5] = 2 / 55.0;
+
+        for (size_t i = 0; i < entries.size(); i++) {
+            entries[i].resize(i + 2);
+        }
+
+        entries[0][0] = 0.25;
+        entries[0][1] = 0.25;
+        entries[1][0] = 3 / 8.0;
+        entries[1][1] = 3 / 32.0;
+        entries[1][2] = 9 / 32.0;
+        entries[2][0] = 12 / 13.0;
+        entries[2][1] = 1932 / 2197.0;
+        entries[2][2] = -7200 / 2197.0;
+        entries[2][3] = 7296 / 2197.0;
+        entries[3][0] = 1.0;
+        entries[3][1] = 439 / 216.0;
+        entries[3][2] = -8.0;
+        entries[3][3] = 3680 / 513.0;
+        entries[3][4] = -845 / 4104.0;
+        entries[4][0] = 0.5;
+        entries[4][1] = -8 / 27.0;
+        entries[4][2] = 2.0;
+        entries[4][3] = -3544 / 2565.0;
+        entries[4][4] = 1859 / 4104.0;
+        entries[4][5] = -11 / 40.0;
+    }
+
+    std::vector<double> entries_high, entries_low;
+    std::vector<std::vector<double>> entries;
+};
+
+
+void print(std::vector<double>& vec) {
+    for (auto& val : vec) {
+        printf("%10.3f ", val);
+    }
+    std::cout << "\n";
+}
+
+void print(std::vector<std::vector<double>>& vec) {
+    for (auto& val : vec) {
+        print(val);
+    }
+}
+
+struct Monstrosity {
+
+double abs_tol, rel_tol, dt_min, dt_max;
+
+std::vector<double> m_yt_eval, m_error_estimate;
+std::vector<std::vector<double>> m_kt_values; // col major!
+
+tableau tab;
+
+using DerivFunction = void(*)(const std::vector<double>& y, double t, std::vector<double>& dydt);
+
+bool step(DerivFunction f, const std::vector<double>& yt, double& t, double& dt,
+          std::vector<double>& ytp1) 
+{
+    assert(0 <= dt_min);
+    assert(dt_min <= dt_max);
+
+    if (dt < dt_min || dt > dt_max) {
+        // mio::log_warning("IntegratorCore: Restricting given step size dt = {} to [{}, {}].", dt, dt_min, dt_max);
+    }
+
+    dt = std::min(dt, dt_max);
+
+    double t_eval; // shifted time for evaluating yt
+    double dt_new; // updated dt
+
+    bool converged     = false; // carry for convergence criterion
+    bool dt_is_invalid = false;
+
+    // if (m_yt_eval.size() != yt.size()) {
+    //     m_yt_eval.resize(yt.size());
+    //     m_kt_values.resize(yt.size(), tab.entries_low.size());
+    // }
+
+    for (size_t j = 1; j < yt.size(); j++) {
+        m_yt_eval[j] = yt[j];
+    }
+
+    while (!converged && !dt_is_invalid) {
+        if (dt < dt_min) {
+            dt_is_invalid = true;
+            dt            = dt_min;
+        }
+        std::cout << "---- step t:" << t << " dt:" << dt << "\n";
+        std::cin.ignore();
+        // compute first column of kt, i.e. kt_0 for each y in yt_eval
+        f(m_yt_eval, t, m_kt_values[0]);
+
+#pragma acc loop
+        for (size_t i = 1; i < m_kt_values.size(); i++) {
+            // we first compute k_n1 for each y_j, then k_n2 for each y_j, etc.
+            t_eval = t;
+            t_eval += tab.entries[i - 1][0] *
+                      dt; // t_eval = t + c_i * h // note: line zero of Butcher tableau not stored in array
+            // use ytp1 as temporary storage for evaluating m_kt_values[i]
+            ytp1 = m_yt_eval;
+            for (size_t k = 1; k < tab.entries[i - 1].size(); k++) {
+                for (size_t j = 1; j < yt.size(); j++) {
+                    ytp1[j] += (dt * tab.entries[i - 1][k]) * m_kt_values[k - 1][j];
+                }
+            }
+            // get the derivatives, i.e., compute kt_i for all y in ytp1: kt_i = f(t_eval, ytp1_low)
+            f(ytp1, t_eval, m_kt_values[i]);
+        }
+        // calculate low order estimate
+#pragma acc parallel loop
+        for (size_t i = 0; i < yt.size(); i++) {
+            ytp1[i] = m_yt_eval[i];
+            for (size_t j = 1; j < m_kt_values.size(); j++) {
+                ytp1[i] += (dt * (m_kt_values[j][i] * tab.entries_low[j]));
+            }
+        }
+        std::cout << "low: "; print(ytp1);
+        // truncation error estimate: yt_low - yt_high = O(h^(p+1)) where p = order of convergence
+#pragma acc parallel loop
+        for (size_t i = 1; i < yt.size(); i++) {
+            m_error_estimate[i] = 0;
+            for (size_t j = 1; j < m_kt_values.size(); j++) {
+                m_error_estimate[i] += dt * std::abs(m_kt_values[j][i] * (tab.entries_high[j] - tab.entries_low[j]));
+            }
+        }
+        std::cout << "err: "; print(m_error_estimate);
+        // calculate mixed tolerance
+        
+        double min_coeff = 2;
+        std::cout << "con: ";
+        // #pragma acc parallel loop
+        for (size_t i = 0; i < yt.size(); i++) {
+            double tmp = m_error_estimate[i] > 0 ? (abs_tol + std::abs(ytp1[i]) * rel_tol) / m_error_estimate[i] : 2;
+            std::cout << tmp << " ";
+            min_coeff = std::min(min_coeff, tmp);
+        }
+        std::cout << "\n min con: " << min_coeff << "\n";
+        converged = (min_coeff <= 1); // convergence criterion
+
+    
+        if (converged || dt_is_invalid) {
+            // if sufficiently exact, return ytp1, which currently contains the lower order approximation
+            // (higher order is not always higher accuracy)
+            t += dt; // this is the t where ytp1 belongs to
+        }
+        // else: repeat the calculation above (with updated dt)
+
+        // compute new value for dt
+        // converged implies eps/error_estimate >= 1, so dt will be increased for the next step
+        // hence !converged implies 0 < eps/error_estimate < 1, strictly decreasing dt
+        dt_new = dt * std::pow(min_coeff, (1. / (tab.entries_low.size() - 1)));
+        // safety factor for more conservative step increases,
+        // and to avoid dt_new -> dt for step decreases when |error_estimate - eps| -> 0
+        dt_new *= 0.9;
+        std::cout << "dt: " << dt << "dt_new: " << dt_new << "\n";
+        // check if updated dt stays within desired bounds and update dt for next step
+        dt = std::min(dt_new, dt_max);
+    }
+    dt = std::max(dt, dt_min);
+    // return 'converged' in favor of '!dt_is_invalid', as these values only differ if step sizing failed,
+    // but the step with size dt_min was accepted.
+    return converged;
+}
+
+};
