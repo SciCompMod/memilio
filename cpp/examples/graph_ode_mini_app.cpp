fda86c280100bbda679b6653253456f398269a4c
Kilian Volmer <13285635+kilianvolmer@users.noreply.github.com>
2025-04-10 15:58:02 +0200

    CHG: final miniapp

--- cpp/examples/graph_ode_mini_app.cpp
+++ cpp/examples/graph_ode_mini_app.cpp
@@ -3,12 +3,21 @@
 #include <random>
 #include <nvtx3/nvToolsExt.h>
 
+size_t num_states = 5*1000;
 
 class Node
 {
     Node(){
         //            S, E, I, R,   S, E, I, R,     S, E, I, R 
-        population = {1000, 0, 0, 0, 1000, 0, 0, 0, 1000, 0, 0, 0};
+        // population = {1000, 0, 0, 0, 1000, 0, 0, 0, 1000, 0, 0, 0};
+        population = std::vector<int>(num_states);
+        for (size_t i = 0; i < num_states; i+=5){
+            population[i] = 1000;
+            population[i+1] = 0;
+            population[i+2] = 0;
+            population[i+3] = 0;
+            population[i+4] = 1000;
+        }
     }
 
 
@@ -28,14 +37,15 @@ void exchange_person(std::vector<int>& from, std::vector<int>& to){
 
 int main(){
     //Initialise nodes
-    std::vector<Node> nodes(4000);
+    size_t num_nodes = 10000;
+    std::vector<Node> nodes(num_nodes);
 
     for (size_t time = 0; time < 1; time ++){
         
         //Infect people (not relevant)
         for(size_t i = 0; i < nodes.size(); i++){
             for(size_t j = 0; j < nodes.size(); j++){
-                if(nodes[i].population[j] > 1 && rand() < 0.5 && (i+1)%4 == 0){
+                if(nodes[i].population[j] > 1 && rand() < 0.5 && (i+1)%5 == 0){
                     nodes[i].population[j] -= 1;
                     nodes[i].population[j+1] += 1;
                 }
@@ -44,29 +54,18 @@ int main(){
 
         nvtxRangePushA("Exchange");
         //Exchange people (to be optimized)
-        for(size_t i = 0; i < nodes.size(); i++){
+        // #pragma acc parallel loop
+        for(size_t i = 0; i < num_nodes; i++){
             #pragma acc parallel loop
-            for(size_t j = 0; j < nodes.size(); j++){
-                exchange_person(nodes[i].population, nodes[j].population);
+            for(size_t j = 0; j < num_nodes; j+=2){
+                exchange_person(nodes[(i+j) % num_nodes].population, nodes[(i+1+j) % num_nodes].population);
+            }
+            #pragma acc parallel loop
+            for(size_t j = 1; j < num_nodes; j+=2){
+                exchange_person(nodes[(i+j) % num_nodes].population, nodes[(i+1+j) % num_nodes].population);
             }
         }
         nvtxRangePop();
 
     }
-    // for(size_t j = 0; j < 100; j++){
-    //     std::vector<double> a(1000000, 1.0);
-    //     std::vector<double> b(1000000, 1.0);
-    //     std::vector<double> c(1000000);
-    //     #pragma acc 
-    //     for (size_t i = 0; i < a.size(); i++){
-    //         c[i] = a[i] + b[i];
-    //     }
-
-    //     double result = 0;
-    //     for (size_t i = 0; i < c.size(); i++){
-    //         result += c[i];
-    //     }
-    // }
-//37 mit OpenACC, 38 ohne
-
 }
