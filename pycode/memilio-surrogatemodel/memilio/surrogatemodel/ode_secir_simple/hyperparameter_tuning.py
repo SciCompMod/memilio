3c09788a532a4b5662aaf265c1b4a48c931edeec
Manuel Heger <mh.heger@gmail.com>
2025-04-16 09:25:19 +0200

    format hyperparameter tuning

--- pycode/memilio-surrogatemodel/memilio/surrogatemodel/ode_secir_simple/hyperparameter_tuning.py
+++ pycode/memilio-surrogatemodel/memilio/surrogatemodel/ode_secir_simple/hyperparameter_tuning.py
@@ -1,5 +1,5 @@
-#from memilio.surrogatemodel.ode_secir_simple import network_architectures
-#from memilio.surrogatemodel.ode_secir_simple.model import split_data, get_test_statistic
+# from memilio.surrogatemodel.ode_secir_simple import network_architectures
+# from memilio.surrogatemodel.ode_secir_simple.model import split_data, get_test_statistic
 import os
 import tensorflow as tf
 import pickle
@@ -8,7 +8,7 @@ import time
 from sklearn.model_selection import KFold
 import numpy as np
 import memilio.surrogatemodel.ode_secir_simple.grid_search as grid_search
-import memilio.surrogatemodel.ode_secir_simple.model as md 
+import memilio.surrogatemodel.ode_secir_simple.model as md
 
 
 label_width = 30
@@ -19,31 +19,32 @@ filename = "data_secir_simple_30days_10k.pickle"
 
 # Saving grid search results
 filename_df = "dataframe_optimizer_paper.csv"
-#filename_df = "dataframe_30days_10k_nodamp.csv"
+# filename_df = "dataframe_30days_10k_nodamp.csv"
 
 # General grid search parameters for the training process:
 early_stop = [100]
 max_epochs = [2]
-losses=[tf.keras.losses.MeanAbsolutePercentageError()]
+losses = [tf.keras.losses.MeanAbsolutePercentageError()]
 optimizers = ['Adam', 'AdamW', 'Nadam', 'SGD', 'Adagrad', 'RMSProp']
-metrics=[[tf.keras.metrics.MeanAbsoluteError(), tf.keras.metrics.MeanAbsolutePercentageError()]]
+metrics = [[tf.keras.metrics.MeanAbsoluteError(
+), tf.keras.metrics.MeanAbsolutePercentageError()]]
 
 # Define grid search parameters for the architecture
-hidden_layers = [1,2]
+hidden_layers = [1, 2]
 neurons_in_hidden_layer = [32]
 activation_function = ['relu', 'elu', 'softmax',  'sigmoid', 'linear', 'tanh']
 models = ["LSTM"]
 
 # Collecting parameters
-training_parameters = [(early, epochs, loss, optimizer, metric) 
-                       for early in early_stop for epochs in max_epochs for loss in losses 
+training_parameters = [(early, epochs, loss, optimizer, metric)
+                       for early in early_stop for epochs in max_epochs for loss in losses
                        for optimizer in optimizers for metric in metrics]
 
 model_parameters = [(label_width, num_outputs, layer, neuron_number, activation, modelname)
-              for layer in hidden_layers for neuron_number in neurons_in_hidden_layer 
-              for activation in activation_function for modelname in models]
+                    for layer in hidden_layers for neuron_number in neurons_in_hidden_layer
+                    for activation in activation_function for modelname in models]
 
-# Loading data 
+# Loading data
 path = os.path.dirname(os.path.realpath(__file__))
 path_data = os.path.join(os.path.dirname(os.path.realpath(
     os.path.dirname(os.path.realpath(path)))), 'data')
@@ -55,7 +56,7 @@ with open(os.path.join(path_data, filename), 'rb') as file:
     data = pickle.load(file)
 
 # Split the data: 80% will be used for grid search with cross-validation, and the remaining 20% is withheld for testing
-data_splitted = md.split_data(data['inputs'], data['labels'],0.8,0,0.2)
+data_splitted = md.split_data(data['inputs'], data['labels'], 0.8, 0, 0.2)
 inputs_grid_search = data_splitted['train_inputs']
 labels_grid_search = data_splitted['train_labels']
 inputs_withhold = data['test_inputs']
@@ -64,8 +65,9 @@ labels_withhold = data['test_labels']
 
 start_hyper = time.perf_counter()
 
-# Performing grid search 
-grid_search.perform_grid_search(model_parameters, inputs_grid_search, labels_grid_search, training_parameters, filename_df)
+# Performing grid search
+grid_search.perform_grid_search(
+    model_parameters, inputs_grid_search, labels_grid_search, training_parameters, filename_df)
 
 elapsed_hyper = time.perf_counter() - start_hyper
 print(
