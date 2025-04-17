61fb5716bb146e5fa7e9d1ea042b8cf8a57b70ed
Kilian Volmer <13285635+kilianvolmer@users.noreply.github.com>
2025-04-17 10:50:24 +0200

    CHG: Combine SBML code into one file, rename it

--- cpp/examples/sbml_to_memilio.cpp
+++ cpp/examples/sbml_to_memilio.cpp
@@ -854,3 +854,55 @@ int format_files(const char* filename)
         std::string("clang-format -i --files= ") + lowercase_name + ".cpp " + lowercase_name + "/*.[hc]*";
     return system(command.c_str());
 }
+
+int main(int argc, char* argv[])
+{
+    if (argc != 2) {
+        std::cout << "Please provide a SBML file at startup!" << std::endl << std::endl;
+        return 1;
+    }
+    const char* filename = argv[1];
+    SBMLDocument* document;
+    SBMLReader reader;
+
+    document = reader.readSBML(filename);
+
+    auto model = document->getModel();
+
+    if (!verify_model_suitability(model)) {
+        return 1;
+    }
+
+    create_folder(filename);
+
+    if (!create_infection_state(model, filename)) {
+        return 1;
+    }
+
+    if (!create_parameters(model, filename)) {
+        return 1;
+    }
+
+    if (!create_model_cpp(filename)) {
+        return 1;
+    }
+
+    if (!create_model_h(model, filename)) {
+        return 1;
+    }
+
+    if (!create_cmake(filename)) {
+        return 1;
+    }
+
+    if (!create_example_cpp(model, filename)) {
+        return 1;
+    }
+
+    if (!modify_cmakelists(filename)) {
+        return 1;
+    }
+
+    return format_files(filename);
+
+}
