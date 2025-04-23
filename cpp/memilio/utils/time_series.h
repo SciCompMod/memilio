8cd82179aeaf1c71265d4e4791e17825c5ad9674
Henrik Zunker <henrik.zunker@dlr.de>
2025-04-23 12:00:47 +0200

    update doc

--- cpp/memilio/utils/time_series.h
+++ cpp/memilio/utils/time_series.h
@@ -539,7 +539,7 @@ public:
      * The first column of the CSV file contains the time points. The remaining columns
      * contain the values at each time point. Column headers can be specified with column_labels.
      *
-     * @param filename Path to the CSV file.conjunction
+     * @param filename Path to the CSV file
      * @param column_labels [Default: {}] Vector of labels for each column after the time column.
      * @param separator [Default: ','] Separator character.
      * @param precision [Default: 6] Number of decimals for floating point values.
