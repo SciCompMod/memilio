4bdfd4a806dae7d48161fcc4474f0e5ded1819b8
Sascha <51127093+xsaschako@users.noreply.github.com>
2025-05-19 11:23:32 +0200

    Fix indentation in abm.cpp: align method chaining for better readability

--- pycode/memilio-simulation/memilio/simulation/bindings/models/abm.cpp
+++ pycode/memilio-simulation/memilio/simulation/bindings/models/abm.cpp
@@ -161,9 +161,9 @@ PYBIND11_MODULE(_simulation_abm, m)
         .def(py::init<const mio::abm::TestingCriteria&, mio::abm::TimeSpan, mio::abm::TimePoint, mio::abm::TimePoint,
                       const mio::abm::TestParameters&, double>(),
              py::arg("testing_criteria"), py::arg("testing_validity_period"), py::arg("start_date"),
-             py::arg("end_date"), py::arg("test_parameters"), py::arg("probability"))
+             py::arg("end_date"), py::arg("test_parameters"), py::arg("probability"));
 
-            pymio::bind_class<mio::abm::ProtectionEvent, pymio::EnablePickling::Never>(m, "ProtectionEvent")
+    pymio::bind_class<mio::abm::ProtectionEvent, pymio::EnablePickling::Never>(m, "ProtectionEvent")
         .def(py::init<mio::abm::ProtectionType, mio::abm::TimePoint>(), py::arg("type"), py::arg("time"))
         .def_readwrite("type", &mio::abm::ProtectionEvent::type)
         .def_readwrite("time", &mio::abm::ProtectionEvent::time);
