afbe19e4a605296520123c8b4c7d324cf39d45ad
jubicker <113909589+jubicker@users.noreply.github.com>
2025-06-10 14:40:36 +0200

    comments

--- pycode/examples/simulation/ABM Demonstrator/post_processing.py
+++ pycode/examples/simulation/ABM Demonstrator/post_processing.py
@@ -1,7 +1,9 @@
 from memilio.simulation import abm
 
 if __name__ == "__main__":
+    # Path to simulation results folder. Has to have a "/" at the end e.g. /Documents/outputs/
     sim_result_file = ""
+    # Folder the aggregated output should be saved to
     save_folder = sim_result_file
     num_sims = 1
     abm.calculate_infections_per_quantity(
