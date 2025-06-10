9ffac99e6c2be28a76dadeafbe121cd7c00861bd
Anna Wendler <106674756+annawendler@users.noreply.github.com>
2025-06-10 12:00:30 +0200

    add plot scripts

---
+++ plot_scripts/plot_compartments.py
@@ -0,0 +1,122 @@
+#############################################################################
+# Copyright (C) 2020-2025 MEmilio
+#
+# Authors: Anna Wendler
+#
+# Contact: Martin J. Kuehn <Martin.Kuehn@DLR.de>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#############################################################################
+import h5py
+import os
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+from memilio.epidata import getDataIntoPandasDataFrame as gd
+
+
+def plot_susceptibles(files, fileending, save_dir=""):
+    """
+    Plots simulation results of Susceptibles.
+
+    @param[in] files Expects list of two files with ODE and IDE simulation results for compartments, respectively, in
+        this order.
+    @param[in] fileending Determines file ending of saved plot. Default is an empty string leading to no further
+        specification.
+    @param[in] save_dir Directory where plot will be stored. Default is an empty string leading to the plot not being
+        saved.
+    """
+    # Define compartments
+    secir_dict = {0: 'Susceptible',  1: 'Infected', 2: 'Recovered'}
+
+    # Define plot.
+    num_plots = 1
+    fig, axs = plt.subplots(1, num_plots, sharex='all', num='Compare files')
+
+    colors = ["C0", "limegreen"]
+    linestyles = ['-', '-']
+    linewidth = 1
+    labels = ["Groundtruth", "IDE result"]
+
+    # Add results to plot.
+    for file in range(len(files)):
+        # Load data.
+        h5file = h5py.File(str(files[file]) + '.h5', 'r')
+
+        if (len(list(h5file.keys())) > 1):
+            raise gd.DataError("File should contain one dataset.")
+        if (len(list(h5file[list(h5file.keys())[0]].keys())) > 3):
+            raise gd.DataError("Expected only one group.")
+
+        data = h5file[list(h5file.keys())[0]]
+
+        # As there should be only one Group, total is the simulation result.
+        total = data['Total'][:, :]
+
+        dates = data['Time'][:]
+
+        # Plot data.
+        for i in range(num_plots):
+            axs.plot(dates,
+                     total[:, i], label=labels[file],  linestyle=linestyles[file], color=colors[file], linewidth=linewidth)
+
+        h5file.close()
+
+    # Define some characteristics of the plot
+    for i in range(num_plots):
+        axs.set_title(secir_dict[i], fontsize=8)
+        axs.set_xlim(left=dates[0], right=dates[-1])
+        axs.grid(True, linestyle='--', alpha=0.5)
+        axs.ticklabel_format(axis='y',
+                             style='sci', scilimits=(0, 0))
+
+    fig.legend(labels, bbox_to_anchor=(0.1, -0.73, 0.8, 0.8),
+               fancybox=False, shadow=False, ncol=1)
+
+    fig.supxlabel('Simulation time [days]')
+    fig.supylabel('Number of individuals')
+    plt.subplots_adjust(left=None, bottom=None, right=None,
+                        top=None, wspace=None, hspace=0.6)
+
+    plt.tight_layout()
+
+    # Save result.
+    if save_dir != "":
+        if not os.path.isdir(save_dir):
+            os.makedirs(save_dir)
+        plt.savefig(save_dir + f"compare_compartments_{fileending}.png",
+                    bbox_inches='tight', dpi=500)
+
+
+if __name__ == '__main__':
+
+    # Path where simulation results (generated with ide_changepoints.cpp) are stored.
+    result_dir = os.path.join(os.path.dirname(
+        __file__),  "../simulation_results/exponential_tmax=1/")
+    # Path where plots will be stored.
+    plot_dir = os.path.join(os.path.dirname(
+        __file__),  "../plots/exponential_tmax=1/")
+
+    gregory_orders = ["1", "2", "3"]
+    dt_ide = "3"
+
+    # for gregory_order in gregory_orders:
+    #     plot_susceptibles([os.path.join(result_dir, f"result_ide_dt=1e-4_gregoryorder=3_finitedifforder=1"),
+    #                        os.path.join(result_dir, f"result_ide_dt=1e-{dt_exp}_gregoryorder={gregory_order}_finitedifforder=1")],
+    #                       fileending=f"dt=1e-{dt_exp}_gregory={gregory_order}", save_dir=plot_dir)
+
+    for gregory_order in gregory_orders:
+        plot_susceptibles([os.path.join(result_dir, f"result_ode_dt=1e-6"),
+                           os.path.join(result_dir, f"result_ide_dt=1e-{dt_ide}_gregoryorder={gregory_order}")],
+                          fileending=f"dt=1e-{dt_ide}_gregory={gregory_order}", save_dir=plot_dir)
--- plot_scripts/plot_convergence.py
+++ plot_scripts/plot_convergence.py
@@ -31,11 +31,11 @@ from matplotlib.transforms import Affine2D
 def read_groundtruth(data_dir, groundtruth_exponent, gregory_order, finite_difference_order, flows=False):
     """ Read groundtruth from data. We define the groundtruth as the results obtained by the ODE model with timestep dt=1e-6.
 
-    @param[in] data_dir Directory where h5 files are stored. 
+    @param[in] data_dir Directory where h5 files are stored.
     @param[in] ode_exponent Exponent that determines time step size via dt =10^{-ode_exponent}.
     @param[in] save_exponent The results of the ODE model were saved using the step size 10^{-save_exponent}.
-    @param[in] flows Bool that determines whether we consider flows or compartments. Default is False. 
-    @returns Dict with results of ODE model.    
+    @param[in] flows Bool that determines whether we consider flows or compartments. Default is False.
+    @returns Dict with results of ODE model.
     """
     model = 'ide'
     results = {model: []}
@@ -45,7 +45,7 @@ def read_groundtruth(data_dir, groundtruth_exponent, gregory_order, finite_diffe
         #     data_dir, f'result_{model}_flows_dt=1e-{ode_exponent:.0f}_savefrequency{save_exponent:.0f}.h5'), 'r')
     else:
         h5file = h5py.File(os.path.join(
-            data_dir, f'result_{model}_dt=1e-{groundtruth_exponent:.0f}_gregoryorder={gregory_order}_finitedifforder={finite_difference_order}.h5'), 'r')
+            data_dir, f'result_{model}_dt=1e-{groundtruth_exponent:.0f}_gregoryorder={gregory_order}.h5'), 'r')
 
     if (len(list(h5file.keys())) > 1):
         raise gd.DataError("File should contain one dataset.")
@@ -75,16 +75,16 @@ def read_groundtruth(data_dir, groundtruth_exponent, gregory_order, finite_diffe
 
 def read_data(data_dir, exponents_ide, gregory_order, finite_difference_order, flows=False):
     """ Read data into a dict, where the keys correspond to the respective model.
-    At the moment we are only storing results of the IDE model here. There, we have an array that contains all results 
-    obtained with the IDE model for all time points for each time step size that is investigated. The results can 
+    At the moment we are only storing results of the IDE model here. There, we have an array that contains all results
+    obtained with the IDE model for all time points for each time step size that is investigated. The results can
     either be compartments or flows as indicated by the flag 'flows'.
 
-    @param[in] data_dir Directory where h5 files are stored. 
+    @param[in] data_dir Directory where h5 files are stored.
     @param[in] ode_exponent Exponent that determines time step size of ODE simulation via dt =10^{-ode_exponent}.
-    @param[in] exponents_ide List of considered exponents that determine time step size of IDE simulation via 
+    @param[in] exponents_ide List of considered exponents that determine time step size of IDE simulation via
     dt =10^{-exponent_ide}.
-    @param[in] flows Bool that determines whether we consider flows or compartments. Default is False. 
-    @returns Dict with results of ODE model per time step size.   
+    @param[in] flows Bool that determines whether we consider flows or compartments. Default is False.
+    @returns Dict with results of ODE model per time step size.
     """
     models = ['ide']
     results = {models[0]: []}
@@ -96,7 +96,7 @@ def read_data(data_dir, exponents_ide, gregory_order, finite_difference_order, f
                 #     data_dir, f'result_{model}_flows_dt=1e-{exponent:.0f}_init_dt_ode=1e-{ode_exponent:.0f}.h5'), 'r')
             else:
                 h5file = h5py.File(os.path.join(
-                    data_dir, f'result_{model}_dt=1e-{exponent:.0f}_gregoryorder={gregory_order}_finitedifforder={finite_difference_order}.h5'), 'r')
+                    data_dir, f'result_{model}_dt=1e-{exponent:.0f}_gregoryorder={gregory_order}.h5'), 'r')
 
             data = h5file[list(h5file.keys())[0]]
 
@@ -121,7 +121,7 @@ def compute_l2_norm(timeseries, timestep):
     """ Computes L2 norm of a time series.
 
     @param[in] timeseries Considered timeseries.
-    @param[in] timestep Time step size. 
+    @param[in] timestep Time step size.
     @returns Norm.
     """
     norm = np.sqrt(timestep * np.sum(timeseries**2))
@@ -133,17 +133,19 @@ def compute_relerror_norm_l2(groundtruth, results, groundtruth_exponent, timeste
     from IDE for all compartments/flows.
 
     @param[in] groundtruth Result obtained with ODE model.
-    @param[in] results Results obtained with IDE model for different time step sizes. 
+    @param[in] results Results obtained with IDE model for different time step sizes.
     @param[in] save_exponent The results of the ODE model were saved using the step size 10^{-save_exponent}.
     @param[in] timesteps_ide List of time steps used in IDE simulations.
-    @param[in] flows Bool that determines whether we consider flows or compartments. Default is False. 
+    @param[in] flows Bool that determines whether we consider flows or compartments. Default is False.
     @param[in] Array that contains computed errors.
     """
     # if flows:
     #     pass
     #     # num_errors = 10
     # else:
-    num_errors = 3
+
+    # For now, only compute error of S compartment.
+    num_errors = 1
     errors = []
 
     # Compute error.
@@ -154,77 +156,66 @@ def compute_relerror_norm_l2(groundtruth, results, groundtruth_exponent, timeste
             scale_timesteps = timestep/pow(10, -groundtruth_exponent)
             num_timepoints = len(results['ide'][i])
 
-            difference = groundtruth['ide'][0][int(scale_timesteps*(num_timepoints -
-                                                                    (finite_difference_order + 1)))::int(scale_timesteps)][:, compartment]-results['ide'][i][finite_difference_order + 1:, compartment]
-            norm_groundtruth = compute_l2_norm(groundtruth['ide'][0][int(scale_timesteps*(num_timepoints -
-                                                                                          (finite_difference_order + 1)))::int(scale_timesteps)][:, compartment], timestep)
-            errors[i].append(compute_l2_norm(
-                difference, timestep)/norm_groundtruth)
+            difference = np.abs(groundtruth['ide'][0][int(
+                finite_difference_order + 1)::int(scale_timesteps)][-1, compartment]-results['ide'][i][-1, compartment])
+            # norm_groundtruth = compute_l2_norm(groundtruth['ide'][0][int(
+            #     (finite_difference_order + 1)):: int(scale_timesteps)][:, compartment], timestep)
+            errors[i].append(difference)
 
     return np.array(errors)
 
 
-def plot_convergence(errors, timesteps_ide, flows=False, save_dir=""):
+def plot_convergence(errors, timesteps_ide, gregory_order, flows=False, save_dir=""):
     """ Plots errors against timesteps with a subplot for each compartment /flow.
 
     @param[in] errors Array that contains computed errors of IDE model compared to groundtruth.
     @param[in] timesteps_ide List of time steps used in IDE simulations.
-    @param[in] flows Bool that determines whether we consider flows or compartments. Default is False. 
-    @param[in] save_dir Directory where plot will be stored. Default is an empty string leading to the plot not being 
+    @param[in] flows Bool that determines whether we consider flows or compartments. Default is False.
+    @param[in] save_dir Directory where plot will be stored. Default is an empty string leading to the plot not being
         saved.
     """
     # Define subplots and labels.
-    if flows:
-        fig, axs = plt.subplots(
-            5, 2, sharex=True,  sharey=True, figsize=(10, 10))
-        num_plots = 10
-        secir_dict = {0: r"$\sigma_S^E$", 1: r"$\sigma_E^C$", 2: r"$\sigma_C^I$", 3: r"$\sigma_C^R$", 4: r"$\sigma_I^H$",
-                      5: r"$\sigma_I^R$", 6: r"$\sigma_H^U$", 7: r"$\sigma_H^R$", 8: r"$\sigma_U^D$", 9: r"$\sigma_U^R$"}
-        labels = [
-            r"$\Vert {\widehat{\sigma}}_{\text{IDE}} - {\widehat{\sigma}}_{\text{ODE}}\Vert_{2,\text{rel}}$", r"$\mathcal{O}(\Delta t)$"]
-    else:
-        fig, axs = plt.subplots(4, 2, sharex=True, figsize=(10, 8))
-        num_plots = 3
-        secir_dict = {0: 'Susceptible', 1:  'Infected', 2:  'Recovered'}
-        labels = [
-            r"$\Vert \widehat{Z}_{\text{IDE}} - \widehat{Z}_{\text{ODE}}\Vert_{2,\text{rel}}$", r"$\mathcal{O}(\Delta t)$"]
+
+    num_plots = 1
+    fig, axs = plt.subplots(1, num_plots, sharex=True, figsize=(10, 8))
+    secir_dict = {0: 'Susceptible', 1:  'Infected', 2:  'Recovered'}
+    labels = [f"Gregory order {gregory_order}", r"$\mathcal{O}(\Delta t)$"]
 
     # Define colors, we use helmholtzdarkblue and helmholtzclaim.
-    colors = [(0, 40/255, 100/255), (20/255, 200/255, 255/255)]
+    colors = ["C0", "limegreen"]
 
     for i in range(num_plots):
         # Plot results.
-        axs[int(i/2), i % 2].plot(timesteps_ide,
-                                  errors[:, i], '-o', color=colors[1])
+        axs.plot(timesteps_ide,
+                 errors[:, i], '-o', color=colors[1])
 
         # Plot comparison line for linear convergence.
         comparison = [dt for dt in timesteps_ide]
-        axs[int(i/2), i % 2].plot(timesteps_ide, comparison,
-                                  '--', color='gray', linewidth=1.2)
+        axs.plot(timesteps_ide, comparison,
+                 '--', color='gray', linewidth=1.2)
 
         # Adapt plots.
-        axs[int(i/2), i % 2].set_xscale("log", base=10)
-        axs[int(i/2), i % 2].set_yscale("log", base=10)
+        axs.set_xscale("log", base=10)
+        axs.set_yscale("log", base=10)
 
-        axs[int(i/2), i % 2].set_title(secir_dict[i], fontsize=10)
-        axs[int(i/2), i % 2].grid(True, linestyle='--', alpha=0.6)
+        axs.set_title(secir_dict[i], fontsize=10)
+        axs.grid(True, linestyle='--', alpha=0.6)
 
     fig.supxlabel(r'Time step $\Delta t$', fontsize=12)
+    fig.supylabel(
+        r"$\Vert \widehat{Z}_{\text{IDE}} - \widehat{Z}_{\text{ODE}}\Vert_{2}$", fontsize=12)
 
     # Invert x axis only for one plot so that sharex=True and invert_xaxis work as intended.
-    axs[0, 0].invert_xaxis()
+    axs.invert_xaxis()
 
-    lgd = fig.legend(labels, ncol=2,  loc='outside lower center',
-                     fontsize=14, bbox_to_anchor=(0.5, -0.06), bbox_transform=fig.transFigure)
+    legend = fig.legend(labels, ncol=2,  loc='outside lower center',
+                        fontsize=14, bbox_to_anchor=(0.5, -0.06), bbox_transform=fig.transFigure)
     plt.tight_layout(pad=0, w_pad=0.5, h_pad=0.1)
     if save_dir != "":
         if not os.path.isdir(save_dir):
             os.makedirs(save_dir)
-        if flows:
-            plt.savefig(f'{save_dir}/convergence_all_flows.png', format='png', bbox_extra_artists=(lgd,), bbox_inches='tight',
-                        dpi=500)
         else:
-            plt.savefig(f'{save_dir}/convergence_all_compartments.png', format='png', bbox_extra_artists=(lgd,), bbox_inches='tight',
+            plt.savefig(f'{save_dir}/convergence_all_compartments.png', format='png', bbox_extra_artists=(legend,), bbox_inches='tight',
                         dpi=500)
 
 
@@ -233,8 +224,8 @@ def plot_convergence_oneplot(errors, timesteps_ide, flows=False, save_dir=""):
 
     @param[in] errors Array that contains computed errors of IDE model compared to groundtruth.
     @param[in] timesteps_ide List of time steps used in IDE simulations.
-    @param[in] flows Bool that determines whether we consider flows or compartments. Default is False. 
-    @param[in] save_dir Directory where plot will be stored. Default is an empty string leading to the plot not being 
+    @param[in] flows Bool that determines whether we consider flows or compartments. Default is False.
+    @param[in] save_dir Directory where plot will be stored. Default is an empty string leading to the plot not being
         saved.
     """
     plt.figure()
@@ -253,7 +244,7 @@ def plot_convergence_oneplot(errors, timesteps_ide, flows=False, save_dir=""):
     if flows:
         num_lines = 2
     else:
-        num_lines = 3
+        num_lines = 1
 
     colors = [plt.cm.viridis(x) for x in np.linspace(0, 1, num_lines)]
 
@@ -298,12 +289,12 @@ def compute_order_of_convergence(errors, timesteps_ide, flows=False):
 
     @param[in] errors Array that contains computed errors of IDE model compared to groundtruth.
     @param[in] timesteps_ide List of time steps used in IDE simulations.
-    @param[in] flows Bool that determines whether we consider flows or compartments. Default is False. 
+    @param[in] flows Bool that determines whether we consider flows or compartments. Default is False.
     """
     if flows:
         num_orders = 2
     else:
-        num_orders = 3
+        num_orders = 1
 
     order = []
     for compartment in range(num_orders):
@@ -315,25 +306,23 @@ def compute_order_of_convergence(errors, timesteps_ide, flows=False):
 
 
 def main():
-    # Paths are valid if script is executed e.g. in
-    # memilio/cpp/simulations/2024_Wendler_Nonstandard_numerical_scheme_for_integro-differential_model.
 
     # Path where simulation results (generated with ide_convergence_rate.cpp) are stored.
     result_dir = os.path.join(os.path.dirname(
-        __file__),  "../simulation_results/")
+        __file__),  "../simulation_results/messina/")
 
     # Path where plots will be stored.
     plot_dir = os.path.join(os.path.dirname(
-        __file__),  "../plots/convergence/")
+        __file__),  "../plots/messina/")
 
-    groundtruth_exponent = 3
+    groundtruth_exponent = 5
 
     gregory_order_groundtruth = 3
     gregory_order_simulation = 1
     finite_difference_order = 1
 
     # The IDE model was simulated using a fixed step size dt=10^{-ide_exponent} for ide_exponent in ide_exponents.
-    ide_exponents = [0, 1, 2]
+    ide_exponents = [1, 2, 3]
     # Calculate time steps resulting from exponents_ide.
     timesteps_ide = []
     for exp in ide_exponents:
@@ -348,18 +337,19 @@ def main():
             result_dir, groundtruth_exponent, gregory_order_groundtruth, finite_difference_order, flow_bool)
 
         # Read results from IDE simulations.
-        results = read_data(result_dir, ide_exponents,  gregory_order_simulation, finite_difference_order,
+        results = read_data(result_dir, ide_exponents, gregory_order_simulation, finite_difference_order,
                             flow_bool)
 
         # Compute relative L2 error norm of IDE results compared to groundtruth.
         relerrors_l2 = compute_relerror_norm_l2(
             groundtruth, results, groundtruth_exponent, timesteps_ide, finite_difference_order, flow_bool)
 
-        # Plot convergence of all compartments/flows in one plot, respectively.
-        plot_convergence_oneplot(
-            relerrors_l2, timesteps_ide, flow_bool, plot_dir)
-        # Plot convergence of all compartments/flows separately.
-        plot_convergence(relerrors_l2, timesteps_ide,  flow_bool, plot_dir)
+        # # Plot convergence of all compartments/flows in one plot, respectively.
+        # plot_convergence_oneplot(
+        #     relerrors_l2, timesteps_ide, flow_bool, plot_dir)
+        # # Plot convergence of all compartments/flows separately.
+        plot_convergence(relerrors_l2, timesteps_ide,
+                         gregory_order_simulation, flow_bool, plot_dir)
 
         # Determine order of convergence
         order = compute_order_of_convergence(
