166341157b18ba6268ece38f9094d08b051bce7d
Manuel Heger <mh.heger@gmail.com>
2025-06-18 08:44:57 +0200

    Updating Tests

--- pycode/memilio-surrogatemodel/memilio/surrogatemodel_test/test_surrogatemodel_ode_secir_groups.py
+++ pycode/memilio-surrogatemodel/memilio/surrogatemodel_test/test_surrogatemodel_ode_secir_groups.py
@@ -717,10 +717,10 @@ class TestSurrogatemodelOdeSecirGroups(fake_filesystem_unittest.TestCase):
 
         model.save_model(mlp1.model,
                          self.path, "mlp_multi_multi")
+        self.assertEqual(len(os.listdir(self.path)), 2)
 
-        path = os.path.join(self.path, "mlp_multi_multi.keras")
-        mlp2 = model.load_model(path)
-        self.assertEqual(mlp1.model.summary(), mlp2.summary())
+        self.assertEqual(os.listdir(self.path),
+                         ['data_secir_groups_10days_2_active.pickle', 'mlp_multi_multi.keras'])
 
 
 if __name__ == '__main__':
