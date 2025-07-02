49db37cd2e6e607a0f6527178b1f104d0d31230f
Manuel Heger <mh.heger@gmail.com>
2025-07-02 07:43:43 +0200

    Ci test

--- pycode/memilio-surrogatemodel/memilio/surrogatemodel_test/test_surrogatemodel_ode_secir_groups.py
+++ pycode/memilio-surrogatemodel/memilio/surrogatemodel_test/test_surrogatemodel_ode_secir_groups.py
@@ -222,6 +222,7 @@ class TestSurrogatemodelOdeSecirGroups(fake_filesystem_unittest.TestCase):
 
 # # Testing network_architectures.py
 
+
     def test_mlp_multi_single(self):
         with self.assertRaises(ValueError) as error:
             network_architectures.mlp_multi_input_single_output(
@@ -732,8 +733,7 @@ class TestSurrogatemodelOdeSecirGroups(fake_filesystem_unittest.TestCase):
             print("-_-_-_-_-_- Datei existiert -_-_-_-_-")
         else:
             print("Neeeeeeeeeiiiiiiiiinnnnnnnn.......nnnnnnn......nnnnnnn")
-        """
-        mlp2 = utils.load_model(path_file)
+        mlp2 = tf.keras.models.load_model(path)
 
         weights1 = mlp1.model.get_weights()
         weights2 = mlp2.get_weights()
