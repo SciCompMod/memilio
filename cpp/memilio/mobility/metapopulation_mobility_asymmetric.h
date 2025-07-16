e03d661ab61654f2f1cb48348fe8392b4f192029
Kilian Volmer <13285635+kilianvolmer@users.noreply.github.com>
2025-07-16 17:27:56 +0200

    CHG: Add functions for parameter lookups

--- cpp/memilio/mobility/metapopulation_mobility_asymmetric.h
+++ cpp/memilio/mobility/metapopulation_mobility_asymmetric.h
@@ -64,15 +64,41 @@ public:
         return _exchanges.top().number;
     };
     /**
-     * @brief Return the timepoint of the next exchange event.
+     * @brief Return the timepoint of the next exchangeclass EdgePropertyT event.
      * 
      * @return auto 
      */
     auto next_event_time()
     {
-        return _exchanges.top();
+        return _exchanges.top().time;
     };
-
+    /**
+     * @brief Return the destination node id of the next exchange
+     * 
+     * @return auto 
+     */
+    auto next_event_node_to()
+    {
+        return _exchanges.top().node_to;
+    }
+    /**
+     * @brief Return a const reference to the next event
+     * 
+     * @return auto 
+     */
+    auto next_event()
+    {
+        return _exchanges.top();
+    }
+    /**
+     * @brief Delete the next event from the heap
+     * 
+     * @return auto 
+     */
+    auto pop_next_event()
+    {
+        return _exchanges.pop()
+    }
     /**
      * @brief Return the ExchangeData for the next exchange event and delete it from the list.
      * 
@@ -99,6 +125,7 @@ private:
     struct ExchangeData {
         double time;
         int number;
+        int node_to;
     };
 
     struct CompareExchangeData {
@@ -112,91 +139,94 @@ private:
     std::priority_queue<ExchangeData, std::vector<ExchangeData>, CompareExchangeData> _exchanges;
 };
 
-// /**
-//  * represents the mobility between two nodes.
-//  */
-// class MobilityEdgeDirected
-// {
-// public:
-//     /**
-//      * create edge with coefficients.
-//      * @param coeffs mobility rate for each group and compartment
-//      */
-//     MobilityEdgeDirected(const MobilityParametersTimed& params)
-//         : m_parameters(params)
-//     {
-//     }
+/**
+ * represents the mobility between two nodes.
+ */
+class MobilityEdgeDirected
+{
+public:
+    /**
+     * create edge with timed movement parameters.
+     * @param params mobility rate for each group and compartment
+     */
+    MobilityEdgeDirected(const MobilityParametersTimed& params)
+        : m_parameters(params)
+    {
+    }
 
-//     /**
-//      * get the mobility parameters.
-//      */
-//     const MobilityParametersTimed& get_parameters() const
-//     {
-//         return m_parameters;
-//     }
+    /**
+         * get the mobility parameters.
+         */
+    const MobilityParametersTimed& get_parameters() const
+    {
+        return m_parameters;
+    }
 
-//     /**
-//      * compute mobility from node_from to node_to for a given event
-//      * @param[in] event index specifying which compartment and age group change nodes
-//      * @param node_from node that people changed from
-//      * @param node_to node that people changed to
-//      */
-//     template <class Sim>
-//     void apply_mobility(size_t event, SimulationNode<Sim>& node_from, SimulationNode<Sim>& node_to);
+    /**
+         * compute mobility from node_from to node_to for a given event
+         * @param[in] event index specifying which compartment and age group change nodes
+         * @param node_from node that people changed from
+         * @param node_to node that people changed to
+         */
+    template <class Sim>
+    void apply_mobility(const Graph<SimulationNode<Sim>, MobilityEdgeDirected>& graph);
 
-// private:
-//     MobilityParametersTimed m_parameters;
-// };
+private:
+    MobilityParametersTimed m_parameters;
+};
 
-// template <class Sim>
-// void MobilityEdgeDirected::apply_mobility(size_t event, SimulationNode<Sim>& node_from, SimulationNode<Sim>& node_to)
-// {
-//     node_from.get_result().get_last_value()[event] -= 1;
-//     node_to.get_result().get_last_value()[event] += 1;
-// }
+template <class Sim>
+void MobilityEdgeDirected::apply_mobility(const Graph<SimulationNode<Sim>, MobilityEdgeDirected>& graph)
+{
+    auto node_to_id = m_parameters.next_event_node_to();
+    auto number     = m_parameters.next_event_number();
+    auto time       = m_parameters.next_event_time();
+    m_parameters.pop_next_event();
+    auto node_to = graph.nodes()[node_to_id];
+}
 
-// /**
-//  * edge functor for mobility-based simulation.
-//  * @see MobilityEdgeDirected::apply_mobility
-//  */
-// template <class Sim, class StochasticEdge>
-// void apply_mobility(StochasticEdge& mobilityEdge, size_t event, SimulationNode<Sim>& node_from,
-//                     SimulationNode<Sim>& node_to)
-// {
-//     mobilityEdge.apply_mobility(event, node_from, node_to);
-// }
+/**get_last_value
+     * edge functor for mobility-based simulation.
+     * @see MobilityEdgeDirected::apply_mobility
+     */
+template <class Sim, class StochasticEdge>
+void apply_mobility(StochasticEdge& mobilityEdge, size_t event, SimulationNode<Sim>& node_from,
+                    SimulationNode<Sim>& node_to)
+{
+    mobilityEdge.apply_mobility(event, node_from, node_to);
+}
 
-// /**
-//  * create a mobility-based simulation.
-//  * After every second time step, for each edge a portion of the population corresponding to the coefficients of the edge
-//  * changes from one node to the other. In the next timestep, the mobile population returns to their "home" node.
-//  * Returns are adjusted based on the development in the target node.
-//  * @param t0 start time of the simulation
-//  * @param dt time step between mobility
-//  * @param graph set up for mobility-based simulation
-//  * @{
-//  */
-// template <class Sim>
-// GraphSimulationStochastic<Graph<SimulationNode<Sim>, MobilityEdgeDirected>>
-// make_mobility_sim(double t0, double dt, const Graph<SimulationNode<Sim>, MobilityEdgeDirected>& graph)
-// {
-//     return make_graph_sim_stochastic(
-//         t0, dt, graph, &advance_model<Sim>,
-//         static_cast<void (*)(MobilityEdgeDirected&, size_t, SimulationNode<Sim>&, SimulationNode<Sim>&)>(
-//             &apply_mobility<Sim, MobilityEdgeDirected>));
-// }
+/**
+     * create a mobility-based simulation.
+     * After every second time step, for each edge a portion of the population corresponding to the coefficients of the edge
+     * changes from one node to the other. In the next timestep, the mobile population returns to their "home" node.
+     * Returns are adjusted based on the development in the target node.
+     * @param t0 start time of the simulation
+     * @param dt time step between mobility
+     * @param graph set up for mobility-based simulation
+     * @{
+     */
+template <class Sim>
+GraphSimulationStochastic<Graph<SimulationNode<Sim>, MobilityEdgeDirected>>
+make_mobility_sim(double t0, double dt, const Graph<SimulationNode<Sim>, MobilityEdgeDirected>& graph)
+{
+    return make_graph_sim_stochastic(
+        t0, dt, graph, &advance_model<Sim>,
+        static_cast<void (*)(MobilityEdgeDirected&, size_t, SimulationNode<Sim>&, SimulationNode<Sim>&)>(
+            &apply_mobility<Sim, MobilityEdgeDirected>));
+}
 
-// template <class Sim>
-// GraphSimulationStochastic<Graph<SimulationNode<Sim>, MobilityEdgeDirected>>
-// make_mobility_sim(double t0, double dt, Graph<SimulationNode<Sim>, MobilityEdgeDirected>&& graph)
-// {
-//     return make_graph_sim_stochastic(
-//         t0, dt, std::move(graph), &advance_model<Sim>,
-//         static_cast<void (*)(MobilityEdgeDirected&, size_t, SimulationNode<Sim>&, SimulationNode<Sim>&)>(
-//             &apply_mobility<Sim, MobilityEdgeDirected>));
-// }
+template <class Sim>
+GraphSimulationStochastic<Graph<SimulationNode<Sim>, MobilityEdgeDirected>>
+make_mobility_sim(double t0, double dt, Graph<SimulationNode<Sim>, MobilityEdgeDirected>&& graph)
+{
+    return make_graph_sim_stochastic(
+        t0, dt, std::move(graph), &advance_model<Sim>,
+        static_cast<void (*)(MobilityEdgeDirected&, size_t, SimulationNode<Sim>&, SimulationNode<Sim>&)>(
+            &apply_mobility<Sim, MobilityEdgeDirected>));
+}
 
-// /** @} */
+/** @} */
 
 } // namespace mio
 
