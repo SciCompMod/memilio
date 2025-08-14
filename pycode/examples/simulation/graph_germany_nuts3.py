4d4bd83a3f839a11df664324e7136b884e50f76e
Carlotta Gerstein <carlotta-gerstein@web.de>
2025-08-14 09:16:08 +0200

    save all regions and handle summary_variables keys different

--- pycode/examples/simulation/graph_germany_nuts3.py
+++ pycode/examples/simulation/graph_germany_nuts3.py
@@ -31,7 +31,8 @@ import pandas as pd
 from memilio.epidata import defaultDict as dd
 import pickle
 
-excluded_ids = [11001, 11002, 11003, 11004, 11005, 11006, 11007, 11008, 11009, 11010, 11011, 11012, 16056, 7338, 9374, 9473, 9573]
+excluded_ids = [11001, 11002, 11003, 11004, 11005, 11006, 11007, 11008, 11009, 11010, 11011, 11012, 16056]
+no_icu_ids = [7338, 9374, 9473, 9573]
 region_ids = [region_id for region_id in dd.County.keys() if region_id not in excluded_ids]
 
 class Simulation:
@@ -93,7 +94,7 @@ class Simulation:
         """
 
         start_damping = datetime.date(
-            2020, 7, 8)
+            year=2020, month=7, day=8)
 
         if start_damping < end_date:
             start_date = (start_damping - self.start_date).days
@@ -166,7 +167,7 @@ class Simulation:
         mobility_graph = osecir.MobilityGraph()
         for node_idx in range(graph.num_nodes):
             node = graph.get_node(node_idx)
-            self.set_npis(node.property.parameters, end_date, damping_values[node_idx // 1000 -1])
+            self.set_npis(node.property.parameters, end_date, damping_values[node.id // 1000 -1])
             mobility_graph.add_node(node.id, node.property)
         for edge_idx in range(graph.num_edges):
             mobility_graph.add_edge(
@@ -176,10 +177,13 @@ class Simulation:
         mobility_sim = osecir.MobilitySimulation(mobility_graph, t0=0, dt=0.5)
         mobility_sim.advance(num_days_sim)
 
-        results = []
-        for node_idx in range(graph.num_nodes):
-            results.append(osecir.interpolate_simulation_result(
-            mobility_sim.graph.get_node(node_idx).property.result))
+        results = {}
+        for node_idx in range(mobility_sim.graph.num_nodes):
+            node = mobility_sim.graph.get_node(node_idx)
+            if node.id in no_icu_ids:
+                results[f'no_icu_region{node_idx}'] = osecir.interpolate_simulation_result(node.property.result)
+            else:
+                results[f'region{node_idx}'] = osecir.interpolate_simulation_result(node.property.result)
          
         return results
 
@@ -195,7 +199,7 @@ def run_germany_nuts3_simulation(damping_values):
     
     results = sim.run(num_days_sim, damping_values)
 
-    return {f'region{region_idx}': results[region_idx] for region_idx, region_id in enumerate(region_ids)}
+    return results
 
 def prior():
     damping_values = np.random.uniform(0.0, 1.0, 16)
@@ -209,7 +213,7 @@ def load_divi_data():
     data = data[data['Date']>= np.datetime64(datetime.date(2020, 7, 1))]
     data = data[data['Date'] <= np.datetime64(datetime.date(2020, 7, 1) + datetime.timedelta(days=50))]
     data = data.drop(columns=['County', 'ICU_ventilated', 'Date'])
-    divi_dict = {f"region{i}": data[data['ID_County'] == region_id]['ICU'].to_numpy().reshape((1, 51, 1)) for i, region_id in enumerate(region_ids)}
+    divi_dict = {f"region{i}": data[data['ID_County'] == region_id]['ICU'].to_numpy().reshape((1, 51, 1)) for i, region_id in enumerate(region_ids) if region_id not in no_icu_ids}
 
     return divi_dict
 
@@ -222,22 +226,23 @@ if __name__ == "__main__":
     import bayesflow as bf
 
     simulator = bf.simulators.make_simulator([prior, run_germany_nuts3_simulation])
-    trainings_data = simulator.sample(100)
+    trainings_data = simulator.sample(1000)
 
-    for region in range(len(region_ids)):
-        trainings_data[f'region{region}'] = trainings_data[f'region{region}'][:,:, 8][..., np.newaxis]
+    for key in trainings_data.keys():
+        if key != 'damping_values':
+            trainings_data[key] = trainings_data[key][:, :, 8][..., np.newaxis]
 
-    with open('validation_data_counties.pickle', 'wb') as f:
+    with open('trainings_data10_counties.pickle', 'wb') as f:
         pickle.dump(trainings_data, f, pickle.HIGHEST_PROTOCOL)
 
-    # with open('trainings_data1_400params.pickle', 'rb') as f:
+    # with open('trainings_data1_counties.pickle', 'rb') as f:
     #     trainings_data = pickle.load(f)
-    # # for i in range(9):
-    # #     with open(f'trainings_data{i+2}_400params.pickle', 'rb') as f:
-    # #         data = pickle.load(f)
-    # #     trainings_data = {k: np.concatenate([trainings_data[k], data[k]]) for k in trainings_data.keys()}
+    # for i in range(9):
+    #     with open(f'trainings_data{i+2}_counties.pickle', 'rb') as f:
+    #         data = pickle.load(f)
+    #     trainings_data = {k: np.concatenate([trainings_data[k], data[k]]) for k in trainings_data.keys()}
 
-    # with open('validation_data_400params.pickle', 'rb') as f:
+    # with open('validation_data_counties.pickle', 'rb') as f:
     #     validation_data = pickle.load(f)
 
     # adapter = (
@@ -246,7 +251,7 @@ if __name__ == "__main__":
     #     .convert_dtype("float64", "float32")
     #     .constrain("damping_values", lower=0.0, upper=1.0)
     #     .rename("damping_values", "inference_variables")
-    #     .concatenate([f'region{i}' for i in range(400)], into="summary_variables", axis=-1)
+    #     .concatenate([f'region{i}' for i in range(len(region_ids)) if region_ids[i] not in no_icu_ids], into="summary_variables", axis=-1)
     #     .log("summary_variables", p1=True)
     # )
 
@@ -263,12 +268,12 @@ if __name__ == "__main__":
     #     standardize='all'  
     # )
 
-    # history = workflow.fit_offline(data=trainings_data, epochs=1, batch_size=32, validation_data=validation_data)
+    # history = workflow.fit_offline(data=trainings_data, epochs=100, batch_size=32, validation_data=validation_data)
 
-    # workflow.approximator.save(filepath=os.path.join(os.path.dirname(__file__), "model_test.keras"))
+    # workflow.approximator.save(filepath=os.path.join(os.path.dirname(__file__), "model_countylvl.keras"))
 
     # plots = workflow.plot_default_diagnostics(test_data=validation_data, calibration_ecdf_kwargs={'difference': True, 'stacked': True})
-    # plots['losses'].savefig('losses_diffusionmodel_400params.png')
-    # plots['recovery'].savefig('recovery_diffusionmodel_400params.png')
-    # plots['calibration_ecdf'].savefig('calibration_ecdf_diffusionmodel_400params.png')
-    # plots['z_score_contraction'].savefig('z_score_contraction_diffusionmodel_400params.png')
+    # plots['losses'].savefig('losses_countylvl.png')
+    # plots['recovery'].savefig('recovery_countylvl.png')
+    # plots['calibration_ecdf'].savefig('calibration_ecdf_countylvl.png')
+    # plots['z_score_contraction'].savefig('z_score_contraction_countylvl.png')
