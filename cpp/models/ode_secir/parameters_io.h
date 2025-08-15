54548eebb0100d0a5e1f496b7a6240960ea4da58
HenrZu <69154294+HenrZu@users.noreply.github.com>
2025-08-15 13:58:21 +0200

    rm no age functions for io

--- cpp/models/ode_secir/parameters_io.h
+++ cpp/models/ode_secir/parameters_io.h
@@ -151,99 +151,6 @@ IOResult<void> set_confirmed_cases_data(std::vector<Model<FP>>& model, std::vect
     return success();
 }
 
-IOResult<void> read_confirmed_cases_noage(
-    std::vector<ConfirmedCasesNoAgeEntry>& rki_data, std::vector<int> const& vregion, Date date,
-    std::vector<double>& vnum_Exposed, std::vector<double>& vnum_InfectedNoSymptoms,
-    std::vector<double>& vnum_InfectedSymptoms, std::vector<double>& vnum_InfectedSevere,
-    std::vector<double>& vnum_icu, std::vector<double>& vnum_death,
-    std::vector<double>& vnum_rec, const std::vector<int>& vt_Exposed,
-    const std::vector<int>& vt_InfectedNoSymptoms,
-    const std::vector<int>& vt_InfectedSymptoms, const std::vector<int>& vt_InfectedSevere,
-    const std::vector<int>& vt_InfectedCritical, const std::vector<double>& vmu_C_R,
-    const std::vector<double>& vmu_I_H, const std::vector<double>& vmu_H_U,
-    const double scaling_factor_inf);
-
-/**
- * @brief Sets populations data from already read case data with multiple age groups into a Model with one age group.
- * @tparam FP Floating point data type, e.g., double.
- * @param[in, out] model Vector of models in which the data is set.
- * @param[in] case_data List of confirmed cases data entries.
- * @param[in] region Vector of keys of the region of interest.
- * @param[in] date Date at which the data is read.
- * @param[in] scaling_factor_inf Factors by which to scale the confirmed cases of rki data.
- */
-template <typename FP = double>
-IOResult<void>
-set_confirmed_cases_noage(std::vector<Model<FP>>& model, std::vector<ConfirmedCasesNoAgeEntry>& case_data,
-                          const std::vector<int>& region, Date date, const double scaling_factor_inf)
-{
-    std::vector<int> t_InfectedNoSymptoms(model.size());
-    std::vector<int> t_Exposed(model.size());
-    std::vector<int> t_InfectedSymptoms(model.size());
-    std::vector<int> t_InfectedSevere(model.size());
-    std::vector<int> t_InfectedCritical(model.size());
-
-    std::vector<double> mu_C_R(model.size());
-    std::vector<double> mu_I_H(model.size());
-    std::vector<double> mu_H_U(model.size());
-    std::vector<double> mu_U_D(model.size());
-
-    for (size_t node = 0; node < model.size(); ++node) {
-
-        t_Exposed[node] = 
-            static_cast<int>(std::round(model[node].parameters.template get<TimeExposed<FP>>()[AgeGroup(0)]));
-        t_InfectedNoSymptoms[node]= static_cast<int>(
-            std::round(model[node].parameters.template get<TimeInfectedNoSymptoms<FP>>()[AgeGroup(0)]));
-        t_InfectedSymptoms[node] = 
-            static_cast<int>(std::round(model[node].parameters.template get<TimeInfectedSymptoms<FP>>()[AgeGroup(0)]));
-        t_InfectedSevere[node] = 
-            static_cast<int>(std::round(model[node].parameters.template get<TimeInfectedSevere<FP>>()[AgeGroup(0)]));
-        t_InfectedCritical[node] = 
-            static_cast<int>(std::round(model[node].parameters.template get<TimeInfectedCritical<FP>>()[AgeGroup(0)]));
-
-        mu_C_R[node] = model[node].parameters.template get<RecoveredPerInfectedNoSymptoms<FP>>()[AgeGroup(0)];
-        mu_I_H[node] = model[node].parameters.template get<SeverePerInfectedSymptoms<FP>>()[AgeGroup(0)];
-        mu_H_U[node] = model[node].parameters.template get<CriticalPerSevere<FP>>()[AgeGroup(0)];
-        mu_U_D[node] = model[node].parameters.template get<DeathsPerCritical<FP>>()[AgeGroup(0)];
-    }
-    std::vector<double> num_InfectedSymptoms(model.size(), 0.0);
-    std::vector<double> num_death(model.size(), 0.0);
-    std::vector<double> num_rec(model.size(), 0.0);
-    std::vector<double> num_Exposed(model.size(), 0.0);
-    std::vector<double> num_InfectedNoSymptoms(model.size(), 0.0);
-    std::vector<double> num_InfectedSevere(model.size(), 0.0);
-    std::vector<double> num_icu(model.size(), 0.0);
-
-    BOOST_OUTCOME_TRY(read_confirmed_cases_noage(case_data, region, date, num_Exposed, num_InfectedNoSymptoms,
-                                                 num_InfectedSymptoms, num_InfectedSevere, num_icu, num_death, num_rec,
-                                                 t_Exposed, t_InfectedNoSymptoms, t_InfectedSymptoms, t_InfectedSevere,
-                                                 t_InfectedCritical, mu_C_R, mu_I_H, mu_H_U, scaling_factor_inf));
-
-    for (size_t node = 0; node < model.size(); node++) {
-        if (num_InfectedSymptoms[node] > 0) {
-                model[node].populations[{AgeGroup(0), InfectionState::Exposed}] = num_Exposed[node];
-                model[node].populations[{AgeGroup(0), InfectionState::InfectedNoSymptoms}] =
-                    num_InfectedNoSymptoms[node];
-                model[node].populations[{AgeGroup(0), InfectionState::InfectedNoSymptomsConfirmed}] = 0;
-                model[node].populations[{AgeGroup(0), InfectionState::InfectedSymptoms}] =
-                    num_InfectedSymptoms[node];
-                model[node].populations[{AgeGroup(0), InfectionState::InfectedSymptomsConfirmed}] = 0;
-                model[node].populations[{AgeGroup(0), InfectionState::InfectedSevere}] = num_InfectedSevere[node];
-                // Only set the number of ICU patients here, if the date is not available in the data.
-                if (!is_divi_data_available(date)) {
-                    model[node].populations[{AgeGroup(0), InfectionState::InfectedCritical}] = num_icu[node];
-                }
-                model[node].populations[{AgeGroup(0), InfectionState::Dead}]      = num_death[node];
-                model[node].populations[{AgeGroup(0), InfectionState::Recovered}] = num_rec[node];
-        }
-        else {
-            log_warning("No infections reported on date {} for region {}. Population data has not been set.", date,
-                        region[node]);
-        }
-    }
-    return success();
-}
-
 /**
  * @brief Sets the infected population for a given model based on confirmed cases data. Here, we
  * read the case data from a file.
@@ -259,8 +166,8 @@ IOResult<void> set_confirmed_cases_data(std::vector<Model<FP>>& model, const std
                                         std::vector<int> const& region, Date date,
                                         const std::vector<double>& scaling_factor_inf)
 {
-    BOOST_OUTCOME_TRY(auto&& case_data, mio::read_confirmed_cases_noage(path));
-    BOOST_OUTCOME_TRY(set_confirmed_cases_noage(model, case_data, region, date, scaling_factor_inf[0]));
+    BOOST_OUTCOME_TRY(auto&& case_data, mio::read_confirmed_cases_data(path));
+    BOOST_OUTCOME_TRY(set_confirmed_cases_data(model, case_data, region, date, scaling_factor_inf[0]));
     return success();
 }
 
@@ -488,7 +395,8 @@ IOResult<void> read_input_data_state(std::vector<Model>& model, Date date, std::
 template <class Model>
 IOResult<void> read_input_data_county(std::vector<Model>& model, Date date, const std::vector<int>& county,
                                       const std::vector<double>& scaling_factor_inf, double scaling_factor_icu,
-                                      const std::string& pydata_dir,int /*num_days*/ = 0, bool /*export_time_series*/ = false)
+                                      const std::string& pydata_dir, int /*num_days*/ = 0,
+                                      bool /*export_time_series*/ = false)
 {
     BOOST_OUTCOME_TRY(
         details::set_divi_data(model, path_join(pydata_dir, "county_divi_ma7.json"), county, date, scaling_factor_icu));
