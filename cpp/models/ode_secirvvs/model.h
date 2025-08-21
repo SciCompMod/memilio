03898c7c980e7282d68c1e33072052c87a200dd3
MaxBetz <104758467+MaxBetzDLR@users.noreply.github.com>
2025-08-21 14:35:26 +0200

    Apply suggestions from code review
    Co-authored-by: reneSchm <49305466+reneSchm@users.noreply.github.com>

--- cpp/models/ode_secirvvs/model.h
+++ cpp/models/ode_secirvvs/model.h
@@ -778,7 +778,7 @@ inline auto simulate(FP t0, FP tmax, FP dt, const Model<FP>& model,
   */
 template <typename FP = ScalarType>
 inline auto simulate_flows(FP t0, FP tmax, FP dt, const Model<FP>& model,
-                           std::unique_ptr<OdeIntegratorCore<FP>> integrator = nullptr)
+                           std::unique_ptr<OdeIntegratorCore<FP>>&& integrator = nullptr)
 {
     return mio::simulate_flows<FP, Model<FP>, Simulation<FP, mio::FlowSimulation<FP, Model<FP>>>>(t0, tmax, dt, model,
                                                                                                   std::move(integrator));
