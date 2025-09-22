5a83b8b03523dc2e7c8341c8828474f247871a03
Sascha <51127093+xsaschako@users.noreply.github.com>
2025-09-22 14:59:39 +0200

    update

--- cpp/benchmarks/covasim_benchmark.py
+++ cpp/benchmarks/covasim_benchmark.py
@@ -42,7 +42,7 @@ def covasim_benchmark(pop_size, n_days=120):
 
 if __name__ == "__main__":
     # Population sizes matching the ABM benchmark
-    pop_sizes = [3200000, 6400000, 12800000]
+    pop_sizes = [3200000]
 
     # If command line argument provided, use that specific size
     if len(sys.argv) > 1:
