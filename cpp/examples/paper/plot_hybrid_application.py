f7395d86f7be15afeab67fafdb6e2904759709fd
jubicker <113909589+jubicker@users.noreply.github.com>
2025-10-16 14:51:19 +0200

    plot

--- cpp/examples/paper/plot_hybrid_application.py
+++ cpp/examples/paper/plot_hybrid_application.py
@@ -4,7 +4,7 @@ import numpy as np
 
 from plotting_settings import *
 
-def plot_ts_output(axis, filename, percentiles, color, compartments):
+def plot_ts_output(axis, filename, percentiles, color, compartments, model):
     while len(percentiles) > 0:
         if len(percentiles) == 1:
             df = pd.read_csv(filename + f"_p{percentiles[0]}.csv")
@@ -13,7 +13,36 @@ def plot_ts_output(axis, filename, percentiles, color, compartments):
         else:
             df_low = pd.read_csv(filename + f"_p{percentiles[0]}.csv")
             df_high = pd.read_csv(filename + f"_p{percentiles[-1]}.csv")
-            axis.fill_between(df_low["Time"], df_low[compartments].sum(axis=1), df_high[compartments].sum(axis=1), alpha=0.3, color=color)
+            axis.plot(df_low["Time"], df_low[compartments].sum(axis=1), color=color, alpha=0.2)
+            axis.plot(df_high["Time"], df_high[compartments].sum(axis=1), color=color, alpha=0.2)
+            if(model == "dabm"):
+                axis.fill_between(df_low["Time"], df_low[compartments].sum(axis=1), df_high[compartments].sum(axis=1), alpha=0.3, facecolor='none', edgecolor=color, hatch='////', linewidth=0.0)
+            elif(model == "hybrid"):
+                axis.fill_between(df_low["Time"], df_low[compartments].sum(axis=1), df_high[compartments].sum(axis=1), alpha=0.3, facecolor='none', edgecolor=color, hatch='\\\\', linewidth=0.0)
+            else:
+                axis.fill_between(df_low["Time"], df_low[compartments].sum(axis=1), df_high[compartments].sum(axis=1), alpha=0.3, color=color)
+            del percentiles[0]
+            del percentiles[-1]
+    return axis
+
+def plot_extinction(axis, filename, percentiles, color, compartments, model):
+    while len(percentiles) > 0:
+        if len(percentiles) == 1:
+            linestyle = "--" if model=="dabm" else "-"
+            df = pd.read_csv(filename + f"_extinction_p{percentiles[0]}.csv")
+            axis.plot(df["Time"], df[compartments].sum(axis=1), color=color, linestyle=linestyle)
+            del percentiles[0]
+        else:
+            df_low = pd.read_csv(filename + f"_extinction_p{percentiles[0]}.csv")
+            df_high = pd.read_csv(filename + f"_extinction_p{percentiles[-1]}.csv")
+            axis.plot(df_low["Time"], df_low[compartments].sum(axis=1), color=color, alpha=0.2)
+            axis.plot(df_high["Time"], df_high[compartments].sum(axis=1), color=color, alpha=0.2)
+            if(model == "dabm"):
+                axis.fill_between(df_low["Time"], df_low[compartments].sum(axis=1), df_high[compartments].sum(axis=1), alpha=0.3, facecolor='none', edgecolor=color, hatch='////', linewidth=0.0)
+            elif(model == "hybrid"):
+                axis.fill_between(df_low["Time"], df_low[compartments].sum(axis=1), df_high[compartments].sum(axis=1), alpha=0.3, facecolor='none', edgecolor=color, hatch='\\\\', linewidth=0.0)
+            else:
+                axis.fill_between(df_low["Time"], df_low[compartments].sum(axis=1), df_high[compartments].sum(axis=1), alpha=0.3, color=color)
             del percentiles[0]
             del percentiles[-1]
     return axis
@@ -28,16 +57,25 @@ panel = [0.18, 0.18, 0.8, 0.8]
 figsize = (5, 3.5)
 fig = plt.figure(figsize=figsize)
 ax = fig.add_axes(panel)
-ax = plot_ts_output(ax, result_dir + "dabm", ["05", "50", "95"], model_colors["dabm"], ["E", "Ins", "InsC", "Isy", "IsyC", "Isev", "Icri"])
-ax = plot_ts_output(ax, result_dir + "osecir", ["05", "50", "95"], model_colors["osecir"], ["E", "Ins", "InsC", "Isy", "IsyC", "Isev", "Icri"])
-ax = plot_ts_output(ax, result_dir + "hybrid", ["05", "50", "95"], model_colors["hybrid"], ["E", "Ins", "InsC", "Isy", "IsyC", "Isev", "Icri"])
+ax = plot_ts_output(ax, result_dir + "dabm", ["05", "50", "95"], model_colors["dabm"], ["E", "Ins", "InsC", "Isy", "IsyC", "Isev", "Icri"], "dabm")
+ax = plot_ts_output(ax, result_dir + "osecir", ["05", "50", "95"], model_colors["osecir"], ["E", "Ins", "InsC", "Isy", "IsyC", "Isev", "Icri"], "osecir")
+ax = plot_ts_output(ax, result_dir + "hybrid", ["05", "50", "95"], model_colors["hybrid"], ["E", "Ins", "InsC", "Isy", "IsyC", "Isev", "Icri"], "hybrid")
 ax.set_ylabel("Infected [#]")
-ax.set_xlabel("Time [days]")
+#ax.set_xlabel("Time [days]")
 fig.savefig(save_dir + "hybrid_application_sim_results.png", dpi=dpi)
 
 fig = plt.figure(figsize=figsize)
 fig.legend(handles=[mpatches.Patch(color=model_colors["dabm"], label="ABM"), mpatches.Patch(color=model_colors["osecir"], label="PBM"), mpatches.Patch(color=model_colors["hybrid"], label="Hybrid")], loc='center', ncol=1)
 fig.savefig(save_dir + "hybrid_application_legend.png", dpi=dpi)
+
+# Plot extinction for ABM and Hybrid
+fig = plt.figure(figsize=figsize)
+ax = fig.add_axes(panel)
+ax = plot_extinction(ax, result_dir + "hybrid", ["05", "50", "95"], model_colors["hybrid"], ["E", "Ins", "InsC", "Isy", "IsyC", "Isev", "Icri"], "hybrid")
+ax = plot_extinction(ax, result_dir + "dabm", ["05", "50", "95"], model_colors["dabm"], ["E", "Ins", "InsC", "Isy", "IsyC", "Isev", "Icri"], "dabm")
+ax.set_ylabel("Infected [#]")
+ax.set_xlabel("Time [days]")
+fig.savefig(save_dir + "hybrid_application_sim_results_ext_surv.png", dpi=dpi)
 
 # Runtimes (mean)
 sim_time = []
