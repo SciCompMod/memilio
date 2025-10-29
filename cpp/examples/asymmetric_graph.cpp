99e42d4c948affd79890c5544da1a49bcf38ac46
Kilian Volmer <13285635+kilianvolmer@users.noreply.github.com>
2025-10-29 11:20:56 +0100

    .

--- cpp/examples/asymmetric_graph.cpp
+++ cpp/examples/asymmetric_graph.cpp
@@ -80,11 +80,7 @@ int main(int /*argc*/, char** /*argv*/)
     mio::log_info("Starting Graph generation");
     {
         mio::timing::AutoTimer<"Graph Nodes Generation"> timer;
-<<<<<<< HEAD
         io::CSVReader<4> farms("../../farms.csv");
-=======
-        io::CSVReader<4> farms("/home/kilian/Documents/projects/memilio-asymmetric-graph/farms.csv");
->>>>>>> dab1c992aa3c028686e0440a7f20cf4de6eb8871
         farms.read_header(io::ignore_extra_column, "farms", "num_cows", "latitude", "longitude");
         int farm_id, num_cows;
         double latitude, longitude;
@@ -108,89 +104,82 @@ int main(int /*argc*/, char** /*argv*/)
     interesting_indices.push_back({Model().populations.get_flat_index({home, InfectionState::I})});
     {
         mio::timing::AutoTimer<"Graph Edges Generation"> timer;
-<<<<<<< HEAD
         io::CSVReader<2> edges("../../edges.csv");
-=======
-        io::CSVReader<2> edges("/home/kilian/Documents/projects/memilio-asymmetric-graph/edges.csv");
->>>>>>> dab1c992aa3c028686e0440a7f20cf4de6eb8871
         edges.read_header(io::ignore_extra_column, "from", "to");
-        size_t from, to;
-        while (edges.read_row(from, to)) {
-            graph.add_edge(from, to, interesting_indices);
-            // graph.lazy_add_edge(from, to, interesting_indices);
-        }
-        // graph.sort_edges();
+        // graph.lazy_add_edge(from, to, interesting_indices);
     }
-    mio::log_info("Graph generated");
+    // graph.sort_edges();
+}
+mio::log_info("Graph generated");
 
-    auto nodes = graph.nodes() | std::views::transform([](const auto& node) {
-                     return &node.property;
-                 });
-    auto tree  = mio::geo::RTree({nodes.begin(), nodes.end()});
-    mio::log_info("RTree generated");
+auto nodes = graph.nodes() | std::views::transform([](const auto& node) {
+                 return &node.property;
+             });
+auto tree  = mio::geo::RTree({nodes.begin(), nodes.end()});
+mio::log_info("RTree generated");
 
-    for (auto& node : graph.nodes()) {
-        node.property.set_regional_neighbors(tree.inrange_indices_query(node.property.get_location(), {2.0}));
-    }
+for (auto& node : graph.nodes()) {
+    node.property.set_regional_neighbors(tree.inrange_indices_query(node.property.get_location(), {2.0}));
+}
 
-    mio::log_info("Neighbors set");
-    auto sim = mio::make_mobility_sim(t0, dt, std::move(graph));
+mio::log_info("Neighbors set");
+auto sim = mio::make_mobility_sim(t0, dt, std::move(graph));
 
-    io::CSVReader<5> exchanges("/home/kilian/Documents/projects/memilio-asymmetric-graph/trade.csv");
-    exchanges.read_header(io::ignore_extra_column, "date", "num_animals", "from", "to", "edge");
+io::CSVReader<5> exchanges("../../trade.csv");
+exchanges.read_header(io::ignore_extra_column, "date", "num_animals", "from", "to", "edge");
 
-    int date, num_animals, edge;
-    size_t from, to;
-    while (exchanges.read_row(date, num_animals, from, to, edge)) {
-        sim.add_exchange(date, num_animals, from, to);
-    }
-    mio::log_info("Exchanges added");
+int date, num_animals, edge;
+size_t from, to;
+while (exchanges.read_row(date, num_animals, from, to, edge)) {
+    sim.add_exchange(date, num_animals, from, to);
+}
+mio::log_info("Exchanges added");
 
-    // #ifdef MEMILIO_ENABLE_OPENMP
-    // #pragma omp parallel for
-    //     for (size_t i = 0; i < 100; ++i) {
-    // #endif
+// #ifdef MEMILIO_ENABLE_OPENMP
+// #pragma omp parallel for
+//     for (size_t i = 0; i < 100; ++i) {
+// #endif
 
-    auto sim2(sim);
-    mio::log_info("new Simulation created");
+auto sim2(sim);
+mio::log_info("new Simulation created");
 
-    sim2.advance(tmax);
-    mio::log_info("Simulation finished");
+sim2.advance(tmax);
+mio::log_info("Simulation finished");
 
-    // #ifdef MEMILIO_ENABLE_OPENMP
-    //     }
-    // #endif
+// #ifdef MEMILIO_ENABLE_OPENMP
+//     }
+// #endif
 
-    // sim2.get_graph().nodes()[28].property.get_result().print_table({"S", "E", "I", "R", "D"});
-    // std::cout << "Second Table" << std::endl;
-    // sim2.get_graph().nodes()[1].property.get_result().print_table({"S", "E", "I", "R", "D"});
+// sim2.get_graph().nodes()[28].property.get_result().print_table({"S", "E", "I", "R", "D"});
+// std::cout << "Second Table" << std::endl;
+// sim2.get_graph().nodes()[1].property.get_result().print_table({"S", "E", "I", "R", "D"});
 
-    // auto& edge_1_0 = sim2.get_graph().edges()[1];
-    // auto& results  = edge_1_0.property.get_mobility_results();
-    // results.print_table({"Commuter Sick", "Commuter Total"});
+// auto& edge_1_0 = sim2.get_graph().edges()[1];
+// auto& results  = edge_1_0.property.get_mobility_results();
+// results.print_table({"Commuter Sick", "Commuter Total"});
 
-    // auto exchange_results = sim2.sum_exchanges();
-    // mio::log_info("Sum of exchanged sick animals: {}", exchange_results[0]);
-    // mio::log_info("Sum of exchanged animals: {}", exchange_results[1]);
+// auto exchange_results = sim2.sum_exchanges();
+// mio::log_info("Sum of exchanged sick animals: {}", exchange_results[0]);
+// mio::log_info("Sum of exchanged animals: {}", exchange_results[1]);
 
-    auto sth = sim2.exchanges_per_timestep().export_csv("Exchange_statistics.csv", {"Commuter Sick", "Commuter Total"});
+auto sth = sim2.exchanges_per_timestep().export_csv("Exchange_statistics.csv", {"Commuter Sick", "Commuter Total"});
 
-    // for (auto node : sim2.get_graph().nodes()) {
-    //     if (node.property.get_result().get_num_time_points() < num_time_points) {
-    //         mio::log_error("Node with inconsistent number of time points in results.");
-    //     }
-    // }
+// for (auto node : sim2.get_graph().nodes()) {
+//     if (node.property.get_result().get_num_time_points() < num_time_points) {
+//         mio::log_error("Node with inconsistent number of time points in results.");
+//     }
+// }
 
-    // exchange_results = sim2.sum_nodes();
-    // mio::log_info("{}, {}, {}, {}", exchange_results[0], exchange_results[1], exchange_results[2], exchange_results[3]);
+// exchange_results = sim2.sum_nodes();
+// mio::log_info("{}, {}, {}, {}", exchange_results[0], exchange_results[1], exchange_results[2], exchange_results[3]);
 
-    sth = sim2.statistics_per_timestep().export_csv("Simulation_statistics.csv");
-    // // auto combined_results = sim2.combine_node_results();
-    // // combined_results.print_table({"S", "E", "I", "R", "D"});
-    // // auto ioresult = combined_results.export_csv("Simulation_results.csv");
+sth = sim2.statistics_per_timestep().export_csv("Simulation_statistics.csv");
+// // auto combined_results = sim2.combine_node_results();
+// // combined_results.print_table({"S", "E", "I", "R", "D"});
+// // auto ioresult = combined_results.export_csv("Simulation_results.csv");
 
-    // sim2.statistics_per_timestep({0, 1, 2, 3, 4}).print_table({"S", "E", "I", "R", "D"});
-    mio::log_info("Finished postprocessing");
+// sim2.statistics_per_timestep({0, 1, 2, 3, 4}).print_table({"S", "E", "I", "R", "D"});
+mio::log_info("Finished postprocessing");
 
-    return 0;
+return 0;
 }
