bdff456fc9289f5fed4a457bc5674701f27ccb39
Kilian Volmer <13285635+kilianvolmer@users.noreply.github.com>
2025-11-25 11:14:32 +0000

    CHG: Benchmarking script for new method comparison

--- cpp/examples/asym_graph_ben.cpp
+++ cpp/examples/asym_graph_ben.cpp
@@ -34,6 +34,7 @@
 #include "thirdparty/csv.h"
 #include <ranges>
 #include <omp.h>
+#include <chrono>
 
 enum class InfectionState
 {
@@ -77,14 +78,11 @@ int main(int /*argc*/, char** /*argv*/)
     adoption_rates.push_back({ICS, R, home, 0.4, {}});
     adoption_rates.push_back({INS, R, home, 0.5, {}});
 
-    mio::Graph<mio::LocationNode<ScalarType, mio::smm::Simulation<ScalarType, 1, InfectionState>>,
-               mio::MobilityEdgeDirected<ScalarType>>
-        graph;
     mio::log_info("Reading CSVs");
     std::vector<ScalarType> latitudes, longitudes;
     std::vector<int> farm_ids, num_cows_vec, dates, num_animals_exchanges;
     std::vector<int> froms, tos, from_exchanges, to_exchanges;
-    io::CSVReader<4> farms("../../farms200000.csv");
+    io::CSVReader<4> farms("../../../farms16mio.csv");
     farms.read_header(io::ignore_extra_column, "farms", "num_cows", "latitude", "longitude");
     int farm_id, num_cows;
     double latitude, longitude;
@@ -94,16 +92,15 @@ int main(int /*argc*/, char** /*argv*/)
         latitudes.push_back(latitude);
         longitudes.push_back(longitude);
     }
-    io::CSVReader<2> edges("../../edges200000.csv");
+    io::CSVReader<2> edges("../../../edges16mio.csv");
     edges.read_header(io::ignore_extra_column, "from", "to");
     size_t from, to;
     while (edges.read_row(from, to)) {
         froms.push_back(from);
         tos.push_back(to);
     }
-    io::CSVReader<4> exchanges("../../trade200000.csv");
+    io::CSVReader<4> exchanges("../../../trade16mio.csv");
     exchanges.read_header(io::ignore_extra_column, "date", "num_animals", "from", "to");
-
     int date, num_animals;
     while (exchanges.read_row(date, num_animals, from, to)) {
         dates.push_back(date);
@@ -111,69 +108,83 @@ int main(int /*argc*/, char** /*argv*/)
         from_exchanges.push_back(from);
         to_exchanges.push_back(to);
     }
-    for (size_t i = 0; i < farm_ids.size(); ++i) {
-        Model curr_model;
-        curr_model.populations[{home, InfectionState::S}]                                = num_cows_vec[i];
-        curr_model.populations[{home, InfectionState::E}]                                = 0;
-        curr_model.populations[{home, InfectionState::I}]                                = 0;
-        curr_model.populations[{home, InfectionState::INS}]                              = 0;
-        curr_model.populations[{home, InfectionState::ICS}]                              = 0;
-        curr_model.populations[{home, InfectionState::R}]                                = 0;
-        curr_model.populations[{home, InfectionState::D}]                                = 0;
-        curr_model.parameters.get<mio::smm::AdoptionRates<ScalarType, InfectionState>>() = adoption_rates;
-        graph.add_node(farm_ids[i], longitudes[i], latitudes[i], curr_model, t0);
-    }
-    auto rng = mio::RandomNumberGenerator();
-
     std::vector<std::vector<size_t>> interesting_indices;
     interesting_indices.push_back({Model().populations.get_flat_index({home, InfectionState::I})});
 
-    for (size_t i = 0; i < froms.size(); ++i) {
-        mio::timing::AutoTimer<"Graph Edges Generation"> timer;
-        graph.add_edge(froms[i], tos[i], interesting_indices);
+    for (size_t num_edges_benchmark = 1024; num_edges_benchmark < 16000000; num_edges_benchmark *= 2) {
+        mio::log_info("Running with {} edges", num_edges_benchmark);
+
+        mio::Graph<mio::LocationNode<ScalarType, mio::smm::Simulation<ScalarType, 1, InfectionState>>,
+                   mio::MobilityEdgeDirected<ScalarType>>
+            graph;
+        mio::GraphBuilder<mio::LocationNode<ScalarType, mio::smm::Simulation<ScalarType, 1, InfectionState>>,
+                          mio::MobilityEdgeDirected<ScalarType>>
+            builder;
+
+        auto start = std::chrono::high_resolution_clock::now();
+
+        for (size_t i = 0; i < farm_ids.size(); ++i) {
+            Model curr_model;
+            curr_model.populations[{home, InfectionState::S}]                                = num_cows_vec[i];
+            curr_model.populations[{home, InfectionState::E}]                                = 0;
+            curr_model.populations[{home, InfectionState::I}]                                = 0;
+            curr_model.populations[{home, InfectionState::INS}]                              = 0;
+            curr_model.populations[{home, InfectionState::ICS}]                              = 0;
+            curr_model.populations[{home, InfectionState::R}]                                = 0;
+            curr_model.populations[{home, InfectionState::D}]                                = 0;
+            curr_model.parameters.get<mio::smm::AdoptionRates<ScalarType, InfectionState>>() = adoption_rates;
+            graph.add_node(farm_ids[i], longitudes[i], latitudes[i], curr_model, t0);
+        }
+        auto rng = mio::RandomNumberGenerator();
+
+        for (size_t i = 0; i < num_edges_benchmark; ++i) {
+            graph.add_edge(froms[i], tos[i], interesting_indices);
+        }
+        auto end      = std::chrono::high_resolution_clock::now();
+        auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+
+        mio::log_info("Classic graph generation finished for {} edges in {} milliseconds", num_edges_benchmark,
+                      duration.count());
+        start = std::chrono::high_resolution_clock::now();
+        for (size_t i = 0; i < farm_ids.size(); ++i) {
+            Model curr_model;
+            curr_model.populations[{home, InfectionState::S}]                                = num_cows_vec[i];
+            curr_model.populations[{home, InfectionState::E}]                                = 0;
+            curr_model.populations[{home, InfectionState::I}]                                = 0;
+            curr_model.populations[{home, InfectionState::INS}]                              = 0;
+            curr_model.populations[{home, InfectionState::ICS}]                              = 0;
+            curr_model.populations[{home, InfectionState::R}]                                = 0;
+            curr_model.populations[{home, InfectionState::D}]                                = 0;
+            curr_model.parameters.get<mio::smm::AdoptionRates<ScalarType, InfectionState>>() = adoption_rates;
+            builder.add_node(farm_ids[i], longitudes[i], latitudes[i], curr_model, t0);
+        }
+        for (size_t i = 0; i < num_edges_benchmark; ++i) {
+            builder.add_edge(from_exchanges[i], to_exchanges[i], interesting_indices);
+        }
+        auto graph2 = builder.build(true);
+        end         = std::chrono::high_resolution_clock::now();
+        duration    = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+
+        mio::log_info("New graph construction finished using {} edges in {} milliseconds", num_edges_benchmark,
+                      duration.count());
+
+        // auto nodes = graph.nodes() | std::views::transform([](const auto& node) {
+        //                  return &node.property;
+        //              });
+        // auto tree  = mio::geo::RTree(nodes.begin(), nodes.end());
+
+        // for (auto& node : graph.nodes()) {
+        //     node.property.set_regional_neighbors(
+        //         tree.in_range_indices_query(node.property.get_location(), {mio::geo::kilometers(2.0)}));
+        // }
+
+        auto sim = mio::make_mobility_sim(t0, dt, std::move(graph));
+
+        for (size_t i = 0; i < dates.size(); ++i) {
+            sim.add_exchange(dates[i], num_animals_exchanges[i], from_exchanges[i], to_exchanges[i]);
+        }
+
+        mio::log_info("Starting and immediately finishing simulation");
     }
-
-    mio::log_info("First graph construction finished");
-
-    mio::Graph<mio::LocationNode<ScalarType, mio::smm::Simulation<ScalarType, 1, InfectionState>>,
-               mio::MobilityEdgeDirected<ScalarType>>
-        graph2;
-    for (size_t i = 0; i < farm_ids.size(); ++i) {
-        Model curr_model;
-        curr_model.populations[{home, InfectionState::S}]                                = num_cows_vec[i];
-        curr_model.populations[{home, InfectionState::E}]                                = 0;
-        curr_model.populations[{home, InfectionState::I}]                                = 0;
-        curr_model.populations[{home, InfectionState::INS}]                              = 0;
-        curr_model.populations[{home, InfectionState::ICS}]                              = 0;
-        curr_model.populations[{home, InfectionState::R}]                                = 0;
-        curr_model.populations[{home, InfectionState::D}]                                = 0;
-        curr_model.parameters.get<mio::smm::AdoptionRates<ScalarType, InfectionState>>() = adoption_rates;
-        graph2.add_node(farm_ids[i], longitudes[i], latitudes[i], curr_model, t0);
-    }
-    for (size_t i = 0; i < froms.size(); ++i) {
-        mio::timing::AutoTimer<"Graph Edges lazy Generation"> timer;
-        graph2.lazy_add_edge(froms[i], tos[i], interesting_indices);
-    }
-    graph2.sort_edges();
-    graph2.make_edges_unique();
-    mio::log_info("Second graph construction finished");
-
-    auto nodes = graph.nodes() | std::views::transform([](const auto& node) {
-                     return &node.property;
-                 });
-    auto tree  = mio::geo::RTree(nodes.begin(), nodes.end());
-
-    for (auto& node : graph.nodes()) {
-        node.property.set_regional_neighbors(
-            tree.in_range_indices_query(node.property.get_location(), {mio::geo::kilometers(2.0)}));
-    }
-
-    auto sim = mio::make_mobility_sim(t0, dt, std::move(graph));
-
-    for (size_t i = 0; i < dates.size(); ++i) {
-        sim.add_exchange(dates[i], num_animals_exchanges[i], from_exchanges[i], to_exchanges[i]);
-    }
-
-    mio::log_info("Starting and immediately finishing simulation");
     return 0;
 }
