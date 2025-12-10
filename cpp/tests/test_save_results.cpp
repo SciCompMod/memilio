27da820db496f620b260d34d627bdd936a62fa02
MaxBetz <104758467+MaxBetzDLR@users.noreply.github.com>
2025-12-10 10:59:48 +0000

    expand test

--- cpp/tests/test_save_results.cpp
+++ cpp/tests/test_save_results.cpp
@@ -229,13 +229,14 @@ TEST(TestSaveResult, save_result_with_params)
 
 TEST(TestSaveResult, save_result_order)
 {
-    // case: check order of results, where lexical ordering would rearrange the results; 
-    // expect: order follows the ids
     std::vector<mio::TimeSeries<double>> results{
         mio::TimeSeries<double>(0, Eigen::VectorX<double>::Constant(1, 0)), 
         mio::TimeSeries<double>(0, Eigen::VectorX<double>::Constant(1, 1)), 
         mio::TimeSeries<double>(0, Eigen::VectorX<double>::Constant(1, 2))};
-    std::vector<int> ids                                  = {1, 2, 10};
+
+    // case: check order of results, where lexical ordering would rearrange the results; 
+    // expect: order follows the ids
+    std::vector<int> ids = {1, 2, 10};
 
     TempFileRegister file_register;
     auto results_file_path  = file_register.get_unique_path("test_result-%%%%-%%%%.h5");
@@ -248,6 +249,21 @@ TEST(TestSaveResult, save_result_order)
     ASSERT_DOUBLE_EQ(results_from_file.value()[0].get_groups()[Eigen::Index(0)][Eigen::Index(0)], 0);
     ASSERT_DOUBLE_EQ(results_from_file.value()[1].get_groups()[Eigen::Index(0)][Eigen::Index(0)], 1);
     ASSERT_DOUBLE_EQ(results_from_file.value()[2].get_groups()[Eigen::Index(0)][Eigen::Index(0)], 2);
+
+    // case: check order of results; 
+    // expect: order is changed due to ids not increasing
+    ids = {1, 10, 2};
+
+    results_file_path  = file_register.get_unique_path("test_result-%%%%-%%%%.h5");
+    save_result_status = mio::save_result(results, ids, 1, results_file_path);
+    ASSERT_TRUE(save_result_status);
+
+    results_from_file = mio::read_result(results_file_path);
+    ASSERT_TRUE(results_from_file);
+
+    ASSERT_DOUBLE_EQ(results_from_file.value()[0].get_groups()[Eigen::Index(0)][Eigen::Index(0)], 0);
+    ASSERT_DOUBLE_EQ(results_from_file.value()[1].get_groups()[Eigen::Index(0)][Eigen::Index(0)], 2);
+    ASSERT_DOUBLE_EQ(results_from_file.value()[2].get_groups()[Eigen::Index(0)][Eigen::Index(0)], 1);
 }
 
 TEST(TestSaveResult, save_percentiles_and_sums)
