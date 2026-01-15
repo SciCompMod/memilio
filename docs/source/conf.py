e64e2e78fe00dc8871e0f360ea8c8e187986b5c0
Kilian Volmer <13285635+kilianvolmer@users.noreply.github.com>
2026-01-15 17:04:51 +0100

    CHG: Check awesome presence

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,7 +18,8 @@ read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
 
 if read_the_docs_build:
 
-    subprocess.call('cd ..; git submodule add https://github.com/jothepro/doxygen-awesome-css.git; cd doxygen-awesome-css; git checkout v2.4.1', shell=True)
+    # subprocess.call('cd ..; git submodule add https://github.com/jothepro/doxygen-awesome-css.git; cd doxygen-awesome-css; git checkout v2.4.1', shell=True)
+    subprocess.call('ls ../doxygen-awesome-css', shell=True)
     subprocess.call('cd ..; doxygen', shell=True)
     subprocess.call('cd ..; doxysphinx build source $READTHEDOCS_OUTPUT/html Doxyfile', shell = True)
 
