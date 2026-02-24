e6e32e3f1e40311137d98a46361d5ea1223d810f
Kilian Volmer <13285635+kilianvolmer@users.noreply.github.com>
2026-02-24 19:39:03 +0100

    CHG: Only store x*dt time after next event

--- cpp/models/smm/simulation.h
+++ cpp/models/smm/simulation.h
@@ -88,14 +88,8 @@ public:
         // iterate over time
         while (current_time + m_waiting_times[next_event] < tmax) {
             if (current_time + m_waiting_times[next_event] >= last_result_time) {
-                auto num_dt = std::floor((current_time + m_waiting_times[next_event] - last_result_time) / m_dt);
-                if (num_dt > 0) {
-                    last_result_time = last_result_time + num_dt * m_dt;
-                    m_result.add_time_point(last_result_time);
-                    // copy from the previous last value
-                    m_result.get_last_value() = m_result[m_result.get_num_time_points() - 2];
-                }
-                last_result_time = std::min(tmax, last_result_time + m_dt);
+                auto num_dt      = std::ceil((current_time + m_waiting_times[next_event] - last_result_time) / m_dt);
+                last_result_time = std::min(tmax, last_result_time + num_dt * m_dt);
                 m_result.add_time_point(last_result_time);
                 // copy from the previous last value
                 m_result.get_last_value() = m_result[m_result.get_num_time_points() - 2];
