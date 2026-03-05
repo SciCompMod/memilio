75528eb5344ba24ede04b0f95f237199a9b19e96
Kilian Volmer <13285635+kilianvolmer@users.noreply.github.com>
2026-03-05 10:35:58 +0100

    retry

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -65,8 +65,8 @@ hoverxref_role_types = {
 }
 
 doxylink = {
-    "CPP-API": ("source/cppapi/html/tagfile.xml", 
-                "source/cppapi/html")
+    "CPP-API": ("cppapi/html/tagfile.xml", 
+                "cppapi/html")
 }
 
 # remove_from_toctrees = ["api/*"]
