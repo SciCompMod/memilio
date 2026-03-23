693157befb7992a519038db77842e5f911f02ff2
jubicker <113909589+jubicker@users.noreply.github.com>
2026-03-23 11:29:50 +0100

    jolly bindings 2

--- cpp/examples/jolly_bindings2.cpp
+++ cpp/examples/jolly_bindings2.cpp
@@ -55,21 +55,19 @@ mio::FarmSimulation<
     mio::Graph<mio::FarmNode<ScalarType, mio::smm::Simulation<ScalarType, InfectionState, mio::Index<InfectionState>,
                                                               mio::regions::Region>>,
                mio::MobilityEdgeDirected<ScalarType>>>
-simulate_with_init(
-    std::string farm_file, ScalarType tmax = 80, ScalarType dt = 1.0, ScalarType suspicion_threshold = 15,
-    ScalarType sensitivity = 0.95, ScalarType h0 = 0.001, ScalarType r0 = 1000, ScalarType alpha = 2,
-    ScalarType infection_baseline = 0.0001, ScalarType culling_factor = 0.99, ScalarType A0_SEI = 0.002,
-    ScalarType A0_EI = 0.1, ScalarType A0_ID = 0.1, ScalarType A0_DeathRate = 0.000128, ScalarType A1_SEI = 0.002,
-    ScalarType A1_EI = 0.1, ScalarType A1_ID = 0.1, ScalarType A1_DeathRate = 0.000128, ScalarType A2_SEI = 0.002,
-    ScalarType A2_EI = 0.1, ScalarType A2_ID = 0.1, ScalarType A2_DeathRate = 0.000588, ScalarType A3_SEI = 0.002,
-    ScalarType A3_EI = 0.1, ScalarType A3_ID = 0.1, ScalarType A3_DeathRate = 0.000588, ScalarType A4_SEI = 0.002,
-    ScalarType A4_EI = 0.1, ScalarType A4_ID = 0.1, ScalarType A4_DeathRate = 0.000588,
-    ScalarType foi_inner_factor0 = 0.5, ScalarType foi_outer_factor0 = 0.5, ScalarType foi_inner_factor1 = 0.5,
-    ScalarType foi_outer_factor1 = 0.5, ScalarType foi_inner_factor2 = 0.5, ScalarType foi_outer_factor2 = 0.5,
-    ScalarType foi_inner_factor3 = 0.5, ScalarType foi_outer_factor3 = 0.5, ScalarType foi_inner_factor4 = 0.5,
-    ScalarType foi_outer_factor4 = 0.5, ScalarType damping0 = 1.0, ScalarType damping1 = 1.0, ScalarType damping2 = 1.0,
-    ScalarType damping3 = 1.0, ScalarType damping4 = 1.0, ScalarType first_infection_day = 0,
-    ScalarType second_infection_day = 2, ScalarType third_infection_day = 2, u_int seed = 434)
+simulate_with_init(std::string farm_file, ScalarType tmax, ScalarType dt, ScalarType suspicion_threshold,
+                   ScalarType sensitivity, ScalarType h0, ScalarType r0, ScalarType alpha,
+                   ScalarType infection_baseline, ScalarType culling_factor, ScalarType A0_SEI, ScalarType A0_EI,
+                   ScalarType A0_ID, ScalarType A0_DeathRate, ScalarType A1_SEI, ScalarType A1_EI, ScalarType A1_ID,
+                   ScalarType A1_DeathRate, ScalarType A2_SEI, ScalarType A2_EI, ScalarType A2_ID,
+                   ScalarType A2_DeathRate, ScalarType A3_SEI, ScalarType A3_EI, ScalarType A3_ID,
+                   ScalarType A3_DeathRate, ScalarType A4_SEI, ScalarType A4_EI, ScalarType A4_ID,
+                   ScalarType A4_DeathRate, ScalarType foi_inner_factor0, ScalarType foi_outer_factor0,
+                   ScalarType foi_inner_factor1, ScalarType foi_outer_factor1, ScalarType foi_inner_factor2,
+                   ScalarType foi_outer_factor2, ScalarType foi_inner_factor3, ScalarType foi_outer_factor3,
+                   ScalarType foi_inner_factor4, ScalarType foi_outer_factor4, ScalarType damping0, ScalarType damping1,
+                   ScalarType damping2, ScalarType damping3, ScalarType damping4, ScalarType first_infection_day,
+                   ScalarType second_infection_day, ScalarType third_infection_day, u_int seed)
 {
     const auto t0 = 0.;
 
@@ -196,7 +194,16 @@ simulate_with_init(
                        {foi_outer_factor0, foi_outer_factor1, foi_outer_factor2, foi_outer_factor3, foi_outer_factor4},
                        {damping0, damping1, damping2, damping3, damping4});
 
+    sim.advance(10);
+    auto result = sim.get_confirmation_dates(10);
+    mio::log_info("Number of infected farms after 10 days: {}", std::count_if(result.begin(), result.end(), [](int r) {
+                      return r >= 0;
+                  }));
     sim.advance(tmax);
+    result = sim.get_confirmation_dates(tmax);
+    mio::log_info("Number of infected farms after tmax: {}", std::count_if(result.begin(), result.end(), [](int r) {
+                      return r >= 0;
+                  }));
 
     return sim; //sim.get_confirmation_dates(tmax);
 }
@@ -210,20 +217,17 @@ simulate(
         mio::Graph<mio::FarmNode<ScalarType, mio::smm::Simulation<ScalarType, InfectionState,
                                                                   mio::Index<InfectionState>, mio::regions::Region>>,
                    mio::MobilityEdgeDirected<ScalarType>>>& sim_to_copy,
-    const ScalarType t, ScalarType tmax = 80, ScalarType dt = 1.0, ScalarType suspicion_threshold = 15,
-    ScalarType sensitivity = 0.95, ScalarType h0 = 0.001, ScalarType r0 = 1000, ScalarType alpha = 2,
-    ScalarType infection_baseline = 0.0001, ScalarType culling_factor = 0.99, ScalarType A0_SEI = 0.002,
-    ScalarType A0_EI = 0.1, ScalarType A0_ID = 0.1, ScalarType A0_DeathRate = 0.000128, ScalarType A1_SEI = 0.002,
-    ScalarType A1_EI = 0.1, ScalarType A1_ID = 0.1, ScalarType A1_DeathRate = 0.000128, ScalarType A2_SEI = 0.002,
-    ScalarType A2_EI = 0.1, ScalarType A2_ID = 0.1, ScalarType A2_DeathRate = 0.000588, ScalarType A3_SEI = 0.002,
-    ScalarType A3_EI = 0.1, ScalarType A3_ID = 0.1, ScalarType A3_DeathRate = 0.000588, ScalarType A4_SEI = 0.002,
-    ScalarType A4_EI = 0.1, ScalarType A4_ID = 0.1, ScalarType A4_DeathRate = 0.000588,
-    ScalarType foi_inner_factor0 = 0.5, ScalarType foi_outer_factor0 = 0.5, ScalarType foi_inner_factor1 = 0.5,
-    ScalarType foi_outer_factor1 = 0.5, ScalarType foi_inner_factor2 = 0.5, ScalarType foi_outer_factor2 = 0.5,
-    ScalarType foi_inner_factor3 = 0.5, ScalarType foi_outer_factor3 = 0.5, ScalarType foi_inner_factor4 = 0.5,
-    ScalarType foi_outer_factor4 = 0.5, ScalarType damping0 = 1.0, ScalarType damping1 = 1.0, ScalarType damping2 = 1.0,
-    ScalarType damping3 = 1.0, ScalarType damping4 = 1.0, ScalarType first_infection_day = 0,
-    ScalarType second_infection_day = 2, ScalarType third_infection_day = 2, u_int seed = 434)
+    const ScalarType t, ScalarType tmax, ScalarType dt, ScalarType suspicion_threshold, ScalarType sensitivity,
+    ScalarType h0, ScalarType r0, ScalarType alpha, ScalarType infection_baseline, ScalarType culling_factor,
+    ScalarType A0_SEI, ScalarType A0_EI, ScalarType A0_ID, ScalarType A0_DeathRate, ScalarType A1_SEI, ScalarType A1_EI,
+    ScalarType A1_ID, ScalarType A1_DeathRate, ScalarType A2_SEI, ScalarType A2_EI, ScalarType A2_ID,
+    ScalarType A2_DeathRate, ScalarType A3_SEI, ScalarType A3_EI, ScalarType A3_ID, ScalarType A3_DeathRate,
+    ScalarType A4_SEI, ScalarType A4_EI, ScalarType A4_ID, ScalarType A4_DeathRate, ScalarType foi_inner_factor0,
+    ScalarType foi_outer_factor0, ScalarType foi_inner_factor1, ScalarType foi_outer_factor1,
+    ScalarType foi_inner_factor2, ScalarType foi_outer_factor2, ScalarType foi_inner_factor3,
+    ScalarType foi_outer_factor3, ScalarType foi_inner_factor4, ScalarType foi_outer_factor4, ScalarType damping0,
+    ScalarType damping1, ScalarType damping2, ScalarType damping3, ScalarType damping4, ScalarType first_infection_day,
+    ScalarType second_infection_day, ScalarType third_infection_day, u_int seed)
 {
     using Status = mio::Index<InfectionState>;
     using mio::regions::Region;
@@ -376,22 +380,56 @@ simulate(
 #ifndef JOLLY_BINDINGS_SKIP_MAIN
 int main()
 {
-    const std::string farm_file = "farms.csv";
+    // Parameters
+    ScalarType dt = 1.0, suspicion_threshold = 0.2, sensitivity = 0.95, h0 = 0.0002, r0 = 4000, alpha = 10,
+               infection_baseline = 0.0001, culling_factor = 1.0;
+    ScalarType A0_SEI = 0.5, A0_EI = 0.3, A0_ID = 0.1, A0_DeathRate = 0.0001;
+    ScalarType A1_SEI = 0.5, A1_EI = 0.3, A1_ID = 0.1, A1_DeathRate = 0.0001;
+    ScalarType A2_SEI = 0.5, A2_EI = 0.3, A2_ID = 0.1, A2_DeathRate = 0.0001;
+    ScalarType A3_SEI = 0.5, A3_EI = 0.3, A3_ID = 0.1, A3_DeathRate = 0.0001;
+    ScalarType A4_SEI = 0.5, A4_EI = 0.3, A4_ID = 0.1, A4_DeathRate = 0.0001;
+    ScalarType foi_inner_factor0 = 1.0, foi_inner_factor1 = 1.0, foi_inner_factor2 = 1.0, foi_inner_factor3 = 1.0,
+               foi_inner_factor4 = 1.0;
+    ScalarType foi_outer_factor0 = 1.0, foi_outer_factor1 = 1.0, foi_outer_factor2 = 1.0, foi_outer_factor3 = 1.0,
+               foi_outer_factor4 = 1.0;
+    ScalarType damping0 = 0.5, damping1 = 1.0, damping2 = 1.0, damping3 = 1.0, damping4 = 1.0;
+    ScalarType first_infection_day = 0, second_infection_day = 2, third_infection_day = 2;
+    u_int seed = 434;
+
+    const std::string farm_file = "/hpc_data/bick_ju/jolly/farms.csv";
     std::cout << "Running one continous simulation...\n";
-    auto sim    = simulate_with_init(farm_file, 60);
+    auto sim = simulate_with_init(
+        farm_file, 60, dt, suspicion_threshold, sensitivity, h0, r0, alpha, infection_baseline, culling_factor, A0_SEI,
+        A0_EI, A0_ID, A0_DeathRate, A1_SEI, A1_EI, A1_ID, A1_DeathRate, A2_SEI, A2_EI, A2_ID, A2_DeathRate, A3_SEI,
+        A3_EI, A3_ID, A3_DeathRate, A4_SEI, A4_EI, A4_ID, A4_DeathRate, foi_inner_factor0, foi_outer_factor0,
+        foi_inner_factor1, foi_outer_factor1, foi_inner_factor2, foi_outer_factor2, foi_inner_factor3,
+        foi_outer_factor3, foi_inner_factor4, foi_outer_factor4, damping0, damping1, damping2, damping3, damping4,
+        first_infection_day, second_infection_day, third_infection_day, seed);
     auto result = sim.get_confirmation_dates(60);
     mio::log_info("Number of infected farms: {}", std::count_if(result.begin(), result.end(), [](int r) {
                       return r >= 0;
                   }));
     std::cout << "Running simulation with break...\n";
-    double tmax = 20;
-    auto sim1   = simulate_with_init(farm_file, tmax);
-    result      = sim1.get_confirmation_dates(tmax);
+    double tmax = 10;
+    auto sim1   = simulate_with_init(
+        farm_file, tmax, dt, suspicion_threshold, sensitivity, h0, r0, alpha, infection_baseline, culling_factor,
+        A0_SEI, A0_EI, A0_ID, A0_DeathRate, A1_SEI, A1_EI, A1_ID, A1_DeathRate, A2_SEI, A2_EI, A2_ID, A2_DeathRate,
+        A3_SEI, A3_EI, A3_ID, A3_DeathRate, A4_SEI, A4_EI, A4_ID, A4_DeathRate, foi_inner_factor0, foi_outer_factor0,
+        foi_inner_factor1, foi_outer_factor1, foi_inner_factor2, foi_outer_factor2, foi_inner_factor3,
+        foi_outer_factor3, foi_inner_factor4, foi_outer_factor4, damping0, damping1, damping2, damping3, damping4,
+        first_infection_day, second_infection_day, third_infection_day, seed);
+    result = sim1.get_confirmation_dates(tmax);
     mio::log_info("Number of infected farms: {}", std::count_if(result.begin(), result.end(), [](int r) {
                       return r >= 0;
                   }));
-    auto sim_continue = simulate(sim1, tmax, tmax + 40);
-    result            = sim_continue.get_confirmation_dates(tmax);
+    auto sim_continue = simulate(
+        sim1, tmax, tmax + 50, dt, suspicion_threshold, sensitivity, h0, r0, alpha, infection_baseline, culling_factor,
+        A0_SEI, A0_EI, A0_ID, A0_DeathRate, A1_SEI, A1_EI, A1_ID, A1_DeathRate, A2_SEI, A2_EI, A2_ID, A2_DeathRate,
+        A3_SEI, A3_EI, A3_ID, A3_DeathRate, A4_SEI, A4_EI, A4_ID, A4_DeathRate, foi_inner_factor0, foi_outer_factor0,
+        foi_inner_factor1, foi_outer_factor1, foi_inner_factor2, foi_outer_factor2, foi_inner_factor3,
+        foi_outer_factor3, foi_inner_factor4, foi_outer_factor4, damping0, damping1, damping2, damping3, damping4,
+        first_infection_day, second_infection_day, third_infection_day, seed);
+    result = sim_continue.get_confirmation_dates(tmax);
     mio::log_info("Number of infected farms: {}", std::count_if(result.begin(), result.end(), [](int r) {
                       return r >= 0;
                   }));
